a16341304380c09fd5ba60ea1f6d9f86110865d0
Dima Malishev <dima.malishev@gmail.com>
2011-08-06 16:08:27 +0300

    php files cleaned, php formatting added

--- web/vesta/api/DB.class.php
+++ web/vesta/api/DB.class.php
@@ -3,246 +3,244 @@
 /**
  * DB 
  * 
- * @author Naumov-Socolov <naumov.socolov@gmail.com>
- * @author Malishev Dima <dima.malishev@gmail.com>
  * @author vesta, http://vestacp.com/
- * @copyright vesta 2011
+ * @author Dmitry Malishev <dima.malishev@gmail.com>
+ * @author Dmitry Naumov-Socolov <naumov.socolov@gmail.com>
+ * @copyright vesta 2010-2011
  */
+ 
 class DB extends AjaxHandler 
 {
     
     /**
-     * List entries
+     * Get DB entries
      * 
      * @param Request $request
-     * @return
+     * @return string - Ajax Reply
      */
     public function getListExecute($request) 
     {
-        $_user = 'vesta';
-        $reply = array();        
+        $_user  = 'vesta';
+        $reply  = array();
         $result = Vesta::execute(Vesta::V_LIST_DB_BASES, array($_user, Config::get('response_type')));
+    
+        foreach ($result['data'] as $db => $record) {
+            $type = $record['TYPE'];
+            if (!isset($reply[$type])) {
+                $reply[$type] = array();
+            }
 
-        foreach ($result['data'] as $db => $record)
-        {
-            $reply[$db] = array(
-                'DB' => $db,
-                'USER' => $record['USER'],
-                'HOST' => $record['HOST'],
-                'TYPE' => $record['TYPE'],
-                'U_DISK' => $record['U_DISK'],
-                'SUSPEND' => $record['SUSPEND'],
-                'DATE' => date(Config::get('ui_date_format', strtotime($record['DATE'])))
-            );
+            $reply[$type][] = array(
+                                'DB'        => $db,
+                                'OWNER'     => 'John Travlolta',
+                                'USERS'     => (array)$record['USER'],
+                                'HOST'      => $record['HOST'],
+                                'TYPE'      => $record['TYPE'],
+                                'U_DISK'    => $record['U_DISK'],
+                                'DISK'      => 2024,
+                                'SUSPEND'   => $record['SUSPEND'],
+                                'DATE'      => date(Config::get('ui_date_format', strtotime($record['DATE'])))
+                              );
         }
-        
-        if (!$result['status'])
-        {
+    
+        if (!$result['status']) {
             $this->errors[] = array($result['error_code'] => $result['error_message']);
         }
-        
+
+        $reply['postgre'][] = array(
+                                'DB'        => 'x',
+                                'OWNER'     => 'John Travlolta',
+                                'USERS'     => array('E'),
+                                'HOST'      => 'xxx',
+                                'TYPE'      => '34',
+                                'U_DISK'    => '0',
+                                'SUSPEND'   => 'false',
+                                'DATE'      => '2011-01-01'
+                              );
+    
         return $this->reply($result['status'], $reply);
     }
 
     /**
-     * Add entry
+     * Add DB entry
      * 
      * @param Request $request
-     * @return
+     * @return string - Ajax Reply
      */
     public function addExecute($request) 
     {
-        $r = new Request();
-        $_s = $r->getSpell();
-        $_user = 'vesta';
-
+        $r      = new Request();
+        $_s     = $r->getSpell();
+        $_user  = 'vesta';
+    
         $params = array(
-            'USER' => $_user,
-            'DB' => $_s['DB'],
-            'DB_USER' => $_s['DB_USER'],
-            'DB_PASSWORD' => $_s['DB_PASSWORD'],
-            'TYPE' => $_s['TYPE']
-        );
-        if ($_s['HOST']) 
-        {
+                    'USER'          => $_user,
+                    'DB'            => $_s['DB'],
+                    'DB_USER'       => $_s['DB_USER'],
+                    'DB_PASSWORD'   => $_s['DB_PASSWORD'],
+                    'TYPE'          => $_s['TYPE']
+                  );
+                  
+        if ($_s['HOST']) {
             $params['HOST'] = $_s['HOST'];
-        }
-
+        }    
+            
         $result = Vesta::execute(Vesta::V_ADD_DB_BASE, $params);
-
-        if (!$result['status'])
-        {
+        
+        if (!$result['status']) {
             $this->errors[] = array($result['error_code'] => $result['error_message']);
         }
-
+    
         return $this->reply($result['status'], $result['data']);
     }
-      
+  
     /**
-     * Delete entry
+     * Delete DB entry
      * 
      * @param Request $request
-     * @return
+     * @return string - Ajax Reply
      */
     public function delExecute($request) 
     {
-        $r = new Request();
-        $_s = $r->getSpell();
-        $_user = 'vesta';
-
+        $r      = new Request();
+        $_s     = $r->getSpell();
+        $_user  = 'vesta';
         $params = array(
-            'USER' => $_user,
-            'DB' => $_user.'_'.$_s['DB']
-        );
-
+                    'USER'  => $_user,
+                    'DB'    => $_user.'_'.$_s['DB']
+                  );
+    
         $result = Vesta::execute(Vesta::V_DEL_DB_BASE, $params);
-
-        if (!$result['status'])
-        {
+    
+        if (!$result['status']) {
             $this->errors[] = array($result['error_code'] => $result['error_message']);
         }
-
+    
         return $this->reply($result['status'], $result['data']);
     }
-
+  
     /**
-     * Change password
+     * Change Password
      * 
      * @param Request $request
-     * @return
+     * @return string - Ajax Reply
      */
     public function changePasswordExecute($request)
     {
-        $r = new Request();
-        $_s = $r->getSpell();
-
-        $_user = 'vesta';
-
+        $r      = new Request();
+        $_s     = $r->getSpell();
+        $_user  = 'vesta';
         $result = array();
         $params = array(
-            'USER' => $_user,
-            'DB' => $_user.'_'.$_s['DB'],
-            'PASSWORD' => $_s['DB_PASSWORD']
-        );
+                    'USER'      => $_user,
+                    'DB'        => $_user.'_'.$_s['DB'],
+                    'PASSWORD'  => $_s['DB_PASSWORD']
+                  );
 
         $result = Vesta::execute(Vesta::V_CHANGE_DB_PASSWORD, $params);
-
-        if (!$result['status'])
-        {
+    
+        if (!$result['status']) {
             $this->errors[] = array($result['error_code'] => $result['error_message']);
         }
-
+    
         return $this->reply($result['status'], $result['data']);
     }
-    
+
     /**
-     * suspend user
+     * Suspend DB entry
      * 
      * @param Request $request
-     * @return
+     * @return string - Ajax Reply
      */
     public function suspendExecute($request)
     {
-        $r = new Request();
-        $_s = $r->getSpell();
-
-        $_user = 'vesta';
-
+        $r      = new Request();
+        $_s     = $r->getSpell();
+        $_user  = 'vesta';
         $params = array(
-            'USER' => $_user,
-            'DB' => $_user.'_'.$_s['DB']
-        );
-
+                    'USER'  => $_user,
+                    'DB'    => $_user.'_'.$_s['DB']
+                  );
+    
         $result = Vesta::execute(Vesta::V_SUSPEND_DB_BASE, $params);
-
-        if (!$result['status'])
-        {
+    
+        if (!$result['status']) {
             $this->errors[] = array($result['error_code'] => $result['error_message']);
         }
-
+    
         return $this->reply($result['status'], $result['data']);
     }
 
     /**
-     * unsuspend entry
+     * Unsuspend DB entry
      * 
      * @param Request $request
-     * @return
+     * @return string - Ajax Reply
      */
     public function unsuspendExecute($request)
     {
-        $r = new Request();
-        $_s = $r->getSpell();
-
-        $_user = 'vesta';
-
+        $r      = new Request();
+        $_s     = $r->getSpell();
+        $_user  = 'vesta';
         $params = array(
-            'USER' => $_user,
-            'DB' => $_user.'_'.$_s['DB']
-        );
-
+                    'USER'  => $_user,
+                    'DB'    => $_user.'_'.$_s['DB']
+                  );
         $result = Vesta::execute(Vesta::V_UNSUSPEND_DB_BASE, $params);
-
-        if (!$result['status']) 
-        {
+    
+        if (!$result['status']) {
             $this->errors[] = array($result['error_code'] => $result['error_message']);
         }
-
+    
         return $this->reply($result['status'], $result['data']);
     }
 
     /**
-     * Batch suspend entries
+     * Batch Suspend DB entries
      * 
      * @param Request $request
-     * @return
+     * @return string - Ajax Reply
      */
     public function suspendAllExecute($request)
     {
-        $r = new Request();
-        $_s = $r->getSpell();
-
-        $_user = 'vesta';
-        $_JOB = $_s['JOB'];
-
+        $r      = new Request();
+        $_s     = $r->getSpell();
+        $_user  = 'vesta';
+        $_JOB   = $_s['JOB'];
+    
         $params = array(
-            'USER' => $_user
-        );
-
+                    'USER' => $_user
+                  );
+    
         $result = Vesta::execute(Vesta::V_SUSPEND_DB_BASES, $params);
-
-        if (!$result['status'])
-        {
+    
+        if (!$result['status']) {
             $this->errors[] = array($result['error_code'] => $result['error_message']);
         }
-
+    
         return $this->reply($result['status'], $result['data']);
     }
 
     /**
-     * Batch unsuspend entries
+     * Batch unsuspend DB entries
      * 
      * @param Request $request
-     * @return
+     * @return string - Ajax Reply
      */
     public function unsuspendAllExecute($request)
     {
-        $r = new Request();
-        $_s = $r->getSpell();
-
-        $_user = 'vesta';
-
+        $r      = new Request();
+        $_s     = $r->getSpell();
+        $_user  = 'vesta';
         $params = array(
-            'USER' => $_user
-        );
-
+                    'USER' => $_user
+                  );
         $result = Vesta::execute(Vesta::V_UNSUSPEND_DB_BASES, $params);
-
-        if (!$result['status'])
-        {
+    
+        if (!$result['status']) {
             $this->errors[] = array($result['error_code'] => $result['error_message']);
         }
-
+    
         return $this->reply($result['status'], $result['data']);
     }
     
--- web/vesta/api/IP.class.php
+++ web/vesta/api/IP.class.php
@@ -2,204 +2,186 @@
 /**
  * IP 
  * 
- * @author Naumov-Socolov <naumov.socolov@gmail.com>
- * @author Malishev Dima <dima.malishev@gmail.com>
  * @author vesta, http://vestacp.com/
+ * @author Dmitry Malishev <dima.malishev@gmail.com>
+ * @author Dmitry Naumov-Socolov <naumov.socolov@gmail.com>
  * @copyright vesta 2010-2011
  */
+ 
 class IP extends AjaxHandler
 {
+    
     /**
-     * List entries
+     * Get IP entries
      * 
      * @param Request $request
-     * @return
-     */
+     * @return string - Ajax Reply
+     */ 
     public function getListExecute($request) 
     {
-        $reply = array();
-    
-        $result = Vesta::execute(Vesta::V_LIST_SYS_IPS, array(Config::get('response_type')));
-        foreach ($result['data'] as $ip => $details) 
-        {
-            $reply[] = array_merge(array(
-                                        'IP_ADDRESS' => $ip,
-                                        'DATE' => date(Config::get('ui_date_format', strtotime($details['DATE'])))
-                                    ), $details);
+        $reply  = array();
+	    $result = Vesta::execute(Vesta::V_LIST_SYS_IPS, array(Config::get('response_type')));
+        foreach ($result['data'] as $ip => $details) {
+            $reply[] = array_merge(
+                            array(
+                                'IP_ADDRESS' => $ip,
+                                'DATE'       => date(Config::get('ui_date_format', strtotime($details['DATE'])))
+                            ), $details);
         }
-    
-        if (!$result['status'])
-        {
+	
+        if (!$result['status']) {
             $this->errors[] = array($result['error_code'] => $result['error_message']);
         }
-    
+	
         return $this->reply($result['status'], $reply);
     }
 
     /**
-     * List user ips entries
+     * Get user's IPs
      * 
      * @param Request $request
-     * @return
-     */
+     * @return string - Ajax Reply
+     */ 
     public function getListUserIpsExecute($request) 
     {
-        $reply = array();
-    
-        $result = Vesta::execute(Vesta::V_LIST_SYS_IPS, array(Config::get('response_type')));
-        foreach ($result['data'] as $ip => $details) 
-        {
-            $reply[] = array_merge(array(
-                                        'IP_ADDRESS' => $ip,
-                                        'DATE' => date(Config::get('ui_date_format', strtotime($details['DATE'])))
-                                ), $details);
+        $reply  = array();
+	    $result = Vesta::execute(Vesta::V_LIST_SYS_IPS, array(Config::get('response_type')));
+        foreach ($result['data'] as $ip => $details) {
+            $reply[] = array_merge(
+                            array(
+                                'IP_ADDRESS' => $ip,
+                                'DATE'       => date(Config::get('ui_date_format', strtotime($details['DATE'])))
+                            ), $details);
         }
-    
-        if (!$result['status'])
-        {
+	
+        if (!$result['status']) {
             $this->errors[] = array($result['error_code'] => $result['error_message']);
         }
-    
+	
         return $this->reply($result['status'], $reply);
     }
     
     /**
-     * Add entry
+     * Add IP entry
      * 
      * @param Request $request
-     * @return
-     */
+     * @return string - Ajax Reply
+     */ 
     public function addExecute($request) 
     {
-        $r = new Request();
-        $_s = $r->getSpell();
-        $_user = 'vesta';
-
+        $r      = new Request();
+        $_s     = $r->getSpell();
+        $_user  = 'vesta';
+      
         $params = array(
-            'IP_ADDRESS' => $_s['IP_ADDRESS'],
-            'MASK' => $_s['MASK'],
-            'INTERFACE' => $_s['INTERFACE'],
-            'OWNER' => $_s['OWNER'],
-            'IP_STATUS' => $_s['IP_STATUS'],
-            'IP_NAME' => $_s['IP_NAME']
-        );
-
+                      'IP_ADDRESS' => $_s['IP_ADDRESS'],
+                      'MASK' => $_s['MASK'],
+                      'INTERFACE' => $_s['INTERFACE'],
+                      'OWNER' => $_s['OWNER'],
+                      'IP_STATUS' => $_s['IP_STATUS'],
+                      'IP_NAME' => $_s['IP_NAME']
+                  );
+      
         $result = Vesta::execute(Vesta::V_ADD_SYS_IP, $params);
-
-
-        if (!$result['status'])
-        {
+      
+        if (!$result['status']) {
             $this->errors[] = array($result['error_code'] => $result['error_message']);
         }
-
+      
         return $this->reply($result['status'], $result['data']);
     }
 
     /**
-     * Delete entry
+     * Delete IP entry
      * 
      * @param Request $request
-     * @return
-     */
+     * @return string - Ajax Reply
+     */ 
     public function delExecute($request) 
     {
-        $r = new Request();
-        $_s = $r->getSpell();
-        $_user = 'vesta';
-
+        $r      = new Request();
+        $_s     = $r->getSpell();
+        $_user  = 'vesta';
         $params = array(
-            'IP_ADDRESS' => $_s['IP_ADDRESS']
-        );
-
+                    'IP_ADDRESS' => $_s['IP_ADDRESS']
+                  );
+      
         $result = Vesta::execute(Vesta::V_DEL_SYS_IP, $params);
-
-        if (!$result['status'])
-        {
+      
+        if (!$result['status']) {
             $this->errors[] = array($result['error_code'] => $result['error_message']);
         }
-
+      
         return $this->reply($result['status'], $result['data']);
     }
 
     /**
-     * Change entry
+     * Change IP entry
      * 
      * @param Request $request
-     * @return
-     */
+     * @return string - Ajax Reply
+     */ 
     public function changeExecute($request)
     {
-        $r = new Request();
-        $_s = $r->getSpell();
-        $_old = $_s['old'];
-        $_new = $_s['new'];
-
-        $_user = 'vesta';
-
-        if ($_old['OWNER'] != $_new['OWNER'])
-        {
+        $r      = new Request();
+        $_s     = $r->getSpell();
+        $_old   = $_s['old'];
+        $_new   = $_s['new'];
+        $_user  = 'vesta';
+   
+        if ($_old['OWNER'] != $_new['OWNER']) {
             $result = array();
             $result = Vesta::execute(Vesta::V_CHANGE_SYS_IP_OWNER, array('OWNER' => $_new['OWNER'], 'IP' => $_new['IP_ADDRESS']));
-            if(!$result['status'])
-            {
+            if (!$result['status']) {
                 $this->status = FALSE;
                 $this->errors['OWNER'] = array($result['error_code'] => $result['error_message']);
             }
         }
 
-        if ($_old['NAME'] != $_new['NAME'])
-        {
+        if ($_old['NAME'] != $_new['NAME']) {
             $result = array();
             $result = Vesta::execute(Vesta::V_CHANGE_SYS_IP_NAME, array('IP' => $_new['IP_ADDRESS'], 'NAME' => $_new['NAME']));
-            if (!$result['status'])
-            {
+            if (!$result['status']) {
                 $this->status = FALSE;
                 $this->errors['NAME'] = array($result['error_code'] => $result['error_message']);
             }
         }
 
-        if ($_old['IP_STATUS'] != $_new['IP_STATUS'])
-        {
+        if ($_old['IP_STATUS'] != $_new['IP_STATUS']) {
             $result = array();
             $result = Vesta::execute(Vesta::V_CHANGE_SYS_IP_STATUS, array('IP' => $_new['IP_ADDRESS'], 'IP_STATUS' => $_new['IP_STATUS']));
-            if (!$result['status'])
-            {
+            if (!$result['status']) {
                 $this->status = FALSE;
                 $this->errors['IP_STATUS'] = array($result['error_code'] => $result['error_message']);
             }
         }
 
-
-        if (!$result['status'])
-        {    
+        if (!$result['status']) {
             $this->errors[] = array($result['error_code'] => $result['error_message']);
         }
-
+      
         return $this->reply($result['status'], $result['data']);
     }
-    
+
     /**
-     * Get sys interfaces
+     * Get Sys interfaces
      * 
      * @param Request $request
-     * @return
-     */
+     * @return string - Ajax Reply
+     */ 
     public function getSysInterfacesExecute($request) 
     {
-        $reply = array();
+        $reply  = array();
+	    $result = Vesta::execute(Vesta::V_LIST_SYS_INTERFACES, array(Config::get('response_type')));
 
-        $result = Vesta::execute(Vesta::V_LIST_SYS_INTERFACES, array(Config::get('response_type')));
-
-        foreach ($result['data'] as $iface) 
-        {
+        foreach ($result['data'] as $iface) {
             $reply[$iface] = $iface;
         }
-
-        if (!$result['status'])
-        {
+	
+        if (!$result['status']) {
             $this->errors[] = array($result['error_code'] => $result['error_message']);
         }
-        
+	
         return $this->reply($result['status'], $reply);
     }
     

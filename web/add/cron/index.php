f14550a5a73939550b708cf128e52d181078caa5
Serghey Rodin <serghey.rodin@gmail.com>
2012-06-22 12:08:26 +0300

    cron add page

---
+++ web/add/cron/index.php
@@ -0,0 +1,78 @@
+<?php
+// Init
+//error_reporting(NULL);
+ob_start();
+session_start();
+$TAB = 'CRON';
+include($_SERVER['DOCUMENT_ROOT']."/inc/main.php");
+
+// Header
+include($_SERVER['DOCUMENT_ROOT'].'/templates/header.html');
+
+// Panel
+top_panel($user,$TAB);
+
+// Are you admin?
+if ($_SESSION['user'] == 'admin') {
+    if (!empty($_POST['ok'])) {
+        // Check input
+        if (empty($_POST['v_min'])) $errors[] = 'minute';
+        if (empty($_POST['v_hour'])) $errors[] = 'hour';
+        if (empty($_POST['v_day'])) $errors[] = 'day';
+        if (empty($_POST['v_month'])) $errors[] = 'month';
+        if (empty($_POST['v_wday'])) $errors[] = 'day of week';
+        if (empty($_POST['v_cmd'])) $errors[] = 'cmd';
+
+        // Protect input
+        $v_min = escapeshellarg($_POST['v_min']);
+        $v_hour = escapeshellarg($_POST['v_hour']);
+        $v_day = escapeshellarg($_POST['v_day']);
+        $v_month = escapeshellarg($_POST['v_month']);
+        $v_wday = escapeshellarg($_POST['v_wday']);
+        $v_cmd = escapeshellarg($_POST['v_cmd']);
+
+        // Check for errors
+        if (!empty($errors[0])) {
+            foreach ($errors as $i => $error) {
+                if ( $i == 0 ) {
+                    $error_msg = $error;
+                } else {
+                    $error_msg = $error_msg.", ".$error;
+                }
+            }
+            $_SESSION['error_msg'] = "Error: field ".$error_msg." can not be blank.";
+        } else {
+            // Add Cron Job
+            exec (VESTA_CMD."v_add_cron_job ".$user." ".$v_min." ".$v_hour." ".$v_day." ".$v_month." ".$v_wday." ".$v_cmd, $output, $return_var);
+            $v_type = $_POST['v_type'];
+            $v_charset = $_POST['v_charset'];
+            if ($return_var != 0) {
+                $error = implode('<br>', $output);
+                if (empty($error)) $error = 'Error: vesta did not return any output.';
+                $_SESSION['error_msg'] = $error;
+                unset($v_password);
+                unset($output);
+            } else {
+                $_SESSION['ok_msg'] = "OK: cron job has been created successfully.";
+                unset($v_min);
+                unset($v_hour);
+                unset($v_day);
+                unset($v_month);
+                unset($v_wday);
+                unset($v_cmd);
+                unset($output);
+            }
+        }
+    }
+    exec (VESTA_CMD."v_list_database_types 'json'", $output, $return_var);
+    $db_types = json_decode(implode('', $output), true);
+    unset($output);
+
+    include($_SERVER['DOCUMENT_ROOT'].'/templates/admin/menu_add_cron.html');
+    include($_SERVER['DOCUMENT_ROOT'].'/templates/admin/add_cron.html');
+    unset($_SESSION['error_msg']);
+    unset($_SESSION['ok_msg']);
+}
+
+// Footer
+include($_SERVER['DOCUMENT_ROOT'].'/templates/footer.html');

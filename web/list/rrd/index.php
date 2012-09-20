61cc4b1dcf7a7d40df7b6a0b72bef45b61d94bac
Serghey Rodin <serghey.rodin@gmail.com>
2012-09-20 17:15:41 +0300

    refactoring rrd section: new html formating

--- web/list/rrd/index.php
+++ web/list/rrd/index.php
@@ -13,13 +13,9 @@ top_panel($user,$TAB);
 
 // Data
 if ($_SESSION['user'] == 'admin') {
-
     exec (VESTA_CMD."v_list_sys_rrd json", $output, $return_var);
-    check_error($return_var);
     $data = json_decode(implode('', $output), true);
     unset($output);
-
-    include($_SERVER['DOCUMENT_ROOT'].'/templates/admin/menu_rrd.html');
     include($_SERVER['DOCUMENT_ROOT'].'/templates/admin/list_rrd.html');
 }
 

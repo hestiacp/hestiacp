2d7b0ad1f279d58eeba623af3783d167b625bfff
Serghey Rodin <serghey.rodin@gmail.com>
2012-11-14 02:14:32 +0200

    fixed cmd path

--- web/list/log/index.php
+++ web/list/log/index.php
@@ -14,7 +14,7 @@ top_panel($user,$TAB);
 // Data
 if ($_SESSION['user'] == 'admin') {
 
-    exec (VESTA_CMD."v_list_user_log $user json", $output, $return_var);
+    exec (VESTA_CMD."v-list-user-log $user json", $output, $return_var);
     check_error($return_var);
     $data = json_decode(implode('', $output), true);
     $data = array_reverse($data);

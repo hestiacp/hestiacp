1aa19c01a039503bbc4e242b1abec143fe6292a0
Serghey Rodin <serghey.rodin@gmail.com>
2012-11-15 13:50:31 +0200

    new pregmatch for user function

--- func/main.sh
+++ func/main.sh
@@ -556,7 +556,8 @@ validate_format_email() {
 
 # Username
 validate_format_username() {
-    if ! [[ "$1" =~ ^[0-z]+(\.[0-z]+)?$ ]] || [[ "${#1}" -gt 28 ]]; then
+    if ! [[ "$1" =~ ^[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)?$ ]] || [[ "${#1}" -gt 28 ]]
+        then
         echo "Error: $2 $1 is not valid"
         log_event "$E_INVALID" "$EVENT"
         exit $E_INVALID

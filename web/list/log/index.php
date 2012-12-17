7816834a32341276eae822899e4ae93e542a5e7d
Serghey Rodin <serghey.rodin@gmail.com>
2012-12-17 17:05:02 +0200

    fixed history log page

--- web/list/log/index.php
+++ web/list/log/index.php
@@ -12,17 +12,14 @@ include($_SERVER['DOCUMENT_ROOT'].'/templates/header.html');
 top_panel($user,$TAB);
 
 // Data
-if ($_SESSION['user'] == 'admin') {
+exec (VESTA_CMD."v-list-user-log $user json", $output, $return_var);
+check_error($return_var);
+$data = json_decode(implode('', $output), true);
+$data = array_reverse($data);
+unset($output);
 
-    exec (VESTA_CMD."v-list-user-log $user json", $output, $return_var);
-    check_error($return_var);
-    $data = json_decode(implode('', $output), true);
-    $data = array_reverse($data);
-    unset($output);
-
-    include($_SERVER['DOCUMENT_ROOT'].'/templates/admin/menu_log.html');
-    include($_SERVER['DOCUMENT_ROOT'].'/templates/admin/list_log.html');
-}
+include($_SERVER['DOCUMENT_ROOT'].'/templates/admin/menu_log.html');
+include($_SERVER['DOCUMENT_ROOT'].'/templates/admin/list_log.html');
 
 // Footer
 include($_SERVER['DOCUMENT_ROOT'].'/templates/footer.html');

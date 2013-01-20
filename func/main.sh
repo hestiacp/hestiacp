a8a86aced9656e678557bc704d537a05e6d6d3b4
Serghey Rodin <serghey.rodin@gmail.com>
2013-01-20 15:46:13 +0200

    imprvoved validator

--- func/main.sh
+++ func/main.sh
@@ -556,12 +556,7 @@ validate_format_email() {
 
 # Username
 validate_format_username() {
-    if ! [[ "$1" =~ ^[[:alnum:]]+([\.|_|-][[:alnum:]]+)?$ ]]; then
-        echo "Error: $2 $1 is not valid"
-        log_event "$E_INVALID" "$EVENT"
-        exit $E_INVALID
-    fi
-    if  [[ "${#1}" -gt 28 ]]; then
+    if ! [[ "$1" =~ ^[[:alnum:]][-|\.|_[:alnum:]]{0,28}[[:alnum:]]$ ]]; then
         echo "Error: $2 $1 is not valid"
         log_event "$E_INVALID" "$EVENT"
         exit $E_INVALID

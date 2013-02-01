01d32a43e1182bc4715198844844fc5441d66a92
Serghey Rodin <serghey.rodin@gmail.com>
2013-02-01 17:30:39 +0200

    bulk update module

---
+++ web/bulk/vesta/index.php
@@ -0,0 +1,24 @@
+<?php
+// Init
+error_reporting(NULL);
+ob_start();
+session_start();
+
+include($_SERVER['DOCUMENT_ROOT']."/inc/main.php");
+
+$pkg = $_POST['pkg'];
+$action = $_POST['action'];
+
+if ($_SESSION['user'] == 'admin') {
+    switch ($action) {
+        case 'update': $cmd='v-update-sys-vesta';
+            break;
+        default: header("Location: /list/updates/"); exit;
+    }
+    foreach ($pkg as $value) {
+        $value = escapeshellarg($value);
+        exec (VESTA_CMD.$cmd." ".$value, $output, $return_var);
+    }
+}
+
+header("Location: /list/updates/");

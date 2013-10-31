8871eae499a7823f9fa44f2ab4de817c11526a92
Serghey Rodin <serghey.rodin@gmail.com>
2013-10-31 13:42:42 +0200

    simplified domain deletion

--- web/delete/web/index.php
+++ web/delete/web/index.php
@@ -13,29 +13,9 @@ if (($_SESSION['user'] == 'admin') && (!empty($_GET['user']))) {
 if (!empty($_GET['domain'])) {
     $v_username = escapeshellarg($user);
     $v_domain = escapeshellarg($_GET['domain']);
-    exec (VESTA_CMD."v-delete-web-domain ".$v_username." ".$v_domain, $output, $return_var);
+    exec (VESTA_CMD."v-delete-domain ".$v_username." ".$v_domain, $output, $return_var);
     check_return_code($return_var,$output);
     unset($output);
-
-    // DNS
-    if (empty($_SESSION['error_msg'])) {
-        exec (VESTA_CMD."v-list-dns-domain ".$v_username." ".$v_domain." json", $output, $lreturn_var);
-        if ($lreturn_var == 0 ) {
-            exec (VESTA_CMD."v-delete-dns-domain ".$v_username." ".$v_domain, $output, $return_var);
-            check_return_code($return_var,$output);
-            unset($output);
-        }
-    }
-
-    // Mail
-    if (empty($_SESSION['error_msg'])) {
-        exec (VESTA_CMD."v-list-mail-domain ".$v_username." ".$v_domain." json", $output, $lreturn_var);
-        if ($lreturn_var == 0 ) {
-            exec (VESTA_CMD."v-delete-mail-domain ".$v_username." ".$v_domain, $output, $return_var);
-            check_return_code($return_var,$output);
-            unset($output);
-        }
-    }
 }
 
 $back = $_SESSION['back'];

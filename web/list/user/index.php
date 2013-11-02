681c71db3ecaa31e667b7a5f04dff7864b0e35c9
Serghey Rodin <serghey.rodin@gmail.com>
2013-11-02 16:44:05 +0200

    simplified user list logic

--- web/list/user/index.php
+++ web/list/user/index.php
@@ -12,18 +12,15 @@ include($_SERVER['DOCUMENT_ROOT'].'/templates/header.html');
 top_panel($user,$TAB);
 
 // Data
-if ($_SESSION['user'] == 'admin') {
-    $cmd = "v-list-user '".$user."' json";
-    if ($user == 'admin') $cmd = "v-list-users json";
-    exec (VESTA_CMD.$cmd, $output, $return_var);
+if ($user == 'admin') {
+    exec (VESTA_CMD . "v-list-users json", $output, $return_var);
     $data = json_decode(implode('', $output), true);
     $data = array_reverse($data,true);
     display_error_block();
     include($_SERVER['DOCUMENT_ROOT'].'/templates/admin/list_user.html');
 } else {
-    exec (VESTA_CMD."v-list-user ".$user." json", $output, $return_var);
+    exec (VESTA_CMD . "v-list-user ".$user." json", $output, $return_var);
     $data = json_decode(implode('', $output), true);
-    //$data = array_reverse($data,true);
     display_error_block();
     include($_SERVER['DOCUMENT_ROOT'].'/templates/user/list_user.html');
 }

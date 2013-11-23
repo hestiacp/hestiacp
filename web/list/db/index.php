563d00dd0264b191abc4261a4fa43ff4b4e714a4
Serghey Rodin <serghey.rodin@gmail.com>
2013-11-23 14:56:58 +0200

    custom phpMyAdmin and phpPgAdmin links

--- web/list/db/index.php
+++ web/list/db/index.php
@@ -11,6 +11,11 @@ include($_SERVER['DOCUMENT_ROOT'].'/templates/header.html');
 // Panel
 top_panel($user,$TAB);
 
+// SYS
+exec (VESTA_CMD."v-list-sys-config json", $output, $return_var);
+$sys = json_decode(implode('', $output), true);
+unset($output);
+
 // Data
 exec (VESTA_CMD."v-list-databases $user json", $output, $return_var);
 $data = json_decode(implode('', $output), true);

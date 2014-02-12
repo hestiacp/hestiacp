c50c7b26753e1081c2a6fd2acb7527cf6b1cd914
very-twi@github.com <dima.malishev@gmail.com>
2014-02-12 12:08:23 +0200

    Updated hints to handle native browser's paste events Hint is update in case page is reloaded and value is set (in case of form errors) Hints are now using secure inserting into DOM avoiding xss Dns record hint added

--- web/js/pages/add.web.js
+++ web/js/pages/add.web.js
@@ -1,23 +1,37 @@
+//
+//
+// Updates ftp username dynamically, showing its prefix
 App.Actions.WEB.update_ftp_username_hint = function(elm, hint) {
-	if (hint.trim() == '') {
-		$(elm).parent().find('.hint').html('');
-	} 
-	if (hint.indexOf(GLOBAL.FTP_USER_PREFIX) == 0) {
-		hint = hint.slice(GLOBAL.FTP_USER_PREFIX.length, hint.length);
-	}
-	$(elm).parent().find('.hint').html(GLOBAL.FTP_USER_PREFIX + hint);
+    if (hint.trim() == '') {
+        $(elm).parent().find('.hint').html('');
+    } 
+    // remove prefix from value in order to eliminate duplicates
+    if (hint.indexOf(GLOBAL.FTP_USER_PREFIX) == 0) {
+        hint = hint.slice(GLOBAL.FTP_USER_PREFIX.length, hint.length);
+    }
+    
+    $(elm).parent().find('.hint').text(GLOBAL.FTP_USER_PREFIX + hint);
 }
 
+//
+// listener that triggers ftp user hint updating
 App.Listeners.WEB.keypress_ftp_username = function() {
-	$('input[name="v_ftp_user"]').bind('keypress', function(evt) {
-		clearTimeout(window.frp_usr_tmt);
-		window.frp_usr_tmt = setTimeout(function() {
-			var elm = $(evt.target);
-			App.Actions.WEB.update_ftp_username_hint(elm, $(elm).val());
-		}, 100);
-	});
+    var ref = $('input[name="v_ftp_user"]');
+    var current_val = ref.val();
+    if (current_val.trim() != '') {
+        App.Actions.DB.update_ftp_username_hint(ref, current_val);
+    }
+    
+    ref.bind('keypress input', function(evt) {
+        clearTimeout(window.frp_usr_tmt);
+        window.frp_usr_tmt = setTimeout(function() {
+            var elm = $(evt.target);
+            App.Actions.WEB.update_ftp_username_hint(elm, $(elm).val());
+        }, 100);
+    });
 }
 
 //
 // Page entry point
+// Trigger listeners
 App.Listeners.WEB.keypress_ftp_username();
--- web/js/pages/edit.web.js
+++ web/js/pages/edit.web.js
@@ -1,21 +1,27 @@
 App.Actions.WEB.update_ftp_username_hint = function(elm, hint) {
-	if (hint.trim() == '') {
-		$(elm).parent().find('.hint').html('');
-	} 
-	if (hint.indexOf(GLOBAL.FTP_USER_PREFIX) == 0) {
-		hint = hint.slice(GLOBAL.FTP_USER_PREFIX.length, hint.length);
-	}
-	$(elm).parent().find('.hint').html(GLOBAL.FTP_USER_PREFIX + hint);
+    if (hint.trim() == '') {
+        $(elm).parent().find('.hint').html('');
+    } 
+    if (hint.indexOf(GLOBAL.FTP_USER_PREFIX) == 0) {
+        hint = hint.slice(GLOBAL.FTP_USER_PREFIX.length, hint.length);
+    }
+    $(elm).parent().find('.hint').text(GLOBAL.FTP_USER_PREFIX + hint);
 }
 
 App.Listeners.WEB.keypress_ftp_username = function() {
-	$('input[name="v_ftp_user"]').bind('keypress', function(evt) {
-		clearTimeout(window.frp_usr_tmt);
-		window.frp_usr_tmt = setTimeout(function() {
-			var elm = $(evt.target);
-			App.Actions.WEB.update_ftp_username_hint(elm, $(elm).val());
-		}, 100);
-	});
+    var ref = $('input[name="v_ftp_user"]');
+    var current_val = ref.val();
+    if (current_val.trim() != '') {
+        App.Actions.DB.update_ftp_username_hint(ref, current_val);
+    }
+    
+    ref.bind('keypress', function(evt) {
+        clearTimeout(window.frp_usr_tmt);
+        window.frp_usr_tmt = setTimeout(function() {
+            var elm = $(evt.target);
+            App.Actions.WEB.update_ftp_username_hint(elm, $(elm).val());
+        }, 100);
+    });
 }
 
 //

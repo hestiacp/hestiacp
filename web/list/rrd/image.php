133a24552863795c9a763c487945a5021aed791b
Serghey Rodin <serghey.rodin@gmail.com>
2014-04-16 14:24:37 +0300

    secured way to display rrd charts

---
+++ web/list/rrd/image.php
@@ -0,0 +1,13 @@
+<?php
+
+session_start();
+if ($_SESSION['user'] != 'admin') exit;
+$real_path = realpath($_SERVER["DOCUMENT_ROOT"].$_SERVER['QUERY_STRING']);
+if (empty($real_path)) exit;
+$dir_name = dirname($real_path);
+$dir_name = dirname($dir_name);
+if ($dir_name != $_SERVER["DOCUMENT_ROOT"].'/rrd') exit;
+header("X-Accel-Redirect: ".$_SERVER['QUERY_STRING']);
+header("Content-Type: image/png");
+
+?>

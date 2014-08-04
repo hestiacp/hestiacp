3dd3c2f454ff91b0e066947741143a22d0d0b22e
Serghey Rodin <serghey.rodin@gmail.com>
2014-08-04 20:43:26 +0300

    added bsdutils package

--- install/vst-install-debian.sh
+++ install/vst-install-debian.sh
@@ -10,12 +10,13 @@ export DEBIAN_FRONTEND=noninteractive
 RHOST='apt.vestacp.com'
 CHOST='c.vestacp.com'
 VERSION='0.9.8/debian'
-software="nginx apache2 apache2-utils apache2.2-common apache2-suexec-custom
-    libapache2-mod-ruid2 libapache2-mod-rpaf libapache2-mod-fcgid bind9 idn
-    mysql-server mysql-common mysql-client php5-common php5-cgi php5-mysql
-    php5-curl libapache2-mod-php5 vsftpd mc exim4 exim4-daemon-heavy
-    clamav-daemon flex dovecot-imapd dovecot-pop3d phpMyAdmin awstats
-    webalizer jwhois rssh git spamassassin roundcube roundcube-mysql
+software="nginx apache2 apache2-utils apache2.2-common bsdutils
+    apache2-suexec-custom libapache2-mod-ruid2 libapache2-mod-rpaf
+    libapache2-mod-fcgid bind9 idn mysql-server mysql-common
+    mysql-client php5-common php5-cgi php5-mysql php5-curl
+    libapache2-mod-php5 vsftpd mc exim4 exim4-daemon-heavy clamav-daemon
+    flex dovecot-imapd dovecot-pop3d phpMyAdmin awstats webalizer
+    jwhois rssh git spamassassin roundcube roundcube-mysql
     roundcube-plugins sudo bc ftp lsof ntpdate rrdtool quota e2fslibs
     dnsutils vesta vesta-nginx vesta-php"
 

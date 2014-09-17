7d702f8cf395e57c85ee17a9994096ae2180a50d
Serghey Rodin <serghey.rodin@gmail.com>
2014-09-18 00:12:05 +0300

    Switch from rpaf to remoteip module

---
+++ upd/switch_rpath.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Switch apache to remoteip module
+
+# Checking rpaf config
+if [ ! -e "/etc/apache2/mods-enabled/rpaf.load" ]; then
+    exit
+fi
+
+# Checking remoteip module
+if [ ! -e "/etc/apache2/mods-available/remoteip.load" ]; then
+    exit
+fi
+
+# Disabling rpaf
+/usr/sbin/a2dismod rpaf > /dev/null 2>&1
+rm -f /etc/apache2/mods-enabled/rpaf.conf
+
+# Enabling remoteip
+/usr/sbin/a2enmod remoteip > /dev/null 2>&1
+
+# Creating configuration
+conf="/etc/apache2/mods-enabled/remoteip.conf"
+echo "<IfModule remoteip_module>" > $conf
+echo "    RemoteIPHeader X-Real-IP" >> $conf
+for ip in $(ls /usr/local/vesta/data/ips); do
+    echo "    RemoteIPInternalProxy $ip" >> $conf
+done
+echo "</IfModule>" >> $conf
+
+# Restarting apache
+/usr/sbin/apachectl restart > /dev/null 2>&1
+
+# EOF
+exit

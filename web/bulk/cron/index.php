ea050062aee571ba9a9b26bb07bb1032df9768c9
Serghey Rodin <serghey.rodin@gmail.com>
2014-10-20 13:10:03 +0300

    Cron reporting handler + minor fixes in firewall templates

--- web/bulk/cron/index.php
+++ web/bulk/cron/index.php
@@ -17,11 +17,39 @@ if ($_SESSION['user'] == 'admin') {
             break;
         case 'unsuspend': $cmd='v-unsuspend-cron-job';
             break;
+        case 'delete-cron-reports': $cmd='v-delete-cron-reports';
+            exec (VESTA_CMD.$cmd." ".$user, $output, $return_var);
+            $_SESSION['error_msg'] = __('Cronjob email reporting has been successfully diabled');
+            unset($output);
+            header("Location: /list/cron/");
+            exit;
+            break;
+        case 'add-cron-reports': $cmd='v-add-cron-reports';
+            exec (VESTA_CMD.$cmd." ".$user, $output, $return_var);
+            $_SESSION['error_msg'] = __('Cronjob email reporting has been successfully enabled');
+            unset($output);
+            header("Location: /list/cron/");
+            exit;
+            break;
         default: header("Location: /list/cron/"); exit;
     }
 } else {
     switch ($action) {
         case 'delete': $cmd='v-delete-cron-job';
+            break;
+        case 'delete-cron-reports': $cmd='v-delete-cron-reports';
+            exec (VESTA_CMD.$cmd." ".$user, $output, $return_var);
+            $_SESSION['error_msg'] = __('Cronjob email reporting has been successfully diabled');
+            unset($output);
+            header("Location: /list/cron/");
+            exit;
+            break;
+        case 'add-cron-reports': $cmd='v-add-cron-reports';
+            exec (VESTA_CMD.$cmd." ".$user, $output, $return_var);
+            $_SESSION['error_msg'] = __('Cronjob email reporting has been successfully enabled');
+            unset($output);
+            header("Location: /list/cron/");
+            exit;
             break;
         default: header("Location: /list/cron/"); exit;
     }

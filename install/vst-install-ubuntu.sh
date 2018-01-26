421ecfa62c16f13a8ec8b67ae2cbc4becee7cc57
Serghey Rodin <skid@vestacp.com>
2018-01-26 16:08:55 +0200

    install apparmor-utils

--- install/vst-install-ubuntu.sh
+++ install/vst-install-ubuntu.sh
@@ -30,7 +30,7 @@ if [ "$release" = '16.04' ]; then
         flex whois rssh git idn zip sudo bc ftp lsof ntpdate rrdtool quota
         e2fslibs bsdutils e2fsprogs curl imagemagick fail2ban dnsutils
         bsdmainutils cron vesta vesta-nginx vesta-php expect vim-common
-        vesta-ioncube vesta-softaculous"
+        vesta-ioncube vesta-softaculous apparmor-utils"
 elif [ "$release" = '16.10' ]; then
     software="nginx apache2 apache2-utils apache2.2-common
         apache2-suexec-custom libapache2-mod-ruid2 libapache2-mod-rpaf
@@ -43,7 +43,7 @@ elif [ "$release" = '16.10' ]; then
         flex whois rssh git idn zip sudo bc ftp lsof ntpdate rrdtool quota
         e2fslibs bsdutils e2fsprogs curl imagemagick fail2ban dnsutils
         bsdmainutils cron vesta vesta-nginx vesta-php expect vim-common
-        vesta-ioncube vesta-softaculous"
+        vesta-ioncube vesta-softaculous apparmor-utils"
 else
     software="nginx apache2 apache2-utils apache2.2-common
         apache2-suexec-custom libapache2-mod-ruid2 libapache2-mod-rpaf

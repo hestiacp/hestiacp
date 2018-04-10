24042941855de11ad748bf77c5f2cfe44214578c
dpeca <peca@mycity.rs>
2018-04-10 03:36:52 +0200

    Redirect to webmail by default

--- web/index.php
+++ web/index.php
@@ -1,8 +1,9 @@
 <?php
-require_once('/usr/local/vesta/web/inc/secure_login.php');
-session_start();
-if (isset($_SESSION['user'])) {
-    header("Location: /list/user/");
-} else {
-    header("Location: /login/");
-}
+//require_once('/usr/local/vesta/web/inc/secure_login.php');
+//session_start();
+//if (isset($_SESSION['user'])) {
+//    header("Location: /list/user/");
+//} else {
+//    header("Location: /login/");
+//}
+header("Location: /webmail/");

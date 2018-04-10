318f9daddef19a85764d3855bc1f288039de493d
dpeca <peca@mycity.rs>
2018-04-10 02:25:45 +0200

    filepath change to secure_login.php

--- web/inc/secure_login.php
+++ web/inc/secure_login.php
@@ -1,5 +1,5 @@
 <?php
-require_once('/usr/local/vesta/web/login_url.php');
+require_once('/usr/local/vesta/web/inc/login_url.php');
 if (isset($_GET[$login_url])) {
     setcookie($login_url, '1', time() + 31536000, '/', $_SERVER['HTTP_HOST'], true);
     header ("Location: /");

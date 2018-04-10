a260e0c6b7705ca6c63a472aef78e6b5dd91ec2c
dpeca <peca@mycity.rs>
2018-04-10 02:42:34 +0200

    Changing redirect location

--- web/inc/secure_login.php
+++ web/inc/secure_login.php
@@ -2,7 +2,7 @@
 require_once('/usr/local/vesta/web/inc/login_url.php');
 if (isset($_GET[$login_url])) {
     setcookie($login_url, '1', time() + 31536000, '/', $_SERVER['HTTP_HOST'], true);
-    header ("Location: /");
+    header ("Location: /login/");
     exit;
 }
 if (!isset($_COOKIE[$login_url])) exit;

c739e5fc485ed627457648b491157efb6647809c
dpeca <peca@mycity.rs>
2018-04-11 13:04:07 +0200

    Avoid pre-auth if login_url.php is not created

--- web/index.php
+++ web/index.php
@@ -1,15 +1,16 @@
 <?php
-//require_once('/usr/local/vesta/web/inc/secure_login.php');
-//session_start();
-//if (isset($_SESSION['user'])) {
-//    header("Location: /list/user/");
-//} else {
-//    header("Location: /login/");
-//}
 
-require_once('/usr/local/vesta/web/inc/login_url.php');
-if (isset($_GET[$login_url])) {
-  require_once('/usr/local/vesta/web/inc/secure_login.php');
+if (!file_exists('/usr/local/vesta/web/inc/login_url.php')) {
+    session_start();
+    if (isset($_SESSION['user'])) {
+        header("Location: /list/user/");
+    } else {
+        header("Location: /login/");
+    }
+} else {
+    require_once('/usr/local/vesta/web/inc/login_url.php');
+    if (isset($_GET[$login_url])) {
+      require_once('/usr/local/vesta/web/inc/secure_login.php');
+    }
+    header("Location: /webmail/");
 }
-
-header("Location: /webmail/");

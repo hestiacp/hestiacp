927c288b272545cd42a41f1a98b8a6e6fbcd5904
myvesta <38690722+myvesta@users.noreply.github.com>
2018-10-12 16:33:02 +0200

    Reverting /web/index.php to default

--- web/index.php
+++ web/index.php
@@ -1,16 +1,7 @@
 <?php
-
-if (!file_exists('/usr/local/vesta/web/inc/login_url.php')) {
-    session_start();
-    if (isset($_SESSION['user'])) {
-        header("Location: /list/user/");
-    } else {
-        header("Location: /login/");
-    }
+session_start();
+if (isset($_SESSION['user'])) {
+    header("Location: /list/user/");
 } else {
-    require_once('/usr/local/vesta/web/inc/login_url.php');
-    if (isset($_GET[$login_url])) {
-      require_once('/usr/local/vesta/web/inc/secure_login.php');
-    }
-    header("Location: /webmail/");
+    header("Location: /login/");
 }

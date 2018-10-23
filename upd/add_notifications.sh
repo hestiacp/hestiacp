579baa71052ead54618b32e491c829929cd0c030
Raphael Schneeberger <rs@scit.ch>
2018-10-23 08:07:15 +0200

    Change path from vestia to hestia.

--- upd/add_notifications.sh
+++ upd/add_notifications.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Add notifications
 
-rm -f /usr/local/vesta/data/users/admin/notifications.conf
-/usr/local/vesta/bin/v-add-user-notification admin "HestiaCP" "Release 0.9.8-23, we've made 1478 commits, fixed 29 bugs and merged 141 pull request."
+rm -f /usr/local/hestia/data/users/admin/notifications.conf
+/usr/local/hestia/bin/v-add-user-notification admin "HestiaCP" "Release 0.9.8-23, we've made 1478 commits, fixed 29 bugs and merged 141 pull request."
 

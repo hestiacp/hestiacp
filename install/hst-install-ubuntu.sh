6cee9ae16302e84f6dfc5d24cf68f4c16c449607
Alexandros Ioannides <alex@ioannides.net>
2018-11-08 00:17:48 +0200

    Update hst-install-ubuntu.sh

--- install/hst-install-ubuntu.sh
+++ install/hst-install-ubuntu.sh
@@ -1412,7 +1412,6 @@ $HESTIA/bin/v-add-cron-hestia-autoupdate
 #                   Hestia Access Info                     #
 #----------------------------------------------------------#
 
-
 # Comparing hostname and IP
 host_ip=$(host $servername| head -n 1 |awk '{print $NF}')
 if [ "$host_ip" = "$ip" ]; then

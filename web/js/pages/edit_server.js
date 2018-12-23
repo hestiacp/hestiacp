847ebb251f9bb5343e07c3ea32b006d1ea4d75bb
Alexandros Ioannides <alex@ioannides.net>
2018-12-23 04:05:28 +0200

    Update edit_server.js

--- web/js/pages/edit_server.js
+++ web/js/pages/edit_server.js
@@ -1,25 +1,8 @@
-$(document).ready(function(){
-    $('select[name=v_filemanager]').change(function(){
-        if($(this).val() == 'yes'){
-            $('.filemanager.description').show();
-        } else {
-            $('.filemanager.description').hide();
-        }
-    });
-
     $('select[name=v_sftp]').change(function(){
         if($(this).val() == 'yes'){
             $('.sftp.description').show();
         } else {
             $('.sftp.description').hide();
-        }
-    });
-
-    $('select[name=v_softaculous]').change(function(){
-        if($(this).val() == 'yes'){
-            $('.softaculous.description').show();
-        } else {
-            $('.softaculous.description').hide();
         }
     });
 

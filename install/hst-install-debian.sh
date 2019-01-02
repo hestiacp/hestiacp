d56d16b2f4bc0038fb0e5652279506ff7f5c1ece
Raphael Schneeberger <rs@scit.ch>
2019-01-02 09:54:05 +0100

    Add additional php packages to fpm setup.

--- install/hst-install-debian.sh
+++ install/hst-install-debian.sh
@@ -627,7 +627,7 @@ fi
 if [ "$phpfpm" = 'yes' ]; then
     fpm="php7.3 php7.3-common php7.3-bcmath php7.3-cli php7.3-curl php7.3-fpm
          php7.3-gd php7.3-intl php7.3-mysql php7.3-soap php7.3-xml php7.3-zip
-         php7.3-mbstring php7.3-json"
+         php7.3-mbstring php7.3-json php7.3-bz2 php7.3-pspell"
     software="$software $fpm"
 fi
 

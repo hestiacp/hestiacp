15a11a9ce5fe0869562fa973523a3e4e142d25fe
Raphael Schneeberger <rs@scit.ch>
2019-03-11 12:25:43 +0100

    Use hostname as 2fa barcode identifier.

--- web/inc/2fa/secret.php
+++ web/inc/2fa/secret.php
@@ -8,6 +8,6 @@ use \RobThree\Auth\TwoFactorAuth;
 $tfa = new TwoFactorAuth('Hestia Control Panel');
 
 $secret = $tfa->createSecret(160);  // Though the default is an 80 bits secret (for backwards compatibility reasons) we recommend creating 160+ bits secrets (see RFC 4226 - Algorithm Requirements)
-$qrcode = $tfa->getQRCodeImageAsDataUri('Hestia Control Panel', $secret);
+$qrcode = $tfa->getQRCodeImageAsDataUri(gethostname(), $secret);
 
 echo $secret . "-" . $qrcode;

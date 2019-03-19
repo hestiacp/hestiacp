d63b79b35144482e310455c16572d16e7001f83a
Kristan Kenney <40798553+kristankenney@users.noreply.github.com>
2019-03-19 00:13:44 -0300

    Add mail SSL flags to user configuration

--- install/upgrade/0.9.8-28.sh
+++ install/upgrade/0.9.8-28.sh
@@ -212,3 +212,28 @@ fi
 if [ -d $HESTIA/install/debian ]; then
     rm -rf $HESTIA/install/debian
 fi
+
+# Update user information for mail domain SSL configuration
+userlist=$(ls --sort=time $HESTIA/data/users/)
+for user in $userlist; do
+    USER_DATA="$HESTIA/data/users/$user"
+    # Update user counter
+    if [ -z "$(grep "U_MAIL_SSL" $USER_DATA/user.conf)" ]; then
+        echo "U_MAIL_SSL='0'" >> $USER_DATA/user.conf
+    else
+        sed -i "s/U_MAIL_SSL=.*/U_MAIL_SSL='0'/g" $USER_DATA/user.conf
+    fi
+
+    # Update mail configuration file
+    conf="$USER_DATA/mail.conf"
+    while read line ; do
+        eval $line
+        
+        add_object_key "mail" 'DOMAIN' "$DOMAIN" 'SSL' 'SUSPENDED'
+        update_object_value 'mail' 'DOMAIN' "$DOMAIN" '$SSL' 'no'
+        
+        add_object_key "mail" 'DOMAIN' "$DOMAIN" 'LETSENCRYPT' 'SUSPENDED'
+        update_object_value 'mail' 'DOMAIN' "$DOMAIN" '$LETSENCRYPT' 'no'
+    done < $conf
+done
+

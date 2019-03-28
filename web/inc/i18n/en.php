f89feb9feb40dff797028efc7d69b2f6117fc146
Kristan Kenney <40798553+kristankenney@users.noreply.github.com>
2019-03-28 01:05:57 -0300

    Add language string for 2FA reset code

--- web/inc/i18n/en.php
+++ web/inc/i18n/en.php
@@ -760,4 +760,5 @@ $LANG['en'] = array(
     '2 Factor Authentication' => 'Two-factor Authentication',
     'Enable 2FA' => 'Enable two-factor authentication',
     'Please scan the code below in your 2FA application:' => 'To finish setting up two-factor authentication, scan the QR code below<br />using an authentication app (such as <a href="https://play.google.com/store/apps/details?id=com.google.android.apps.authenticator2">Google Authenticator</a>):',
+    '2FA Reset Code:' => 'Account Recovery Code:',
 );

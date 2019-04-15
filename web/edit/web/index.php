822af76ef14bf4415ddaf10d3f76df08ff92ac51
Kristan Kenney <40798553+kristankenney@users.noreply.github.com>
2019-04-15 17:42:15 -0300

    Sign mail domain when signing web domain if available

--- web/edit/web/index.php
+++ web/edit/web/index.php
@@ -449,11 +449,9 @@ if (!empty($_POST['save'])) {
     // Add Lets Encrypt Mail domain support
     if ((!empty($_POST['v_ssl'])) && ( $v_letsencrypt == 'no' ) && (!empty($_POST['v_letsencrypt'])) && empty($_SESSION['error_msg'])) {
         $l_aliases = str_replace("\n", ',', $v_aliases);
-        exec (HESTIA_CMD."v-add-letsencrypt-domain ".$user." ".$v_domain." ' ' 'yes'", $output, $return_var);
+        exec (HESTIA_CMD."v-add-letsencrypt-domain ".$user." ".$v_domain." '' 'yes'", $output, $return_var);
         check_return_code($return_var,$output);
         unset($output);
-        $v_letsencrypt = 'yes';
-        $v_ssl = 'yes';
         $restart_web = 'yes';
         $restart_proxy = 'yes';
     }

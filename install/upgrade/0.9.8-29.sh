e338bf73d12871677506d510f8eb001c05dab1b4
Raphael Schneeberger <rs@scit.ch>
2019-04-16 15:12:41 +0200

    Fix versioning in sed.

--- install/upgrade/0.9.8-29.sh
+++ install/upgrade/0.9.8-29.sh
@@ -129,8 +129,8 @@ php_versions=$( ls -l /etc/php/ | grep ^d | wc -l )
 if [ "$php_versions" -gt 1 ]; then
     for v in $(ls /etc/php/); do
         cp -f $hestiacp/php-fpm/dummy.conf /etc/php/$d/fpm/pool.d/
-        v=$(echo "$v" | sed -e 's/[.]//')
-        sed -i "/s/9999/9999$v/g" /etc/php/$v/fpm/pool.d/dummy.conf
+        v1=$(echo "$v" | sed -e 's/[.]//')
+        sed -i "s/9999/9999$v1/g" /etc/php/$v/fpm/pool.d/dummy.conf
     done
 fi
 

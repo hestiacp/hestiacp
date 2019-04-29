970726a3a978d94ecdd021050314ff2c157b7eea
Kristan Kenney <40798553+kristankenney@users.noreply.github.com>
2019-04-29 16:48:56 -0400

    Export PATH as global variable
    Updated to export PATH variable globally instead of being limited to the root user.

--- install/hst-install-ubuntu.sh
+++ install/hst-install-ubuntu.sh
@@ -878,9 +878,9 @@ chmod 440 /etc/sudoers.d/admin
 echo "export HESTIA='$HESTIA'" > /etc/profile.d/hestia.sh
 chmod 755 /etc/profile.d/hestia.sh
 source /etc/profile.d/hestia.sh
-echo 'PATH=$PATH:'$HESTIA'/bin' >> /root/.bash_profile
-echo 'export PATH' >> /root/.bash_profile
-source /root/.bash_profile
+echo 'PATH=$PATH:'$HESTIA'/bin' >> /etc/profile
+echo 'export PATH' >> /etc/profile
+source /etc/profile
 
 # Configuring logrotate for Hestia logs
 cp -f $hestiacp/logrotate/hestia /etc/logrotate.d/hestia

a81597918f259514d4244b83eacbd6652f98e9da
Alexandros Ioannides <alex@ioannides.net>
2019-05-02 03:59:07 +0300

    Obsolete support for older Ubuntu versions

--- install/hst-install-ubuntu.sh
+++ install/hst-install-ubuntu.sh
@@ -1469,10 +1469,8 @@ if [ "$exim" = 'yes' ] && [ "$mysql" = 'yes' ]; then
     touch /var/log/roundcube/errors
     chmod 640 /var/log/roundcube/errors
     chown www-data:adm /var/log/roundcube/errors
-    if [ "$release" = '16.04' ] || [ "$release" = '18.04' ]; then
-        mv /etc/roundcube/db.inc.php /etc/roundcube/debian-db-roundcube.php
-        mv /etc/roundcube/main.inc.php /etc/roundcube/config.inc.php
-    fi
+    mv /etc/roundcube/db.inc.php /etc/roundcube/debian-db-roundcube.php
+    mv /etc/roundcube/main.inc.php /etc/roundcube/config.inc.php
     chmod 640           /etc/roundcube/debian-db*
     chown root:www-data /etc/roundcube/debian-db*
 

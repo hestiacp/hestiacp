005a3d117a4f75e58d107e4f8b2b1d16a8a46a84
Kristan Kenney <40798553+kristankenney@users.noreply.github.com>
2019-05-12 02:49:43 -0300

    Remove additional configuration files when suspending mail domains

--- func/rebuild.sh
+++ func/rebuild.sh
@@ -483,9 +483,10 @@ rebuild_mail_domain_conf() {
                 $HOMEDIR/$user/conf/mail/$domain/dkim.pem
         fi
 
-        # Removing symbolic link if domain is suspended
+        # Removing configuration files if domain is suspended
         if [ "$SUSPENDED" = 'yes' ]; then
             rm -f /etc/exim/domains/$domain_idn
+            rm -f /etc/dovecot/conf.d/domains/$domain_idn.conf
         fi
 
         # Adding mail directiry

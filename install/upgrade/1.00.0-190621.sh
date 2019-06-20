e8a466be3fad0fd2f2a1d8efb96520a841e549dd
Raphael Schneeberger <rs@scit.ch>
2019-06-20 21:02:25 +0200

    Rework the upgrade routine to support different version changes.

---
+++ install/upgrade/1.00.0-190621.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Define global variables
+if [ -z "$HESTIA" ] || [ ! -f "${HESTIA}/conf/hestia.conf" ]; then
+    export HESTIA="/usr/local/hestia"
+    export BIN="/usr/local/hestia/bin"
+fi
+
+# Set backup folder
+HESTIA_BACKUP="/root/hst_upgrade/$(date +%d%m%Y%H%M)"
+
+# Set installation source folder
+hestiacp="$HESTIA/install/deb"
+
+# Load hestia.conf
+source /usr/local/hestia/conf/hestia.conf
+
+# Get hestia version
+version=$(dpkg -l | awk '$2=="hestia" { print $3 }')
+
+# Compare version for upgrade routine
+if [ "$version" != "1.00.0-190618" ]; then
+    source $HESTIA/install/upgrade/1.00.0-190618.sh
+fi
+
+# Place additional commands below.

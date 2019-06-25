36190a1ba1dbdd9a0a6f6d0fb51996c9e46d30bd
Kristan Kenney <kristan.kenney@outlook.com>
2019-06-25 13:16:13 -0300

    Fix issue with upgrade script w/dash shell

--- install/upgrade/versions/1.0.1.sh
+++ install/upgrade/versions/1.0.1.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Hestia Control Panel upgrade script for target version 1.0.1
 
@@ -7,7 +7,7 @@
 #######################################################################################
 
 # Ensure that users from previous releases are set to the correct stable release branch
-if [ $RELEASE_BRANCH = "master" ] || [ $RELEASE_BRANCH = "develop" ]; then
+if [ ! -z "$RELEASE_BRANCH" ] && [ "$RELEASE_BRANCH" = "master" ] || [ "$RELEASE_BRANCH" = "develop" ]; then
     echo "(*) Updating default release branch configuration..."
     sed -i "/RELEASE_BRANCH/d" $HESTIA/conf/hestia.conf
     echo "RELEASE_BRANCH='release'" >> $HESTIA/conf/hestia.conf

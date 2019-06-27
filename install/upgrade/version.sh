16c8cd15ba9fe3501e93c02245c9b88c3d5e00fb
Kristan Kenney <kristan.kenney@outlook.com>
2019-06-27 14:50:36 -0300

    Initial commit of versioning for 1.0.2

--- install/upgrade/version.sh
+++ install/upgrade/version.sh
@@ -26,6 +26,10 @@ if [ $VERSION = "0.9.8-29" ]; then
     VERSION="1.00.0-190618"
 fi
 if [ $VERSION = "0.10.00" ] || [ $VERSION = "1.00.0-190618" ] || [ $VERSION = "1.00.0-190621" ]; then
+    source /usr/local/hestia/install/upgrade/versions/1.0.1.sh
+    VERSION="1.0.1"
+fi
+if [ $VERSION = "1.0.1" ]; then
     source /usr/local/hestia/install/upgrade/versions/$version.sh
     VERSION="$version"
 fi

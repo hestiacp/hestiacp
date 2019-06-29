f946880ef953b25ed945665539f1460f30c75690
Kristan Kenney <kristan.kenney@outlook.com>
2019-06-29 16:47:47 -0300

    Initial commit for version 1.0.3 development

--- install/upgrade/versions/1.0.3.sh
+++ install/upgrade/versions/1.0.3.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Hestia Control Panel upgrade script for target version 1.0.5
+# Hestia Control Panel upgrade script for target version 1.0.3
 
 #######################################################################################
 #######                      Place additional commands below.                   #######

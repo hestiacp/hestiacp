1ca5414c5bf7a027dc55a92f6aa5c544c377ec0e
Alexandros Ioannides <alex@ioannides.net>
2019-07-01 12:17:19 +0300

    Update 1.0.3.sh

--- install/upgrade/versions/1.0.3.sh
+++ install/upgrade/versions/1.0.3.sh
@@ -13,4 +13,13 @@ if [ -z $THEME ]; then
 fi
 
 # Replace dhparam 1024 with dhparam 4096
-cp -f $HESTIA/install/deb/ssl/dhparam.pem /etc/ssl
+echo "(*) Updating dhparam to 4096-bit..."
+mv /etc/ssl/dhparam.pem $HESTIA_BACKUP/conf/
+cp -rf $HESTIA/install/deb/ssl/dhparam.pem /etc/ssl/
+systemctl reload nginx
+
+# Enhance Dovecot security
+echo "(*) Enhancing Dovecot security..."
+mv /etc/dovecot/conf.d/10-ssl.conf $HESTIA_BACKUP/conf/
+cp -rf $HESTIA/install/deb/dovecot/10-ssl.conf /etc/dovecot/conf.d/
+systemctl restart dovecot

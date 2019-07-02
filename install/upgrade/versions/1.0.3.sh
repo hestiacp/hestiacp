d88d6a306f277d3e60129dca7f134008063f05bb
Kristan Kenney <40798553+kristankenney@users.noreply.github.com>
2019-07-02 01:40:46 -0300

    Update install file variable path in upgrade script

--- install/upgrade/versions/1.0.3.sh
+++ install/upgrade/versions/1.0.3.sh
@@ -15,7 +15,7 @@ fi
 # Replace dhparam 1024 with dhparam 4096
 echo "(*) Installing 4096-bit SSL security certificate..."
 mv /etc/ssl/dhparam.pem $HESTIA_BACKUP/conf/
-cp -rf $HESTIA/install/deb/ssl/dhparam.pem /etc/ssl/
+cp -rf $hestiacp/ssl/dhparam.pem /etc/ssl/
 chmod 600 /etc/ssl/dhparams.pem
 
 # Enhance Vsftpd security
@@ -26,7 +26,7 @@ sed -i "s|ssl_tlsv1=YES|ssl_tlsv1=NO|g" /etc/vsftpd.conf
 # Enhance Dovecot security
 echo "(*) Modifying Dovecot SSL configuration..."
 mv /etc/dovecot/conf.d/10-ssl.conf $HESTIA_BACKUP/conf/
-cp -rf $HESTIA/install/deb/dovecot/10-ssl.conf /etc/dovecot/conf.d/
+cp -rf $hestiacp/dovecot/10-ssl.conf /etc/dovecot/conf.d/
 
 # Update DNS resolvers in hestia-nginx's configuration
 echo "(*) Updating DNS resolvers for Hestia Internal Web Server..."

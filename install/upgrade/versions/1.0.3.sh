1cbe8aac6a049062f72ee90de38b3ac490318851
Kristan Kenney <40798553+kristankenney@users.noreply.github.com>
2019-07-02 01:49:05 -0300

    Fix copy methods in upgrade script

--- install/upgrade/versions/1.0.3.sh
+++ install/upgrade/versions/1.0.3.sh
@@ -15,18 +15,18 @@ fi
 # Replace dhparam 1024 with dhparam 4096
 echo "(*) Installing 4096-bit SSL security certificate..."
 mv /etc/ssl/dhparam.pem $HESTIA_BACKUP/conf/
-cp -rf $hestiacp/ssl/dhparam.pem /etc/ssl/
+cp -f $hestiacp/ssl/dhparam.pem /etc/ssl/
 chmod 600 /etc/ssl/dhparams.pem
 
 # Enhance Vsftpd security
 echo "(*) Modifying Vsftpd SSL configuration..."
-cp -rf /etc/vsftpd.conf $HESTIA_BACKUP/conf/
+cp -f /etc/vsftpd.conf $HESTIA_BACKUP/conf/
 sed -i "s|ssl_tlsv1=YES|ssl_tlsv1=NO|g" /etc/vsftpd.conf
 
 # Enhance Dovecot security
 echo "(*) Modifying Dovecot SSL configuration..."
 mv /etc/dovecot/conf.d/10-ssl.conf $HESTIA_BACKUP/conf/
-cp -rf $hestiacp/dovecot/conf.d/10-ssl.conf /etc/dovecot/conf.d/
+cp -f $hestiacp/dovecot/conf.d/10-ssl.conf /etc/dovecot/conf.d/
 
 # Update DNS resolvers in hestia-nginx's configuration
 echo "(*) Updating DNS resolvers for Hestia Internal Web Server..."

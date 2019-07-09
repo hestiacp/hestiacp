24447f8b116f202af7e4bc50cd5d35e534f3f249
Kristan Kenney <40798553+kristankenney@users.noreply.github.com>
2019-07-09 05:24:02 -0300

    Remove rebuild routine from 1.0.3.sh

--- install/upgrade/versions/1.0.3.sh
+++ install/upgrade/versions/1.0.3.sh
@@ -6,8 +6,3 @@
 #######                      Place additional commands below.                   #######
 #######################################################################################
 
-# Update webmail templates to rebuild all mail domains.
-if [ ! -z "$IMAP_SYSTEM" ]; then
-    echo "(*) Update and rebuild mail domains..."
-    $BIN/v-update-mail-templates > /dev/null 2>&1
-fi

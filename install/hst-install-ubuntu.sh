17d926db2df11620df99a39d6adc494417da1fc6
Alexandros Ioannides <alex@focus-net.net>
2020-01-03 18:04:21 +0200

    Minor spelling corrections

--- install/hst-install-ubuntu.sh
+++ install/hst-install-ubuntu.sh
@@ -33,10 +33,10 @@ software="apache2 apache2.2-common apache2-suexec-custom apache2-utils
     exim4-daemon-heavy expect fail2ban flex ftp git idn imagemagick
     libapache2-mod-fcgid libapache2-mod-php$fpm_v libapache2-mod-rpaf
     libapache2-mod-ruid2 lsof mc mariadb-client mariadb-common mariadb-server
-    nginx  ntpdate php$fpm_v php$fpm_v-cgi php$fpm_v-common php$fpm_v-curl
-    phpmyadmin php$fpm_v-mysql php$fpm_v-imap php$fpm_v-ldap php$fpm_v-apcu 
+    nginx ntpdate php$fpm_v php$fpm_v-cgi php$fpm_v-common php$fpm_v-curl
+    phpmyadmin php$fpm_v-mysql php$fpm_v-imap php$fpm_v-ldap php$fpm_v-apcu
     phppgadmin php$fpm_v-pgsql php$fpm_v-zip php$fpm_v-bz2 php$fpm_v-cli
-    php$fpm_v-common php$fpm_v-gd php$fpm_v-intl php$fpm_v-json php$fpm_v-zip 
+    php$fpm_v-common php$fpm_v-gd php$fpm_v-intl php$fpm_v-json php$fpm_v-zip
     php$fpm_v-mbstring php$fpm_v-opcache php$fpm_v-pspell php$fpm_v-readline
     php$fpm_v-xml postgresql postgresql-contrib proftpd-basic quota
     roundcube-core roundcube-mysql roundcube-plugins rrdtool rssh spamassassin

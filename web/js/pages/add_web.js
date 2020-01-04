2c80ec025eb2f6c7a692f69c8c849999b03bc362
Raphael Schneeberger <rs@scit.ch>
2020-01-04 14:46:22 +0100

    Bring back www-alias to rework the function.

--- web/js/pages/add_web.js
+++ web/js/pages/add_web.js
@@ -172,6 +172,14 @@ App.Listeners.WEB.keypress_domain_name();
 
 
 $(function() {
+    $('#v_domain').change(function() {
+        var prefix = 'www.';
+        if (((document.getElementById('v_domain').value).split(".")).length > 2) {
+            document.getElementById('v_aliases').value = "";
+        } else {
+            document.getElementById('v_aliases').value = prefix + document.getElementById('v_domain').value;
+        }
+    });
     App.Actions.WEB.toggle_letsencrypt($('input[name=v_letsencrypt]'))
 
     $('select[name="v_stats"]').change(function(evt){

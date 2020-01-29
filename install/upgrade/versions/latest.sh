e274922517ddf76049a0a7e17a137d4dd796dd0e
Raphael Schneeberger <rs@scit.ch>
2020-01-29 13:01:35 +0100

    Do not hide the php migration output.

--- install/upgrade/versions/latest.sh
+++ install/upgrade/versions/latest.sh
@@ -191,5 +191,5 @@ fi
 num_php_versions=$(ls -d /etc/php/*/fpm/pool.d 2>/dev/null |wc -l)
 if [ "$num_php_versions" -gt 1 ] && [ -z "$WEB_BACKEND" ]; then
     echo "(*) Migrate to new multiphp backend system..."
-    bash $HESTIA/install/upgrade/manual/migrate_multiphp.sh > /dev/null 2>&1
+    bash $HESTIA/install/upgrade/manual/migrate_multiphp.sh
 fi

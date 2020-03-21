7b3c4c6c8f571c19774a4eefcdfaedb6411a6941
Kristan Kenney <40798553+kristankenney@users.noreply.github.com>
2020-03-21 00:48:18 -0300

    Fix issue with rc.local not being executable

--- install/hst-install-ubuntu.sh
+++ install/hst-install-ubuntu.sh
@@ -1621,6 +1621,12 @@ if [ ! -z "$pub_ip" ] && [ "$pub_ip" != "$ip" ]; then
     if [ -e /etc/rc.local ]; then
         sed -i '/exit 0/d' /etc/rc.local
     fi
+
+    check_rclocal=$(cat /etc/rc.local | grep "#!")
+    if [ -z "$check_rclocal" ]; then
+        echo "#!/bin/sh" >> /etc/rc.local
+    fi
+
     echo "$HESTIA/bin/v-update-sys-ip" >> /etc/rc.local
     echo "exit 0" >> /etc/rc.local
     chmod +x /etc/rc.local

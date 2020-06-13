70ed8134682e5e775682a2caf027d34e43d36b1c
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2020-06-13 23:37:15 +0200

    #848 Addons
    As discussed...
    
    Instead downloading direct from server and forward it to the user
    
    A task is created and execute with the /usr/local/hestia/bin/v-update-sys-queue backup command
    
    File downloaded and a notification send to the user so he can download it. After 24 hours it is removed...

--- web/download/backup/index.php
+++ web/download/backup/index.php
@@ -1,50 +1,31 @@
 <?php
 // Init
 error_reporting(NULL);
+ob_start();
 session_start();
 include($_SERVER['DOCUMENT_ROOT']."/inc/main.php");
-$backup = basename($_GET['backup']);
 
-$downloaded = false;
-// Check if the backup exists
-if (!file_exists('/backup/'.$backup)) {
-    //try to download
-    if ((!empty($_SESSION['user'])) && ($_SESSION['user'] != 'admin')) {
-        if (strpos($backup, $user.'.') === 0) {
-            exec(HESTIA_CMD."v-download-backup " . escapeshellarg($backup), $output, $return_var);
-            check_return_code($return_var,$output);
-            unset($output);
-            if(!$_SESSION['error_msg']){
-                $downloaded = true;
-            }
-        }else{
-            exit(0);
-        }
-    }else{
-        exec(HESTIA_CMD."v-download-backup " . escapeshellarg($backup),$output, $return_var);
-        check_return_code($return_var,$output);
-        unset($output);
-        if(!$_SESSION['error_msg']){
-            $downloaded = true;
-        }
-    }
-    if (!file_exists('/backup/'.$backup)) {
-        exit(0);
-        $downloaded = false;
-    }
+// Check token
+if ((!isset($_GET['token'])) || ($_SESSION['token'] != $_GET['token'])) {
+    header('Location: /login/');
+    exit();
 }
 
-// Data
-if ($_SESSION['user'] == 'admin') {
-    header('Content-type: application/gzip');
-    header("Content-Disposition: attachment; filename=\"".$backup."\";" ); 
-    header("X-Accel-Redirect: /backup/" . $backup);
-}
-
-if ((!empty($_SESSION['user'])) && ($_SESSION['user'] != 'admin')) {
-    if (strpos($backup, $user.'.') === 0) {
-        header('Content-type: application/gzip');
-        header("Content-Disposition: attachment; filename=\"".$backup."\";" ); 
-        header("X-Accel-Redirect: /backup/" . $backup);
+$v_username = escapeshellarg($user);
+exec (HESTIA_CMD."v-schedule-user-backup ".$v_username, $output, $return_var);
+if ($return_var == 0) {
+    $_SESSION['error_msg'] = __('BACKUP_SCHEDULED');
+} else {
+    $_SESSION['error_msg'] = implode('<br>', $output);
+    if (empty($_SESSION['error_msg'])) {
+        $_SESSION['error_msg'] = __('Error: hestia did not return any output.');
     }
+
+    if ($return_var == 4) {
+        $_SESSION['error_msg'] = __('BACKUP_EXISTS');
+    }
+
 }
+unset($output);
+header("Location: /list/backup/");
+exit;

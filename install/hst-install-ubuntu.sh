6d850d92ad6269c58e234b4a6d93f7c10fa09b7e
Raphael Schneeberger <rs@scit.ch>
2020-06-14 18:48:12 +0200

    Remove libonig5 from installer.

--- install/hst-install-ubuntu.sh
+++ install/hst-install-ubuntu.sh
@@ -44,7 +44,7 @@ software="apache2 apache2.2-common apache2-suexec-custom apache2-utils
     postgresql postgresql-contrib proftpd-basic quota roundcube-core
     roundcube-mysql roundcube-plugins rrdtool rssh spamassassin sudo hestia
     hestia-nginx hestia-php vim-common vsftpd whois zip acl sysstat setpriv
-    libonig5 ipset"
+    ipset"
 
 # Defining help function
 help() {
@@ -804,9 +804,7 @@ fi
 if [ "$release" = '20.04' ]; then
     software=$(echo "$software" | sed -e "s/rssh//")
 fi
-if [ "$release" != '20.04' ]; then
-    software=$(echo "$software" | sed -e "s/libonig5//")
-fi
+
 
 #----------------------------------------------------------#
 #                 Disable Apparmor on LXC                  #

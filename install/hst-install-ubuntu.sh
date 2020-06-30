ededfbd6048acafd09772589afe2faab4292a09c
Raphael Schneeberger <rs@scit.ch>
2020-06-30 16:31:42 +0200

    Add libzip5 to software list to prevent a mix of v4 and v5 on 20.04.

--- install/hst-install-ubuntu.sh
+++ install/hst-install-ubuntu.sh
@@ -44,7 +44,7 @@ software="apache2 apache2.2-common apache2-suexec-custom apache2-utils
     postgresql postgresql-contrib proftpd-basic quota roundcube-core
     roundcube-mysql roundcube-plugins rrdtool rssh spamassassin sudo hestia
     hestia-nginx hestia-php vim-common vsftpd whois zip acl sysstat setpriv
-    ipset libonig5"
+    ipset libonig5 libzip5"
 
 # Defining help function
 help() {
@@ -803,9 +803,11 @@ if [ -d "$withdebs" ]; then
 fi
 if [ "$release" = '16.04' ]; then
     software=$(echo "$software" | sed -e "s/libonig5/libonig2/")
+    software=$(echo "$software" | sed -e "s/libzip5/libonig4/")
 fi
 if [ "$release" = '18.04' ]; then
     software=$(echo "$software" | sed -e "s/libonig5/libonig4/")
+    software=$(echo "$software" | sed -e "s/libzip5/libzip4/")
 fi
 if [ "$release" = '20.04' ]; then
     software=$(echo "$software" | sed -e "s/setpriv/util-linux/")

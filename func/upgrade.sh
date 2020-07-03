a85de3266547ca820daa63f0919ff8e4f3f25437
Raphael Schneeberger <rs@scit.ch>
2020-07-03 17:29:54 +0200

    Adjust version upgrades,add "burned" version number 1.1.2.

--- func/upgrade.sh
+++ func/upgrade.sh
@@ -147,20 +147,20 @@ upgrade_start_routine() {
 
     # Upgrade to Version 1.1.0
     if [ $VERSION = "1.0.6" ]; then
-        source $HESTIA/install/upgrade/versions/latest.sh
+        source $HESTIA/install/upgrade/versions/previous/1.1.0.sh
         VERSION="1.1.0"
         upgrade_refresh_config
     fi
 
     # Upgrade to Version 1.1.1
     if [ $VERSION = "1.1.0" ]; then
-        source $HESTIA/install/upgrade/versions/latest.sh
+        source $HESTIA/install/upgrade/versions/previous/1.1.1.sh
         VERSION="1.1.1"
         upgrade_refresh_config
     fi
 
     # Upgrade to Version 1.2.0
-    if [ $VERSION = "1.1.1" ]; then
+    if [ $VERSION = "1.1.1" || $VERSION = "1.1.2" ]; then
         source $HESTIA/install/upgrade/versions/latest.sh
         VERSION="$new_version"
         upgrade_refresh_config

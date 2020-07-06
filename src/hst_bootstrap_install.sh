179796fb24702ec2a59e91149e56b2c3bc681374
Kristan Kenney <40798553+kristankenney@users.noreply.github.com>
2020-07-06 02:33:17 -0300

    [DevTools] Installation bootstrapper for test builds
    Automation of compile and install. DO NOT USE FOR PRODUCTION INSTALLATIONS.

---
+++ src/hst_bootstrap_install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Clean installation bootstrap for development purposes only
+# Usage:    ./hst_bootstrap_install.sh [fork] [branch] [os]
+# Example:  ./hst_bootstrap_install.sh hestiacp master ubuntu
+
+# Define variables
+fork=$1
+branch=$2
+os=$3
+
+# Download specified installer and compiler
+wget https://raw.githubusercontent.com/$fork/hestiacp/$branch/install/hst-install-$os.sh
+wget https://raw.githubusercontent.com/$fork/hestiacp/$branch/src/hst_autocompile.sh
+
+# Execute compiler and build hestia core package
+chmod +x hst_autocompile.sh
+./hst_autocompile.sh --hestia master no
+
+# Execute Hestia Control Panel installer with default dummy options for testing
+bash hst-install-$os.sh -f -y no -e admin@test.local -p P@ssw0rd -s hestia-$branch-$os.test.local --with-debs /tmp/hestiacp-src/debs

6d19d2ab7dba43353ddc22c6af9021e575eee74c
vestacp-user <67346926+vestacp-user@users.noreply.github.com>
2020-07-24 10:20:34 +0000

    Update hst-install-debian.sh

--- install/hst-install-debian.sh
+++ install/hst-install-debian.sh
@@ -310,40 +310,10 @@ apt-get -qq update
 # Creating backup directory
 mkdir -p $hst_backups
 
-# Checking wget
-if [ ! -e '/usr/bin/wget' ]; then
-    echo "[ * ] Installing wget..."
-    apt-get -y install wget >> $LOG
-    check_result $? "Can't install wget"
-fi
-
-# Checking curl
-if [ ! -e '/usr/bin/curl' ]; then
-    echo "[ * ] Installing curl..."
-    apt-get -y install curl >> $LOG
-    check_result $? "Can't install curl"
-fi
-
-# Checking dirmngr
-if [ ! -e '/usr/bin/dirmngr' ]; then
-    echo "[ * ] Installing dirmngr..."
-    apt-get -y install dirmngr >> $LOG
-    check_result $? "Can't install dirmngr"
-fi
-
-# Check if apt-transport-https is installed
-if [ ! -e '/usr/lib/apt/methods/https' ]; then
-    echo "[ * ] Installing apt-transport-https..."
-    apt-get -y install apt-transport-https >> $LOG
-    check_result $? "Can't install apt-transport-https"
-fi
-
-# Check if gnupg or gnupg2 is installed
-if [ ! -e '/usr/lib/gnupg2' ] || [ ! -e '/usr/lib/gnupg' ]; then
-    echo "[ * ] Installing gnupg2..."
-    apt-get -y install gnupg2 >> $LOG
-    check_result $? "Can't install gnupg2"
-fi
+# Pre-install packages
+echo "[ * ] Enforcing pre-install packages requirements..."
+apt-get -y install apt-transport-https curl dirmngr gnupg ntpdate wget >> $LOG
+check_result $? "Pre-install packages failed, check logs"
 
 # Check if apparmor is installed
 if [ $(dpkg-query -W -f='${Status}' apparmor 2>/dev/null | grep -c "ok installed") -eq 0 ]; then

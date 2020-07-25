e20beed459d2540d955c1c1e7186e867378bd384
Kristan Kenney <40798553+kristankenney@users.noreply.github.com>
2020-07-25 08:46:59 -0300

    Minor fixes to upgrade script

--- install/upgrade/versions/latest.sh
+++ install/upgrade/versions/latest.sh
@@ -8,7 +8,6 @@
 
 # Update template files to add warnings
 # Backup current templates
-cp -r -f $HESTIA/data/templates/* $HESTIA_BACKUP/templates/
 echo "[ ! ] Updating default web domain templates..."
 $BIN/v-update-web-templates
 echo "[ ! ] Updating default mail domain templates..."
@@ -19,7 +18,9 @@ $BIN/v-update-dns-templates
 # Enhance Vsftpd security
 if [ "$FTP_SYSTEM" = "vsftpd" ]; then
     echo "[ ! ] Hardening Vsftpd TLS configuration..."
-    cp -f /etc/vsftpd.conf $HESTIA_BACKUP/conf/
+    if [ -e /etc/vsftpd.conf ]; then
+        rm -f /etc/vsftpd.conf
+    fi
     cp -f $HESTIA_INSTALL_DIR/vsftpd/vsftpd.conf /etc/
     chmod 644 /etc/vsftpd.conf
 fi

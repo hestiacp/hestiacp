c3c5635c90fd52f37f440dcf1e50000e88ee6462
James Buren <braewoods+mgh@braewoods.net>
2020-07-28 11:09:09 -0500

    Remove chroot jail directories when deleting it

--- func/main.sh
+++ func/main.sh
@@ -1172,4 +1172,5 @@ delete_chroot_jail() {
     systemctl disable srv-jail-$user-home.mount > /dev/null 2>&1
     rm -f /etc/systemd/system/srv-jail-$user-home.mount
     systemctl daemon-reload > /dev/null 2>&1
+    rmdir -p /srv/jail/$user > /dev/null 2>&1
 }

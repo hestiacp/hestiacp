f30b980b9f268d3c26e0f1f0eff44bb4858cd021
David Kang <3746349+hahagu@users.noreply.github.com>
2020-08-26 17:10:37 +0900

    NodeJS Support

--- install/upgrade/versions/1.3.0.sh
+++ install/upgrade/versions/1.3.0.sh
@@ -5,3 +5,13 @@
 #######################################################################################
 #######                      Place additional commands below.                   #######
 #######################################################################################
+
+# Add NPM to the default writeable folder list
+echo "[ * ] Updating default writable folders for all users..."
+for user in $($HESTIA/bin/v-list-sys-users plain); do
+    mkdir -p \
+        $HOMEDIR/$user/.npm
+
+    chown $user:$user \
+        $HOMEDIR/$user/.npm
+done

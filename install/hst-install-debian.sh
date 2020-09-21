61e59adb2a075e6aabb4219336f5c46de41af3be
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2020-09-21 12:10:52 +0200

    Upgrade installer + Manual upgrade 10.4 to 10.5 (#1219)
    * Upgrade installer + Manual upgrade 10.4 to 10.5
    
    * Update CHANGELOG.md

--- install/hst-install-debian.sh
+++ install/hst-install-debian.sh
@@ -27,7 +27,7 @@ HESTIA_INSTALL_VER='1.3.0~beta'
 pma_v='5.0.2'
 multiphp_v=("5.6" "7.0" "7.1" "7.2" "7.3" "7.4")
 fpm_v="7.3"
-mariadb_v="10.4"
+mariadb_v="10.5"
 
 if [ "$release" -eq 9 ]; then
     software="nginx apache2 apache2-utils apache2-suexec-custom

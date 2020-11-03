a9996c29525ef2e073e2c4e989753a0b63792ab0
cmstew <cmstew@gmail.com>
2020-11-02 21:40:59 -0700

    Fixes bug where the same user gets added multiple times to the ip file.

--- install/upgrade/versions/1.3.1.sh
+++ install/upgrade/versions/1.3.1.sh
@@ -5,3 +5,15 @@
 #######################################################################################
 #######                      Place additional commands below.                   #######
 #######################################################################################
+
+# Remove duplicate values in U_SYS_USERS variable for ips
+for ip in $(ls $HESTIA/data/ips/); do
+    current_usr=$(grep "U_SYS_USERS=" $HESTIA/data/ips/$ip |cut -f 2 -d \')
+    
+    new_usr=$(echo $current_usr | sed 's/,/\n/g' | sort | uniq | paste -sd,)
+
+    if [ ! -z $new_usr ]; then
+        sed -i "s/U_SYS_USERS='$current_usr'/U_SYS_USERS='$new_usr'/g" $HESTIA/data/ips/$ip
+    fi
+done
+

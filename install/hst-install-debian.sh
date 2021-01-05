f9a4342cc21c4bb755d410191cf691808077298f
Raphael Schneeberger <rs@scit.ch>
2021-01-05 10:15:02 +0100

    Increase version number to 1.4.0

--- install/hst-install-debian.sh
+++ install/hst-install-debian.sh
@@ -23,7 +23,7 @@ HESTIA_INSTALL_DIR="$HESTIA/install/deb"
 VERBOSE='no'
 
 # Define software versions
-HESTIA_INSTALL_VER='1.3.3~alpha'
+HESTIA_INSTALL_VER='1.4.0~alpha'
 pma_v='5.0.4'
 multiphp_v=("5.6" "7.0" "7.1" "7.2" "7.3" "7.4" "8.0")
 fpm_v="7.4"

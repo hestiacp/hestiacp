67289e09cbb31dc104561b3043f239e761300310
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2021-01-08 11:55:10 +0000

    Add rainloop support to Hestia Default activated plugins: - add-x-originating-ip-header (from rainloop github) -hestia-change-password (from local source) Changes made: - Change default admin username - Change default admin password - Change default admin key - Enabled contacts / adressbook via mysql on default - Enabled the plugins listed + set up default settings
    File get created in ~/.rainloop with the default login data!

--- func/domain.sh
+++ func/domain.sh
@@ -745,6 +745,7 @@ add_webmail_config() {
         override_alias="mail.$domain"
     fi
     
+    echo "$1/$2"
     cat $MAILTPL/$1/$2 | \
         sed -e "s|%ip%|$local_ip|g" \
             -e "s|%domain%|$WEBMAIL_ALIAS.$domain|g" \

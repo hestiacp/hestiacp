8607b72b8b2a2f994a1f4e39f706cc613b777fe8
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2021-03-07 15:10:42 +0000

    Change default to no

--- func/upgrade.sh
+++ func/upgrade.sh
@@ -146,8 +146,8 @@ upgrade_health_check() {
 
     # Inactive session timeout
     if [ -z "$ALLOW_USERS_SYSTEM" ]; then
-        echo "[ ! ] Adding missing variable to hestia.conf: ALLOW_USERS_SYSTEM ('yes')"
-        $BIN/v-change-sys-config-value "ALLOW_USERS_SYSTEM" "yes"
+        echo "[ ! ] Adding missing variable to hestia.conf: ALLOW_USERS_SYSTEM ('no')"
+        $BIN/v-change-sys-config-value "ALLOW_USERS_SYSTEM" "no"
     fi    
     
     echo "[ * ] Health check complete. Starting upgrade from $VERSION to $new_version..."

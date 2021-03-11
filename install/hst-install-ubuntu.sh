1430f43f64a28c3d8d7592848ef8783e2882f2c1
Kristan Kenney <40798553+kristankenney@users.noreply.github.com>
2021-03-11 09:53:12 -0400

    Bump phpMyAdmin to v5.1.0

--- install/hst-install-ubuntu.sh
+++ install/hst-install-ubuntu.sh
@@ -24,7 +24,7 @@ VERBOSE='no'
 
 # Define software versions
 HESTIA_INSTALL_VER='1.3.4~beta'
-pma_v='5.0.4'
+pma_v='5.1.0'
 multiphp_v=("5.6" "7.0" "7.1" "7.2" "7.3" "7.4" "8.0")
 fpm_v="7.4"
 mariadb_v="10.5"

80e6ed6b37b4256198c225618d3be72dc335bf5c
danielalexis <danielalexis.o.pereira@gmail.com>
2021-03-23 13:14:18 +0100

    list sys config endpoint

---
+++ web/api/v2/list/sys-config/index.php
@@ -0,0 +1,42 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT']."/api/v2/authentication.php");
+//HESTIA_CMD is set on authentication.php
+$headers = getallheaders();
+header('Content-Type: application/json');
+
+
+if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+    echo('{"Error": "Method not allowed"}');
+    exit();
+}
+
+if (!isAPIenabled()) {
+    echo('{"Error": "API not enabled"}');
+    exit();
+}
+
+
+if (!isset($headers["Auth"])){
+    echo('{"Error": "Missing Authentication"}');
+    exit();
+}
+
+$auth = authAPI($headers["Auth"]);
+if (!$auth){
+    echo('{"Error": "No valid API key"}');
+    exit();
+} else {
+    $userinfo = json_decode($auth, true); //decode json into array
+    if ($userinfo["role"] == "admin") {
+        exec(HESTIA_CMD."v-list-sys-config json" , $output, $return_var);
+        echo(implode('', $output));
+        unset($output);
+        exit();
+    } else {
+        echo('{"Error": "Unauthorized"}');
+        exit();
+    }
+    
+    
+}
+?>

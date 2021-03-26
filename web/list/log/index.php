cd8c76b200c562d6f97a14a013623fe1e63482fd
Kristan Kenney <40798553+kristankenney@users.noreply.github.com>
2021-03-26 15:53:35 -0300

    Allow system administrator to view system log

--- web/list/log/index.php
+++ web/list/log/index.php
@@ -6,6 +6,9 @@ $TAB = 'LOG';
 include($_SERVER['DOCUMENT_ROOT']."/inc/main.php");
 
 // Data
+if (($_SESSION['userContext'] === "admin") && ($_GET['user'])) {
+    $user=escapeshellarg($_GET['user']);
+}
 exec (HESTIA_CMD."v-list-user-log $user json", $output, $return_var);
 check_error($return_var);
 $data = json_decode(implode('', $output), true);

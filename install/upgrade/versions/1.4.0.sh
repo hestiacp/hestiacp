c739b409e50087ca5f2d7de29a1be3e333d5fc73
Alexandros Ioannides <alex@focus-net.net>
2021-04-02 01:34:20 +0300

    Consolidate nginx templates

--- install/upgrade/versions/1.4.0.sh
+++ install/upgrade/versions/1.4.0.sh
@@ -126,3 +126,13 @@ if [ ! -f "$HESTIA/conf/defaults/hestia.conf" ]; then
     fi
     cp -f $HESTIA/conf/hestia.conf $HESTIA/conf/defaults/hestia.conf
 fi
+
+# Consolidate nginx (standalone) templates used by active websites
+if [ "$WEB_SYSTEM" = "nginx" ]; then
+    echo "[ * ] Consolidating nginx templates for Drupal & CodeIgniter..."
+    sed -i "s|TPL='drupal6'|TPL='drupal'|g" $HESTIA/data/users/*/web.conf
+    sed -i "s|TPL='drupal7'|TPL='drupal'|g" $HESTIA/data/users/*/web.conf
+    sed -i "s|TPL='drupal8'|TPL='drupal'|g" $HESTIA/data/users/*/web.conf
+    sed -i "s|TPL='codeigniter2'|TPL='codeigniter'|g" $HESTIA/data/users/*/web.conf
+    sed -i "s|TPL='codeigniter3'|TPL='codeigniter'|g" $HESTIA/data/users/*/web.conf
+fi

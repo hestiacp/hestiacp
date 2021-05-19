98a92bcb0fb34ba733c7b9c881aeb7eb8985d8a6
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2021-05-19 13:40:59 +0200

    Add upgrade script for default php version www.conf
    www.conf is used for Roundcube, Rainloop and phpmyadmin
    Removal of the "www.conf" php version will cause issues with Rainloop not working. Current script updates it to the latests version of PHP installed. If that is not wanted use this script
    + Updated permission upgrade_php.sh

---
+++ install/upgrade/manual/upgrade_multi_php.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Update www.conf to the a different version so users can safely deleted older php version. 
+# www.conf is used for Roundcube, Rainloop and phpmyadmin
+# Removal of the "www.conf" php version will cause issues with Rainloop not working. Current script updates it to the latests version of PHP installed. If that is not wanted use this script
+
+version=$1
+if [ ! -x "$(command -v php)" ]; then
+    echo "PHP is not installed. Aborting."
+    exit 1
+fi
+
+# Verify php version format
+if [[ ! $version =~ ^[0-9]\.[0-9]+ ]]; then
+    echo "The PHP version format is invalid, it should look like [0-9].[0-9]."
+    echo "Example:  7.0, 7.4"
+    exit
+fi
+
+if [ ! -f /etc/php/$version/fpm/pool.d/dummy.conf ]; then
+    echo "PHP versions doesn't exists"
+    exit;
+fi
+
+rm -f /etc/php/*/fpm/pool.d/www.conf
+cp -f $HESTIA/install/deb/php-fpm/www.conf /etc/php/$version/fpm/pool.d/www.conf
+$HESTIA/bin/v-restart-web-backend

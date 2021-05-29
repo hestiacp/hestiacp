2cf9f0c601d054677c91450e383a468af0f066cd
s0t <s0t@users.noreply.github.com>
2021-05-30 01:23:17 +0300

    Update index.php

--- web/logout/index.php
+++ web/logout/index.php
@@ -11,17 +11,15 @@ if (!empty($_SESSION['look'])) {
     # Remove current path for filemanager
     unset($_SESSION['_sf2_attributes']);
     unset($_SESSION['_sf2_meta']);
-    header("Location: /");
+    header('Location: /');
 } else {
-    if($_SESSION['token'] && $_SESSION['user']){
+    if ($_SESSION['token'] && $_SESSION['user']){
         unset($_SESSION['userTheme']);
         $v_user = escapeshellarg($_SESSION['user']);
         $v_session_id = escapeshellarg($_SESSION['token']);
-        exec(HESTIA_CMD."v-log-user-logout ".$v_user." ".$v_session_id, $output, $return_var);
+        exec(HESTIA_CMD . 'v-log-user-logout ' . $v_user . ' ' . $v_session_id, $output, $return_var);
     }
-    
     session_destroy();
-    header("Location: /login/");
+    header('Location: /login/');
 }
 exit;
-?>

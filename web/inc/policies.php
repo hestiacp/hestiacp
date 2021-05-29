ebde800e2a43f767199e17a29aedf51d637a8c55
s0t <s0t@users.noreply.github.com>
2021-05-29 14:01:21 +0300

    Update policies.php

--- web/inc/policies.php
+++ web/inc/policies.php
@@ -1,7 +1,7 @@
 <?php
 
-    if (($_SESSION['userContext'] === 'user') && ($panel[$user]['SUSPENDED'] === 'yes') && ($_SESSION['POLICY_USER_VIEW_SUSPENDED'] === 'yes') ||
-       ($_SESSION['userContext'] === 'admin') && ($_SESSION['look'] === 'admin') && ($_SESSION['POLICY_SYSTEM_PROTECTED_ADMIN'] === 'yes')) {
+    if ((($_SESSION['userContext'] === 'user') && ($panel[$user]['SUSPENDED'] === 'yes') && ($_SESSION['POLICY_USER_VIEW_SUSPENDED'] === 'yes')) ||
+       (($_SESSION['userContext'] === 'admin') && ($_SESSION['look'] === 'admin') && ($_SESSION['POLICY_SYSTEM_PROTECTED_ADMIN'] === 'yes'))) {
       $read_only = 'true';
     }
 

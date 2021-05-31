45894844b16725b0817311f19733303360071084
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2021-05-31 13:33:34 +0200

    Add extra rule to 1.4.2
    On v-update-firewall the iptables.rules will be rewritten/generated and If this rule is skipped it will be not created 
    https://github.com/myrevery/hestiacp/blob/f0c7c54261c289fb10ea283618dda71b7cddd320/bin/v-update-firewall#L79-L84

--- install/upgrade/versions/1.4.2.sh
+++ install/upgrade/versions/1.4.2.sh
@@ -9,6 +9,8 @@
 # Optimize loading firewall rules
 if [ "$FIREWALL_SYSTEM" = "iptables" ]; then
     echo "[ * ] Fix the issue of loading firewall rules..."
+    # Add rule to ensure the rule will be added when we update the firewall / /etc/iptables.rules
+    iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
     rm -f /usr/lib/networkd-dispatcher/routable.d/50-ifup-hooks /etc/network/if-pre-up.d/iptables
     $BIN/v-update-firewall
 fi

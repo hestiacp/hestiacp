20b0fd922148206abea5accfc2bf1178a6dadb24
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2021-05-31 14:14:51 +0200

    Allow interactive to skip reboot (#1843)
    * Allow interactive to skip reboot
    
    * Update install/hst-install-debian.sh
    
    Co-authored-by: Wojciech Smoliński <wojsmol@wp.pl>
    
    * Update install/hst-install-ubuntu.sh
    
    Co-authored-by: Wojciech Smoliński <wojsmol@wp.pl>
    
    Co-authored-by: Wojciech Smoliński <wojsmol@wp.pl>

--- install/hst-install-debian.sh
+++ install/hst-install-debian.sh
@@ -1934,16 +1934,17 @@ rm -f $tmpfile
 # Add welcome message to notification panel
 $HESTIA/bin/v-add-user-notification admin 'Welcome to Hestia Control Panel!' '<br>You are now ready to begin <a href="/add/user/">adding user accounts</a> and <a href="/add/web/">domains</a>. For help and assistance, view the <a href="https://docs.hestiacp.com/" target="_new">documentation</a> or visit our <a href="https://forum.hestiacp.com/" target="_new">user forum</a>.<br><br>Please report any bugs or issues via <a href="https://github.com/hestiacp/hestiacp/issues" target="_new"><i class="fab fa-github"></i> GitHub</a>.<br><br><b>Have a wonderful day!</b><br><br><i class="fas fa-heart status-icon red"></i> The Hestia Control Panel development team'
 
-echo "[ ! ] IMPORTANT: System will reboot"
-echo ""
-if [ "$interactive" = 'yes' ]; then
-    echo -n " Press any key to continue!"
-    read reboot
-fi
-
 # Clean-up
 # Sort final configuration file
 sort_config_file
 
-reboot
+if [ "$interactive" = 'yes' ]; then
+    echo "[ ! ] IMPORTANT: System will reboot"
+    echo ""
+    echo -n " Press any key to continue!"
+    read reboot
+    reboot
+else
+    echo "[ ! ] IMPORTANT: You must logout or restart the server before continuing"
+fi
 # EOF

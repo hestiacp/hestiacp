6d398f6a51ec3870641df16afdb0a5c7b67e42ed
myrevery <16082142+myrevery@users.noreply.github.com>
2021-05-31 18:00:30 -0700

    Fix potential issue of updating to Nginx 1.21.0

--- install/upgrade/versions/1.4.2.sh
+++ install/upgrade/versions/1.4.2.sh
@@ -14,3 +14,16 @@ if [ "$FIREWALL_SYSTEM" = "iptables" ]; then
     rm -f /usr/lib/networkd-dispatcher/routable.d/50-ifup-hooks /etc/network/if-pre-up.d/iptables
     $BIN/v-update-firewall
 fi
+
+# Fix potential issue of updating to Nginx 1.21.0
+if [ "WEB_SYSTEM" = "nginx" ] || [ "PROXY_SYSTEM" = "nginx" ]; then
+    default_conf="/etc/nginx/conf.d/default.conf"
+    nginx_conf="/etc/nginx/nginx.conf"
+
+    [ -f "${default_conf}" ]          && mv -f ${default_conf} ${default_conf}.dpkg-dist
+    [ -f "${default_conf}.dpkg-new" ] && mv -f ${default_conf}.dpkg-new ${default_conf}.dpkg-dist
+    [ -f "${nginx_conf}.dpkg-new" ]   && mv -f ${nginx_conf}.dpkg-new ${nginx_conf}.dpkg-dist
+    [ -f "${nginx_conf}.dpkg-old" ]   && mv -f ${nginx_conf} ${nginx_conf}.dpkg-dist \
+                                      && rm -f ${nginx_conf}.dpkg-old \
+                                      && cp -f $HESTIA/install/deb/nginx/nginx.conf /etc/nginx/
+fi

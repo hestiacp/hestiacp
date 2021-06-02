b83891f83d52e700b542a3c5e9343b12d6b9ba43
wojsmol <wojsmol@wp.pl>
2021-06-03 01:41:12 +0200

    fix version in upgrade script

--- install/upgrade/versions/1.4.3.sh
+++ install/upgrade/versions/1.4.3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Hestia Control Panel upgrade script for target version 1.4.2
+# Hestia Control Panel upgrade script for target version 1.4.3
 
 #######################################################################################
 #######                      Place additional commands below.                   #######

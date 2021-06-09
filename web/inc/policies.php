620332a8bc20f24c3132e2feb1f0942010c24272
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2021-06-09 22:12:22 +0200

    Fix multiple issues  (#1899)
    * Update top_js.html
    
    Fix: "Warning: The type attribute for the script element is not needed and should be omitted."
    
    * Update end_js.html
    
    Fix: "Warning: The type attribute for the script element is not needed and should be omitted."
    
    * Update css.html
    
    Fix: "Warning: The type attribute for the link element is not needed and should be omitted."
    
    * Update main.php
    
    Fix php style
    
    * Update main.php
    
    * Update main.php
    
    * Update policies.php
    
    * Update policies.php
    
    * Update secure_login.php
    
    * Update query-3.6.0.min.js
    
    * Update top_js.html
    
    * Update footer.html
    
    * Update header.html
    
    * Update header.html
    
    * Update index.php
    
    Fix php style
    
    * Update main.php
    
    Fix php style and optimize code
    
    * Update index.php
    
    Fix php style and optimize code
    
    * Update index.php
    
    * Update css.html
    
    Fix load custom theme
    
    * Update hotkeys.html
    
    Optimize code, fix error
    
    * Update hotkeys.html
    
    fix }
    
    * Update index.php
    
    * Fix XSS issue with list rrd
    
    * Fix XSS issue on search page
    
    * Fix XSS issue on login page
    
    - Remove hidden user field
    - htmlspecialchars username
    - Delete old session when generate new session.
    
    * XSS issue with $_GET['user']
    
    * Update changelog + improve regenerate session code
    
    * Allow static files to be cached
    
    Change release branch or enable debug mode to disable the caching
    
    * Force redirect user to login
    
    * Improve error message 
    
    Replace "Message" sub.domain.com allready exsists with rv-add-web-domain idn lalal.xxx.nu
    Error: xxx.nu belongs to a different user
    
    * Fix issues with login screen
    
    * Update changelog
    
    Co-authored-by: s0t <s0t@users.noreply.github.com>

--- web/inc/policies.php
+++ web/inc/policies.php
@@ -1,15 +1,10 @@
 <?php
 
-    if (($_SESSION['userContext'] === 'user') && ($panel[$user]['SUSPENDED'] === 'yes') && ($_SESSION['POLICY_USER_VIEW_SUSPENDED'] === 'yes')) {
-      $read_only='true';
-    }
-    
-    if (($_SESSION['userContext'] === 'admin') && ($_SESSION['look'] === 'admin') && ($_SESSION['POLICY_SYSTEM_PROTECTED_ADMIN'] === 'yes')) {
-      $read_only='true';
+    if ((($_SESSION['userContext'] === 'user') && ($panel[$user]['SUSPENDED'] === 'yes') && ($_SESSION['POLICY_USER_VIEW_SUSPENDED'] === 'yes')) ||
+       (($_SESSION['userContext'] === 'admin') && ($_SESSION['look'] === 'admin') && ($_SESSION['POLICY_SYSTEM_PROTECTED_ADMIN'] === 'yes'))) {
+      $read_only = 'true';
     }
 
     if ($read_only === 'true') {
-      $display_mode='disabled';
+      $display_mode = 'disabled';
     }
-
-?>

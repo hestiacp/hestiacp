b019f3cc7e8aa22039880c84dc4386ed6cd6afb1
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2021-06-24 11:48:55 +0200

    Include upgrade script

--- install/upgrade/versions/1.4.4.sh
+++ install/upgrade/versions/1.4.4.sh
@@ -18,3 +18,20 @@ if [ "$PHPMYADMIN_KEY" != "" ]; then
     $BIN/v-delete-sys-pma-sso 
     $BIN/v-add-sys-pma-sso 
 fi
+
+if [ -d "/etc/phpmyadmin/" ]; then 
+    # limit access to /etc/phpmyadmin/ and /usr/share/phpmyadmin/tmp and so on
+    chown -R root:www-data /etc/phpmyadmin/
+    chmod -R 640  /etc/phpmyadmin/*
+    if [ -d "/etc/phpmyadmin/conf.d/" ]; then 
+        chmod 750 /etc/phpmyadmin/conf.d/
+    fi
+    if [ -d "/var/lib/phpmyadmin/tmp" ]; then 
+        chown root:www-data /usr/share/phpmyadmin/tmp
+        chmod 770 /usr/share/phpmyadmin/tmp
+    fi
+    if [ -d "/var/lib/phpmyadmin/tmp" ]; then 
+        chmod 770 /var/lib/phpmyadmin/tmp
+        chown root:www-data /usr/share/phpmyadmin/tmp
+    fi
+fi

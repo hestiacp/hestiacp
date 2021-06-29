a266d884759e37f84e1c541822b7955a9d9ecf1d
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2021-06-29 13:35:12 +0200

    Mute output v-add-sys-pma-sso
    And v-delete-sys-pma-sso

--- install/upgrade/versions/1.4.4.sh
+++ install/upgrade/versions/1.4.4.sh
@@ -33,6 +33,6 @@ fi
 # Reset PMA SSO to fix bug with Nginx + Apache2 
 if [ "$PHPMYADMIN_KEY" != "" ]; then
     echo "[ * ] Refressh hestia-sso for PMA..."
-    $BIN/v-delete-sys-pma-sso 
-    $BIN/v-add-sys-pma-sso 
+    $BIN/v-delete-sys-pma-sso quiet
+    $BIN/v-add-sys-pma-sso quiet
 fi

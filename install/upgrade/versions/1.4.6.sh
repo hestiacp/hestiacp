295b9b43da6e5088309d933b4529871e3dc59886
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2021-07-07 19:18:15 +0200

    Fix if statement

--- install/upgrade/versions/1.4.6.sh
+++ install/upgrade/versions/1.4.6.sh
@@ -6,6 +6,6 @@
 #######                      Place additional commands below.                   #######
 #######################################################################################
 
-if [ -z "$DB_PMA_ALIAS" ]; then 
+if [ ! -z "$DB_PMA_ALIAS" ]; then 
     $HESTIA/bin/v-change-sys-db-alias 'pma' $DB_PMA_ALIAS
 fi

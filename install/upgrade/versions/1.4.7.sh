9b8611d7d4c3f48efa3585bfda1d36087a24a02d
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2021-07-15 09:03:18 +0200

    Fix #1984 phppgadmin not working on apache2 systems

--- install/upgrade/versions/1.4.7.sh
+++ install/upgrade/versions/1.4.7.sh
@@ -6,3 +6,6 @@
 #######                      Place additional commands below.                   #######
 #######################################################################################
 
+if [ ! -z "$DB_PGA_ALIAS" ]; then 
+    $HESTIA/bin/v-change-sys-db-alias 'pga' $DB_PGA_ALIAS
+fi

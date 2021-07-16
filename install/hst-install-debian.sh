6dbabd5ef555c71a4ac7807dbae4e896e05777ab
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2021-07-16 06:48:36 +0200

    Prepare for 1.4.8~alpha

--- install/hst-install-debian.sh
+++ install/hst-install-debian.sh
@@ -23,7 +23,7 @@ HESTIA_INSTALL_DIR="$HESTIA/install/deb"
 VERBOSE='no'
 
 # Define software versions
-HESTIA_INSTALL_VER='1.4.7'
+HESTIA_INSTALL_VER='1.4.7~alpha'
 pma_v='5.1.1'
 rc_v="1.4.11"
 multiphp_v=("5.6" "7.0" "7.1" "7.2" "7.3" "7.4" "8.0")
--- install/upgrade/versions/1.4.8.sh
+++ install/upgrade/versions/1.4.8.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# Hestia Control Panel upgrade script for target version 1.4.7
+# Hestia Control Panel upgrade script for target version 1.4.8
 
 #######################################################################################
 #######                      Place additional commands below.                   #######
 #######################################################################################
-
-if [ ! -z "$DB_PGA_ALIAS" ]; then 
-    $HESTIA/bin/v-change-sys-db-alias 'pga' $DB_PGA_ALIAS
-fi

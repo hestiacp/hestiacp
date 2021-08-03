58182e8554903583a00c1321dc40a70d416af5e5
erikdemarco <42645368+erikdemarco@users.noreply.github.com>
2021-08-03 21:30:17 +0700

    Delete old configuration when changing template
    When we want to change nginx template. The current hestiacp will only check if there is a 'proxy_cache_path' setting for this domain in '/etc/nginx/conf.d/01_caching_pool.conf'. if there is, hestia will skip adding config again. Because hestia will assume the configuration still the same. Remember, Other template can also change this config.
    
    We need to delete previous config and reinstall our config again. Just to make sure if other template changing "proxy_cache_path" configuration. for example:
    -) previous template changed " keys_zone" parameter, when we changed to 'caching' template. it will cause: "new_name" zone "example.com" is unknown
    -) previous template changed "levels" parameter, when we changed to 'caching' template. it will cause: "example.com" had previously different levels

--- install/deb/templates/web/nginx/caching.sh
+++ install/deb/templates/web/nginx/caching.sh
@@ -10,6 +10,9 @@ str="proxy_cache_path /var/cache/nginx/$domain levels=2"
 str="$str keys_zone=$domain:10m inactive=60m max_size=512m;" 
 conf='/etc/nginx/conf.d/01_caching_pool.conf'
 if [ -e "$conf" ]; then
+    if [ -n "$(grep "=${domain}:" $conf)" ]; then
+        sed -i "/=${domain}:/d" $conf
+    fi
     if [ -z "$(grep "=${domain}:" $conf)" ]; then
         echo "$str" >> $conf
     fi

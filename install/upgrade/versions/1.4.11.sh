01952c88db2d2771a6fcbf4037ca709eab482dab
myrevery <16082142+myrevery@users.noreply.github.com>
2021-08-16 12:43:00 -0700

    Add 1.4.11.sh for update firewall loading script

---
+++ install/upgrade/versions/1.4.11.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Hestia Control Panel upgrade script for target version 1.4.11
+
+#######################################################################################
+#######                      Place additional commands below.                   #######
+#######################################################################################
+
+# Fix the potential issue of loading firewall rules
+if [ "$FIREWALL_SYSTEM" = "iptables" ]; then
+    echo "[ * ] Fix the potential issue of loading firewall rules..."
+    # Just in case, delete the legacy version loading script again to prevent any residue
+    rm -f /usr/lib/networkd-dispatcher/routable.d/50-ifup-hooks /etc/network/if-pre-up.d/iptables
+    $BIN/v-update-firewall
+fi

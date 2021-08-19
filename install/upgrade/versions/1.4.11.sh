a15c851304bf24adad8b1dea0d4e7360af4f9f51
myrevery <16082142+myrevery@users.noreply.github.com>
2021-08-19 11:12:00 -0700

    Delete the old version loading script

--- install/upgrade/versions/1.4.11.sh
+++ install/upgrade/versions/1.4.11.sh
@@ -11,5 +11,7 @@ if [ "$FIREWALL_SYSTEM" = "iptables" ]; then
     echo "[ * ] Fix the potential issue of loading firewall rules..."
     # Just in case, delete the legacy version loading script again to prevent any residue
     rm -f /usr/lib/networkd-dispatcher/routable.d/50-ifup-hooks /etc/network/if-pre-up.d/iptables
+    # The firewall rules are loading by Systemd, the old loading script is no longer needed
+    rm -f /usr/lib/networkd-dispatcher/routable.d/10-hestia-iptables /etc/network/if-pre-up.d/hestia-iptables
     $BIN/v-update-firewall
 fi

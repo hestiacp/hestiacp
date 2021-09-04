dfbbd959e2c428aa4c41efcf66f8dc2832ef8076
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2021-09-04 13:35:36 +0200

    Add csrf check logout

--- web/logout/index.php
+++ web/logout/index.php
@@ -1,30 +1,31 @@
 <?php
+
 session_start();
 
 // Main include
 include($_SERVER['DOCUMENT_ROOT'] . '/inc/main.php');
+if ((!$_GET['token']) || ($_SESSION['token'] != $_GET['token'])) {
+    header('location: /list/user/');
+    exit();
+}
 
 if (!empty($_SESSION['look'])) {
-    if ((!$_GET['token']) || ($_SESSION['token'] != $_GET['token'])) {
-        header('location: /list/user/');
-        exit();
-    }
     $v_user = escapeshellarg($_SESSION['look']);
     $v_impersonator = escapeshellarg($_SESSION['user']);
-    exec (HESTIA_CMD . "v-log-action system 'Warning' 'Security' 'User impersonation session ended (User: $v_user, Administrator: $v_impersonator)'", $output, $return_var);
+    exec(HESTIA_CMD . "v-log-action system 'Warning' 'Security' 'User impersonation session ended (User: $v_user, Administrator: $v_impersonator)'", $output, $return_var);
     unset($_SESSION['look']);
     # Remove current path for filemanager
     unset($_SESSION['_sf2_attributes']);
     unset($_SESSION['_sf2_meta']);
     header('Location: /');
 } else {
-    if ($_SESSION['token'] && $_SESSION['user']){
+    if ($_SESSION['token'] && $_SESSION['user']) {
         unset($_SESSION['userTheme']);
         $v_user = escapeshellarg($_SESSION['user']);
         $v_session_id = escapeshellarg($_SESSION['token']);
         exec(HESTIA_CMD . 'v-log-user-logout ' . $v_user . ' ' . $v_session_id, $output, $return_var);
     }
-    
+
     unset($_SESSION);
     session_unset();
     session_destroy();

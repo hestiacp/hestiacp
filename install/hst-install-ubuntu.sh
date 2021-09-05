bcf4a7f92aa14a8bdca914e66e381653f85fdbe9
Nikolai X. Shadeauxs <services@vontainment.com>
2021-09-05 19:15:52 -0400

    added $no-cache as a map in cachemap.conf under /nginx/conf/ as it must be loaded in http. If is evil. Updated the installer  to place the file and updated the templates.
    installers
    
    installers updated for change

--- install/hst-install-ubuntu.sh
+++ install/hst-install-ubuntu.sh
@@ -1310,6 +1310,7 @@ rm -f /etc/nginx/conf.d/*.conf
 cp -f $HESTIA_INSTALL_DIR/nginx/nginx.conf /etc/nginx/
 cp -f $HESTIA_INSTALL_DIR/nginx/status.conf /etc/nginx/conf.d/
 cp -f $HESTIA_INSTALL_DIR/nginx/agents.conf /etc/nginx/conf.d/
+cp -f $HESTIA_INSTALL_DIR/nginx/cachemap.conf /etc/nginx/conf.d/
 cp -f $HESTIA_INSTALL_DIR/nginx/phpmyadmin.inc /etc/nginx/conf.d/
 cp -f $HESTIA_INSTALL_DIR/nginx/phppgadmin.inc /etc/nginx/conf.d/
 cp -f $HESTIA_INSTALL_DIR/logrotate/nginx /etc/logrotate.d/

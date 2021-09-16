eda7b24c565ad72edba9d2aea7c692038164a4be
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2021-09-16 20:17:47 +0200

    Add add_upgrade_message instead old method
    + Improved backup old config files in /etc/

--- install/upgrade/versions/1.4.13.sh
+++ install/upgrade/versions/1.4.13.sh
@@ -5,15 +5,14 @@
 #######################################################################################
 #######                      Place additional commands below.                   #######
 #######################################################################################
-####### New Feature: UPGRADE_MESSAGE                                            #######
+####### Pass trough information to the end user incase of a issue or problem    #######
 #######                                                                         #######
-####### Add your text to UPGRADE_MESSAGE to include a message to the upgrade    #######
-####### email. Do not overwrite the variable, it could already contains prior   #######
-####### content of another upgrade script. Please add it using:                 #######
+####### Use add_upgrade_message "My message here" to include a message          #######
+####### to the upgrade email. Please add it using:                              #######
 #######                                                                         #######
-####### UPGRADE_MESSAGE="$UPGRADE_MESSAGE\nYour Upgrade Notification Text"      #######
+####### add_upgrade_message "My message here"                                   #######
 #######                                                                         #######
-####### Always start and end with \n to generate a new line.                    #######
+####### Use again to create a new line                                          #######
 #######################################################################################
 
 
@@ -21,7 +20,7 @@ servername=$(hostname -f);
 # Check if hostname is valid according to RFC1178
 if [[ $(echo "$servername" | grep -o "\." | wc -l) -lt 2 ]] || [[ $servername =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
     echo "[ * ] Hostname does not follow  RFC1178 standard Please check email send regarding the update!"
-    UPGRADE_MESSAGE="$UPGRADE_MESSAGE\nWe've noticed that you're using a invalid hostname. Please have a look at the RFC1178 standard (https://datatracker.ietf.org/doc/html/rfc1178) and use a valid one (ex. hostname.domain.tld). You can change the hostname using v-change-sys-hostname and also add a ssl certificate using v-add-letsencypt-host (proper dns A record mandatory). You'll find more informations in our documentation: https://docs.hestiacp.com/admin_docs/web/ssl_certificates.html#how-to-setup-let-s-encrypt-for-the-control-panel"
+    add_upgrade_message "We've noticed that you're using a invalid hostname. Please have a look at the RFC1178 standard (https://datatracker.ietf.org/doc/html/rfc1178) and use a valid one (ex. hostname.domain.tld). You can change the hostname using v-change-sys-hostname and also add a ssl certificate using v-add-letsencypt-host (proper dns A record mandatory). You'll find more informations in our documentation: https://docs.hestiacp.com/admin_docs/web/ssl_certificates.html#how-to-setup-let-s-encrypt-for-the-control-panel"
     $HESTIA/bin/v-add-user-notification admin "Invalid Hostname detected" "Warning: We've noticed that you're using a invalid hostname. Please have a look at the <a href=\"https://datatracker.ietf.org/doc/html/rfc1178\" target=\"_blank\">RFC1178 standard</a> and use a valid one (ex. hostname.domain.tld). You can change the hostname using v-change-sys-hostname and also add a ssl certificate using v-add-letsencypt-host (proper dns A record mandatory). You'll find more informations in our <a href=\"https://docs.hestiacp.com/admin_docs/web/ssl_certificates.html#how-to-setup-let-s-encrypt-for-the-control-panel\" target=\"_blank\">documentation</a>."
 fi
 
---
+++ install/upgrade/versions/1.4.14.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Hestia Control Panel upgrade script for target version 1.4.13
+
+#######################################################################################
+#######                      Place additional commands below.                   #######
+#######################################################################################
+####### Pass trough information to the end user incase of a issue or problem    #######
+#######                                                                         #######
+####### Use add_upgrade_message "My message here" to include a message          #######
+####### to the upgrade email. Please add it using:                              #######
+#######                                                                         #######
+####### add_upgrade_message "My message here"                                   #######
+#######                                                                         #######
+####### Use again to create a new line                                          #######
+#######################################################################################
+

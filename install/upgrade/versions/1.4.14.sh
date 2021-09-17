f0727be635eecae81daf8cecc6cbb8fe9601ef54
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2021-09-17 14:21:29 +0200

    Add check for current servers / admins to be warned

--- install/upgrade/versions/1.4.14.sh
+++ install/upgrade/versions/1.4.14.sh
@@ -15,3 +15,7 @@
 ####### You can use \n within the string to create new lines.                   #######
 #######################################################################################
 
+if [ -f "/etc/network/interfaces" ] && [ -f "/etc/netplan/60-hestia.yaml" ]; then
+    add_upgrade_message "Warning: Please check your network config!\n\nDuring this update a network compatibility issues has been detected. Both /etc/network/interfaces and /etc/netplan/60-hestia.yaml exists. This can lead to issues after a system reboot. Please review your network configuration."
+    $HESTIA/bin/v-add-user-notification admin "Invalid network configuration detected\n\nDuring this update a network compatibility issues has been detected. Both /etc/network/interfaces and /etc/netplan/60-hestia.yaml exists. This can lead to issues after a system reboot. Please review your network configuration."
+fi

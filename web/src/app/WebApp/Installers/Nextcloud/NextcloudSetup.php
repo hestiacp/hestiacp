cf26aff452d1568d7b56784e338f6ca46b0fb7d1
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2021-10-04 09:31:17 +0200

    Update quick installer
    Prestashop 1.7.7.1 to 1.7.7.8
    Mediawiki 1.36.1 to 1.36.2
    Nextcloud 21.0 to 22.2
    Opencart 3.0.3.3 to 3.0.3.8

--- web/src/app/WebApp/Installers/Nextcloud/NextcloudSetup.php
+++ web/src/app/WebApp/Installers/Nextcloud/NextcloudSetup.php
@@ -2,20 +2,20 @@
 
 namespace Hestia\WebApp\Installers\Nextcloud;
 
-use \Hestia\WebApp\Installers\BaseSetup as BaseSetup;
+use Hestia\WebApp\Installers\BaseSetup as BaseSetup;
 
-class NextcloudSetup extends BaseSetup {
-
-    protected $appInfo = [ 
+class NextcloudSetup extends BaseSetup
+{
+    protected $appInfo = [
         'name' => 'Nextcloud',
         'group' => 'cloud',
         'enabled' => true,
-        'version' => '21.0.0',
+        'version' => '22.2.0',
         'thumbnail' => 'nextcloud-thumb.png'
     ];
-    
+
     protected $appname = 'nextcloud';
-    
+
     protected $config = [
         'form' => [
             'username' => ['value'=>'admin'],
@@ -23,19 +23,19 @@ class NextcloudSetup extends BaseSetup {
             ],
         'database' => true,
         'resources' => [
-            'archive'  => [ 'src' => 'https://download.nextcloud.com/server/releases/nextcloud-21.0.0.tar.bz2' ]
+            'archive'  => [ 'src' => 'https://download.nextcloud.com/server/releases/nextcloud-22.2.0.tar.bz2' ]
         ],
     ];
-    
-    public function install(array $options = null) : bool
+
+    public function install(array $options = null): bool
     {
         parent::install($options);
-        
-        $this->appcontext->runUser('v-copy-fs-file',[$this->getDocRoot(".htaccess.txt"), $this->getDocRoot(".htaccess")]);
-        
-        // install nextcloud 
+
+        $this->appcontext->runUser('v-copy-fs-file', [$this->getDocRoot(".htaccess.txt"), $this->getDocRoot(".htaccess")]);
+
+        // install nextcloud
         $this->appcontext->runUser('v-run-cli-cmd', ['/usr/bin/php',
-            $this->getDocRoot('occ'), 
+            $this->getDocRoot('occ'),
             'maintenance:install',
             '--database mysql',
             '--database-name '.$this->appcontext->user() . '_' .$options['database_name'],
@@ -44,13 +44,16 @@ class NextcloudSetup extends BaseSetup {
             '--admin-user '.$options['username'],
             '--admin-pass '.$options['password']
             ], $status);
-            
-        $this->appcontext->runUser('v-run-cli-cmd',  
+
+        $this->appcontext->runUser(
+            'v-run-cli-cmd',
             ['/usr/bin/php',
-                $this->getDocRoot('occ'), 
+                $this->getDocRoot('occ'),
                 'config:system:set',
                 'trusted_domains 2 --value='.$this->domain
-            ], $status2);
+            ],
+            $status2
+        );
         return ($status->code === 0 && $status2->code === 0);
     }
 }

aa3a56c13f8b6c1f8b9c63bb4fc5f7bb8f3e336b
Arturo Blanco <ablanco@ablanco.es>
2021-11-15 21:27:00 +0100

    Update Custom Limits by domain/user or default (limit.conf)

--- install/hst-install-ubuntu.sh
+++ install/hst-install-ubuntu.sh
@@ -1680,7 +1680,7 @@ if [ "$exim" = 'yes' ]; then
     cp -f $HESTIA_INSTALL_DIR/exim/exim4.conf.template /etc/exim4/
     cp -f $HESTIA_INSTALL_DIR/exim/dnsbl.conf /etc/exim4/
     cp -f $HESTIA_INSTALL_DIR/exim/spam-blocks.conf /etc/exim4/
-	cp -f $HESTIA_INSTALL_DIR/exim/send-limits.conf /etc/exim4/
+	cp -f $HESTIA_INSTALL_DIR/exim/limit.conf /etc/exim4/
     touch /etc/exim4/white-blocks.conf
 
     if [ "$spamd" = 'yes' ]; then

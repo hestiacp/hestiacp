4c48d169b9336268ebf0c61a92777b977273d5f0
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2021-11-26 09:48:04 +0100

    Allow the use of # as value in config files

--- func/main.sh
+++ func/main.sh
@@ -1347,7 +1347,7 @@ source_conf(){
   while IFS='= ' read -r lhs rhs
   do
       if [[ ! $lhs =~ ^\ *# && -n $lhs ]]; then
-          rhs="${rhs%%\#*}"    # Del in line right comments
+          rhs="${rhs%%^\#*}"    # Del in line right comments
           rhs="${rhs%%*( )}"   # Del trailing spaces
           rhs="${rhs%\'*}"     # Del opening string quotes 
           rhs="${rhs#\'*}"     # Del closing string quotes 

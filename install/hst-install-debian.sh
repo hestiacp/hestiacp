0b70ae858e7e22d0b5283fbfb7e6fffb491517f7
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2021-12-16 16:19:40 +0100

    Remove extra fi

--- install/hst-install-debian.sh
+++ install/hst-install-debian.sh
@@ -714,7 +714,6 @@ fi
 echo "[ * ] Hestia Control Panel"
 echo "deb [arch=$ARCH signed-by=/usr/share/keyrings/hestia-keyring.gpg] https://$RHOST/ $codename main" > $apt/hestia.list
 gpg --no-default-keyring --keyring /usr/share/keyrings/hestia-keyring.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys A189E93654F0B0E5 >/dev/null 2>&1
-fi
 
 # Installing PostgreSQL repo
 if [ "$postgresql" = 'yes' ]; then

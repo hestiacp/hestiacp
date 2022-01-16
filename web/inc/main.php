ba9bc90b53f9ea7245008d9934250cc7c8fc62e0
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2022-01-16 09:52:11 +0100

    Add check if /vendor/autoload exists

--- web/inc/main.php
+++ web/inc/main.php
@@ -4,8 +4,12 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-require 'vendor/autoload.php';
+if(!file_exists('vendor/autoload.php')){
+    trigger_error('Unable able to load required libaries. Please run v-add-sys-phpmailer in command line');
+    exit('Unable able to load required libaries. Please run v-add-sys-phpmailer in command line');
+}
 
+require 'vendor/autoload.php';
 session_start();
 
 

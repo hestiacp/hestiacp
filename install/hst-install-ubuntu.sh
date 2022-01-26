3386fe4c5014bffd14ce66ad96b95ba1920ecd96
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2022-01-26 10:49:45 +0100

    Update versions

--- install/hst-install-ubuntu.sh
+++ install/hst-install-ubuntu.sh
@@ -31,7 +31,7 @@ HESTIA_INSTALL_DIR="$HESTIA/install/deb"
 VERBOSE='no'
 
 # Define software versions
-HESTIA_INSTALL_VER='1.5.5~alpha'
+HESTIA_INSTALL_VER='1.5.5'
 # Dependencies
 pma_v='5.1.2'
 rc_v="1.5.1"

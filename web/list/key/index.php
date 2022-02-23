bd9013c1a2942ca5749a53d2b55958c285a16ae1
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2022-02-23 10:20:30 +0100

    Fix linting error

--- web/list/key/index.php
+++ web/list/key/index.php
@@ -11,7 +11,7 @@ if (($_SESSION['userContext'] === 'admin') && (!empty($_GET['user']))) {
 exec (HESTIA_CMD . "v-list-user-ssh-key ".$user." json", $output, $return_var);
 if($return_var > 0){
     check_return_code_redirect($return_var,$output,'/');
-})
+}
 $data = json_decode(implode('', $output), true);
 
 // Render page\

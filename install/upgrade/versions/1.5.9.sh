cfd5263f6b1e653a3f9e439cb8f65dd805ccfee4
Clark Chen <9372896+clarkchentw@users.noreply.github.com>
2022-02-28 08:21:14 +0800

    Add upgrade code

--- install/upgrade/versions/1.5.9.sh
+++ install/upgrade/versions/1.5.9.sh
@@ -20,3 +20,16 @@ upgrade_config_set_value 'UPGRADE_UPDATE_DNS_TEMPLATES' 'false'
 upgrade_config_set_value 'UPGRADE_UPDATE_MAIL_TEMPLATES' 'false'
 upgrade_config_set_value 'UPGRADE_REBUILD_USERS' 'false'
 upgrade_config_set_value 'UPGRADE_UPDATE_FILEMANAGER_CONFIG' 'false'
+
+# Upgrading Mail System
+if [ "$MAIL_SYSTEM" == "exim4" ]; then
+    if ! grep -q "send_via_unauthenticated_smtp_relay" /etc/exim4/exim4.conf.template; then
+
+        echo '[ * ] Enabling SMTP relay support...'
+        # Add smtp relay router
+        insert='send_via_unauthenticated_smtp_relay:\n  driver = manualroute\n  address_data = SMTP_RELAY_HOST:SMTP_RELAY_PORT\n  domains = !+local_domains\n  require_files = SMTP_RELAY_FILE\n  condition = ${if eq{SMTP_RELAY_USER}{}}\n  transport = remote_smtp\n  route_list = * ${extract{1}{:}{$address_data}}::${extract{2}{:}{$address_data}}\n  no_more\n  no_verify\n'
+
+        line=$(expr $(sed -n '/begin routers/=' /etc/exim4/exim4.conf.template) + 2)
+        sed -i "${line}i $insert" /etc/exim4/exim4.conf.template
+    fi
+fi

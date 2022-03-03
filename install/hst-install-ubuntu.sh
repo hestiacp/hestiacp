31fa0defaf1826b45e9bd9c119cc071346bd0432
Raphael Schneeberger <rs@scit.ch>
2022-03-03 18:25:10 +0100

    Relase 1.5.10

--- install/hst-install-ubuntu.sh
+++ install/hst-install-ubuntu.sh
@@ -31,7 +31,7 @@ HESTIA_INSTALL_DIR="$HESTIA/install/deb"
 VERBOSE='no'
 
 # Define software versions
-HESTIA_INSTALL_VER='1.5.10~rc'
+HESTIA_INSTALL_VER='1.5.10'
 # Dependencies
 pma_v='5.1.3'
 rc_v="1.5.2"

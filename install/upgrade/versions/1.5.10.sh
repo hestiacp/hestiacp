0db021192c5e559c753b469c983b5b0ebfa19d57
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2022-03-03 14:29:37 +0100

    Execute command for real

--- install/upgrade/versions/1.5.10.sh
+++ install/upgrade/versions/1.5.10.sh
@@ -33,7 +33,7 @@ mysql_version2=$(echo $mysql_version | grep -o -E '[0-9]*.[0-9]' | head -n1 );
 if [ "$mysql_version2" = "10.6" ]; then 
     test=$(mysql -e "select * from mysql.global_priv;" | grep root | grep unix_socket);
     if [ -z "$test" ]; then 
-        echo "execute update";
-        #mysql --defaults-file=/usr/local/hestia/conf/.mysql.localhost -e "UPDATE mysql.global_priv SET priv=json_set(priv, '$.password_last_changed', UNIX_TIMESTAMP(), '$.plugin', 'mysql_native_password', '$.authentication_string', 'invalid', '$.auth_or', json_array(json_object(), json_object('plugin', 'unix_socket'))) WHERE User='root';"
+        echo "[ * ] Fixed an issue with MariaDB stating up"
+        mysql --defaults-file=/usr/local/hestia/conf/.mysql.localhost -e "UPDATE mysql.global_priv SET priv=json_set(priv, '$.password_last_changed', UNIX_TIMESTAMP(), '$.plugin', 'mysql_native_password', '$.authentication_string', 'invalid', '$.auth_or', json_array(json_object(), json_object('plugin', 'unix_socket'))) WHERE User='root';"
     fi
 fi 

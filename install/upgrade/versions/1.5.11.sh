00d50d0b327c58095f78e8b9c1fa0c7a2c4fb5c0
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2022-03-07 20:50:03 +0100

    Update upgrade script

--- install/upgrade/versions/1.5.11.sh
+++ install/upgrade/versions/1.5.11.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Hestia Control Panel upgrade script for target version 1.5.10
+# Hestia Control Panel upgrade script for target version 1.5.11
 
 #######################################################################################
 #######                      Place additional commands below.                   #######
@@ -21,20 +21,17 @@ upgrade_config_set_value 'UPGRADE_UPDATE_MAIL_TEMPLATES' 'false'
 upgrade_config_set_value 'UPGRADE_REBUILD_USERS' 'false'
 upgrade_config_set_value 'UPGRADE_UPDATE_FILEMANAGER_CONFIG' 'false'
 
-# shellcheck source=/usr/local/hestia/func/db.sh
-source $HESTIA/func/db.sh
+PORT=$(cat $HESTIA/nginx/conf/nginx.conf | grep "listen" | sed 's/[^0-9]*//g')
 
-if [ -n "$(echo $DB_SYSTEM | grep -w mysql)" ]; then
-    mysql_connect 'localhost'
-    version=$(mysql --defaults-file=/usr/local/hestia/conf/.mysql.localhost -e 'SELECT VERSION()')
-    mysql_version=$(echo $version | grep -o -E '[0-9]*.[0-9].[0-9]+' | head -n1);
-    mysql_version2=$(echo $mysql_version | grep -o -E '[0-9]*.[0-9]' | head -n1 );
+if [ "$PORT" != "8083" ]; then 
+    # Update F2B chains config
+    if [ -f "$HESTIA/data/firewall/chains.conf" ]; then
+        # Update value in chains.conf
+        sed -i "s/PORT='8083'/PORT='$PORT'/g" $HESTIA/data/firewall/chains.conf
+    fi
     
-    if [ "$mysql_version2" = "10.6" ]; then 
-        test=$(mysql -e "select * from mysql.global_priv;" | grep root | grep unix_socket);
-        if [ -z "$test" ]; then 
-            echo "[ ! ] Updating MariaDB permissions to fix startup issue "
-            mysql --defaults-file=/usr/local/hestia/conf/.mysql.localhost -e "UPDATE mysql.global_priv SET priv=json_set(priv, '$.password_last_changed', UNIX_TIMESTAMP(), '$.plugin', 'mysql_native_password', '$.authentication_string', 'invalid', '$.auth_or', json_array(json_object(), json_object('plugin', 'unix_socket'))) WHERE User='root';"
-        fi
+    # Restart services
+    if [ -n "$FIREWALL_SYSTEM" ] && [ "$FIREWALL_SYSTEM" != no ]; then
+        $HESTIA/bin/v-restart-service iptables
     fi
 fi

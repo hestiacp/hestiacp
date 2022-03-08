5e0d881eb379a2863d0f90f33132565482159078
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2022-03-08 11:26:27 +0100

    Update 1.5.11.sh

--- install/upgrade/versions/1.5.11.sh
+++ install/upgrade/versions/1.5.11.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Hestia Control Panel upgrade script for target version 1.5.10
+# Hestia Control Panel upgrade script for target version 1.5.11
 
 #######################################################################################
 #######                      Place additional commands below.                   #######

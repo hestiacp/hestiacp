a26eea3721d720f7a1719081578594033f1d7f64
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2022-03-28 09:37:43 +0200

    Update versions to 1.6.0~alpha

--- install/hst-install-debian.sh
+++ install/hst-install-debian.sh
@@ -34,7 +34,7 @@ VERBOSE='no'
 HESTIA_INSTALL_VER='1.5.12~alpha'
 # Dependencies
 pma_v='5.1.3'
-rc_v="1.5.2"
+rc_v="1.6.0"
 multiphp_v=("5.6" "7.0" "7.1" "7.2" "7.3" "7.4" "8.0" "8.1")
 fpm_v="8.0"
 mariadb_v="10.6"

e3793337453992db2263e7f9d608f83a2796a5db
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2022-04-08 21:24:43 +0200

    Change order

--- install/hst-install-debian.sh
+++ install/hst-install-debian.sh
@@ -60,10 +60,10 @@ elif [ "$release" -eq 10 ] || [ "$release" -eq 11 ]; then
         apache2-suexec-pristine libapache2-mod-fcgid libapache2-mod-php$fpm_v
         php$fpm_v php$fpm_v-common php$fpm_v-cgi php$fpm_v-mysql php$fpm_v-curl
         php$fpm_v-pgsql php$fpm_v-imagick php$fpm_v-imap php$fpm_v-ldap
-        php$fpm_v-apcu awstats php$fpm_v-zip php$fpm_v-bz2 php$fpm_v-cli
+        php$fpm_v-apcu php$fpm_v-zip php$fpm_v-bz2 php$fpm_v-cli
         php$fpm_v-gd php$fpm_v-intl php$fpm_v-mbstring
         php$fpm_v-opcache php$fpm_v-pspell php$fpm_v-readline php$fpm_v-xml
-        vsftpd proftpd-basic bind9 exim4 exim4-daemon-heavy
+        awstats vsftpd proftpd-basic bind9 exim4 exim4-daemon-heavy
         clamav-daemon spamassassin dovecot-imapd dovecot-pop3d dovecot-sieve dovecot-managesieved 
         net-tools mariadb-client mariadb-common mariadb-server postgresql
         postgresql-contrib phppgadmin mc flex whois git idn unzip zip sudo bc ftp lsof

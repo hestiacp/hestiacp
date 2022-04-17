afbfa89ada6449a64286cc9ffef20f396899e610
Clark Chen <9372896+clarkchentw@users.noreply.github.com>
2022-04-17 07:13:27 -0500

    Add support to send mail to unauthenticated smtp relay (#2441)
    * Add support to send mail to unauthenticated smtp relay(e.g. Internal mail gateway)
    * Update script for 1.6.0 release
    
    Co-authored-by: Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>

--- install/upgrade/versions/1.5.9.sh
+++ install/upgrade/versions/1.5.9.sh
@@ -20,3 +20,5 @@ upgrade_config_set_value 'UPGRADE_UPDATE_DNS_TEMPLATES' 'false'
 upgrade_config_set_value 'UPGRADE_UPDATE_MAIL_TEMPLATES' 'false'
 upgrade_config_set_value 'UPGRADE_REBUILD_USERS' 'false'
 upgrade_config_set_value 'UPGRADE_UPDATE_FILEMANAGER_CONFIG' 'false'
+
+
--- install/upgrade/versions/1.6.0.sh
+++ install/upgrade/versions/1.6.0.sh
@@ -28,6 +28,19 @@ if [ "$MAIL_SYSTEM" = "exim4" ]; then
     sed -i '115,250 s/warn    ratelimit     = 100 \/ 1h \/ strict \/ $authenticated_id/warn    ratelimit     = ${eval:$acl_c_msg_limit \/ 2} \/ 1h \/ strict \/ $authenticated_id/g' /etc/exim4/exim4.conf.template
     # Add missing limit.conf file
     cp $HESTIA_INSTALL_DIR/exim/limit.conf /etc/exim4/limit.conf
+    
+    # Upgrading Mail System
+    if [ "$MAIL_SYSTEM" == "exim4" ]; then
+        if ! grep -q "send_via_unauthenticated_smtp_relay" /etc/exim4/exim4.conf.template; then
+    
+            echo '[ * ] Enabling SMTP relay support...'
+            # Add smtp relay router
+            insert='send_via_unauthenticated_smtp_relay:\n  driver = manualroute\n  address_data = SMTP_RELAY_HOST:SMTP_RELAY_PORT\n  domains = !+local_domains\n  require_files = SMTP_RELAY_FILE\n  condition = ${if eq{SMTP_RELAY_USER}{}}\n  transport = remote_smtp\n  route_list = * ${extract{1}{:}{$address_data}}::${extract{2}{:}{$address_data}}\n  no_more\n  no_verify\n'
+    
+            line=$(expr $(sed -n '/begin routers/=' /etc/exim4/exim4.conf.template) + 2)
+            sed -i "${line}i $insert" /etc/exim4/exim4.conf.template
+        fi
+    fi
 fi
 
 # Adding LE autorenew cronjob if there are none

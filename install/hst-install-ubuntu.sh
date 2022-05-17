0179f73c135b9b82aef6d430d4b175c3b0c060b0
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2022-05-17 11:21:29 +0200

    Update phpmyadmin version in installer

--- install/hst-install-ubuntu.sh
+++ install/hst-install-ubuntu.sh
@@ -33,7 +33,7 @@ VERBOSE='no'
 # Define software versions
 HESTIA_INSTALL_VER='1.5.15'
 # Dependencies
-pma_v='5.1.3'
+pma_v='5.1.4'
 rc_v="1.5.2"
 multiphp_v=("5.6" "7.0" "7.1" "7.2" "7.3" "7.4" "8.0" "8.1")
 fpm_v="8.0"

3ecd2f11164cc6bfe61cf1a3f9677c5e757f2317
Wojciech Smoliński <wojsmol@wp.pl>
2022-07-05 13:40:40 +0200

    Fix includes after 2550 (#2722)
    * Fix includes after 2550
    
    * Add upgrade script

--- install/upgrade/versions/1.6.3.sh
+++ install/upgrade/versions/1.6.3.sh
@@ -17,7 +17,7 @@
 
 upgrade_config_set_value 'UPGRADE_UPDATE_WEB_TEMPLATES' 'no'
 upgrade_config_set_value 'UPGRADE_UPDATE_DNS_TEMPLATES' 'no'
-upgrade_config_set_value 'UPGRADE_UPDATE_MAIL_TEMPLATES' 'no'
-upgrade_config_set_value 'UPGRADE_REBUILD_USERS' 'no'
+upgrade_config_set_value 'UPGRADE_UPDATE_MAIL_TEMPLATES' 'yes'
+upgrade_config_set_value 'UPGRADE_REBUILD_USERS' 'yes'
 upgrade_config_set_value 'UPGRADE_UPDATE_FILEMANAGER_CONFIG' 'false'
 

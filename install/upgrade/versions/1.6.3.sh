5c477f6cb502cb43a34dbbbd1ba9e92aa7cef7c2
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2022-07-06 21:16:18 +0200

    Add upgrade script
    @myvesta for the fix

--- install/upgrade/versions/1.6.3.sh
+++ install/upgrade/versions/1.6.3.sh
@@ -21,3 +21,10 @@ upgrade_config_set_value 'UPGRADE_UPDATE_MAIL_TEMPLATES' 'yes'
 upgrade_config_set_value 'UPGRADE_REBUILD_USERS' 'yes'
 upgrade_config_set_value 'UPGRADE_UPDATE_FILEMANAGER_CONFIG' 'false'
 
+if [ -f /etc/exim4/exim4.conf.template ]; then 
+    host=$(cat /etc/exim4/exim4.conf.template | grep hosts_try_fastopen);
+    if [ -z "$host" ]; then
+        echo "[ * ] Fix an issue with sending large attachments to Google / Gmail"
+        sed -i '/dkim_strict = .*/a hosts_try_fastopen = !*.l.google.com' /etc/exim4/exim4.conf.template
+    fi
+fi

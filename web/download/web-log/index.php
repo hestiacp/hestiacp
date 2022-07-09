c5b40ffb4919c926896ef29260fd34bea0008602
hanshenrik <divinity76@gmail.com>
2022-07-09 10:59:28 +0200

    avoid out-of-memory serving large logfiles
    large logfiles previously resulted in out-of-memory errors, see https://github.com/hestiacp/hestiacp/issues/2736

--- web/download/web-log/index.php
+++ web/download/web-log/index.php
@@ -27,10 +27,15 @@ if ($_GET['type'] == 'access') {
 if ($_GET['type'] == 'error') {
     $type = 'error';
 }
-
-exec(HESTIA_CMD."v-list-web-domain-".$type."log $user ".$v_domain." 5000", $output, $return_var);
-if ($return_var == 0) {
-    foreach ($output as $file) {
-        echo $file . "\n";
-    }
+$cmd = implode(" ",array(
+    escapeshellarg(HESTIA_CMD."v-list-web-domain-".$type."log"),
+    escapeshellarg($user),
+    escapeshellarg($v_domain),
+    "5000"
+));
+passthru($cmd, $return_var);
+if($return_var != 0) {
+    $errstr = "Internal server error: command returned non-zero: {$return_var}: {$cmd}";
+    echo $errstr;
+    throw new Exception($errstr); // make sure it ends up in an errorlog somewhere
 }

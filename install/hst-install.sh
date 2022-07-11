e9bd85059d515c6631c72ef0d01adc9de572791c
Istiak Ferdous <git@istiak.com>
2022-07-11 14:05:07 +0600

    modify install script to support RHEL

--- install/hst-install.sh
+++ install/hst-install.sh
@@ -10,6 +10,7 @@
 #
 # Debian 9, 10, 11
 # Ubuntu 18.04, 20.04, 22.04
+# AlmaLinux, EuroLinux, Red Hat EnterPrise Linux, Rocky Linux 8, 9
 #
 # ======================================================== #
 
@@ -40,13 +41,13 @@ if [ ! -z "$(grep ^admin: /etc/group)" ] && [ -z "$1" ]; then
 fi
 
 # Detect OS
-if [ -e "/etc/os-release" ]; then
+if [ -e "/etc/os-release" ] && [ ! -e "/etc/redhat-release" ]; then
     type=$(grep "^ID=" /etc/os-release | cut -f 2 -d '=')
     if [ "$type" = "ubuntu" ]; then
         # Check if lsb_release is installed
         if [ -e '/usr/bin/lsb_release' ]; then
             release="$(lsb_release -s -r)"
-            VERSION='ubuntu'            
+            VERSION='ubuntu'
         else
             echo "lsb_release is currently not installed, please install it:"
             echo "apt-get update && apt-get install lsb_release"
@@ -55,6 +56,21 @@ if [ -e "/etc/os-release" ]; then
     elif [ "$type" = "debian" ]; then
         release=$(cat /etc/debian_version|grep -o "[0-9]\{1,2\}"|head -n1)
         VERSION='debian'
+    fi
+elif [ -e "/etc/os-release" ] && [ -e "/etc/redhat-release" ]; then
+    type=$(grep "^ID=" /etc/os-release | cut -f 2 -d '"')
+    if [ "$type" = "rhel" ]; then
+        release=$(cat /etc/redhat-release | cut -f 1 -d '.' | awk '{print $3}')
+        VERSION='rhel'
+    elif [ "$type" = "almalinux" ]; then
+        release=$(cat /etc/redhat-release | cut -f 1 -d '.' | awk '{print $3}')
+        VERSION='almalinux'
+    elif [ "$type" = "eurolinux" ]; then
+        release=$(cat /etc/redhat-release | cut -f 1 -d '.' | awk '{print $3}')
+        VERSION='eurolinux'
+    elif [ "$type" = "rocky" ]; then
+        release=$(cat /etc/redhat-release | cut -f 1 -d '.' | awk '{print $3}')
+        VERSION='rockylinux'
     fi
 else
     type="NoSupport"
@@ -67,6 +83,7 @@ no_support_message(){
     echo "****************************************************"
     echo "  Debian 10, 11"
     echo "  Ubuntu 18.04, 20.04, 22.04 LTS"
+    echo "  AlmaLinux, EuroLinux, Red Hat EnterPrise Linux, Rocky Linux 8,9"
     echo ""
     exit 1;
 }
@@ -104,6 +121,8 @@ check_wget_curl(){
 # Check for supported operating system before proceeding with download
 # of OS-specific installer, and throw error message if unsupported OS detected.
 if [[ "$release" =~ ^(10|11|18.04|20.04|22.04)$ ]]; then
+    check_wget_curl $*
+elif [[ -e "/etc/redhat-release" ]] && [[ "$release" =~ ^(8|9)$ ]]; then
     check_wget_curl $*
 else
     no_support_message

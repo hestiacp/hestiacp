a051b0ef832849fe8868207827ea18307945e754
hanshenrik <divinity76@gmail.com>
2022-07-13 17:41:15 +0200

    fix buggy traversion detection code
    ref https://github.com/hestiacp/hestiacp/issues/2760

--- web/src/app/System/HestiaApp.php
+++ web/src/app/System/HestiaApp.php
@@ -16,13 +16,13 @@ class HestiaApp
 
     public function run(string $cmd, $args, &$cmd_result=null): bool
     {
-        $cli_script = realpath(HESTIA_CMD . '/' . $cmd);
-        if(!str_starts_with((string)$cli_script, HESTIA_CMD."/" )){
-            $errstr = "$cmd is trying to traverse outside of " .HESTIA_CMD;
+        $cli_script = realpath(HESTIA_DIR_BIN . $cmd);
+        if(!str_starts_with((string)$cli_script, HESTIA_DIR_BIN )){
+            $errstr = "$cmd is trying to traverse outside of " .HESTIA_DIR_BIN;
             trigger_error($errstr);
             throw new \Exception($errstr);   
         }
-        $cli_script = escapeshellarg($cli_script);
+        $cli_script = '/usr/bin/sudo '. escapeshellarg($cli_script);
 
         if (!empty($args) && is_array($args)) {
             foreach ($args as $arg) {

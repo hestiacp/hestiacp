66d27f0eccdf95d7edd6b8979ef2bb5af49de329
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2022-07-14 17:22:18 +0200

    Allow version selection via version

--- web/src/app/WebApp/Installers/Resources/WpResource.php
+++ web/src/app/WebApp/Installers/Resources/WpResource.php
@@ -9,10 +9,10 @@ class WpResource
     private $appcontext;
     private $options;
 
-    public function __construct(HestiaApp $appcontext, $data, $destination, $options)
+    public function __construct(HestiaApp $appcontext, $data, $destination, $options, $appinfo)
     {
         $this->appcontext = $appcontext;
-        $this->appcontext->runWp(['core', 'download', '--locale='.$options['language'], '--path='.$destination], $status);
+        $this->appcontext->runWp(['core', 'download', '--locale='.$options['language'], '--version='.$appinfo['version'], '--path='.$destination], $status);
 
         if ($status->code !== 0) {
             throw new \Exception("Error fetching WP resource: " . $status->text);

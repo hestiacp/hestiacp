f8359971fe56d206780512ddae8c5ae798e40ddf
Ice Lake <30789544+istiak101@users.noreply.github.com>
2022-07-19 14:35:49 +0600

    RHEL & 1:1 distro support (#2749)
    * RHEL version checker
    
    * use arch binary instead uname
    
    * modify install script to support RHEL
    
    * add support for RPM based distro
    
    * add RPM repositories
    
    * choose libdir depending on architecture
    
    * common files for both debian and rhel
    
    As @jaapmarcus raised concern to have common config files for both debian and rhel in github comment.
    
    https://github.com/hestiacp/hestiacp/pull/2731#pullrequestreview-1030683914
    
    This is initial commit to have a common base config files for both debian and rhel.
    
    * Disable bats testing for branch
    
    * architecture dependent result dir
    
    * quota & linting for RHEL
    
    * HestiaCP repo
    
    * RHEL support in installer
    
    Co-authored-by: Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>

--- install/hst-install.sh
+++ install/hst-install.sh
@@ -10,6 +10,7 @@
 #
 # Debian 9, 10, 11
 # Ubuntu 18.04, 20.04, 22.04
+# AlmaLinux, EuroLinux, Red Hat EnterPrise Linux, Rocky Linux 8, 9
 #
 # ======================================================== #
 
@@ -40,13 +41,13 @@ if [ ! -z "$(grep ^admin: /etc/group)" ] && [ -z "$1" ]; then
 fi
 
 # Detect OS
-if [ -e "/etc/os-release" ]; then
+if [ -e "/etc/os-release" ] && [ ! -e "/etc/redhat-release" ]; then
     type=$(grep "^ID=" /etc/os-release | cut -f 2 -d '=')
     if [ "$type" = "ubuntu" ]; then
         # Check if lsb_release is installed
         if [ -e '/usr/bin/lsb_release' ]; then
             release="$(lsb_release -s -r)"
-            VERSION='ubuntu'            
+            VERSION='ubuntu'
         else
             echo "lsb_release is currently not installed, please install it:"
             echo "apt-get update && apt-get install lsb_release"
@@ -55,6 +56,21 @@ if [ -e "/etc/os-release" ]; then
     elif [ "$type" = "debian" ]; then
         release=$(cat /etc/debian_version|grep -o "[0-9]\{1,2\}"|head -n1)
         VERSION='debian'
+    fi
+elif [ -e "/etc/os-release" ] && [ -e "/etc/redhat-release" ]; then
+    type=$(grep "^ID=" /etc/os-release | cut -f 2 -d '"')
+    if [ "$type" = "rhel" ]; then
+        release=$(cat /etc/redhat-release | cut -f 1 -d '.' | awk '{print $3}')
+        VERSION='rhel'
+    elif [ "$type" = "almalinux" ]; then
+        release=$(cat /etc/redhat-release | cut -f 1 -d '.' | awk '{print $3}')
+        VERSION='almalinux'
+    elif [ "$type" = "eurolinux" ]; then
+        release=$(cat /etc/redhat-release | cut -f 1 -d '.' | awk '{print $3}')
+        VERSION='eurolinux'
+    elif [ "$type" = "rocky" ]; then
+        release=$(cat /etc/redhat-release | cut -f 1 -d '.' | awk '{print $3}')
+        VERSION='rockylinux'
     fi
 else
     type="NoSupport"
@@ -67,6 +83,8 @@ no_support_message(){
     echo "****************************************************"
     echo "  Debian 10, 11"
     echo "  Ubuntu 18.04, 20.04, 22.04 LTS"
+    # Commenting this out for now
+    # echo "  AlmaLinux, EuroLinux, Red Hat EnterPrise Linux, Rocky Linux 8,9"
     echo ""
     exit 1;
 }
@@ -78,25 +96,47 @@ fi
 check_wget_curl(){
     # Check wget
     if [ -e '/usr/bin/wget' ]; then
-        wget -q https://raw.githubusercontent.com/hestiacp/hestiacp/release/install/hst-install-$type.sh -O hst-install-$type.sh
-        if [ "$?" -eq '0' ]; then
-            bash hst-install-$type.sh $*
-            exit
+        if [ -e '/etc/redhat-release' ]; then
+            wget -q https://raw.githubusercontent.com/hestiacp/hestiacp/release/install/hst-install-rhel.sh -O hst-install-rhel.sh
+            if [ "$?" -eq '0' ]; then
+                bash hst-install-rhel.sh $*
+                exit
+            else
+                echo "Error: hst-install-rhel.sh download failed."
+                exit 1
+            fi
         else
-            echo "Error: hst-install-$type.sh download failed."
-            exit 1
+            wget -q https://raw.githubusercontent.com/hestiacp/hestiacp/release/install/hst-install-$type.sh -O hst-install-$type.sh
+            if [ "$?" -eq '0' ]; then
+                bash hst-install-$type.sh $*
+                exit
+            else
+                echo "Error: hst-install-$type.sh download failed."
+                exit 1
+            fi
         fi
     fi
 
     # Check curl
     if [ -e '/usr/bin/curl' ]; then
-        curl -s -O https://raw.githubusercontent.com/hestiacp/hestiacp/release/install/hst-install-$type.sh
-        if [ "$?" -eq '0' ]; then
-            bash hst-install-$type.sh $*
-            exit
+        if [ -e '/etc/redhat-release' ]; then
+            curl -s -O https://raw.githubusercontent.com/hestiacp/hestiacp/release/install/hst-install-rhel.sh
+            if [ "$?" -eq '0' ]; then
+                bash hst-install-rhel.sh $*
+                exit
+            else
+                echo "Error: hst-install-rhel.sh download failed."
+                exit 1
+            fi
         else
-            echo "Error: hst-install-$type.sh download failed."
-            exit 1
+            curl -s -O https://raw.githubusercontent.com/hestiacp/hestiacp/release/install/hst-install-$type.sh
+            if [ "$?" -eq '0' ]; then
+                bash hst-install-$type.sh $*
+                exit
+            else
+                echo "Error: hst-install-$type.sh download failed."
+                exit 1
+            fi
         fi
     fi
 }
@@ -104,6 +144,8 @@ check_wget_curl(){
 # Check for supported operating system before proceeding with download
 # of OS-specific installer, and throw error message if unsupported OS detected.
 if [[ "$release" =~ ^(10|11|18.04|20.04|22.04)$ ]]; then
+    check_wget_curl $*
+elif [[ -e "/etc/redhat-release" ]] && [[ "$release" =~ ^(8|9)$ ]]; then
     check_wget_curl $*
 else
     no_support_message

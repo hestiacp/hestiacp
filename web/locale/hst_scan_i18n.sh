19d5ef47781e048b3830679c7caf621009c01bc3
myrevery <16082142+myrevery@users.noreply.github.com>
2022-07-19 01:24:01 -0700

    Add missing translation strings (#2778)

--- web/locale/hst_scan_i18n.sh
+++ web/locale/hst_scan_i18n.sh
@@ -14,6 +14,13 @@ echo "" > hestiacp.pot
 find ../.. \( -name '*.php' -o -name '*.html' -o -name '*.sh' \) | xgettext --output=hestiacp.pot --language=PHP --join-existing -f -
 OLDIFS=$IFS
 IFS=$'\n'
+# Scan the description string for list updates page
+for string in $(awk -F'DESCR=' '/data=".+ DESCR=[^"]/ {print $2}' $HESTIA/bin/v-list-sys-hestia-updates | cut -d\' -f2); do
+    if [ -z "$(grep "\"$string\"" hestiacp.pot)" ]; then
+        echo -e "\n#: ../../bin/v-list-sys-hestia-updates:"$(grep -n "$string" $HESTIA/bin/v-list-sys-hestia-updates | cut -d: -f1)"\nmsgid \"$string\"\nmsgstr \"\"" >> hestiacp.pot
+    fi
+done
+# Scan the description string for list server page
 for string in $(awk -F'SYSTEM=' '/data=".+ SYSTEM=[^"]/ {print $2}' $HESTIA/bin/v-list-sys-services | cut -d\' -f2); do
     if [ -z "$(grep "\"$string\"" hestiacp.pot)" ]; then
         echo -e "\n#: ../../bin/v-list-sys-services:"$(grep -n "$string" $HESTIA/bin/v-list-sys-services | cut -d: -f1)"\nmsgid \"$string\"\nmsgstr \"\"" >> hestiacp.pot

7aa2d739183f9f1eb8bc2627a0d9c41f64151e4a
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2022-08-03 16:33:04 +0200

    Update versions

---
+++ install/upgrade/versions/1.6.6.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Hestia Control Panel upgrade script for target version 1.6.6
+
+#######################################################################################
+#######                      Place additional commands below.                   #######
+#######################################################################################
+####### Pass through information to the end user in case of a issue or problem  #######
+#######                                                                         #######
+####### Use add_upgrade_message "My message here" to include a message          #######
+####### in the upgrade notification email. Example:                             #######
+#######                                                                         #######
+####### add_upgrade_message "My message here"                                   #######
+#######                                                                         #######
+####### You can use \n within the string to create new lines.                   #######
+#######################################################################################
+
+upgrade_config_set_value 'UPGRADE_UPDATE_WEB_TEMPLATES' 'no'
+upgrade_config_set_value 'UPGRADE_UPDATE_DNS_TEMPLATES' 'no'
+upgrade_config_set_value 'UPGRADE_UPDATE_MAIL_TEMPLATES' 'no'
+upgrade_config_set_value 'UPGRADE_REBUILD_USERS' 'no'
+upgrade_config_set_value 'UPGRADE_UPDATE_FILEMANAGER_CONFIG' 'false'
+
+if [ -f "/etc/roundcube/config.inc.php" ]; then
+    if [ -n "$(grep 'ssl://localhost' /etc/roundcube/config.inc.php)" ]; then
+        # Echo prepare for 1.6.0 update
+        sed -i "s|ssl://localhost|localhost|g" /etc/roundcube/config.inc.php
+        sed -i "s|993|143|g" /etc/roundcube/config.inc.php
+    fi
+fi

8e8f760691300fe5be799235b1bd7bd2e00020e7
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2022-08-28 21:26:01 +0200

    Fix bug causing v-delete-letsencrypt-domain to set restart to ' ' (#2878)

--- web/edit/mail/index.php
+++ web/edit/mail/index.php
@@ -333,7 +333,7 @@ if ((!empty($_POST['save'])) && (!empty($_GET['domain'])) && (empty($_GET['accou
 
     // Delete Lets Encrypt support
     if (($v_letsencrypt == 'yes') && (empty($_POST['v_letsencrypt']) || empty($_POST['v_ssl'])) && (empty($_SESSION['error_msg']))) {
-        exec(HESTIA_CMD."v-delete-letsencrypt-domain ".$user." ".quoteshellarg($v_domain)." ' ' 'yes'", $output, $return_var);
+        exec(HESTIA_CMD."v-delete-letsencrypt-domain ".$user." ".quoteshellarg($v_domain)." '' 'yes'", $output, $return_var);
         check_return_code($return_var, $output);
         unset($output);
         $v_ssl_crt = '';

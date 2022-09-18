4d901c8226949c516556e8a9275c4e474e8fcf43
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2022-09-18 17:51:44 +0200

    Changes to web ui

--- web/js/pages/edit_server.js
+++ web/js/pages/edit_server.js
@@ -2,9 +2,15 @@ $('#backup_type').change(function (){
    if(this.value == 'b2'){
        $('#backup_bucket').show();
        $('#backup_sftp').hide();
+       $('#backup_rclone').hide(); 
+   }else if(this.value == 'rclone'){
+      $('#backup_bucket').hide();
+      $('#backup_sftp').hide();
+      $('#backup_rclone').show(); 
    }else{
-       $('#backup_bucket').hide();
-       $('#backup_sftp').show();
+      $('#backup_bucket').hide();
+      $('#backup_sftp').show();
+      $('#backup_rclone').hide(); 
    }
 });
 

af740bc8a5b69d9aeb2ce5c7f18d0aa86ff70394
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2022-11-01 09:55:49 +0100

    Add debug info 1.7.0.sh step

--- install/upgrade/versions/1.7.0.sh
+++ install/upgrade/versions/1.7.0.sh
@@ -23,12 +23,14 @@ upgrade_config_set_value 'UPGRADE_UPDATE_FILEMANAGER_CONFIG' 'false'
 
 # Make sure to sync install quoteshell arg
 if [ "$FILE_MANAGER" = "true" ]; then
+    echo "[ * ] Force update filemanager..."
     $HESTIA/bin/v-delete-sys-filemanager quiet
     $HESTIA/bin/v-add-sys-filemanager quiet
 fi
 
 packages=$(ls --sort=time $HESTIA/data/packages |grep .pkg)
 for package in $packages; do
+    echo "[ * ] Update existing packages to support rate limit mail accounts..."
     if [ -z "$(grep -e 'RATE_LIMIT' $HESTIA/data/packages/$package)" ]; then
        echo "RATE_LIMIT='200'" >> $HESTIA/data/packages/$package
     fi

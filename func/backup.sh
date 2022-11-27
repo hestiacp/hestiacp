0ff01c10f809add8db16f0ca09d7751a0414b174
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2022-11-27 14:19:44 +0100

    Delete backup when local set to no
    - Fix deleting backups when more then 1 backup should be kept
    
    Use /tmp/hestia-backup.txt instead /tmp/hestia-backup
    
    
    Fix typo

--- func/backup.sh
+++ func/backup.sh
@@ -523,7 +523,7 @@ b2_backup() {
     backup_list=$(b2 ls --long $BUCKET $user | cut -f 1 -d ' ' 2>/dev/null)
     backups_count=$(echo "$backup_list" |wc -l)
     if [ "$backups_count" -ge "$BACKUPS" ]; then
-        backups_rm_number=$((backups_count - BACKUPS))
+        backups_rm_number=$(($backups_count - $BACKUPS))
         for backup in $(echo "$backup_list" |head -n $backups_rm_number); do
             backup_file_name=$(b2 get-file-info $backup | grep fileName | cut -f 4 -d '"' 2>/dev/null)
             echo -e "$(date "+%F %T") Rotated b2 backup: $backup_file_name"
@@ -590,13 +590,16 @@ rclone_backup(){
 
         backup_list=$(rclone lsf $HOST:$BPATH | cut -d' ' -f1 |grep "^$user\.");
         backups_count=$(echo "$backup_list" |wc -l)
-        backups_rm_number=$((backups_count - BACKUPS))
+        backups_rm_number=$(($backups_count - $BACKUPS))
         if [ "$backups_count" -ge "$BACKUPS" ]; then
         for backup in $(echo "$backup_list" |head -n $backups_rm_number); do
             echo "Delete file: $backup"
             rclone deletefile $HOST:$BPATH/$backup
         done
         fi
+    fi
+    if [ "$localbackup" != 'yes' ]; then
+        rm -f $user.$backup_new_date.tar
     fi
 }
 

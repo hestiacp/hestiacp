a342daa9f5f5bbecdeee5a14313e5541da6dce7c
Jakob Bouchard <jakob@jakobbouchard.dev>
2022-11-28 11:59:52 -0500

    Add nginx, sh and sql prettier plugins

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,4 +1,30 @@
 module.exports = {
+	// Plugins
+	plugins: [
+		"@prettier/plugin-php",
+		"prettier-plugin-nginx",
+		"prettier-plugin-sh",
+		"prettier-plugin-sql",
+	],
+	pluginSearchDirs: ["."],
+	// PHP Settings
 	phpVersion: "8.1",
 	braceStyle: "1tbs",
+	// Overrides for some files
+	overrides: [
+		// Hestia CLI
+		{
+			files: ["bin/v-*", "src/deb/*/{postinst,preinst,hestia,postrm}", "install/common/api/*"],
+			options: {
+				parser: "sh",
+			},
+		},
+		// Nginx config
+		{
+			files: ["**/nginx/*.inc", "**/nginx/*.conf"],
+			options: {
+				parser: "nginx",
+			},
+		},
+	],
 };

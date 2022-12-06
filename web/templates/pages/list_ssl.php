176ef0c4a1d9c0a2b449ab3f114eb87815e7d1f0
Jakob Bouchard <jakob@jakobbouchard.dev>
2022-12-06 14:47:49 -0500

    Remove list_ssl.js

--- web/templates/pages/list_ssl.php
+++ web/templates/pages/list_ssl.php
@@ -2,48 +2,94 @@
 <div class="toolbar">
 	<div class="toolbar-inner">
 		<div class="toolbar-right">
-
 		</div>
 	</div>
 </div>
 <!-- End toolbar -->
 
+<!-- Begin form -->
 <div class="container animate__animated animate__fadeIn">
-
 	<form id="vstobjects" name="v_generate_csr" method="post">
 		<input type="hidden" name="token" value="<?=$_SESSION['token']?>">
 
 		<div class="form-container">
 			<h1 class="form-title"><?=_('Generating CSR');?></h1>
 			<?php show_alert_message($_SESSION);?>
-			<div class="u-mb20">
+			<div
+				x-data="{
+					text: '<?= $v_crt ?>',
+					blob() {
+						return window.URL.createObjectURL(new Blob([this.text], { type: 'text/plain' }))
+					}
+				}"
+				class="u-mb20"
+			>
 				<label for="v_crt" class="form-label">
 					<?=_('SSL Certificate');?>
-					<a href="javascript:saveTextToBlob('<?php echo htmlentities($v_domain);?>.crt', 'v_crt');"><i class="fas fa-download"></i></a>
+					<a
+						x-bind:href="blob()"
+						download="<?= htmlentities($v_domain) ?>.crt"
+					><i class="fas fa-download"></i></a>
 				</label>
-				<textarea class="form-control u-min-height100" name="v_crt" id="v_crt"><?=$v_crt ?></textarea>
+				<textarea
+					x-model="text"
+					class="form-control u-min-height100"
+					name="v_crt"
+					id="v_crt"
+				></textarea>
 			</div>
-			<div class="u-mb20">
+			<div
+				x-data="{
+					text: '<?= $v_key ?>',
+					blob() {
+						return window.URL.createObjectURL(new Blob([this.text], { type: 'text/plain' }))
+					}
+				}"
+				class="u-mb20"
+			>
 				<label for="v_key" class="form-label">
 					<?=_('SSL Key');?>
-					<a href="javascript:saveTextToBlob('<?php echo htmlentities($v_domain);?>.key', 'v_key');"><i class="fas fa-download"></i></a>
+					<a
+						x-bind:href="blob()"
+						download="<?= htmlentities($v_domain) ?>.key"
+					><i class="fas fa-download"></i></a>
 				</label>
-				<textarea class="form-control u-min-height100" name="v_key" id="v_key"><?=$v_key ?></textarea>
+				<textarea
+					x-model="text"
+					class="form-control u-min-height100"
+					name="v_key"
+					id="v_key"
+				></textarea>
 			</div>
-			<div class="u-mb20">
+			<div
+				x-data="{
+					text: '<?= $v_csr ?>',
+					blob() {
+						return window.URL.createObjectURL(new Blob([this.text], { type: 'text/plain' }))
+					}
+				}"
+				class="u-mb20"
+			>
 				<label for="v_csr" class="form-label">
 					<?=_('SSL CSR');?>
-					<a href="javascript:saveTextToBlob('<?php echo htmlentities($v_domain);?>.csr', 'v_crt');"><i class="fas fa-download"></i></a>
+					<a
+						x-bind:href="blob()"
+						download="<?= htmlentities($v_domain) ?>.csr"
+					><i class="fas fa-download"></i></a>
 				</label>
-				<textarea class="form-control u-min-height100" name="v_csr" id="v_csr"><?=$v_csr ?></textarea>
+				<textarea
+					x-model="text"
+					class="form-control u-min-height100"
+					name="v_csr"
+					id="v_csr"
+				></textarea>
 			</div>
 			<div>
-				<button type="button" class="button button-secondary" onclick="<?=$back?>">
+				<button type="button" class="button button-secondary" onclick="<?= $back ?>">
 					<?=_('Back');?>
 				</button>
 			</div>
 		</div>
-
 	</form>
-
 </div>
+<!-- End form -->

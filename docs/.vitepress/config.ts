687ec6a41319a930237f1bc63db8bfcd592b1b14
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2022-12-14 23:00:00 +0100

    Update pattern for editLink
    Changed to:
    pattern: "https://github.com/hestiacp/hestiacp/edit/main/docs/docs/:path",

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
 		outline: [2, 3],
 
 		editLink: {
-			pattern: "https://github.com/hestiacp/hestiacp-docs/edit/main/docs/:path",
+			pattern: "https://github.com/hestiacp/hestiacp/edit/main/docs/docs/:path",
 			text: "Edit this page on GitHub",
 		},
 

d516d42185bb2f8b931ad84394a84d4c1a85c3f2
Jakob Bouchard <jakob@jakobbouchard.dev>
2023-02-05 07:50:33 -0500

    Start cleaning up templates (#3217)
    * Remove list_ssl.js
    
    * Remove setup_webapp.js
    
    * Manually format edit_server.php and delete .js
    
    * Maybe fix error due to certificate having newlines
    
    * Fix textarea
    
    * Add type button to generate button
    
    ---------
    
    Co-authored-by: Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>

--- web/js/pages/setup_webapp.js
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-function applyRandomStringToTarget(target, min_length = 16) {
-	document.querySelector(`#${target}`).value = randomString(min_length);
-}

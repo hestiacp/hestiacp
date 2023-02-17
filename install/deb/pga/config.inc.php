636cf331810e72ea2fbc61d6c9c0d784c0482e92
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2023-02-17 22:26:30 +0100

    Update config.php.inc

--- install/deb/pga/config.inc.php
+++ install/deb/pga/config.inc.php
@@ -78,6 +78,15 @@ $conf["default_lang"] = "auto";
 // 'disable' disables AutoComplete.
 $conf["autocomplete"] = "default on";
 
+// If extra session security is true, then PHP's session cookies will have
+// SameSite cookie flags set to prevent CSRF attacks.  If you're using
+// auto-start sessions, autostarted sessions will be destroyed and
+// restarted with SameSite on.  If this this solution is not acceptable for
+// your situation, you will need to either turn off auot-start sessions, or
+// turn off secure sessions.  Versions of PHP below 7.3 do not have access
+// to this feature and will be vulnerable to CSRF attacks.
+$conf['extra_session_security'] = true;
+
 // If extra login security is true, then logins via phpPgAdmin with no
 // password or certain usernames (pgsql, postgres, root, administrator)
 // will be denied. Only set this false once you have read the FAQ and

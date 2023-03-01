e51cbf8d4be2d0e23c6084e43f144f031f646d60
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2023-03-01 14:17:20 +0100

    Update docs/_data/features.ts
    Co-authored-by: Jakob Bouchard <jakob@jakobbouchard.dev>

--- docs/_data/features.ts
+++ docs/_data/features.ts
@@ -64,7 +64,7 @@ export const dns: FeatureListItem[] = [
 
 export const databases: FeatureListItem[] = [
 	{ text: "Support for MariaDB 10.2 -> 11.0 with 10.11 as default" },
-	{ text: "Support for Mysql 8" },
+	{ text: "Support for MySQL 8" },
 	{ text: "Support for PostgreSQL" },
 	{ text: "Latest version of phpMyAdmin and phpPgAdmin" },
 ];

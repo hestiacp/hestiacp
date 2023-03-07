5c9e3887e0a1022fc3dfcdf739b87728752ed050
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2023-03-07 23:55:06 +0100

    Update 2fa authentication plugin

--- web/inc/2fa/secret.php
+++ web/inc/2fa/secret.php
@@ -1,15 +1,7 @@
 <?php
 
-session_start();
-if (isset($_SESSION["userContext"]) === false && php_sapi_name() !== "cli") {
-	exit();
-}
-
-require_once "/usr/local/hestia/web/inc/2fa/loader.php";
-Loader::register("./", "RobThree\\Auth");
-
 use RobThree\Auth\TwoFactorAuth;
-
+require_once "vendor/autoload.php";
 $tfa = new TwoFactorAuth("Hestia Control Panel");
 
 $secret = $tfa->createSecret(160); // Though the default is an 80 bits secret (for backwards compatibility reasons) we recommend creating 160+ bits secrets (see RFC 4226 - Algorithm Requirements)

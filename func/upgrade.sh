77d6fac0e8e8241a89194229700c4ea4de1ce521
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2023-03-21 14:24:46 +0100

    multiple small bug fixes (#3364)
    * Fix: Show / Hide certificate text
    
    * Fixed upgrade text typo + Missing phpmyadmin version
    
    * Fix redirect on delete system logs
    
    * Update update text
    
    * Remove white line
    
    * Update .pot file
    
    * Update translations
    
    * Fix delete log behavouir
    
    * See suggestion

--- func/upgrade.sh
+++ func/upgrade.sh
@@ -601,7 +601,7 @@ upgrade_phpmyadmin() {
 	if [ -n "$(echo $DB_SYSTEM | grep -w 'mysql')" ]; then
 		pma_version=$(jq -r .version /usr/share/phpmyadmin/package.json)
 		if version_ge "$pma_version" "$pma_v"; then
-			echo "[ * ] phpMyAdmin is up to date (${pma_release_file##*-})..."
+			echo "[ * ] phpMyAdmin is up to date (${pma_version})..."
 			# Update permissions
 			if [ -e /var/lib/phpmyadmin/blowfish_secret.inc.php ]; then
 				chown root:www-data /var/lib/phpmyadmin/blowfish_secret.inc.php
@@ -706,7 +706,7 @@ upgrade_rainloop() {
 }
 
 upgrade_dependencies() {
-	echo "[ ! ] Update Hesita PHP dependencies"
+	echo "[ ! ] Update Hestia PHP dependencies..."
 	$BIN/v-add-sys-dependencies
 }
 
@@ -734,7 +734,7 @@ upgrade_rebuild_dns_templates() {
 upgrade_rebuild_users() {
 	if [ "$UPGRADE_REBUILD_USERS" = "true" ]; then
 		if [ "$DEBUG_MODE" = "true" ]; then
-			echo "[ * ] Rebuilding user accounts and domains:"
+			echo "[ * ] Rebuilding user accounts and domains:..."
 		else
 			echo "[ * ] Rebuilding user accounts and domains, this may take a few minutes..."
 		fi

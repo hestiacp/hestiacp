275f36d603113a41689fe694bef9af4f804d5ecd
myrevery <16082142+myrevery@users.noreply.github.com>
2023-03-24 17:48:00 -0700

    Apply update for path change of blacklist.sh

--- install/upgrade/versions/1.7.1.sh
+++ install/upgrade/versions/1.7.1.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# Hestia Control Panel upgrade script for target version 1.6.15
+# Hestia Control Panel upgrade script for target version 1.7.1
 
 #######################################################################################
 #######                      Place additional commands below.                   #######
 #######################################################################################
+####### upgrade_config_set_value only accepts true or false.                    #######
+#######                                                                         #######
 ####### Pass through information to the end user in case of a issue or problem  #######
 #######                                                                         #######
 ####### Use add_upgrade_message "My message here" to include a message          #######
@@ -15,13 +17,23 @@
 ####### You can use \n within the string to create new lines.                   #######
 #######################################################################################
 
-upgrade_config_set_value 'UPGRADE_UPDATE_WEB_TEMPLATES' 'no'
-upgrade_config_set_value 'UPGRADE_UPDATE_DNS_TEMPLATES' 'no'
-upgrade_config_set_value 'UPGRADE_UPDATE_MAIL_TEMPLATES' 'no'
-upgrade_config_set_value 'UPGRADE_REBUILD_USERS' 'no'
+upgrade_config_set_value 'UPGRADE_UPDATE_WEB_TEMPLATES' 'false'
+upgrade_config_set_value 'UPGRADE_UPDATE_DNS_TEMPLATES' 'false'
+upgrade_config_set_value 'UPGRADE_UPDATE_MAIL_TEMPLATES' 'false'
+upgrade_config_set_value 'UPGRADE_REBUILD_USERS' 'false'
 upgrade_config_set_value 'UPGRADE_UPDATE_FILEMANAGER_CONFIG' 'false'
 
-grep 'v-change-user-password' $HESTIA/data/api/billing
-if [ $? -ne 0 ]; then
-	sed -i "s/v-make-tmp-file'/v-make-tmp-file,v-change-user-password'/g" $HESTIA/data/api/billing
+# Moved from 1.6.15.sh
+if ! (grep -q 'v-change-user-password' $HESTIA/data/api/billing); then
+	sed -i "s|v-make-tmp-file'|v-make-tmp-file,v-change-user-password'|g" $HESTIA/data/api/billing
+fi
+
+# Apply update for path change of built-in IPset blacklist.sh
+if [ "$FIREWALL_SYSTEM" = "iptables" ]; then
+	rm -f $HESTIA/data/firewall/ipset/blacklist.sh
+
+	if ($BIN/v-list-firewall-ipset plain | grep -q '/install/deb/firewall/ipset/blacklist.sh'); then
+		echo "[ * ] Update the path of IPset blacklist.sh..."
+		sed -i 's|/install/deb/firewall/ipset/blacklist.sh|/install/common/firewall/ipset/blacklist.sh|g' $HESTIA/data/firewall/ipset.conf
+	fi
 fi

633fc3a0a8a34ae3450bb8e991a30f49e2c4a6d5
Alec Rust <me@alecrust.com>
2023-03-29 20:05:59 +0100

    Replace jQuery UI tabs with vanilla JS

--- web/js/pages/add_cron.js
+++ web/js/pages/add_cron.js
@@ -1,6 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
-	// TODO: Replace jQuery UI tabs with something else
-	$('.js-cron-tabs').tabs();
+	const tabs = document.querySelector('.js-tabs');
+	if (tabs) {
+		const tabItems = tabs.querySelectorAll('.tabs-item');
+		const panels = tabs.querySelectorAll('.tabs-panel');
+		tabItems.forEach((tab) => {
+			tab.addEventListener('click', (event) => {
+				// Reset state
+				panels.forEach((panel) => (panel.hidden = true));
+				tabItems.forEach((tab) => {
+					tab.setAttribute('aria-selected', false);
+					tab.setAttribute('tabindex', -1);
+				});
+
+				// Show the selected panel
+				const tabId = event.target.getAttribute('id');
+				const panel = document.querySelector(`[aria-labelledby="${tabId}"]`);
+				panel.hidden = false;
+
+				// Mark the selected tab as active
+				event.target.setAttribute('aria-selected', true);
+				event.target.setAttribute('tabindex', 0);
+				event.target.focus();
+			});
+		});
+	}
 
 	const generateCronButtons = document.querySelectorAll('.js-generate-cron');
 	generateCronButtons.forEach((button) => {

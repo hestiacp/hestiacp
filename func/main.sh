e5da641ae2f62ef4c751bf8542cce01daff9c253
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2023-04-06 07:17:39 +0200

    Update main.sh
    Only do leading / trailings spaces

--- func/main.sh
+++ func/main.sh
@@ -1400,7 +1400,7 @@ format_domain() {
 		domain=$(echo "$domain" | sed -e "s/^[.]*//")
 	fi
 	# Remove white spaces
-	domain=$(echo $domain | tr -d '[:space:]')
+	domain=$(echo $domain | sed 's/^[ \t]*//;s/[ \t]*$//')
 }
 
 format_domain_idn() {

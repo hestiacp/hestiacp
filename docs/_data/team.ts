2fbf4b44e353b52c90319a6e84e3f8b01e2abc90
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2023-04-10 18:30:08 +0200

    Add install script generator  (#3355)
    * Add support for an install generator
    
    * Update colors
    
    * Add select box for languages
    
    * Tidy install generator UI a bit
    
    * Tidy
    
    * Use dialog to display install string
    
    * Add dependencies + Conflicts
    
    VSFTP + Proftpd and  Mysql And Mysql8 can't go together
    - Sieve requires Dovecot
    - Dovecot requires Exim and so on
    
    * Update Teams page
    
    * Remove exta code
    
    * Refine install generator
    
    * Improve install modal
    
    * Simplify install generator code
    
    ---------
    
    Co-authored-by: Alec Rust <me@alecrust.com>

--- docs/_data/team.ts
+++ docs/_data/team.ts
@@ -36,7 +36,7 @@ export const teamMembers: DefaultTheme.TeamMember[] = [
 		orgLink: "https://prosomo.com",
 		links: [
 			{ icon: "github", link: "https://github.com/jakobbouchard" },
-			{ icon: "linkedin", link: "https://linkedin.com/in/bouchardjakob" },
+			{ icon: "linkedin", link: "https://linkedin.com/in/jakobbouchard" },
 			{
 				icon: {
 					svg: '<svg role="img" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><title>Website</title><path stroke-linecap="round" stroke-linejoin="round" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" /></svg>',

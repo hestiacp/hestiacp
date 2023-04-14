6de1691af9d4b70b5fe53a7b07b8495e6ec45e7b
Alec Rust <me@alecrust.com>
2023-04-14 17:37:17 +0100

    Refactor add/remove nameserver JS
    To reduce duplication.

--- web/js/main.js
+++ web/js/main.js
@@ -228,6 +228,38 @@ document.addEventListener('alpine:init', () => {
 	}));
 });
 
+// Add new name server input
+const addNsButton = document.querySelector('.js-add-ns');
+if (addNsButton) {
+	addNsButton.addEventListener('click', () => {
+		const currentNsInputs = document.querySelectorAll('input[name^=v_ns]');
+		const inputCount = currentNsInputs.length;
+
+		if (inputCount < 8) {
+			const template = currentNsInputs[0].parentElement.cloneNode(true);
+			const templateNsInput = template.querySelector('input');
+
+			templateNsInput.removeAttribute('value');
+			templateNsInput.name = `v_ns${inputCount + 1}`;
+			addNsButton.insertAdjacentElement('beforebegin', template);
+		}
+
+		if (inputCount === 7) {
+			addNsButton.classList.add('u-hidden');
+		}
+	});
+}
+
+// Remove name server input
+document.querySelectorAll('.js-remove-ns').forEach((removeNsButton) => {
+	removeNsButton.addEventListener('click', () => {
+		removeNsButton.parentElement.remove();
+		const currentNsInputs = document.querySelectorAll('input[name^=v_ns]');
+		currentNsInputs.forEach((input, index) => (input.name = `v_ns${index + 1}`));
+		document.querySelector('.js-add-ns').classList.remove('u-hidden');
+	});
+});
+
 // Intercept clicks on .js-confirm-action links and display dialog
 document.querySelectorAll('.js-confirm-action').forEach((triggerLink) => {
 	triggerLink.addEventListener('click', (evt) => {

7adeedce9b997e864e9c3f1303e312e759e75ac5
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2023-05-15 21:35:26 +0200

    Issue in check

--- install/upgrade/versions/1.8.0.sh
+++ install/upgrade/versions/1.8.0.sh
@@ -24,7 +24,7 @@ upgrade_config_set_value 'UPGRADE_REBUILD_USERS' 'false'
 upgrade_config_set_value 'UPGRADE_UPDATE_FILEMANAGER_CONFIG' 'false'
 
 if [ "$IMAP_SYSTEM" = "dovecot" ]; then
-	if grep -qw "^extra_groups = mail$" /etc/dovecot/conf.d/10-master.conf 2> /dev/null; then
+	if ! grep -qw "^extra_groups = mail$" /etc/dovecot/conf.d/10-master.conf 2> /dev/null; then
 		sed -i "s/^service auth {/service auth {\n  extra_groups = mail\n/g" /etc/dovecot/conf.d/10-master.conf
 	fi
 fi

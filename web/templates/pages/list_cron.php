27e6dbedf9cf3ff4f29fcdfd188aa4f758ea9510
Alec Rust <me@alecrust.com>
2023-05-21 21:38:56 +0100

    Refactor table HTML (#3606)

--- web/templates/pages/list_cron.php
+++ web/templates/pages/list_cron.php
@@ -65,98 +65,100 @@
 </div>
 <!-- End toolbar -->
 
-<div class="container units">
-	<div class="header units-header">
-		<div class="l-unit__col l-unit__col--right">
-			<div class="clearfix l-unit__stat-col--left super-compact">
-				<input type="checkbox" class="js-toggle-all-checkbox" title="<?= _("Select all") ?>" <?= $display_mode ?>>
-			</div>
-			<div class="clearfix l-unit__stat-col--left wide-5"><b><?= _("Command") ?></b></div>
-			<div class="clearfix l-unit__stat-col--left compact-2 u-text-right"><b>&nbsp;</b></div>
-			<div class="clearfix l-unit__stat-col--left compact-3 u-text-center"><b><?= _("Minute") ?></b></div>
-			<div class="clearfix l-unit__stat-col--left compact-3 u-text-center"><b><?= _("Hour") ?></b></div>
-			<div class="clearfix l-unit__stat-col--left compact-3 u-text-center"><b><?= _("Day") ?></b></div>
-			<div class="clearfix l-unit__stat-col--left compact-3 u-text-center"><b><?= _("Month") ?></b></div>
-			<div class="clearfix l-unit__stat-col--left compact-3 u-text-center"><b><?= _("Day of Week") ?></b></div>
-		</div>
-	</div>
-
-	<!-- Begin cron job list item loop -->
-	<?php
-		foreach ($data as $key => $value) {
-			++$i;
-			if ($data[$key]['SUSPENDED'] == 'yes') {
-				$status = 'suspended';
-				$spnd_action = 'unsuspend';
-				$spnd_action_title = _('Unsuspend');
-				$spnd_icon = 'fa-play';
-				$spnd_confirmation = _('Are you sure you want to unsuspend the cron job?') ;
-			} else {
-				$status = 'active';
-				$spnd_action = 'suspend';
-				$spnd_action_title = _('Suspend');
-				$spnd_icon = 'fa-pause';
-				$spnd_confirmation = _('Are you sure you want to suspend the cron job?') ;
-			}
-		?>
-		<div class="l-unit <?php if($status == 'suspended') echo 'l-unit--suspended'; ?> animate__animated animate__fadeIn js-unit"
-			data-sort-date="<?=strtotime($data[$key]['DATE'].' '.$data[$key]['TIME'])?>"
-			data-sort-name="<?=htmlspecialchars($data[$key]['CMD'], ENT_NOQUOTES)?>">
+<div class="container">
+	<div class="units">
+		<div class="header units-header">
 			<div class="l-unit__col l-unit__col--right">
 				<div class="clearfix l-unit__stat-col--left super-compact">
-					<input id="check<?= $i ?>" class="js-unit-checkbox" type="checkbox" title="<?= _("Select") ?>" name="job[]" value="<?= $key ?>" <?= $display_mode ?>>
+					<input type="checkbox" class="js-toggle-all-checkbox" title="<?= _("Select all") ?>" <?= $display_mode ?>>
 				</div>
-				<div class="clearfix l-unit__stat-col--left wide-5 truncate">
-					<?php if ($read_only === "true" || $data[$key]["SUSPENDED"] == "yes") { ?>
-						<b><?= htmlspecialchars($data[$key]["CMD"], ENT_NOQUOTES) ?></b>
-					<?php } else { ?>
-						<b><a href="/edit/cron/?job=<?=$data[$key]['JOB']?>&token=<?=$_SESSION['token']?>" title="<?= _("Edit Cron Job") ?>: <?=htmlspecialchars($data[$key]['CMD'], ENT_NOQUOTES)?>"><?=htmlspecialchars($data[$key]['CMD'], ENT_NOQUOTES)?></a></b>
-					<?php } ?>
-				</div>
-				<!-- START QUICK ACTION TOOLBAR AREA -->
-				<div class="clearfix l-unit__stat-col--left compact-2 u-text-right">
-					<div class="l-unit-toolbar__col l-unit-toolbar__col--right u-noselect">
-						<div class="actions-panel clearfix">
-							<?php if ($read_only === "true") { ?>
-								<!-- Restrict other administrators from editing, deleting, or suspending 'admin' user cron jobs -->
-								&nbsp;
-							<?php } else { ?>
-								<?php if ($data[$key]['SUSPENDED'] == 'no') {?>
-									<div class="actions-panel__col actions-panel__download shortcut-enter" data-key-action="href"><a href="/edit/cron/?job=<?=$data[$key]['JOB']?>&token=<?=$_SESSION['token']?>" title="<?= _("Edit Cron Job") ?>"><i class="fas fa-pencil icon-orange icon-dim"></i></a></div>
-								<?php } ?>
-								<div class="actions-panel__col actions-panel__suspend shortcut-s" data-key-action="js">
-									<a
-										class="data-controls js-confirm-action"
-										href="/<?= $spnd_action ?>/cron/?job=<?= $data[$key]["JOB"] ?>&token=<?= $_SESSION["token"] ?>"
-										data-confirm-title="<?= $spnd_action_title ?>"
-										data-confirm-message="<?= sprintf($spnd_confirmation, $key) ?>"
-									>
-										<i class="fas <?= $spnd_icon ?> icon-highlight icon-dim"></i>
-									</a>
-								</div>
-								<div class="actions-panel__col actions-panel__delete shortcut-delete" data-key-action="js">
-									<a
-										class="data-controls js-confirm-action"
-										href="/delete/cron/?job=<?= $data[$key]["JOB"] ?>&token=<?= $_SESSION["token"] ?>"
-										data-confirm-title="<?= _("Delete") ?>"
-										data-confirm-message="<?= sprintf(_("Are you sure you want to delete the cron job?"), $key) ?>"
-									>
-										<i class="fas fa-trash icon-red icon-dim"></i>
-									</a>
-								</div>
-							<?php } ?>
-						</div>
-					</div>
-				</div>
-				<!-- END QUICK ACTION TOOLBAR AREA -->
-				<div class="clearfix l-unit__stat-col--left compact-3 u-text-center"><?= $data[$key]["MIN"] ?></div>
-				<div class="clearfix l-unit__stat-col--left compact-3 u-text-center"><?= $data[$key]["HOUR"] ?></div>
-				<div class="clearfix l-unit__stat-col--left compact-3 u-text-center"><?= $data[$key]["DAY"] ?></div>
-				<div class="clearfix l-unit__stat-col--left compact-3 u-text-center"><?= $data[$key]["MONTH"] ?></div>
-				<div class="clearfix l-unit__stat-col--left compact-3 u-text-center"><?= $data[$key]["WDAY"] ?></div>
+				<div class="clearfix l-unit__stat-col--left wide-5"><b><?= _("Command") ?></b></div>
+				<div class="clearfix l-unit__stat-col--left compact-2 u-text-right"><b>&nbsp;</b></div>
+				<div class="clearfix l-unit__stat-col--left compact-3 u-text-center"><b><?= _("Minute") ?></b></div>
+				<div class="clearfix l-unit__stat-col--left compact-3 u-text-center"><b><?= _("Hour") ?></b></div>
+				<div class="clearfix l-unit__stat-col--left compact-3 u-text-center"><b><?= _("Day") ?></b></div>
+				<div class="clearfix l-unit__stat-col--left compact-3 u-text-center"><b><?= _("Month") ?></b></div>
+				<div class="clearfix l-unit__stat-col--left compact-3 u-text-center"><b><?= _("Day of Week") ?></b></div>
 			</div>
 		</div>
-	<?php } ?>
+
+		<!-- Begin cron job list item loop -->
+		<?php
+			foreach ($data as $key => $value) {
+				++$i;
+				if ($data[$key]['SUSPENDED'] == 'yes') {
+					$status = 'suspended';
+					$spnd_action = 'unsuspend';
+					$spnd_action_title = _('Unsuspend');
+					$spnd_icon = 'fa-play';
+					$spnd_confirmation = _('Are you sure you want to unsuspend the cron job?') ;
+				} else {
+					$status = 'active';
+					$spnd_action = 'suspend';
+					$spnd_action_title = _('Suspend');
+					$spnd_icon = 'fa-pause';
+					$spnd_confirmation = _('Are you sure you want to suspend the cron job?') ;
+				}
+			?>
+			<div class="l-unit <?php if($status == 'suspended') echo 'l-unit--suspended'; ?> animate__animated animate__fadeIn js-unit"
+				data-sort-date="<?=strtotime($data[$key]['DATE'].' '.$data[$key]['TIME'])?>"
+				data-sort-name="<?=htmlspecialchars($data[$key]['CMD'], ENT_NOQUOTES)?>">
+				<div class="l-unit__col l-unit__col--right">
+					<div class="clearfix l-unit__stat-col--left super-compact">
+						<input id="check<?= $i ?>" class="js-unit-checkbox" type="checkbox" title="<?= _("Select") ?>" name="job[]" value="<?= $key ?>" <?= $display_mode ?>>
+					</div>
+					<div class="clearfix l-unit__stat-col--left wide-5 truncate">
+						<?php if ($read_only === "true" || $data[$key]["SUSPENDED"] == "yes") { ?>
+							<b><?= htmlspecialchars($data[$key]["CMD"], ENT_NOQUOTES) ?></b>
+						<?php } else { ?>
+							<b><a href="/edit/cron/?job=<?=$data[$key]['JOB']?>&token=<?=$_SESSION['token']?>" title="<?= _("Edit Cron Job") ?>: <?=htmlspecialchars($data[$key]['CMD'], ENT_NOQUOTES)?>"><?=htmlspecialchars($data[$key]['CMD'], ENT_NOQUOTES)?></a></b>
+						<?php } ?>
+					</div>
+					<!-- START QUICK ACTION TOOLBAR AREA -->
+					<div class="clearfix l-unit__stat-col--left compact-2 u-text-right">
+						<div class="l-unit-toolbar__col l-unit-toolbar__col--right u-noselect">
+							<div class="actions-panel clearfix">
+								<?php if ($read_only === "true") { ?>
+									<!-- Restrict other administrators from editing, deleting, or suspending 'admin' user cron jobs -->
+									&nbsp;
+								<?php } else { ?>
+									<?php if ($data[$key]['SUSPENDED'] == 'no') {?>
+										<div class="actions-panel__col actions-panel__download shortcut-enter" data-key-action="href"><a href="/edit/cron/?job=<?=$data[$key]['JOB']?>&token=<?=$_SESSION['token']?>" title="<?= _("Edit Cron Job") ?>"><i class="fas fa-pencil icon-orange icon-dim"></i></a></div>
+									<?php } ?>
+									<div class="actions-panel__col actions-panel__suspend shortcut-s" data-key-action="js">
+										<a
+											class="data-controls js-confirm-action"
+											href="/<?= $spnd_action ?>/cron/?job=<?= $data[$key]["JOB"] ?>&token=<?= $_SESSION["token"] ?>"
+											data-confirm-title="<?= $spnd_action_title ?>"
+											data-confirm-message="<?= sprintf($spnd_confirmation, $key) ?>"
+										>
+											<i class="fas <?= $spnd_icon ?> icon-highlight icon-dim"></i>
+										</a>
+									</div>
+									<div class="actions-panel__col actions-panel__delete shortcut-delete" data-key-action="js">
+										<a
+											class="data-controls js-confirm-action"
+											href="/delete/cron/?job=<?= $data[$key]["JOB"] ?>&token=<?= $_SESSION["token"] ?>"
+											data-confirm-title="<?= _("Delete") ?>"
+											data-confirm-message="<?= sprintf(_("Are you sure you want to delete the cron job?"), $key) ?>"
+										>
+											<i class="fas fa-trash icon-red icon-dim"></i>
+										</a>
+									</div>
+								<?php } ?>
+							</div>
+						</div>
+					</div>
+					<!-- END QUICK ACTION TOOLBAR AREA -->
+					<div class="clearfix l-unit__stat-col--left compact-3 u-text-center"><?= $data[$key]["MIN"] ?></div>
+					<div class="clearfix l-unit__stat-col--left compact-3 u-text-center"><?= $data[$key]["HOUR"] ?></div>
+					<div class="clearfix l-unit__stat-col--left compact-3 u-text-center"><?= $data[$key]["DAY"] ?></div>
+					<div class="clearfix l-unit__stat-col--left compact-3 u-text-center"><?= $data[$key]["MONTH"] ?></div>
+					<div class="clearfix l-unit__stat-col--left compact-3 u-text-center"><?= $data[$key]["WDAY"] ?></div>
+				</div>
+			</div>
+		<?php } ?>
+	</div>
 </div>
 
 <footer class="app-footer">
--- web/templates/pages/list_search.php
+++ web/templates/pages/list_search.php
@@ -37,144 +37,146 @@
 </div>
 <!-- End toolbar -->
 
-<div class="container units">
-	<div class="header units-header">
-		<div class="l-unit__col l-unit__col--right">
-			<div class="clearfix l-unit__stat-col--left super-compact">
-				&nbsp;
-			</div>
-			<div class="clearfix l-unit__stat-col--left u-text-center compact-2"><b><?= _("Status") ?></b></div>
-			<div class="clearfix l-unit__stat-col--left wide-5"><b><?= _("Search Results") ?></b></div>
-			<div class="clearfix l-unit__stat-col--left compact-3"><b>&nbsp;</b></div>
-			<div class="clearfix l-unit__stat-col--left u-text-center"><b><?= _("Date") ?></b></div>
-			<div class="clearfix l-unit__stat-col--left u-text-center"><b><?= _("Owner") ?></b></div>
-			<div class="clearfix l-unit__stat-col--left u-text-center"><b><?= _("Type") ?></b></div>
-		</div>
-	</div>
-
-	<!-- Begin search result item loop -->
-	<?php
-		foreach ($data as $key => $value) {
-			++$i;
-
-			if ($value['SUSPENDED'] == 'yes') {
-				$status = 'suspended';
-				$spnd_action = 'unsuspend';
-			} else {
-				$status = 'active';
-				$spnd_action = 'suspend';
-			}
-
-			if ($value['TYPE'] == 'db') {
-				$object = 'database';
-			} else {
-				$object = strtolower($value['TYPE'] . ' ' . $value['KEY']);
-			}
-
-			$uniq_id = $value['TYPE'] . '-';
-			if ($value['KEY'] == 'ACCOUNT'){
-				$uniq_id .= 'acc-';
-			}
-			$uniq_id .= sha1($value['RESULT']);
-		?>
-		<div class="l-unit <?php if($status == 'suspended') echo 'l-unit--suspended'; ?> animate__animated animate__fadeIn js-unit"
-			data-uniq-id="<?=$uniq_id?>"
-			data-sort-date="<?=strtotime($value['DATE'].' '.$value['TIME'])?>"
-			data-sort-name="<?=$value['RESULT']?>"
-			data-sort-type="<?=_($object)?>"
-			data-sort-owner="<?=$value['USER']?>"
-			data-sort-status="<?=$status?>"
-			style="<?php if (($_SESSION['POLICY_SYSTEM_HIDE_ADMIN'] === 'yes') && ($value['USER']) === 'admin') { echo 'display: none;';}?>">
-
+<div class="container">
+	<div class="units">
+		<div class="header units-header">
 			<div class="l-unit__col l-unit__col--right">
-				<div class="clearfix l-unit__stat-col--left super-compact u-text-center">
-					<?php
-						if ($object === 'web domain') {
-								$icon = 'fa-earth-americas';
-						}
-						if ($object === 'mail domain') {
-								$icon = 'fa-envelopes-bulk';
-						}
-						if ($object === 'dns domain') {
-								$icon = 'fa-book-atlas';
-						}
-						if ($object === 'dns record') {
-								$icon = 'fa-book-atlas';
-						}
-						if ($object === 'database') {
-								$icon = 'fa-database';
-						}
-						if ($object === 'cron job') {
-								$icon = 'fa-clock';
-						}
-					?>
-					<i class="fa <?=$icon;?> icon-dim"></i>
+				<div class="clearfix l-unit__stat-col--left super-compact">
+					&nbsp;
 				</div>
-				<div class="clearfix l-unit__stat-col--left compact-2 u-text-center">
-					<b>
-						<?php if ($status === "active") { ?>
-							<i class="fas fa-circle-check icon-green"></i>
-						<?php } ?>
-						<?php if ($status === "suspended") { ?>
-							<i class="fas fa-triangle-exclamation icon-orange"></i>
-						<?php } ?>
-					</b>
-				</div>
-				<div class="clearfix l-unit__stat-col--left wide-5 truncate">
-					<?php
-						if ($value['KEY'] == 'RECORD') {
-							$edit_lnk = '/edit/'.$value['TYPE'].'/?domain='.$value['PARENT'].'&record_id='.$value['LINK'].'&user='.$value['USER'];
-						}
-						if ($value['KEY'] == 'ACCOUNT') {
-							$edit_lnk = '/edit/'.$value['TYPE'].'/?domain='.$value['PARENT'].'&account='.$value['LINK'].'&user='.$value['USER'];
-						}
-						if ($value['KEY'] == 'JOB') {
-							$edit_lnk = '/edit/'.$value['TYPE'].'/?job='.$value['LINK'].'&user='.$value['USER'];
-						}
-						if ($value['KEY'] == 'DATABASE') {
-							$edit_lnk = '/edit/'.$value['TYPE'].'/?database='.$value['RESULT'].'&user='.$value['USER'];
-						}
-						if (($value['KEY'] != 'RECORD') && ($value['KEY'] != 'ACCOUNT') && ($value['KEY'] != 'JOB') && ($value['KEY'] != 'DATABASE') ) {
-							$edit_lnk = '/edit/'.$value['TYPE'].'/?'.strtolower($value['KEY']).'='.$value['RESULT'].'&user='.$value['USER'];
-						}
-					?>
-					<b>
+				<div class="clearfix l-unit__stat-col--left u-text-center compact-2"><b><?= _("Status") ?></b></div>
+				<div class="clearfix l-unit__stat-col--left wide-5"><b><?= _("Search Results") ?></b></div>
+				<div class="clearfix l-unit__stat-col--left compact-3"><b>&nbsp;</b></div>
+				<div class="clearfix l-unit__stat-col--left u-text-center"><b><?= _("Date") ?></b></div>
+				<div class="clearfix l-unit__stat-col--left u-text-center"><b><?= _("Owner") ?></b></div>
+				<div class="clearfix l-unit__stat-col--left u-text-center"><b><?= _("Type") ?></b></div>
+			</div>
+		</div>
+
+		<!-- Begin search result item loop -->
+		<?php
+			foreach ($data as $key => $value) {
+				++$i;
+
+				if ($value['SUSPENDED'] == 'yes') {
+					$status = 'suspended';
+					$spnd_action = 'unsuspend';
+				} else {
+					$status = 'active';
+					$spnd_action = 'suspend';
+				}
+
+				if ($value['TYPE'] == 'db') {
+					$object = 'database';
+				} else {
+					$object = strtolower($value['TYPE'] . ' ' . $value['KEY']);
+				}
+
+				$uniq_id = $value['TYPE'] . '-';
+				if ($value['KEY'] == 'ACCOUNT'){
+					$uniq_id .= 'acc-';
+				}
+				$uniq_id .= sha1($value['RESULT']);
+			?>
+			<div class="l-unit <?php if($status == 'suspended') echo 'l-unit--suspended'; ?> animate__animated animate__fadeIn js-unit"
+				data-uniq-id="<?=$uniq_id?>"
+				data-sort-date="<?=strtotime($value['DATE'].' '.$value['TIME'])?>"
+				data-sort-name="<?=$value['RESULT']?>"
+				data-sort-type="<?=_($object)?>"
+				data-sort-owner="<?=$value['USER']?>"
+				data-sort-status="<?=$status?>"
+				style="<?php if (($_SESSION['POLICY_SYSTEM_HIDE_ADMIN'] === 'yes') && ($value['USER']) === 'admin') { echo 'display: none;';}?>">
+
+				<div class="l-unit__col l-unit__col--right">
+					<div class="clearfix l-unit__stat-col--left super-compact u-text-center">
 						<?php
-							if (($_SESSION['userContext'] === 'admin') && ($_SESSION['user'] !== 'admin') && ($value['USER'] === 'admin') && ($_SESSION['POLICY_SYSTEM_PROTECTED_ADMIN'] === 'yes')) {
-								echo $value['RESULT'];
-							} else {
-								if ($value['USER'] == $_SESSION['user']) {
-									$href = $edit_lnk.'&token='.$_SESSION['token'];
-								} else {
-									$href = '/login/?loginas='.$value['USER'].'&token='.$_SESSION['token'].'&edit_link='.urlencode($edit_lnk);
-								}
-								echo '<a href="' . $href . '">' . $value['RESULT'] . '</a>';
+							if ($object === 'web domain') {
+									$icon = 'fa-earth-americas';
+							}
+							if ($object === 'mail domain') {
+									$icon = 'fa-envelopes-bulk';
+							}
+							if ($object === 'dns domain') {
+									$icon = 'fa-book-atlas';
+							}
+							if ($object === 'dns record') {
+									$icon = 'fa-book-atlas';
+							}
+							if ($object === 'database') {
+									$icon = 'fa-database';
+							}
+							if ($object === 'cron job') {
+									$icon = 'fa-clock';
 							}
 						?>
-					</b>
-				</div>
-				<div class="clearfix l-unit__stat-col--left u-text-right compact-3">
-					<div class="l-unit-toolbar__col l-unit-toolbar__col--right u-noselect">
-						<div class="actions-panel clearfix">
-							&nbsp;
+						<i class="fa <?=$icon;?> icon-dim"></i>
+					</div>
+					<div class="clearfix l-unit__stat-col--left compact-2 u-text-center">
+						<b>
+							<?php if ($status === "active") { ?>
+								<i class="fas fa-circle-check icon-green"></i>
+							<?php } ?>
+							<?php if ($status === "suspended") { ?>
+								<i class="fas fa-triangle-exclamation icon-orange"></i>
+							<?php } ?>
+						</b>
+					</div>
+					<div class="clearfix l-unit__stat-col--left wide-5 truncate">
+						<?php
+							if ($value['KEY'] == 'RECORD') {
+								$edit_lnk = '/edit/'.$value['TYPE'].'/?domain='.$value['PARENT'].'&record_id='.$value['LINK'].'&user='.$value['USER'];
+							}
+							if ($value['KEY'] == 'ACCOUNT') {
+								$edit_lnk = '/edit/'.$value['TYPE'].'/?domain='.$value['PARENT'].'&account='.$value['LINK'].'&user='.$value['USER'];
+							}
+							if ($value['KEY'] == 'JOB') {
+								$edit_lnk = '/edit/'.$value['TYPE'].'/?job='.$value['LINK'].'&user='.$value['USER'];
+							}
+							if ($value['KEY'] == 'DATABASE') {
+								$edit_lnk = '/edit/'.$value['TYPE'].'/?database='.$value['RESULT'].'&user='.$value['USER'];
+							}
+							if (($value['KEY'] != 'RECORD') && ($value['KEY'] != 'ACCOUNT') && ($value['KEY'] != 'JOB') && ($value['KEY'] != 'DATABASE') ) {
+								$edit_lnk = '/edit/'.$value['TYPE'].'/?'.strtolower($value['KEY']).'='.$value['RESULT'].'&user='.$value['USER'];
+							}
+						?>
+						<b>
+							<?php
+								if (($_SESSION['userContext'] === 'admin') && ($_SESSION['user'] !== 'admin') && ($value['USER'] === 'admin') && ($_SESSION['POLICY_SYSTEM_PROTECTED_ADMIN'] === 'yes')) {
+									echo $value['RESULT'];
+								} else {
+									if ($value['USER'] == $_SESSION['user']) {
+										$href = $edit_lnk.'&token='.$_SESSION['token'];
+									} else {
+										$href = '/login/?loginas='.$value['USER'].'&token='.$_SESSION['token'].'&edit_link='.urlencode($edit_lnk);
+									}
+									echo '<a href="' . $href . '">' . $value['RESULT'] . '</a>';
+								}
+							?>
+						</b>
+					</div>
+					<div class="clearfix l-unit__stat-col--left u-text-right compact-3">
+						<div class="l-unit-toolbar__col l-unit-toolbar__col--right u-noselect">
+							<div class="actions-panel clearfix">
+								&nbsp;
+							</div>
 						</div>
 					</div>
+					<div class="clearfix l-unit__stat-col--left u-text-center"><?= translate_date($value["DATE"]) ?></div>
+					<div class="clearfix l-unit__stat-col--left u-text-center">
+						<b>
+							<a href="/search/?q=<?=htmlentities($_GET['q']); ?>&u=<?=$value['USER']; ?>&token=<?=$_SESSION['token']?>"><?=$value['USER']; ?></a>
+							<?php if (!($_SESSION['POLICY_SYSTEM_HIDE_ADMIN'] === 'yes' && $value['USER'] !== 'admin') && ($_SESSION['userContext'] === 'admin')) { ?>
+								<a href="/login/?loginas=<?=$value['USER']?>&token=<?=$_SESSION['token']?>" title="<?= _("Log in as") ?> <?=$value['USER']?>">
+									<i class="fas fa-right-to-bracket icon-green icon-dim"></i>
+								</a>
+							<?php } ?>
+						</b>
+					</div>
+					<div class="clearfix l-unit__stat-col--left u-text-center"><?= _($object) ?></b></div>
 				</div>
-				<div class="clearfix l-unit__stat-col--left u-text-center"><?= translate_date($value["DATE"]) ?></div>
-				<div class="clearfix l-unit__stat-col--left u-text-center">
-					<b>
-						<a href="/search/?q=<?=htmlentities($_GET['q']); ?>&u=<?=$value['USER']; ?>&token=<?=$_SESSION['token']?>"><?=$value['USER']; ?></a>
-						<?php if (!($_SESSION['POLICY_SYSTEM_HIDE_ADMIN'] === 'yes' && $value['USER'] !== 'admin') && ($_SESSION['userContext'] === 'admin')) { ?>
-							<a href="/login/?loginas=<?=$value['USER']?>&token=<?=$_SESSION['token']?>" title="<?= _("Log in as") ?> <?=$value['USER']?>">
-								<i class="fas fa-right-to-bracket icon-green icon-dim"></i>
-							</a>
-						<?php } ?>
-					</b>
-				</div>
-				<div class="clearfix l-unit__stat-col--left u-text-center"><?= _($object) ?></b></div>
 			</div>
-		</div>
-	<?php } ?>
+		<?php } ?>
+	</div>
 </div>
 
 <footer class="app-footer">

f97bbf129ce0a1fb9fe648d4f2095db3a90144c3
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2023-06-07 11:46:11 +0200

    fix interface detection proxmox

--- func/main.sh
+++ func/main.sh
@@ -1027,6 +1027,10 @@ is_int_format_valid() {
 is_interface_format_valid() {
 	# Detect "physical" NICs only (virtual NICs created by Docker, WireGuard etc. are excluded)
 	nic_names="$(ip -d -j link show | jq -r '.[] | if .link_type == "loopback" // .linkinfo.info_kind then empty else .ifname, if .altnames then .altnames[] else empty end end')"
+	# Proxmox return empty value for $physical_nics
+	if [ -z "$nic_names" ]; then
+		nic_names="$(ip -d -j link show | jq -r '.[] | if .link_type == "loopback" then empty else .ifname, if .altnames then .altnames[] else empty end end')"
+	fi
 	if [ -z "$(echo "$nic_names" | grep -x "$1")" ]; then
 		check_result "$E_INVALID" "invalid interface format :: $1"
 	fi

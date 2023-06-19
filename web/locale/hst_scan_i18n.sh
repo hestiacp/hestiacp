392fce5c6f5800751ca26170149734db5de36200
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2023-06-19 21:09:41 +0200

    Sync with crowdin

--- web/locale/hst_scan_i18n.sh
+++ web/locale/hst_scan_i18n.sh
@@ -6,37 +6,32 @@ if [ ! -e /usr/bin/xgettext ]; then
 	exit 3
 fi
 
-delete=0
-
-echo "[ * ] Remove old hestiacp.pot and generate new one"
-rm hestiacp.pot
+echo "[ * ] Move hestiacp.pot to Move hestiacp.pot.old"
+mv hestiacp.pot hestiacp.pot.old
 echo "" > hestiacp.pot
+
+echo "[ * ] Search *.php *.html and *.sh for php based gettext functions"
 find ../.. \( -name '*.php' -o -name '*.html' -o -name '*.sh' \) | xgettext --output=hestiacp.pot --language=PHP --join-existing -f -
 OLDIFS=$IFS
 IFS=$'\n'
 # Scan the description string for list updates page
-for string in $(awk -F'DESCR=' '/data=".+ DESCR=[^"]/ {print $2}' $HESTIA/bin/v-list-sys-hestia-updates | cut -d\' -f2); do
+for string in $(awk -F'DESCR=' '/data=".+ DESCR=[^"]/ {print $2}' ../../bin/v-list-sys-hestia-updates | cut -d\' -f2); do
 	if [ -z "$(grep "\"$string\"" hestiacp.pot)" ]; then
-		echo -e "\n#: ../../bin/v-list-sys-hestia-updates:"$(grep -n "$string" $HESTIA/bin/v-list-sys-hestia-updates | cut -d: -f1)"\nmsgid \"$string\"\nmsgstr \"\"" >> hestiacp.pot
+		echo -e "\n#: ../../bin/v-list-sys-hestia-updates:"$(grep -n "$string" ../../bin/v-list-sys-hestia-updates | cut -d: -f1)"\nmsgid \"$string\"\nmsgstr \"\"" >> hestiacp.pot
 	fi
 done
 # Scan the description string for list server page
-for string in $(awk -F'SYSTEM=' '/data=".+ SYSTEM=[^"]/ {print $2}' $HESTIA/bin/v-list-sys-services | cut -d\' -f2); do
+for string in $(awk -F'SYSTEM=' '/data=".+ SYSTEM=[^"]/ {print $2}' ../../bin/v-list-sys-services | cut -d\' -f2); do
 	if [ -z "$(grep "\"$string\"" hestiacp.pot)" ]; then
-		echo -e "\n#: ../../bin/v-list-sys-services:"$(grep -n "$string" $HESTIA/bin/v-list-sys-services | cut -d: -f1)"\nmsgid \"$string\"\nmsgstr \"\"" >> hestiacp.pot
+		echo -e "\n#: ../../bin/v-list-sys-services:"$(grep -n "$string" ../../bin/v-list-sys-services | cut -d: -f1)"\nmsgid \"$string\"\nmsgstr \"\"" >> hestiacp.pot
 	fi
 done
 IFS=$OLDIFS
 
-echo "[ * ] Scan language folders"
-languages=$(ls -d $HESTIA/web/locale/*/ | awk -F'/' '{print $(NF-1)}')
-echo "[ * ] Update hestiacp.pot with new files"
-for lang in $languages; do
-	if [ -e "$HESTIA/web/locale/$lang/LC_MESSAGES/hestiacp.po" ]; then
-		echo "[ * ] Update $lang "
-		mv $HESTIA/web/locale/$lang/LC_MESSAGES/hestiacp.po $HESTIA/web/locale/$lang/LC_MESSAGES/hestiacp.po.bak
-		msgmerge --verbose "$HESTIA/web/locale/$lang/LC_MESSAGES/hestiacp.po.bak" "$HESTIA/web/locale/hestiacp.pot" > $HESTIA/web/locale/$lang/LC_MESSAGES/hestiacp.po
-		rm $HESTIA/web/locale/$lang/LC_MESSAGES/hestiacp.po.bak
-	fi
-done
-echo "[ ! ] Update complete"
+# Prevent only date change become a commit
+if [ $(diff hestiacp.pot hestiacp.pot.old | wc -l) != 2 ]; then
+	rm hestiacp.pot
+	mv hestiacp.pot.old hestiacp.pot
+else
+	rm hestiacp.pot.old
+fi

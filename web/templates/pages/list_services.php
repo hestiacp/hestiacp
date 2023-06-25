f4592ce0acd598d3a7f0792cfaa0f76556bc91de
Alec Rust <me@alecrust.com>
2023-06-25 14:20:10 +0100

    Show heading on list views on mobile

--- web/templates/pages/list_services.php
+++ web/templates/pages/list_services.php
@@ -89,6 +89,8 @@
 		</div>
 	</div>
 
+	<h1 class="u-text-center u-hide-desktop u-pr30 u-mb20 u-pl30"><?= _("Services") ?></h1>
+
 	<div class="units-table js-units-container">
 		<div class="units-table-header">
 			<div class="units-table-cell">

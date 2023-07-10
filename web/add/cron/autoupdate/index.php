ff8a10b1fc03b443e55ac39f1a7148cc8f7b9eae
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2023-07-11 00:18:02 +0200

    Limit v-change-user-ns only to admin user  (#3761)
    * Limit v-change-ns only to admin users
    
    * Fix bug with edit user and viewing / deleting logs
    
    * Allow admin always change state

--- web/add/cron/autoupdate/index.php
+++ web/add/cron/autoupdate/index.php
@@ -5,7 +5,10 @@ include $_SERVER["DOCUMENT_ROOT"] . "/inc/main.php";
 // Check token
 verify_csrf($_GET);
 
-if ($_SESSION["userContext"] === "admin" && $_SESSION["POLICY_SYSTEM_HIDE_SERVICES"] == "no") {
+if (
+	($_SESSION["userContext"] === "admin" && $_SESSION["POLICY_SYSTEM_HIDE_SERVICES"] == "no") ||
+	$_SESSION["user"] == "admin"
+) {
 	exec(HESTIA_CMD . "v-add-cron-hestia-autoupdate", $output, $return_var);
 	unset($output);
 }

40471c6c77aa2738d174516e393f8705ad1b8cc2
Kristan Kenney <git@kristankenney.ca>
2023-07-24 23:09:24 -0300

    Add hint class to Load Average details label

--- web/templates/pages/list_services.php
+++ web/templates/pages/list_services.php
@@ -74,7 +74,7 @@
 					</span>
 				</li>
 				<li class="server-summary-item">
-					<span class="server-summary-list-label"><?= _("Load Average") ?> (1m / 5m / 15m)</span>
+					<span class="server-summary-list-label"><?= _("Load Average") ?> <span class="hint">(1m / 5m / 15m)</span></span>
 					<span class="server-summary-list-value">
 						<?= $sys["sysinfo"]["LOADAVERAGE"] ?>
 					</span>

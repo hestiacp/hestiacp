7a5b95e82a56aa34438efaa4da52bed568fa3f0d
Jakob Bouchard <jakob@jakobbouchard.dev>
2023-07-26 11:30:04 -0400

    Make terminal accessible to regular users

--- web/list/terminal/index.php
+++ web/list/terminal/index.php
@@ -5,12 +5,6 @@ $TAB = "TERMINAL";
 // Main include
 include $_SERVER["DOCUMENT_ROOT"] . "/inc/main.php";
 
-// Check user
-if ($_SESSION["userContext"] != "admin") {
-	header("Location: /list/user");
-	exit();
-}
-
 // Render page
 render_page($user, $TAB, "list_terminal");
 

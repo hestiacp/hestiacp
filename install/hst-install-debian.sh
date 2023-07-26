b7808941ba2d8db6355d3e508f7630e23bc38c47
Alexandros Ioannides <alex@ioannides.net>
2023-07-26 15:40:10 +0300

    Change default PHP-FPM version to 8.2 (#3861)
    * Change default php-fpm version to 8.2 for Debian
    
    * Change default php-fpm version to 8.2 for Ubuntu

--- install/hst-install-debian.sh
+++ install/hst-install-debian.sh
@@ -34,7 +34,7 @@ VERBOSE='no'
 HESTIA_INSTALL_VER='1.8.4~beta'
 # Dependencies
 multiphp_v=("5.6" "7.0" "7.1" "7.2" "7.3" "7.4" "8.0" "8.1" "8.2")
-fpm_v="8.1"
+fpm_v="8.2"
 mariadb_v="10.11"
 
 # Defining software pack for all distros

bb90f45c3809a6fd6c636f9034fcee19f456a9b2
Jakob Bouchard <jakob@jakobbouchard.dev>
2023-07-25 23:57:13 -0400

    Remove some messages

--- web/js/src/webTerminal.js
+++ web/js/src/webTerminal.js
@@ -7,11 +7,9 @@ export default async function initWebTerminal() {
 	const Terminal = await loadXterm();
 	const terminal = new Terminal();
 	terminal.open(container);
-	terminal.write('Connecting to server...');
 
 	const socket = new WebSocket(`wss://${window.location.host}/_shell/`);
 	socket.addEventListener('open', (_) => {
-		terminal.reset();
 		terminal.onData((data) => socket.send(data));
 		socket.addEventListener('message', (evt) => terminal.write(evt.data));
 	});

198a0f078e0080deaa0bcfd971dea46fae2b3e3d
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2023-07-30 14:39:37 +0200

    Add ROLE to sessions
    And use it in the checks

--- web/inc/main.php
+++ web/inc/main.php
@@ -108,6 +108,7 @@ if (isset($_SESSION["user"])) {
 	$data = json_decode(implode("", $output), true);
 	unset($output, $return_var);
 	$_SESSION["login_shell"] = $data[$username]["SHELL"];
+	$_SESSION["role"] = $data[$username]["ROLE"];
 	unset($data, $username);
 }
 

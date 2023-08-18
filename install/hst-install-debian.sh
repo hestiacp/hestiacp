5a38304cb1d33cff1c3c49f8b15f0d2163ad7db3
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2023-08-18 14:46:39 +0200

    It was missing...

--- install/hst-install-debian.sh
+++ install/hst-install-debian.sh
@@ -111,6 +111,19 @@ check_result() {
 	fi
 }
 
+# Source conf in installer
+source_conf() {
+	while IFS='= ' read -r lhs rhs; do
+		if [[ ! $lhs =~ ^\ *# && -n $lhs ]]; then
+			rhs="${rhs%%^\#*}" # Del in line right comments
+			rhs="${rhs%%*( )}" # Del trailing spaces
+			rhs="${rhs%\'*}"   # Del opening string quotes
+			rhs="${rhs#\'*}"   # Del closing string quotes
+			declare -g $lhs="$rhs"
+		fi
+	done < $1
+}
+
 # Defining function to set default value
 set_default_value() {
 	eval variable=\$$1

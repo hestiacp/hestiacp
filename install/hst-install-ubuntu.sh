478e7e9122eb4fcb60dcce68c9589f948365d645
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2023-10-18 23:59:37 +0200

    Make changes for new install

--- install/hst-install-ubuntu.sh
+++ install/hst-install-ubuntu.sh
@@ -1025,6 +1025,9 @@ rm -f /usr/sbin/policy-rc.d
 
 echo "[ * ] Configuring system settings..."
 
+# Create user for php-fpm configs
+/usr/sbin/useradd "hestimail" -c "$email" --no-create-home
+
 # Enable SFTP subsystem for SSH
 sftp_subsys_enabled=$(grep -iE "^#?.*subsystem.+(sftp )?sftp-server" /etc/ssh/sshd_config)
 if [ -n "$sftp_subsys_enabled" ]; then

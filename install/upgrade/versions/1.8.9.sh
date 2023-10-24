acb766e1db53de70534524b3fbc2270689112630
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2023-10-24 10:32:56 +0200

    Limit permissions dummy.conf and www.conf prevent permission escalation  (#4095)
    * Apply changes to default config
    
    If users want less stricter system they can undo it
    
    * Make changes for new install
    
    * Fix few sed commands
    
    * And CLI as well
    
    * Update script
    
    
    * Replace dummy.conf with unix socket 
    
    Use correct file
    
    Add comment on changes

--- install/upgrade/versions/1.8.9.sh
+++ install/upgrade/versions/1.8.9.sh
@@ -33,3 +33,18 @@ if [ -f /etc/nginx/nginx.conf ]; then
 	echo "[ * ] Mitigate HTTP/2 Rapid Reset Attack via Nginx CVE CVE-2023-44487"
 	sed -i -E 's/(.*keepalive_requests\s{1,})10000;/\11000;/' /etc/nginx/nginx.conf /usr/local/hestia/nginx/conf/nginx.conf
 fi
+
+# Fix security issue wit FPM pools
+if [ -z "$(grep ^hestiamail: /etc/passwd)" ]; then
+	echo "[ * ] Limit permissions www.conf and dummy.conf"
+	/usr/sbin/useradd "hestiamail" -c "$email" --no-create-home
+
+	sed -i "s/user = www-data/user = hestiamail/g" /etc/php/*/fpm/pool.d/www.conf
+
+	php_versions=$($BIN/v-list-sys-php plain)
+	# Substitute php-fpm service name formats
+	for version in $php_versions; do
+		cp -f $HESTIA_INSTALL_DIR/php-fpm/dummy.conf /etc/php/$version/fpm/pool.d/
+		sed -i "s/%backend_version%/$version/g" /etc/php/$version/fpm/pool.d/dummy.conf
+	done
+fi

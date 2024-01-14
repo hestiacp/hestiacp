527f2bd4c21d4fd8ccbea347b866590ed1ed7d41
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2024-01-14 14:23:00 +0100

    Add php8.3 to web ui (#4187)
    * Add 8.3 in edit server interface
    
    * Fix Filegator config not replaced
    
    upgrade_config_set_value 'UPGRADE_UPDATE_FILEMANAGER_CONFIG' 'true'
    
    instead of upgrade_config_set_value 'UPGRADE_UPDATE_FILEMANAGER_CONFIG' 'yes'
    
    * Update Docs to include PHP8.3
    
    * Make sure www-data can write to /var/cache/nginx/micro/$domain

--- install/upgrade/versions/1.8.12.sh
+++ install/upgrade/versions/1.8.12.sh
@@ -22,3 +22,15 @@ upgrade_config_set_value 'UPGRADE_UPDATE_DNS_TEMPLATES' 'false'
 upgrade_config_set_value 'UPGRADE_UPDATE_MAIL_TEMPLATES' 'false'
 upgrade_config_set_value 'UPGRADE_REBUILD_USERS' 'false'
 upgrade_config_set_value 'UPGRADE_UPDATE_FILEMANAGER_CONFIG' 'true'
+
+# Folder paths
+SM_INSTALL_DIR="/var/lib/snappymail"
+SM_CONFIG_DIR="/etc/snappymail"
+SM_LOG="/var/log/snappymail"
+
+if [ -d "/var/lib/snappymail" ]; then
+	chown hestiamail:hestiamail /var/lib/snappymail
+	chown hestiamail:hestiamail /etc/snappymail
+fi
+
+sed -i "s/disable_functions =.*/disable_functions = pcntl_alarm,pcntl_fork,pcntl_waitpid,pcntl_wait,pcntl_wifexited,pcntl_wifstopped,pcntl_wifsignaled,pcntl_wifcontinued,pcntl_wexitstatus,pcntl_wtermsig,pcntl_wstopsig,pcntl_signal,pcntl_signal_dispatch,pcntl_get_last_error,pcntl_strerror,pcntl_sigprocmask,pcntl_sigwaitinfo,pcntl_sigtimedwait,pcntl_exec,pcntl_getpriority,pcntl_setpriority/g" /etc/php/*/cli/php.ini

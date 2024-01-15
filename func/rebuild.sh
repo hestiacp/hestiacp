9314156a7718b20d70aa40c7da2b96725da4bcaf
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2024-01-15 19:11:53 +0100

    Don't run v-add-user-jail for every user during rebuild

--- func/rebuild.sh
+++ func/rebuild.sh
@@ -126,8 +126,9 @@ rebuild_user_conf() {
 	chown root:root $HOMEDIR/$user/conf
 
 	$BIN/v-add-user-sftp-jail "$user"
-
-	$BIN/v-add-user-ssh-jail "$user"
+	if [ "$SHELL_JAIL_ENABLED" == "yes" ]; then
+		$BIN/v-add-user-ssh-jail "$user"
+	fi
 
 	# Update disk pipe
 	sed -i "/ $user$/d" $HESTIA/data/queue/disk.pipe

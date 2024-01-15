7af9fa5e64e74d9e4b8df4065179fd12e222b686
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2024-01-15 09:54:31 +0100

    Fix issues with manual upgrade script resetting.. (#4105)
    * Fix Nginx + Apache2 to Nginx Only setup script
    
    * Other scripts with the same issue

--- install/upgrade/manual/migrate_ngnix_apache_nginx-php-fpm.sh
+++ install/upgrade/manual/migrate_ngnix_apache_nginx-php-fpm.sh
@@ -34,22 +34,28 @@ fi
 #----------------------------------------------------------#
 
 # Remove apache2 from config
-sed -i "/^WEB_PORT/d" $HESTIA/conf/hestia.conf
-sed -i "/^WEB_SSL/d" $HESTIA/conf/hestia.conf
-sed -i "/^WEB_SSL_PORT/d" $HESTIA/conf/hestia.conf
-sed -i "/^WEB_RGROUPS/d" $HESTIA/conf/hestia.conf
-sed -i "/^WEB_SYSTEM/d" $HESTIA/conf/hestia.conf
+sed -i "/^WEB_PORT/d" $HESTIA/conf/hestia.conf $HESTIA/conf/defaults/hestia.conf
+sed -i "/^WEB_SSL/d" $HESTIA/conf/hestia.conf $HESTIA/conf/defaults/hestia.conf
+sed -i "/^WEB_SSL_PORT/d" $HESTIA/conf/hestia.conf $HESTIA/conf/defaults/hestia.conf
+sed -i "/^WEB_RGROUPS/d" $HESTIA/conf/hestia.conf $HESTIA/conf/defaults/hestia.conf
+sed -i "/^WEB_SYSTEM/d" $HESTIA/conf/hestia.conf $HESTIA/conf/defaults/hestia.conf
 
 # Remove nginx (proxy) from config
-sed -i "/^PROXY_PORT/d" $HESTIA/conf/hestia.conf
-sed -i "/^PROXY_SSL_PORT/d" $HESTIA/conf/hestia.conf
-sed -i "/^PROXY_SYSTEM/d" $HESTIA/conf/hestia.conf
+sed -i "/^PROXY_PORT/d" $HESTIA/conf/hestia.conf $HESTIA/conf/defaults/hestia.conf
+sed -i "/^PROXY_SSL_PORT/d" $HESTIA/conf/hestia.conf $HESTIA/conf/defaults/hestia.conf
+sed -i "/^PROXY_SYSTEM/d" $HESTIA/conf/hestia.conf $HESTIA/conf/defaults/hestia.conf
 
 # Add Nginx settings to config
 echo "WEB_PORT='80'" >> $HESTIA/conf/hestia.conf
 echo "WEB_SSL='openssl'" >> $HESTIA/conf/hestia.conf
 echo "WEB_SSL_PORT='443'" >> $HESTIA/conf/hestia.conf
 echo "WEB_SYSTEM='nginx'" >> $HESTIA/conf/hestia.conf
+
+# Add Nginx settings to config
+echo "WEB_PORT='80'" >> $HESTIA/conf/defaults/hestia.conf
+echo "WEB_SSL='openssl'" >> $HESTIA/conf/defaults/hestia.conf
+echo "WEB_SSL_PORT='443'" >> $HESTIA/conf/defaults/hestia.conf
+echo "WEB_SYSTEM='nginx'" >> $HESTIA/conf/defaults/hestia.conf
 
 rm $HESTIA/conf/defaults/hestia.conf
 cp $HESTIA/conf/hestia.conf $HESTIA/conf/defaults/hestia.conf

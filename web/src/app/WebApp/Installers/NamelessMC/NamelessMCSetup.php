dbe7dea529598ecb1abe0f55d8859c1b5089bad7
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2024-11-18 10:22:22 +0100

    Run NPM format

--- web/src/app/WebApp/Installers/NamelessMC/NamelessMCSetup.php
+++ web/src/app/WebApp/Installers/NamelessMC/NamelessMCSetup.php
@@ -5,49 +5,49 @@ namespace Hestia\WebApp\Installers\NamelessMC;
 use Hestia\System\Util;
 use Hestia\WebApp\Installers\BaseSetup as BaseSetup;
 
-class NamelessMCSetup extends BaseSetup
-{
-    protected $appInfo = [
-        'name' => 'NamelessMC',
-        'group' => 'cms',
-        'enabled' => true,
-        'version' => '2.1.2',
-        'thumbnail' => 'namelessmc.png'
-    ];
+class NamelessMCSetup extends BaseSetup {
+	protected $appInfo = [
+		"name" => "NamelessMC",
+		"group" => "cms",
+		"enabled" => true,
+		"version" => "2.1.2",
+		"thumbnail" => "namelessmc.png",
+	];
 
-    protected $appname = 'namelessmc';
-    protected $config = [
-        'form' => [
-            'protocol' => [
-                'type' => 'select',
-                'options' => ['http','https'],
-                'value' => 'https'
-            ],
+	protected $appname = "namelessmc";
+	protected $config = [
+		"form" => [
+			"protocol" => [
+				"type" => "select",
+				"options" => ["http", "https"],
+				"value" => "https",
+			],
+		],
+		"database" => false,
+		"resources" => [
+			"archive" => [
+				"src" =>
+					"https://github.com/NamelessMC/Nameless/releases/download/v2.1.2/nameless-deps-dist.zip",
+			],
+		],
+		"server" => [
+			"nginx" => [
+				"template" => "namelessmc",
+			],
+			"apache2" => [
+				"template" => "namelessmc",
+			],
+			"php" => [
+				"supported" => ["7.4", "8.0", "8.1"],
+			],
+		],
+	];
 
-        ],
-        'database' => false,
-        'resources' => [
-            'archive'  => [ 'src' => 'https://github.com/NamelessMC/Nameless/releases/download/v2.1.2/nameless-deps-dist.zip' ],
-        ],
-        'server' => [
-            'nginx' => [
-                'template' => 'namelessmc',
-            ],
-            'apache2' => [
-                'template' => 'namelessmc',
-            ],
-            'php' => [
-                'supported' => [ '7.4','8.0','8.1' ],
-            ],
-        ],
-    ];
+	public function install(array $options = null) {
+		parent::install($options);
 
-    public function install(array $options = null)
-    {
-        parent::install($options);
+		$status = 0;
 
-        $status = 0;
-
-        return ($status === 0);
-    }
+		return $status === 0;
+	}
 }

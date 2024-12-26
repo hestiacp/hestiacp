7c4cc159cb3058095a83ab3c7f2004e164d33c4d
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2024-12-26 14:42:17 +0100

    RUn npm format

--- web/src/app/WebApp/Installers/WordPress/WordPressSetup.php
+++ web/src/app/WebApp/Installers/WordPress/WordPressSetup.php
@@ -80,10 +80,10 @@ class WordpressSetup extends BaseSetup {
 		);
 		foreach ($result->raw as $line_num => $line) {
 			if (str_starts_with($line, '$table_prefix =')) {
-				$result->raw[$line_num] =
-					sprintf( "\$table_prefix = %s;\r\n",
-						var_export("wp_".Util::generate_string(5, false)."_",true)
-					);
+				$result->raw[$line_num] = sprintf(
+					"\$table_prefix = %s;\r\n",
+					var_export("wp_" . Util::generate_string(5, false) . "_", true),
+				);
 				continue;
 			}
 			if (!preg_match('/^define\(\s*\'([A-Z_]+)\',([ ]+)/', $line, $match)) {

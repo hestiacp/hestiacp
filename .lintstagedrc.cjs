1249a8fdbeafa395c036db97e307c2f827819111
Alec Rust <me@alecrust.com>
2025-03-06 20:07:06 +0000

    Simplify Biome lint commands

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -4,7 +4,7 @@ module.exports = {
 	// Run Stylelint on CSS files
 	'*.css': 'stylelint --fix --allow-empty-input',
 	// Run Biome on TS, TSX, JS, JSX files
-	'*.{ts,js}?(x)': 'npx @biomejs/biome lint --write --no-errors-on-unmatched',
+	'*.{ts,js}?(x)': 'biome lint --write --no-errors-on-unmatched',
 	// Run Prettier where supported
 	'*': 'prettier --write --ignore-unknown',
 };

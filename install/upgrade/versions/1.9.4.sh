b7b2f3b10cbfd5b727bb8cb835bc236bdfbc6660
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2025-03-10 08:52:50 +0100

    Update sed

--- install/upgrade/versions/1.9.4.sh
+++ install/upgrade/versions/1.9.4.sh
@@ -28,5 +28,5 @@ if grep -q "internal-sftp-server" /etc/ssh/sshd_config; then
 fi
 
 if grep -q "Subsystem sftp /usr/lib/sftp-server-" /etc/ssh/sshd_config; then
-	sed -i 's/Subsystem sftp \/usr\/lib\/sftp-server/Subsystem sftp \/usr\/lib\/sftp-server/' /etc/ssh/sshd_config
+	sed -i 's/Subsystem sftp \/usr\/lib\/sftp-server-.*/Subsystem sftp \/usr\/lib\/sftp-server/' /etc/ssh/sshd_config
 fi

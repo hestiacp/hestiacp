906f49b4636cc42502963e97b96305a7ef042c45
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2025-03-24 08:01:48 +0100

    Fix bug in v-add-sys-sftp-jail when ssh jail is active (#4873)
    * Fix bug in v-add-sys-sftp-jail where -server get appended hen ssh jail is active
    
    * Add update script
    
    * Fix regex
    
    * Fix sed
    
    * Update sed

---
+++ install/upgrade/versions/1.9.4.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Hestia Control Panel upgrade script for target version 1.9.3
+
+#######################################################################################
+#######                      Place additional commands below.                   #######
+#######################################################################################
+####### upgrade_config_set_value only accepts true or false.                    #######
+#######                                                                         #######
+####### Pass through information to the end user in case of a issue or problem  #######
+#######                                                                         #######
+####### Use add_upgrade_message "My message here" to include a message          #######
+####### in the upgrade notification email. Example:                             #######
+#######                                                                         #######
+####### add_upgrade_message "My message here"                                   #######
+#######                                                                         #######
+####### You can use \n within the string to create new lines.                   #######
+#######################################################################################
+
+upgrade_config_set_value 'UPGRADE_UPDATE_WEB_TEMPLATES' 'false'
+upgrade_config_set_value 'UPGRADE_UPDATE_DNS_TEMPLATES' 'false'
+upgrade_config_set_value 'UPGRADE_UPDATE_MAIL_TEMPLATES' 'false'
+upgrade_config_set_value 'UPGRADE_REBUILD_USERS' 'no'
+upgrade_config_set_value 'UPGRADE_UPDATE_FILEMANAGER_CONFIG' 'true'
+
+if grep -q "internal-sftp-server" /etc/ssh/sshd_config; then
+	sed -i 's/Subsystem sftp internal-sftp-.*/Subsystem sftp internal-sftp/' /etc/ssh/sshd_config
+fi
+
+if grep -q "Subsystem sftp /usr/lib/sftp-server-" /etc/ssh/sshd_config; then
+	sed -i 's/Subsystem sftp \/usr\/lib\/sftp-server-.*/Subsystem sftp \/usr\/lib\/sftp-server/' /etc/ssh/sshd_config
+fi

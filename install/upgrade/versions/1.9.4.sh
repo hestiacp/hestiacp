20b2ae10c5ab2d64de12782346c154cdd02015e5
sahsanu <sahsanu@users.noreply.github.com>
2025-07-14 09:36:25 +0200

    Fix: 421 error on all web and mail domains after Apache 2.4.64 update (#5058)
    * Fix: 421 error on all web and mail domains after Apache 2.4.64 update
    
    * Modify 1.9.4.sh script to also update mail templates

--- install/upgrade/versions/1.9.4.sh
+++ install/upgrade/versions/1.9.4.sh
@@ -19,7 +19,7 @@
 
 upgrade_config_set_value 'UPGRADE_UPDATE_WEB_TEMPLATES' 'false'
 upgrade_config_set_value 'UPGRADE_UPDATE_DNS_TEMPLATES' 'false'
-upgrade_config_set_value 'UPGRADE_UPDATE_MAIL_TEMPLATES' 'false'
+upgrade_config_set_value 'UPGRADE_UPDATE_MAIL_TEMPLATES' 'true'
 upgrade_config_set_value 'UPGRADE_REBUILD_USERS' 'no'
 upgrade_config_set_value 'UPGRADE_UPDATE_FILEMANAGER_CONFIG' 'true'
 

7cd9310c2a9a386f9f4715aa378c327c0c547690
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2025-07-21 09:44:57 +0200

    Prepare for 1.9.4 release

--- install/upgrade/versions/1.9.4.sh
+++ install/upgrade/versions/1.9.4.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Hestia Control Panel upgrade script for target version 1.9.3
+# Hestia Control Panel upgrade script for target version 1.9.4
 
 #######################################################################################
 #######                      Place additional commands below.                   #######

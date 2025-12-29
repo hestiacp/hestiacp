1bd9a0338624d7c2c4cba6224095b4fcb7f73c56
zapper88144 <zapper8814@hotmail.com>
2025-12-29 06:38:01 +0000

    style(add_mail): reflow long strings and attributes to satisfy PHPCS line-length rules

--- web/templates/pages/add_mail.php
+++ web/templates/pages/add_mail.php
@@ -7,9 +7,9 @@
             </a>
         </div>
         <div class="toolbar-buttons">
-                <button type="submit" class="button" form="main-form">
-                    <i class="fas fa-floppy-disk icon-purple"></i><?= _("Save") ?>
-                </button>
+            <button type="submit" class="button" form="main-form">
+                <i class="fas fa-floppy-disk icon-purple"></i><?= _("Save") ?>
+            </button>
         </div>
     </div>
 </div>
@@ -23,8 +23,7 @@
         }"
         id="main-form"
         name="v_add_mail"
-        method="post"
-    >
+        method="post">
         <input type="hidden" name="token" value="<?= $_SESSION["token"] ?>">
         <input type="hidden" name="ok" value="Add">
 
@@ -34,45 +33,79 @@
             <?php if ($_SESSION["role"] == "admin" && $accept !== "true") { ?>
                 <div class="alert alert-danger" role="alert">
                     <i class="fas fa-exclamation"></i>
-                    <p><?= htmlify_trans(sprintf(_("It is strongly advised to {create a standard user account} before adding %s to the server due to the increased privileges the admin account possesses and potential security risks."), _('a mail domain')), '</a>', '<a href="/add/user/">'); ?></p>
+                    <?php
+                    $warning = sprintf(
+                        _(
+                            "It is strongly advised to {create a standard user account} before adding %s to the server "
+                                . "due to the increased privileges the admin account possesses "
+                                . "and potential security risks."
+                        ),
+                        _('a mail domain')
+                    );
+                    ?>
+                    <p>
+                        <?= htmlify_trans($warning, '</a>', '<a href="/add/user/">'); ?>
+                    </p>
                 </div>
             <?php } ?>
             <?php if ($_SESSION["role"] == "admin" && empty($accept)) { ?>
                 <div class="u-side-by-side u-mt20">
                     <a href="/add/user/" class="button u-width-full u-mr10"><?= _("Add User") ?></a>
-                    <a href="/add/mail/?accept=true" class="button button-danger u-width-full u-ml10"><?= _("Continue") ?></a>
+                    <a href="/add/mail/?accept=true" class="button button-danger u-width-full u-ml10">
+                        <?= _("Continue") ?>
+                    </a>
                 </div>
             <?php } ?>
             <?php if (($_SESSION["role"] == "admin" && $accept === "true") || $_SESSION["role"] !== "admin") { ?>
                 <div class="u-mb20">
                     <label for="v_domain" class="form-label"><?= _("Domain") ?></label>
-                    <input type="text" class="form-control" name="v_domain" id="v_domain" value="<?= htmlentities(trim($v_domain, "'")) ?>" required>
+                    <input
+                        type="text"
+                        class="form-control"
+                        name="v_domain"
+                        id="v_domain"
+                        value="<?= htmlentities(trim($v_domain, "'")) ?>"
+                        required>
                 </div>
                 <?php if ($_SESSION["WEBMAIL_SYSTEM"]) { ?>
                     <div class="u-mb20">
                         <label for="v_webmail" class="form-label"><?= _("Webmail Client") ?></label>
                         <select class="form-select" name="v_webmail" id="v_webmail" tabindex="6">
-                            <?php foreach ($webmail_clients as $client) {
-                                echo "\t\t\t\t\t\t\t<option value=\"" . htmlentities($client) . "\"";
-                                if (( $v_webmail == $client )) {
-                                    echo ' selected' ;
-                                }
-                                echo ">" . htmlentities(ucfirst($client)) . "</option>\n";
-                            }
-                            ?>
-                            <option value="" <?php if (empty($v_webmail) || $v_webmail == 'disabled') { echo "selected"; }?>><?= _("Disabled") ?></option>
+                            <?php foreach ($webmail_clients as $client) : ?>
+                                <option
+                                    value="<?= htmlentities($client) ?>"
+                                    <?= ($v_webmail == $client) ? 'selected' : '' ?>>
+                                    <?= htmlentities(ucfirst($client)) ?>
+                                </option>
+                            <?php endforeach; ?>
+                            <option value=""
+                                <?php if (empty($v_webmail) || $v_webmail == 'disabled') : ?>
+                                selected
+                                <?php endif; ?>>
+                                <?= _("Disabled") ?>
+                            </option>
                         </select>
                     </div>
                 <?php } ?>
                 <?php if (!empty($_SESSION["ANTISPAM_SYSTEM"])) { ?>
                     <div class="form-check u-mb10">
-                        <input class="form-check-input" type="checkbox" name="v_antispam" id="v_antispam" <?= (empty($v_antispam) || $v_antispam == 'yes') ? 'checked' : '' ?>>
+                        <input
+                            class="form-check-input"
+                            type="checkbox"
+                            name="v_antispam"
+                            id="v_antispam"
+                            <?= (empty($v_antispam) || $v_antispam == "yes") ? "checked" : "" ?>>
                         <label for="v_antispam">
                             <?= _("Spam Filter") ?>
                         </label>
                     </div>
                     <div class="form-check u-mb10">
-                        <input class="form-check-input" type="checkbox" name="v_reject" id="v_reject" <?= (empty($v_reject) || $v_reject == 'yes') ? 'checked' : '' ?>>
+                        <input
+                            class="form-check-input"
+                            type="checkbox"
+                            name="v_reject"
+                            id="v_reject"
+                            <?= (empty($v_reject) || $v_reject == 'yes') ? 'checked' : '' ?>>
                         <label for="v_reject">
                             <?= _("Reject Spam") ?>
                         </label>
@@ -80,20 +113,35 @@
                 <?php } ?>
                 <?php if (!empty($_SESSION['ANTIVIRUS_SYSTEM'])) { ?>
                     <div class="form-check u-mb10">
-                        <input class="form-check-input" type="checkbox" name="v_antivirus" id="v_antivirus" <?= (empty($v_antivirus) || $v_antivirus == 'yes') ? 'checked' : '' ?>>
+                        <input
+                            class="form-check-input"
+                            type="checkbox"
+                            name="v_antivirus"
+                            id="v_antivirus"
+                            <?= (empty($v_antivirus) || $v_antivirus == 'yes') ? 'checked' : '' ?>>
                         <label for="v_antivirus">
                             <?= _("Anti-Virus") ?>
                         </label>
                     </div>
                 <?php } ?>
                 <div class="form-check u-mb10">
-                    <input class="form-check-input" type="checkbox" name="v_dkim" id="v_dkim" <?= (isset($v_dkim) && $v_dkim == 'yes') ? 'checked' : '' ?>>
+                    <input
+                        class="form-check-input"
+                        type="checkbox"
+                        name="v_dkim"
+                        id="v_dkim"
+                        <?= (isset($v_dkim) && $v_dkim == 'yes') ? 'checked' : '' ?>>
                     <label for="v_dkim">
                         <?= _("DKIM Support") ?>
                     </label>
                 </div>
                 <div class="form-check u-mb10">
-                    <input x-model="hasSmtpRelay" class="form-check-input" type="checkbox" name="v_smtp_relay" id="v_smtp_relay">
+                    <input
+                        x-model="hasSmtpRelay"
+                        class="form-check-input"
+                        type="checkbox"
+                        name="v_smtp_relay"
+                        id="v_smtp_relay">
                     <label for="v_smtp_relay">
                         <?= _("SMTP Relay") ?>
                     </label>
@@ -101,15 +149,33 @@
                 <div x-cloak x-show="hasSmtpRelay" id="smtp_relay_table" class="u-pl30">
                     <div class="u-mb10">
                         <label for="v_smtp_relay_host" class="form-label"><?= _("Host") ?></label>
-                        <input type="text" class="form-control" name="v_smtp_relay_host" id="v_smtp_relay_host" value="<?= htmlentities(trim($v_smtp_relay_host, "'")) ?>">
+                        <input
+                            type="text"
+                            class="form-control"
+                            name="v_smtp_relay_host"
+                            id="v_smtp_relay_host"
+                            value="<?= htmlentities(trim($v_smtp_relay_host, "'")) ?>">
+
                     </div>
                     <div class="u-mb10">
                         <label for="v_smtp_relay_port" class="form-label"><?= _("Port") ?></label>
-                        <input type="text" class="form-control" name="v_smtp_relay_port" id="v_smtp_relay_port" value="<?= htmlentities(trim($v_smtp_relay_port, "'")) ?>">
+                        <input
+                            type="text"
+                            class="form-control"
+                            name="v_smtp_relay_port"
+                            id="v_smtp_relay_port"
+                            value="<?= htmlentities(trim($v_smtp_relay_port, "'")) ?>">
+
                     </div>
                     <div class="u-mb10">
                         <label for="v_smtp_relay_user" class="form-label"><?= _("Username") ?></label>
-                        <input type="text" class="form-control" name="v_smtp_relay_user" id="v_smtp_relay_user" value="<?= htmlentities(trim($v_smtp_relay_user, "'")) ?>">
+                        <input
+                            type="text"
+                            class="form-control"
+                            name="v_smtp_relay_user"
+                            id="v_smtp_relay_user"
+                            value="<?= htmlentities(trim($v_smtp_relay_user, "'")) ?>">
+
                     </div>
                     <div class="u-mb10">
                         <label for="v_smtp_relay_pass" class="form-label"><?= _("Password") ?></label>

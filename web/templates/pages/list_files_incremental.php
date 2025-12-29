b0a7ad268a1491ee9318d62f4ff96672ba7a3f9a
zapper88144 <zapper8814@hotmail.com>
2025-12-29 15:36:27 +0000

    style(list_files_incremental): precompute hrefs, fix missing input tag, and split long attributes

--- web/templates/pages/list_files_incremental.php
+++ web/templates/pages/list_files_incremental.php
@@ -2,44 +2,53 @@
 <div class="toolbar">
     <div class="toolbar-inner">
         <div class="toolbar-buttons">
-            <?php if ($read_only !== "true") { ?>
-                <?php if (str_starts_with($files[0]['path'], '/home/' . $user_plain) && $files[0]['path'] != '/home/' . $user_plain) {
-                    ?>
-            <a class="button button-secondary" id="btn-back" href="/list/backup/incremental/?snapshot=<?= htmlentities($_GET["snapshot"]) ?>&browse=yes&folder=<?=htmlentities($files[0]['path'])?>/../&token=<?=$_SESSION["token"]?>"><i class="fas fa-arrow-left icon-blue"></i><?= _("Back") ?></a>
-                <?php } else {
-                    ?>
-            <a class="button button-secondary" id="btn-back" href="/list/backup/incremental/?token=<?=$_SESSION["token"]?>"><i class="fas fa-arrow-left icon-blue"></i><?= _("Back") ?></a>
-                    <?php
+            <?php if ($read_only !== "true") {
+                if (str_starts_with($files[0]['path'], '/home/' . $user_plain) && $files[0]['path'] != '/home/' . $user_plain) {
+                    $back_href = '/list/backup/incremental/?snapshot=' . htmlentities($_GET['snapshot'])
+                        . '&browse=yes&folder=' . htmlentities($files[0]['path'])
+                        . '/../&token=' . $_SESSION['token'];
+                } else {
+                    $back_href = '/list/backup/incremental/?token=' . $_SESSION['token'];
                 }
                 ?>
+                <a class="button button-secondary" id="btn-back" href="<?= $back_href ?>">
+                    <i class="fas fa-arrow-left icon-blue"></i><?= _("Back") ?>
+                </a>
             <?php } ?>
         </div>
         <div class="toolbar-right">
-                    <?php if ($read_only !== "true") { ?>
-                    <form x-data x-bind="BulkEdit" action="/bulk/restore/" method="post">
-                        <input type="hidden" name="backup" value="<?=htmlentities($_GET["snapshot"]);?>"
-                        <input type="hidden" name="token" value="<?= $_SESSION["token"] ?>">
-                        <select class="form-select" name="action">
-                            <option value=""><?= _("Apply to selected") ?></option>
-                            <option value="delete"><?= _("Restore Files") ?></option>
-                        </select>
-                        <button type="submit" class="toolbar-input-submit" title="<?= _("Apply to selected") ?>">
-                            <i class="fas fa-arrow-right"></i>
-                        </button>
-                    </form>
-                    <?php } ?>
-                <div class="toolbar-search">
-                    <form action="/search/" method="get">
-                        <input type="hidden" name="token" value="<?= $_SESSION["token"] ?>">
-                        <input type="search" class="form-control js-search-input" name="q" value="<?= isset($_POST['q']) ? htmlspecialchars($_POST['q']) : '' ?>" title="<?= _("Search") ?>">
-                        <button type="submit" class="toolbar-input-submit" title="<?= _("Search") ?>">
-                            <i class="fas fa-magnifying-glass"></i>
-                        </button>
-                    </form>
-                </div>
+            <?php if ($read_only !== "true") { ?>
+                <form x-data x-bind="BulkEdit" action="/bulk/restore/" method="post">
+                    <?php $snapshot = htmlentities($_GET['snapshot']); ?>
+                    <input type="hidden" name="backup" value="<?= $snapshot ?>">
+                    <input type="hidden" name="token" value="<?= $_SESSION['token'] ?>">
+                    <select class="form-select" name="action">
+                        <option value=""><?= _("Apply to selected") ?></option>
+                        <option value="delete"><?= _("Restore Files") ?></option>
+                    </select>
+                    <button type="submit" class="toolbar-input-submit" title="<?= _("Apply to selected") ?>">
+                        <i class="fas fa-arrow-right"></i>
+                    </button>
+                </form>
+            <?php } ?>
+            <div class="toolbar-search">
+                <form action="/search/" method="get">
+                    <?php $search_value = isset($_POST['q']) ? htmlspecialchars($_POST['q']) : ''; ?>
+                    <input type="hidden" name="token" value="<?= $_SESSION['token'] ?>">
+                    <input
+                        type="search"
+                        class="form-control js-search-input"
+                        name="q"
+                        value="<?= $search_value ?>"
+                        title="<?= _("Search") ?>">
+                    <button type="submit" class="toolbar-input-submit" title="<?= _("Search") ?>">
+                        <i class="fas fa-magnifying-glass"></i>
+                    </button>
+                </form>
             </div>
         </div>
     </div>
+</div>
 <!-- End toolbar -->
 
 <div class="container">
@@ -63,7 +72,12 @@
                         <div class="units-table-cell">
                         </div>
                         <div class="units-table-cell units-table-heading-cell u-text-bold">
-                                <b><a href="/list/backup/incremental/?snapshot=<?= htmlentities($_GET["snapshot"]) ?>&browse=yes&folder=<?=htmlentities($files[0]['path'])?>/../&token=<?=$_SESSION["token"]?>"><i class="fas fa-folder icon-dim u-mr5"></i>..</a></b>
+                            <?php
+                            $parent_href = '/list/backup/incremental/?snapshot=' . $snapshot
+                                . '&browse=yes&folder=' . htmlentities($files[0]['path'])
+                                . '/../&token=' . $_SESSION['token'];
+                            ?>
+                            <b><a href="<?= $parent_href ?>"><i class="fas fa-folder icon-dim u-mr5"></i>..</a></b>
                         </div>
                         <div class="units-table-cell">
                         </div>
@@ -82,43 +96,60 @@
                     <div class="units-table-row js-unit">
                         <div class="units-table-cell">
                             <div>
-                                <input id="check<?= $i ?>" class="ch-toggle" type="checkbox" name="files[]" value="<?=htmlentities($file['path'])?>">
+                                <input
+                                    id="check<?= $i ?>"
+                                    class="ch-toggle"
+                                    type="checkbox"
+                                    name="files[]"
+                                    value="<?= htmlentities($file['path']) ?>">
                             </div>
                         </div>
                         <div class="units-table-cell">
                             <div>
-                            <?php if ($file['type'] == 'dir') {
-                                if (str_starts_with($file['path'], '/home/' . $user_plain . '/conf')) {
-                                    ?>
-                                <b><i class="fas fa-folder icon-dim u-mr5"></i><?=$file['name']?></b>
-                                    <?php
+                                <?php if ($file['type'] == 'dir') {
+                                    if (str_starts_with($file['path'], '/home/' . $user_plain . '/conf')) {
+                                        ?>
+                                        <b><i class="fas fa-folder icon-dim u-mr5"></i><?= $file['name'] ?></b>
+                                        <?php
+                                    } else {
+                                        ?>
+                                        <?php
+                                        $dir_href = '/list/backup/incremental/?snapshot=' . $snapshot
+                                            . '&browse=yes&folder=' . htmlentities($file['path'])
+                                            . '&token=' . $_SESSION['token'];
+                                        ?>
+                                        <b><a href="<?= $dir_href ?>"><i class="fas fa-folder icon-dim u-mr5"></i><?= $file['name'] ?></a></b>
+                                        <?php
+                                    }
                                 } else {
                                     ?>
-                                        <b><a href="/list/backup/incremental/?snapshot=<?= htmlentities($_GET["snapshot"]) ?>&browse=yes&folder=<?=htmlentities($file['path'])?>&token=<?=$_SESSION["token"]?>"><i class="fas fa-folder icon-dim u-mr5"></i><?=$file['name']?></a></b>
+                                    <b><i class="fas fa-file icon-dim u-mr5"></i><?= $file['name'] ?></b>
                                     <?php
-                                }
-                            } else {
-                                ?>
-                                        <b><i class="fas fa-file icon-dim u-mr5"></i><?=$file['name']?></b>
-                                    <?php
-                            }?>
+                                } ?>
                             </div>
                         </div>
                         <div class="units-table-cell">
-                            <a href="/schedule/restore/incremental/?snapshot=<?= htmlentities($_GET["snapshot"]) ?>&type=file&object=<?=htmlentities($file['path'])?>&token=<?= $_SESSION["token"] ?>" title="<?= _("Restore") ?>">
+                            <?php
+                            $restore_href = '/schedule/restore/incremental/?snapshot=' . $snapshot
+                                . '&type=file&object=' . htmlentities($file['path'])
+                                . '&token=' . $_SESSION['token'];
+                            ?>
+                            <a
+                                href="<?= $restore_href ?>"
+                                title="<?= _("Restore") ?>">
                                 <i class="fas fa-arrow-rotate-left icon-green icon-dim u-mr5"></i>
                             </a>
                         </div>
                         <div class="units-table-cell">
                             <span class="u-hide-desktop u-text-bold"><?= _("Type") ?>:</span>
                             <span class="u-text-bold">
-                                <?=getTransByType($file['type']);?>
+                                <?= getTransByType($file['type']); ?>
                             </span>
                         </div>
                         <div class="units-table-cell">
                             <span class="u-hide-desktop u-text-bold"><?= _("Date / Time") ?>:</span>
                             <span class="u-text-bold">
-                                <?=convert_datetime($file['ctime'], 'Y-m-d  H:i:s');?>
+                                <?= convert_datetime($file['ctime'], 'Y-m-d  H:i:s'); ?>
                             </span>
                         </div>
                     </div>

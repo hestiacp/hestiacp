b45cbea9801f33e7041020098a6e3482832c922f
zapper88144 <zapper8814@hotmail.com>
2025-12-29 16:17:27 +0000

    chore(style): run phpcbf on list_dns_rec.php to fix spacing/indentation

--- web/templates/pages/list_dns_rec.php
+++ web/templates/pages/list_dns_rec.php
@@ -27,29 +27,35 @@
                         <?= $label ?> <i class="fas fa-arrow-down-a-z"></i>
                     </span>
                 </button>
+                <?php $sort_date_active = (isset($_SESSION['userSortOrder']) && $_SESSION['userSortOrder'] === 'date') ? 'active' : ''; ?>
                 <ul class="toolbar-sorting-menu js-sorting-menu u-hidden">
                     <li data-entity="sort-date" data-sort-as-int="1">
-                        <span class="name <?php if ($_SESSION['userSortOrder'] === 'date') {
-                            echo 'active';
-                                          } ?>"><?= _("Date") ?> <i class="fas fa-arrow-down-a-z"></i></span><span class="up"><i class="fas fa-arrow-up-a-z"></i></span>
+                        <span class="name <?= $sort_date_active ?>"><?= _("Date") ?> <i class="fas fa-arrow-down-a-z"></i></span>
+                        <span class="up"><i class="fas fa-arrow-up-a-z"></i></span>
                     </li>
                     <li data-entity="sort-value">
-                        <span class="name"><?= _("IP or Value") ?> <i class="fas fa-arrow-down-a-z"></i></span><span class="up"><i class="fas fa-arrow-up-a-z"></i></span>
+                        <span class="name"><?= _("IP or Value") ?> <i class="fas fa-arrow-down-a-z"></i></span>
+                        <span class="up"><i class="fas fa-arrow-up-a-z"></i></span>
                     </li>
                     <li data-entity="sort-record">
-                        <span class="name"><?= _("Record") ?> <i class="fas fa-arrow-down-a-z"></i></span><span class="up"><i class="fas fa-arrow-up-a-z"></i></span>
+                        <span class="name"><?= _("Record") ?> <i class="fas fa-arrow-down-a-z"></i></span>
+                        <span class="up"><i class="fas fa-arrow-up-a-z"></i></span>
                     </li>
                     <li data-entity="sort-ttl" data-sort-as-int="1">
-                        <span class="name"><?= _("TTL") ?> <i class="fas fa-arrow-down-a-z"></i></span><span class="up"><i class="fas fa-arrow-up-a-z"></i></span>
+                        <span class="name"><?= _("TTL") ?> <i class="fas fa-arrow-down-a-z"></i></span>
+                        <span class="up"><i class="fas fa-arrow-up-a-z"></i></span>
                     </li>
                     <li data-entity="sort-type">
-                        <span class="name"><?= _("Type") ?> <i class="fas fa-arrow-down-a-z"></i></span><span class="up"><i class="fas fa-arrow-up-a-z"></i></span>
+                        <span class="name"><?= _("Type") ?> <i class="fas fa-arrow-down-a-z"></i></span>
+                        <span class="up"><i class="fas fa-arrow-up-a-z"></i></span>
                     </li>
                 </ul>
                 <?php if ($read_only !== "true") { ?>
+                    <?php $domain_html = htmlentities($_GET['domain']);
+                    $session_token = $_SESSION['token']; ?>
                     <form x-data x-bind="BulkEdit" action="/bulk/dns/" method="post">
-                        <input type="hidden" name="domain" value="<?= htmlentities($_GET["domain"]) ?>">
-                        <input type="hidden" name="token" value="<?= $_SESSION["token"] ?>">
+                        <input type="hidden" name="domain" value="<?= $domain_html ?>">
+                        <input type="hidden" name="token" value="<?= $session_token ?>">
                         <select class="form-select" name="action">
                             <option value=""><?= _("Apply to selected") ?></option>
                             <option value="suspend"><?= _("Suspend") ?></option>
@@ -63,9 +69,15 @@
                 <?php } ?>
             </div>
             <div class="toolbar-search">
+                <?php $search_value = isset($_POST['q']) ? htmlspecialchars($_POST['q']) : ''; ?>
                 <form action="/search/" method="get">
-                    <input type="hidden" name="token" value="<?= $_SESSION["token"] ?>">
-                    <input type="search" class="form-control js-search-input" name="q" value="<?= isset($_POST['q']) ? htmlspecialchars($_POST['q']) : '' ?>" title="<?= _("Search") ?>">
+                    <input type="hidden" name="token" value="<?= $session_token ?>">
+                    <input
+                        type="search"
+                        class="form-control js-search-input"
+                        name="q"
+                        value="<?= $search_value ?>"
+                        title="<?= _("Search") ?>">
                     <button type="submit" class="toolbar-input-submit" title="<?= _("Search") ?>">
                         <i class="fas fa-magnifying-glass"></i>
                     </button>
@@ -104,7 +116,7 @@
             }
             ?>
             <div class="units-table-row <?php if ($status == 'suspended') {
-                echo 'disabled';
+                                            echo 'disabled';
                                         } ?> js-unit"
                 data-sort-date="<?= strtotime($data[$key]['DATE'] . ' ' . $data[$key]['TIME']) ?>"
                 data-sort-record="<?= $data[$key]['RECORD'] ?>"
@@ -113,25 +125,40 @@
                 data-sort-value="<?= $data[$key]['VALUE'] ?>">
                 <div class="units-table-cell">
                     <div>
-                        <input id="check<?= $data[$key]["ID"] ?>" class="js-unit-checkbox" type="checkbox" title="<?= _("Select") ?>" name="record[]" value="<?= $data[$key]["ID"] ?>" <?= $display_mode ?>>
+                        <?php $rec_id = $data[$key]['ID']; ?>
+                        <input
+                            id="check<?= $rec_id ?>"
+                            class="js-unit-checkbox"
+                            type="checkbox"
+                            title="<?= _("Select") ?>"
+                            name="record[]"
+                            value="<?= $rec_id ?>"
+                            <?= $display_mode ?>>
                         <label for="check<?= $data[$key]["ID"] ?>" class="u-hide-desktop"><?= _("Select") ?></label>
                     </div>
                 </div>
                 <div class="units-table-cell units-table-heading-cell u-text-bold">
                     <span class="u-hide-desktop"><?= _("Record") ?>:</span>
-                <?php if (($read_only === 'true') || ($data[$key]['SUSPENDED'] == 'yes')) { ?>
+                    <?php if (($read_only === 'true') || ($data[$key]['SUSPENDED'] == 'yes')) { ?>
                         <?= substr($data[$key]['RECORD'], 0, 12);
                         if (strlen($data[$key]['RECORD']) > 12) {
                             echo '...';
                         } ?>
-                <?php } else { ?>
-                        <a href="/edit/dns/?domain=<?= htmlspecialchars($_GET['domain']) ?>&record_id=<?= $data[$key]['ID'] ?>&token=<?= $_SESSION['token'] ?>" title="<?= _("Edit DNS Record") . ': ' . htmlspecialchars($data[$key]['RECORD']) ?>">
-                            <?= substr($data[$key]['RECORD'], 0, 12);
-                            if (strlen($data[$key]['RECORD']) > 12) {
-                                echo '...';
-                            } ?>
+                    <?php } else { ?>
+                        <?php
+                        $record = $data[$key]['RECORD'];
+                        $rec_display = substr($record, 0, 12) . (strlen($record) > 12 ? '...' : '');
+                        $edit_href = '/edit/dns/?domain=' . $domain_html
+                            . '&record_id=' . $data[$key]['ID']
+                            . '&token=' . $session_token;
+                        $edit_title = _("Edit DNS Record") . ': ' . htmlspecialchars($record);
+                        ?>
+                        <a
+                            href="<?= $edit_href ?>"
+                            title="<?= $edit_title ?>">
+                            <?= $rec_display ?>
                         </a>
-                <?php } ?>
+                    <?php } ?>
                 </div>
                 <div class="units-table-cell">
                     <?php if ($read_only !== "true") { ?>
@@ -139,24 +166,30 @@
                             <?php if ($read_only !== "true") { ?>
                                 <?php if ($data[$key]["SUSPENDED"] == "no") { ?>
                                     <li class="units-table-row-action shortcut-enter" data-key-action="href">
+                                        <?php $action_edit_href = '/edit/dns/?domain=' . $domain_html . '&record_id=' . $data[$key]['ID'] . '&token=' . $session_token;
+                                        $action_edit_title = _("Edit DNS Record"); ?>
                                         <a
                                             class="units-table-row-action-link"
-                                            href="/edit/dns/?domain=<?= htmlspecialchars($_GET["domain"]) ?>&record_id=<?= $data[$key]["ID"] ?>&token=<?= $_SESSION["token"] ?>"
-                                            title="<?= _("Edit DNS Record") ?>"
-                                        >
+                                            href="<?= $action_edit_href ?>"
+                                            title="<?= $action_edit_title ?>">
                                             <i class="fas fa-pencil icon-orange"></i>
                                             <span class="u-hide-desktop"><?= _("Edit DNS Record") ?></span>
                                         </a>
                                     </li>
                                 <?php } ?>
                                 <li class="units-table-row-action shortcut-delete" data-key-action="js">
+                                    <?php
+                                    $delete_href = '/delete/dns/?domain=' . $domain_html
+                                        . '&record_id=' . $data[$key]['ID']
+                                        . '&token=' . $session_token;
+                                    $delete_confirm_msg = sprintf(_("Are you sure you want to delete record %s?"), $key);
+                                    ?>
                                     <a
                                         class="units-table-row-action-link data-controls js-confirm-action"
-                                        href="/delete/dns/?domain=<?= htmlspecialchars($_GET["domain"]) ?>&record_id=<?= $data[$key]["ID"] ?>&token=<?= $_SESSION["token"] ?>"
+                                        href="<?= $delete_href ?>"
                                         title="<?= _("Delete") ?>"
                                         data-confirm-title="<?= _("Delete") ?>"
-                                        data-confirm-message="<?= sprintf(_("Are you sure you want to delete record %s?"), $key) ?>"
-                                    >
+                                        data-confirm-message="<?= $delete_confirm_msg ?>">
                                         <i class="fas fa-trash icon-red"></i>
                                         <span class="u-hide-desktop"><?= _("Delete") ?></span>
                                     </a>
@@ -184,7 +217,7 @@
                 <div class="units-table-cell">
                     <span class="u-hide-desktop u-text-bold"><?= _("IP or Value") ?>:</span>
                     <span class="u-text-break">
-                    <?= htmlspecialchars($data[$key]["VALUE"], ENT_QUOTES, "UTF-8") ?>
+                        <?= htmlspecialchars($data[$key]["VALUE"], ENT_QUOTES, "UTF-8") ?>
                     </span>
                 </div>
             </div>

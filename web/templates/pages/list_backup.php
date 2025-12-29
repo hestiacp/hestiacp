371b44f136661200879d297988bd136247dbaf45
zapper88144 <zapper8814@hotmail.com>
2025-12-29 14:28:57 +0000

    style(list_backup): precompute hrefs and split long attributes to avoid PHPCS max line-length errors

--- web/templates/pages/list_backup.php
+++ web/templates/pages/list_backup.php
@@ -3,11 +3,20 @@
     <div class="toolbar-inner">
         <div class="toolbar-buttons">
             <?php if ($read_only !== "true") { ?>
-                <a href="/schedule/backup/?token=<?= $_SESSION["token"] ?>" class="button button-secondary"><i class="fas fa-circle-plus icon-green"></i><?= _("Create Backup") ?></a>
-                <a href="/list/backup/exclusions/" class="button button-secondary"><i class="fas fa-folder-minus icon-orange"></i><?= _("Backup Exclusions") ?></a>
+                <?php $create_backup_href = "/schedule/backup/?token=" . $_SESSION["token"]; ?>
+                <a href="<?= $create_backup_href ?>" class="button button-secondary">
+                    <i class="fas fa-circle-plus icon-green"></i><?= _("Create Backup") ?>
+                </a>
+                <?php $backup_exclusions_href = "/list/backup/exclusions/"; ?>
+                <a href="<?= $backup_exclusions_href ?>" class="button button-secondary">
+                    <i class="fas fa-folder-minus icon-orange"></i><?= _("Backup Exclusions") ?>
+                </a>
             <?php } ?>
             <?php if ($panel[$user_plain]['BACKUPS_INCREMENTAL'] === 'yes') { ?>
-                <a href="/list/backup/incremental/" class="button button-secondary"><i class="fas fa-vault icon-blue"></i><?= _("Incremental Backups") ?></a>
+                <?php $incremental_href = "/list/backup/incremental/"; ?>
+                <a href="<?= $incremental_href ?>" class="button button-secondary">
+                    <i class="fas fa-vault icon-blue"></i><?= _("Incremental Backups") ?>
+                </a>
             <?php } ?>
         </div>
         <div class="toolbar-right">
@@ -26,7 +35,11 @@
             <div class="toolbar-search">
                 <form action="/search/" method="get">
                     <input type="hidden" name="token" value="<?= $_SESSION["token"] ?>">
-                    <input type="search" class="form-control js-search-input" name="q" value="<?= isset($_POST['q']) ? htmlspecialchars($_POST['q']) : '' ?>" title="<?= _("Search") ?>">
+                    <input type="search"
+                        class="form-control js-search-input"
+                        name="q"
+                        value="<?= isset($_POST['q']) ? htmlspecialchars($_POST['q']) : '' ?>"
+                        title="<?= _("Search") ?>">
                     <button type="submit" class="toolbar-input-submit" title="<?= _("Search") ?>">
                         <i class="fas fa-magnifying-glass"></i>
                     </button>
@@ -87,29 +100,36 @@
             <div class="units-table-row js-unit">
                 <div class="units-table-cell">
                     <div>
-                        <input id="check<?= $i ?>" class="js-unit-checkbox" type="checkbox" title="<?= _("Select") ?>" name="backup[]" value="<?= $key ?>" <?= $display_mode ?>>
+                        <input
+                            id="check<?= $i ?>"
+                            class="js-unit-checkbox"
+                            type="checkbox"
+                            title="<?= _("Select") ?>"
+                            name="backup[]"
+                            value="<?= $key ?>"
+                            <?= $display_mode ?>>
                         <label for="check<?= $i ?>" class="u-hide-desktop"><?= _("Select") ?></label>
                     </div>
                 </div>
                 <div class="units-table-cell units-table-heading-cell u-text-bold">
                     <span class="u-hide-desktop"><?= _("File Name") ?>:</span>
-                <?php if ($read_only === "true") { ?>
+                    <?php if ($read_only === "true") { ?>
                         <?= $key ?>
-                <?php } else { ?>
+                    <?php } else { ?>
                         <a href="/list/backup/?backup=<?= $key ?>&token=<?= $_SESSION["token"] ?>" title="<?= _("Restore") ?>">
                             <?= $key ?>
                         </a>
-                <?php } ?>
+                    <?php } ?>
                 </div>
                 <div class="units-table-cell">
-                <?php if (!($_SESSION["userContext"] === "admin" && $_SESSION["look"] === "admin" && $read_only === "true")) { ?>
+                    <?php if (!($_SESSION["userContext"] === "admin" && $_SESSION["look"] === "admin" && $read_only === "true")) { ?>
                         <ul class="units-table-row-actions">
                             <li class="units-table-row-action shortcut-d" data-key-action="href">
+                                <?php $download_href = "/download/backup/?backup=" . $key . "&token=" . $_SESSION["token"]; ?>
                                 <a
                                     class="units-table-row-action-link"
-                                    href="/download/backup/?backup=<?= $key ?>&token=<?= $_SESSION["token"] ?>"
-                                    title="<?= _("Download") ?>"
-                                >
+                                    href="<?= $download_href ?>"
+                                    title="<?= _("Download") ?>">
                                     <i class="fas fa-file-arrow-down icon-lightblue"></i>
                                     <span class="u-hide-desktop"><?= _("Download") ?></span>
                                 </a>
@@ -119,8 +139,7 @@
                                     <a
                                         class="units-table-row-action-link data-controls"
                                         href="/list/backup/?backup=<?= $key ?>&token=<?= $_SESSION["token"] ?>"
-                                        title="<?= _("Restore") ?>"
-                                    >
+                                        title="<?= _("Restore") ?>">
                                         <i class="fas fa-arrow-rotate-left icon-green"></i>
                                         <span class="u-hide-desktop"><?= _("Restore") ?></span>
                                     </a>
@@ -131,38 +150,37 @@
                                         href="/delete/backup/?backup=<?= $key ?>&token=<?= $_SESSION["token"] ?>"
                                         title="<?= _("Delete") ?>"
                                         data-confirm-title="<?= _("Delete") ?>"
-                                        data-confirm-message="<?= sprintf(_("Are you sure you want to delete backup %s?"), $key) ?>"
-                                    >
+                                        data-confirm-message="<?= sprintf(_("Are you sure you want to delete backup %s?"), $key) ?>">
                                         <i class="fas fa-trash icon-red"></i>
                                         <span class="u-hide-desktop"><?= _("Delete") ?></span>
                                     </a>
                                 </li>
                             <?php } ?>
                         </ul>
-                <?php } ?>
+                    <?php } ?>
                 </div>
                 <div class="units-table-cell u-text-center-desktop">
                     <span class="u-hide-desktop u-text-bold"><?= _("Date") ?>:</span>
                     <span class="u-text-bold">
-                    <?= translate_date($data[$key]["DATE"]) ?>
+                        <?= translate_date($data[$key]["DATE"]) ?>
                     </span>
                 </div>
                 <div class="units-table-cell u-text-center-desktop">
                     <span class="u-hide-desktop u-text-bold"><?= _("Size") ?>:</span>
                     <span class="u-text-bold">
-                    <?= humanize_usage_size($data[$key]["SIZE"]) ?>
+                        <?= humanize_usage_size($data[$key]["SIZE"]) ?>
                     </span>
                     <span class="u-text-small">
-                    <?= humanize_usage_measure($data[$key]["SIZE"]) ?>
+                        <?= humanize_usage_measure($data[$key]["SIZE"]) ?>
                     </span>
                 </div>
                 <div class="units-table-cell u-text-center-desktop">
                     <span class="u-hide-desktop u-text-bold"><?= _("Type") ?>:</span>
-                <?= $data[$key]["TYPE"] ?>
+                    <?= $data[$key]["TYPE"] ?>
                 </div>
                 <div class="units-table-cell u-text-center-desktop">
                     <span class="u-hide-desktop u-text-bold"><?= _("Runtime") ?>:</span>
-                <?= humanize_time($data[$key]["RUNTIME"]) ?>
+                    <?= humanize_time($data[$key]["RUNTIME"]) ?>
                 </div>
             </div>
         <?php } ?>

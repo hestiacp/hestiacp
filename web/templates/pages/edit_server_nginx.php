30e5cd255a2faaf9d48cd35709187df3e2f5dc7b
zapper88144 <zapper8814@hotmail.com>
2025-12-29 15:40:12 +0000

    style(edit_server_nginx): split long attributes and precompute values to fix PHPCS max line-length errors

--- web/templates/pages/edit_server_nginx.php
+++ web/templates/pages/edit_server_nginx.php
@@ -24,6 +24,19 @@
         <input type="hidden" name="token" value="<?= $_SESSION["token"] ?>">
         <input type="hidden" name="save" value="save">
 
+        <?php
+        $worker_processes_val = htmlentities($v_worker_processes);
+        $worker_connections_val = htmlentities($v_worker_connections);
+        $client_max_body_size_val = htmlentities($v_client_max_body_size);
+        $send_timeout_val = htmlentities($v_send_timeout);
+        $proxy_connect_timeout_val = htmlentities($v_proxy_connect_timeout);
+        $proxy_send_timeout_val = htmlentities($v_proxy_send_timeout);
+        $proxy_read_timeout_val = htmlentities($v_proxy_read_timeout);
+        $gzip_val = htmlentities($v_gzip);
+        $gzip_comp_level_val = htmlentities($v_gzip_comp_level);
+        $charset_val = htmlentities($v_charset);
+        ?>
+
         <div class="form-container">
             <h1 class="u-mb20"><?= _("Configure Server") ?>: <?= $v_service_name ?></h1>
             <?php show_alert_message($_SESSION); ?>
@@ -34,39 +47,102 @@
                 </div>
                 <div class="u-mb10">
                     <label for="v_worker_connections" class="form-label">worker_connections</label>
-                    <input type="text" class="form-control" data-regexp="worker_connections" data-prev-value="<?= htmlentities($v_worker_connections) ?>" name="v_worker_connections" id="v_worker_connections" value="<?= htmlentities($v_worker_connections) ?>">
+                    <input
+                        type="text"
+                        class="form-control"
+                        data-regexp="worker_connections"
+                        data-prev-value="<?= $worker_connections_val ?>"
+                        name="v_worker_connections"
+                        id="v_worker_connections"
+                        value="<?= $worker_connections_val ?>">
                 </div>
                 <div class="u-mb10">
                     <label for="v_client_max_body_size" class="form-label">client_max_body_size</label>
-                    <input type="text" class="form-control" data-regexp="client_max_body_size" data-prev-value="<?= htmlentities($v_client_max_body_size) ?>" name="v_client_max_body_size" id="v_client_max_body_size" value="<?= htmlentities($v_client_max_body_size) ?>">
+                    <input
+                        type="text"
+                        class="form-control"
+                        data-regexp="client_max_body_size"
+                        data-prev-value="<?= $client_max_body_size_val ?>"
+                        name="v_client_max_body_size"
+                        id="v_client_max_body_size"
+                        value="<?= $client_max_body_size_val ?>">
                 </div>
                 <div class="u-mb10">
                     <label for="v_send_timeout" class="form-label">send_timeout</label>
-                    <input type="text" class="form-control" data-regexp="send_timeout" data-prev-value="<?= htmlentities($v_send_timeout) ?>" name="v_send_timeout" id="v_send_timeout" value="<?= htmlentities($v_send_timeout) ?>">
+                    <input
+                        type="text"
+                        class="form-control"
+                        data-regexp="send_timeout"
+                        data-prev-value="<?= $send_timeout_val ?>"
+                        name="v_send_timeout"
+                        id="v_send_timeout"
+                        value="<?= $send_timeout_val ?>">
                 </div>
                 <div class="u-mb10">
                     <label for="v_proxy_connect_timeout" class="form-label">proxy_connect_timeout</label>
-                    <input type="text" class="form-control" data-regexp="proxy_connect_timeout" data-prev-value="<?= htmlentities($v_proxy_connect_timeout) ?>" name="v_proxy_connect_timeout" id="v_proxy_connect_timeout" value="<?= htmlentities($v_proxy_connect_timeout) ?>">
+                    <input
+                        type="text"
+                        class="form-control"
+                        data-regexp="proxy_connect_timeout"
+                        data-prev-value="<?= $proxy_connect_timeout_val ?>"
+                        name="v_proxy_connect_timeout"
+                        id="v_proxy_connect_timeout"
+                        value="<?= $proxy_connect_timeout_val ?>">
                 </div>
                 <div class="u-mb10">
                     <label for="v_proxy_send_timeout" class="form-label">proxy_send_timeout</label>
-                    <input type="text" class="form-control" data-regexp="proxy_send_timeout" data-prev-value="<?= htmlentities($v_proxy_send_timeout) ?>" name="v_proxy_send_timeout" id="v_proxy_send_timeout" value="<?= htmlentities($v_proxy_send_timeout) ?>">
+                    <input
+                        type="text"
+                        class="form-control"
+                        data-regexp="proxy_send_timeout"
+                        data-prev-value="<?= $proxy_send_timeout_val ?>"
+                        name="v_proxy_send_timeout"
+                        id="v_proxy_send_timeout"
+                        value="<?= $proxy_send_timeout_val ?>">
                 </div>
                 <div class="u-mb10">
                     <label for="v_proxy_read_timeout" class="form-label">proxy_read_timeout</label>
-                    <input type="text" class="form-control" data-regexp="proxy_read_timeout" data-prev-value="<?= htmlentities($v_proxy_read_timeout) ?>" name="v_proxy_read_timeout" id="v_proxy_read_timeout" value="<?= htmlentities($v_proxy_read_timeout) ?>">
+                    <input
+                        type="text"
+                        class="form-control"
+                        data-regexp="proxy_read_timeout"
+                        data-prev-value="<?= $proxy_read_timeout_val ?>"
+                        name="v_proxy_read_timeout"
+                        id="v_proxy_read_timeout"
+                        value="<?= $proxy_read_timeout_val ?>">
                 </div>
                 <div class="u-mb10">
                     <label for="v_gzip" class="form-label">gzip</label>
-                    <input type="text" class="form-control" data-regexp="gzip" data-prev-value="<?= htmlentities($v_gzip) ?>" name="v_gzip" id="v_gzip" value="<?= htmlentities($v_gzip) ?>">
+                    <input
+                        type="text"
+                        class="form-control"
+                        data-regexp="gzip"
+                        data-prev-value="<?= $gzip_val ?>"
+                        name="v_gzip"
+                        id="v_gzip"
+                        value="<?= $gzip_val ?>">
                 </div>
                 <div class="u-mb10">
                     <label for="v_gzip_comp_level" class="form-label">gzip_comp_level</label>
-                    <input type="text" class="form-control" data-regexp="gzip_comp_level" data-prev-value="<?= htmlentities($v_gzip_comp_level) ?>" name="v_gzip_comp_level" id="v_gzip_comp_level" value="<?= htmlentities($v_gzip_comp_level) ?>">
+                    <input
+                        type="text"
+                        class="form-control"
+                        data-regexp="gzip_comp_level"
+                        data-prev-value="<?= $gzip_comp_level_val ?>"
+                        name="v_gzip_comp_level"
+                        id="v_gzip_comp_level"
+                        value="<?= $gzip_comp_level_val ?>">
                 </div>
                 <div class="u-mb20">
                     <label for="v_charset" class="form-label">charset</label>
-                    <input type="text" class="form-control" data-regexp="charset" data-prev-value="<?= htmlentities($v_charset) ?>" name="v_charset" id="v_charset" value="<?= htmlentities($v_charset) ?>">
+                    <input
+                        type="text"
+                        class="form-control"
+                        data-regexp="charset"
+                        data-prev-value="<?= $charset_val ?>"
+                        name="v_charset"
+                        id="v_charset"
+                        value="<?= $charset_val ?>">
                 </div>
                 <div class="u-mb20">
                     <button type="button" class="button button-secondary js-toggle-options">
@@ -74,9 +150,8 @@
                     </button>
                 </div>
             </div>
-            <div class="js-advanced-options <?php if (empty($v_adv)) {
-                echo 'u-hidden';
-                                            } ?>">
+            <?php $v_adv_class = empty($v_adv) ? 'u-hidden' : ''; ?>
+            <div class="js-advanced-options <?= $v_adv_class ?>">
                 <div class="u-mb20">
                     <button type="button" class="button button-secondary js-toggle-options">
                         <?= _("Basic Options") ?>

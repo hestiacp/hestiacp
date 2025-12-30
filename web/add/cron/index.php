16f4a548ddef1a63f3b778beb670a15916c5fa2b
zapper88144 <zapper8814@hotmail.com>
2025-12-30 22:56:45 +0000

    chore(style): apply PSR-12 fixes to web/add/* small batch

--- web/add/cron/index.php
+++ web/add/cron/index.php
@@ -8,102 +8,102 @@ include $_SERVER["DOCUMENT_ROOT"] . "/inc/main.php";
 
 // Check POST request
 if (!empty($_POST["ok"])) {
-	// Check token
-	verify_csrf($_POST);
+    // Check token
+    verify_csrf($_POST);
 
-	// Check empty fields
-	if (!isset($_POST["v_min"]) || $_POST["v_min"] == "") {
-		$errors[] = _("Minute");
-	}
-	if (!isset($_POST["v_hour"]) || $_POST["v_hour"] == "") {
-		$errors[] = _("Hour");
-	}
-	if (!isset($_POST["v_day"]) || $_POST["v_day"] == "") {
-		$errors[] = _("Day");
-	}
-	if (!isset($_POST["v_month"]) || $_POST["v_month"] == "") {
-		$errors[] = _("Month");
-	}
-	if (!isset($_POST["v_wday"]) || $_POST["v_wday"] == "") {
-		$errors[] = _("Day of Week");
-	}
-	if (!isset($_POST["v_cmd"]) || $_POST["v_cmd"] == "") {
-		$errors[] = _("Command");
-	}
-	if (!empty($errors[0])) {
-		foreach ($errors as $i => $error) {
-			if ($i == 0) {
-				$error_msg = $error;
-			} else {
-				$error_msg = $error_msg . ", " . $error;
-			}
-		}
-		$_SESSION["error_msg"] = sprintf(_('Field "%s" can not be blank.'), $error_msg);
-	}
+    // Check empty fields
+    if (!isset($_POST["v_min"]) || $_POST["v_min"] == "") {
+        $errors[] = _("Minute");
+    }
+    if (!isset($_POST["v_hour"]) || $_POST["v_hour"] == "") {
+        $errors[] = _("Hour");
+    }
+    if (!isset($_POST["v_day"]) || $_POST["v_day"] == "") {
+        $errors[] = _("Day");
+    }
+    if (!isset($_POST["v_month"]) || $_POST["v_month"] == "") {
+        $errors[] = _("Month");
+    }
+    if (!isset($_POST["v_wday"]) || $_POST["v_wday"] == "") {
+        $errors[] = _("Day of Week");
+    }
+    if (!isset($_POST["v_cmd"]) || $_POST["v_cmd"] == "") {
+        $errors[] = _("Command");
+    }
+    if (!empty($errors[0])) {
+        foreach ($errors as $i => $error) {
+            if ($i == 0) {
+                $error_msg = $error;
+            } else {
+                $error_msg = $error_msg . ", " . $error;
+            }
+        }
+        $_SESSION["error_msg"] = sprintf(_('Field "%s" can not be blank.'), $error_msg);
+    }
 
-	// Protect input
-	$v_min = escapeshellarg($_POST["v_min"]);
-	$v_hour = escapeshellarg($_POST["v_hour"]);
-	$v_day = escapeshellarg($_POST["v_day"]);
-	$v_month = escapeshellarg($_POST["v_month"]);
-	$v_wday = escapeshellarg($_POST["v_wday"]);
-	$v_cmd = escapeshellarg($_POST["v_cmd"]);
+    // Protect input
+    $v_min = escapeshellarg($_POST["v_min"]);
+    $v_hour = escapeshellarg($_POST["v_hour"]);
+    $v_day = escapeshellarg($_POST["v_day"]);
+    $v_month = escapeshellarg($_POST["v_month"]);
+    $v_wday = escapeshellarg($_POST["v_wday"]);
+    $v_cmd = escapeshellarg($_POST["v_cmd"]);
 
-	// Add cron job
-	if (empty($_SESSION["error_msg"])) {
-		exec(
-			HESTIA_CMD .
-				"v-add-cron-job " .
-				$user .
-				" " .
-				$v_min .
-				" " .
-				$v_hour .
-				" " .
-				$v_day .
-				" " .
-				$v_month .
-				" " .
-				$v_wday .
-				" " .
-				$v_cmd,
-			$output,
-			$return_var,
-		);
-		check_return_code($return_var, $output);
-		unset($output);
-	}
+    // Add cron job
+    if (empty($_SESSION["error_msg"])) {
+        exec(
+            HESTIA_CMD .
+                "v-add-cron-job " .
+                $user .
+                " " .
+                $v_min .
+                " " .
+                $v_hour .
+                " " .
+                $v_day .
+                " " .
+                $v_month .
+                " " .
+                $v_wday .
+                " " .
+                $v_cmd,
+            $output,
+            $return_var,
+        );
+        check_return_code($return_var, $output);
+        unset($output);
+    }
 
-	// Flush field values on success
-	if (empty($_SESSION["error_msg"])) {
-		$_SESSION["ok_msg"] = _("Cron job has been created successfully.");
-		unset($v_min);
-		unset($v_hour);
-		unset($v_day);
-		unset($v_month);
-		unset($v_wday);
-		unset($v_cmd);
-		unset($output);
-	}
+    // Flush field values on success
+    if (empty($_SESSION["error_msg"])) {
+        $_SESSION["ok_msg"] = _("Cron job has been created successfully.");
+        unset($v_min);
+        unset($v_hour);
+        unset($v_day);
+        unset($v_month);
+        unset($v_wday);
+        unset($v_cmd);
+        unset($output);
+    }
 }
 
 if (empty($v_cmd)) {
-	$v_cmd = "";
+    $v_cmd = "";
 }
 if (empty($v_month)) {
-	$v_month = "";
+    $v_month = "";
 }
 if (empty($v_day)) {
-	$v_day = "";
+    $v_day = "";
 }
 if (empty($v_wday)) {
-	$v_wday = "";
+    $v_wday = "";
 }
 if (empty($v_hour)) {
-	$v_hour = "";
+    $v_hour = "";
 }
 if (empty($v_min)) {
-	$v_min = "";
+    $v_min = "";
 }
 
 // Get current system time
--- web/add/key/index.php
+++ web/add/key/index.php
@@ -8,72 +8,72 @@ include $_SERVER["DOCUMENT_ROOT"] . "/inc/main.php";
 
 // Check POST request
 if (!empty($_POST["ok"])) {
-	// Check token
-	verify_csrf($_POST);
+    // Check token
+    verify_csrf($_POST);
 
-	// Check empty fields
-	if (empty($_POST["v_key"])) {
-		$errors[] = _("SSH Key");
-	}
-	if (!empty($errors[0])) {
-		foreach ($errors as $i => $error) {
-			if ($i == 0) {
-				$error_msg = $error;
-			} else {
-				$error_msg = $error_msg . ", " . $error;
-			}
-		}
-		$_SESSION["error_msg"] = sprintf(_('Field "%s" can not be blank.'), $error_msg);
-	}
+    // Check empty fields
+    if (empty($_POST["v_key"])) {
+        $errors[] = _("SSH Key");
+    }
+    if (!empty($errors[0])) {
+        foreach ($errors as $i => $error) {
+            if ($i == 0) {
+                $error_msg = $error;
+            } else {
+                $error_msg = $error_msg . ", " . $error;
+            }
+        }
+        $_SESSION["error_msg"] = sprintf(_('Field "%s" can not be blank.'), $error_msg);
+    }
 
-	if ($_SESSION["userContext"] === "admin" && !empty($_GET["user"])) {
-		$user = escapeshellarg($_GET["user"]);
-	}
+    if ($_SESSION["userContext"] === "admin" && !empty($_GET["user"])) {
+        $user = escapeshellarg($_GET["user"]);
+    }
 
-	if (empty($_SESSION["error_msg"])) {
-		if ($_POST) {
-			//key if key already exists
-			exec(HESTIA_CMD . "v-list-user-ssh-key " . $user . " json", $output, $return_var);
-			$data = json_decode(implode("", $output), true);
-			unset($output);
-			$keylist = [];
-			$idlist = [];
-			foreach ($data as $key => $value) {
-				$idlist[] = trim($data[$key]["ID"]);
-				$keylist[] = trim($data[$key]["KEY"]);
-			}
+    if (empty($_SESSION["error_msg"])) {
+        if ($_POST) {
+            //key if key already exists
+            exec(HESTIA_CMD . "v-list-user-ssh-key " . $user . " json", $output, $return_var);
+            $data = json_decode(implode("", $output), true);
+            unset($output);
+            $keylist = [];
+            $idlist = [];
+            foreach ($data as $key => $value) {
+                $idlist[] = trim($data[$key]["ID"]);
+                $keylist[] = trim($data[$key]["KEY"]);
+            }
 
-			$v_key_parts = explode(" ", $_POST["v_key"]);
-			$key_id = trim($v_key_parts[2]);
-			if ($v_key_parts[2] == "") {
-				$v_key_parts[2] = md5(time());
-				$_POST["v_key"] .= " " . $v_key_parts[2];
-			}
+            $v_key_parts = explode(" ", $_POST["v_key"]);
+            $key_id = trim($v_key_parts[2]);
+            if ($v_key_parts[2] == "") {
+                $v_key_parts[2] = md5(time());
+                $_POST["v_key"] .= " " . $v_key_parts[2];
+            }
 
-			//for deleting / revoking key the last part user@domain is used therefore needs to be unique
-			//maybe consider adding random generated message or even an human read able string set by user?
-			if (in_array($v_key_parts[2], $idlist)) {
-				$_SESSION["error_msg"] = _("SSH Key already exists.");
-			}
-			if (in_array($v_key_parts[1], $keylist)) {
-				$_SESSION["error_msg"] = _("SSH Key already exists.");
-			}
-			$v_key = escapeshellarg(trim($_POST["v_key"]));
-		}
-	}
+            //for deleting / revoking key the last part user@domain is used therefore needs to be unique
+            //maybe consider adding random generated message or even an human read able string set by user?
+            if (in_array($v_key_parts[2], $idlist)) {
+                $_SESSION["error_msg"] = _("SSH Key already exists.");
+            }
+            if (in_array($v_key_parts[1], $keylist)) {
+                $_SESSION["error_msg"] = _("SSH Key already exists.");
+            }
+            $v_key = escapeshellarg(trim($_POST["v_key"]));
+        }
+    }
 
-	if (empty($_SESSION["error_msg"])) {
-		exec(HESTIA_CMD . "v-add-user-ssh-key " . $user . " " . $v_key, $output, $return_var);
-		check_return_code($return_var, $output);
-	}
-	unset($output);
-	// Flush field values on success
-	if (empty($_SESSION["error_msg"])) {
-		$_SESSION["ok_msg"] = _("SSH Key has been created successfully.");
-	}
+    if (empty($_SESSION["error_msg"])) {
+        exec(HESTIA_CMD . "v-add-user-ssh-key " . $user . " " . $v_key, $output, $return_var);
+        check_return_code($return_var, $output);
+    }
+    unset($output);
+    // Flush field values on success
+    if (empty($_SESSION["error_msg"])) {
+        $_SESSION["ok_msg"] = _("SSH Key has been created successfully.");
+    }
 }
 if (empty($v_key)) {
-	$v_key = "";
+    $v_key = "";
 }
 render_page($user, $TAB, "add_key");
 

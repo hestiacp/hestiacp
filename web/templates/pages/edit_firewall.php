e1e007109630c5cb7a90eff29c8786c6cb163231
zapper88144 <zapper8814@hotmail.com>
2025-12-30 02:00:04 +0000

    chore(style): precompute/split long attributes in edit_firewall.php

--- web/templates/pages/edit_firewall.php
+++ web/templates/pages/edit_firewall.php
@@ -21,39 +21,53 @@
         <input type="hidden" name="token" value="<?= $_SESSION["token"] ?>">
         <input type="hidden" name="save" value="save">
 
+        <?php
+        $ipset_lists_attr = htmlspecialchars($ipset_lists_json, ENT_QUOTES, "UTF-8");
+        $port_placeholder = _("All ports: 0, Range: 80-82, List: 80,443,8080,8443");
+        $v_port_clean = htmlentities(trim($v_port, "'"));
+        $v_ip_clean = htmlentities(trim($v_ip, "'"));
+        $v_comment_clean = htmlentities(trim($v_comment, "'"));
+        ?>
+
         <div class="form-container">
             <h1 class="u-mb20"><?= _("Edit Firewall Rule") ?></h1>
             <?php show_alert_message($_SESSION); ?>
             <div class="u-mb10">
                 <label for="v_action" class="form-label"><?= _("Action") ?></label>
                 <select class="form-select" name="v_action" id="v_action">
-                    <option value="DROP" <?php if ((!empty($v_action)) && ( $v_action == "DROP" )) {
-                        echo 'selected';
-                                         }?>><?= _("DROP") ?></option>
-                    <option value="ACCEPT" <?php if ((!empty($v_action)) && ( $v_action == "ACCEPT" )) {
-                        echo 'selected';
-                                           }?>><?= _("ACCEPT") ?></option>
+                    <option value="DROP" <?php if ((!empty($v_action)) && ($v_action == "DROP")) {
+                                                echo 'selected';
+                                         } ?>><?= _("DROP") ?></option>
+                    <option value="ACCEPT" <?php if ((!empty($v_action)) && ($v_action == "ACCEPT")) {
+                                                echo 'selected';
+                                           } ?>><?= _("ACCEPT") ?></option>
                 </select>
             </div>
             <div class="u-mb10">
                 <label for="v_protocol" class="form-label"><?= _("Protocol") ?></label>
                 <select class="form-select" name="v_protocol" id="v_protocol">
-                    <option value="TCP" <?php if ((!empty($v_protocol)) && ( $v_protocol == "TCP" )) {
-                        echo 'selected';
-                                        }?>>TCP</option>
-                    <option value="UDP" <?php if ((!empty($v_protocol)) && ( $v_protocol == "UDP" )) {
-                        echo 'selected';
-                                        }?>>UDP</option>
-                    <option value="ICMP" <?php if ((!empty($v_protocol)) && ( $v_protocol == "ICMP" )) {
-                        echo 'selected';
-                                         }?>>ICMP</option>
+                    <option value="TCP" <?php if ((!empty($v_protocol)) && ($v_protocol == "TCP")) {
+                                            echo 'selected';
+                                        } ?>>TCP</option>
+                    <option value="UDP" <?php if ((!empty($v_protocol)) && ($v_protocol == "UDP")) {
+                                            echo 'selected';
+                                        } ?>>UDP</option>
+                    <option value="ICMP" <?php if ((!empty($v_protocol)) && ($v_protocol == "ICMP")) {
+                                                echo 'selected';
+                                         } ?>>ICMP</option>
                 </select>
             </div>
             <div class="u-mb10">
                 <label for="v_port" class="form-label">
                     <?= _("Port") ?> <span class="optional">(<?= _("Ranges and lists are acceptable") ?>)</span>
                 </label>
-                <input type="text" class="form-control" name="v_port" id="v_port" value="<?= htmlentities(trim($v_port, "'")) ?>" placeholder="<?= _("All ports: 0, Range: 80-82, List: 80,443,8080,8443") ?>">
+                <input
+                    type="text"
+                    class="form-control"
+                    name="v_port"
+                    id="v_port"
+                    value="<?= $v_port_clean ?>"
+                    placeholder="<?= $port_placeholder ?>">
             </div>
             <div class="u-mb10">
                 <label for="v_ip" class="form-label">
@@ -64,18 +78,28 @@
                         class="form-select js-ip-list-select"
                         tabindex="-1"
                         onchange="this.nextElementSibling.value=this.value"
-                        data-ipset-lists="<?= htmlspecialchars($ipset_lists_json, ENT_QUOTES, "UTF-8") ?>"
-                    >
+                        data-ipset-lists="<?= $ipset_lists_attr ?>">
                         <option value=""><?= _("Clear") ?></option>
                     </select>
-                    <input type="text" class="form-control list-editor" name="v_ip" id="v_ip" value="<?= htmlentities(trim($v_ip, "'")) ?>">
+                    <input
+                        type="text"
+                        class="form-control list-editor"
+                        name="v_ip"
+                        id="v_ip"
+                        value="<?= $v_ip_clean ?>">
                 </div>
             </div>
             <div class="u-mb10">
                 <label for="v_comment" class="form-label">
                     <?= _("Comment") ?> <span class="optional">(<?= _("Optional") ?>)</span>
                 </label>
-                <input type="text" class="form-control" name="v_comment" id="v_comment" maxlength="255" value="<?= htmlentities(trim($v_comment, "'")) ?>">
+                <input
+                    type="text"
+                    class="form-control"
+                    name="v_comment"
+                    id="v_comment"
+                    maxlength="255"
+                    value="<?= $v_comment_clean ?>">
             </div>
         </div>
 

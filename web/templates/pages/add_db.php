419c6489804c48ec2bd7fd20eb9feb00920bba53
zapper88144 <zapper8814@hotmail.com>
2025-12-30 15:24:07 +0000

    chore(style): split long strings, add missing charsets and simplify charset options in add_db.php

--- web/templates/pages/add_db.php
+++ web/templates/pages/add_db.php
@@ -35,8 +35,11 @@
         $db_email_val = htmlentities(trim($v_db_email, "'"));
         $create_account_warning = sprintf(
             _(
-                "It is strongly advised to {create a standard user account} before adding %s to the server "
-                    . "due to the increased privileges the admin account possesses and potential security risks."
+                "It is strongly advised to {create a standard user account} before "
+                    . "adding %s to the server"
+            ) . ' ' . _(
+                "Due to the increased privileges the admin account possesses and "
+                    . "potential security risks."
             ),
             _("a database")
         );
@@ -45,14 +48,16 @@
         $type_options = '';
         foreach ($db_types as $dval) {
             $selected = (!empty($v_type) && $dval == $v_type) ? ' selected' : '';
-            $type_options .= "<option value=\"" . htmlentities($dval) . "\"" . $selected . ">" . htmlentities($dval) . "</option>\n";
+            $type_options .= "<option value=\"" . htmlentities($dval) . "\""
+                . $selected . ">" . htmlentities($dval) . "</option>\n";
         }
 
         // Prebuild host options
         $host_options = '';
         foreach ($db_hosts as $hval) {
             $selected = (!empty($v_host) && $hval == $v_host) ? ' selected' : '';
-            $host_options .= "<option value=\"" . htmlentities($hval) . "\"" . $selected . ">" . htmlentities($hval) . "</option>\n";
+            $host_options .= "<option value=\"" . htmlentities($hval) . "\""
+                . $selected . ">" . htmlentities($hval) . "</option>\n";
         }
         ?>
 
@@ -66,9 +71,19 @@
                 </div>
             <?php } ?>
             <?php if ($_SESSION["role"] == "admin" && empty($accept)) { ?>
+                <?php $add_user_href = '/add/user/';
+                $add_db_accept_href = '/add/db/?accept=true'; ?>
                 <div class="u-side-by-side u-mt20">
-                    <a href="/add/user/" class="button u-width-full u-mr10"><?= _("Add User") ?></a>
-                    <a href="/add/db/?accept=true" class="button button-danger u-width-full u-ml10"><?= _("Continue") ?></a>
+                    <a
+                        class="button u-width-full u-mr10"
+                        href="<?= $add_user_href ?>">
+                        <?= _("Add User") ?>
+                    </a>
+                    <a
+                        class="button button-danger u-width-full u-ml10"
+                        href="<?= $add_db_accept_href ?>">
+                        <?= _("Continue") ?>
+                    </a>
                 </div>
             <?php } ?>
             <?php if (($_SESSION["role"] == "admin" && $accept === "true") || $_SESSION["role"] !== "admin") { ?>
@@ -96,9 +111,10 @@
                     </select>
                 </div>
                 <div class="u-mb10">
+                    <?php $max_username_msg = sprintf(_("Maximum %s characters length, including prefix"), 32); ?>
                     <label for="v_dbuser" class="form-label u-side-by-side">
                         <?= _("Username") ?>
-                        <em><small>(<?= sprintf(_("Maximum %s characters length, including prefix"), 32) ?>)</small></em>
+                        <em><small>(<?= $max_username_msg ?>)</small></em>
                     </label>
                     <input
                         type="text"
@@ -111,7 +127,10 @@
                 <div class="u-mb10">
                     <label for="v_password" class="form-label">
                         <?= _("Password") ?>
-                        <button type="button" title="<?= _("Generate") ?>" class="u-unstyled-button u-ml5 js-generate-password">
+                        <button
+                            type="button"
+                            title="<?= _("Generate") ?>"
+                            class="u-unstyled-button u-ml5 js-generate-password">
                             <i class="fas fa-arrows-rotate icon-green"></i>
                         </button>
                     </label>
@@ -149,124 +168,62 @@
                     <div class="u-mb10">
                         <label for="v_charset" class="form-label"><?= _("Charset") ?></label>
                         <select class="form-select" name="v_charset" id="v_charset">
-                            <option value=big5 <?php if ((!empty($v_charset)) && ($v_charset == 'big5')) {
-                                                    echo 'selected';
-                                               } ?>>big5</option>
-                            <option value=dec8 <?php if ((!empty($v_charset)) && ($v_charset == 'dec8')) {
-                                                    echo 'selected';
-                                               } ?>>dec8</option>
-                            <option value=cp850 <?php if ((!empty($v_charset)) && ($v_charset == 'cp850')) {
-                                                    echo 'selected';
-                                                } ?>>cp850</option>
-                            <option value=hp8 <?php if ((!empty($v_charset)) && ($v_charset == 'hp8')) {
-                                                    echo 'selected';
-                                              } ?>>hp8</option>
-                            <option value=koi8r <?php if ((!empty($v_charset)) && ($v_charset == 'koi8r')) {
-                                                    echo 'selected';
-                                                } ?>>koi8r</option>
-                            <option value=latin1 <?php if ((!empty($v_charset)) && ($v_charset == 'latin1')) {
-                                                        echo 'selected';
-                                                 } ?>>latin1</option>
-                            <option value=latin2 <?php if ((!empty($v_charset)) && ($v_charset == 'latin2')) {
-                                                        echo 'selected';
-                                                 } ?>>latin2</option>
-                            <option value=swe7 <?php if ((!empty($v_charset)) && ($v_charset == 'swe7')) {
-                                                    echo 'selected';
-                                               } ?>>swe7</option>
-                            <option value=ascii <?php if ((!empty($v_charset)) && ($v_charset == 'ascii')) {
-                                                    echo 'selected';
-                                                } ?>>ascii</option>
-                            <option value=ujis <?php if ((!empty($v_charset)) && ($v_charset == 'ujis')) {
-                                                    echo 'selected';
-                                               } ?>>ujis</option>
-                            <option value=sjis <?php if ((!empty($v_charset)) && ($v_charset == 'sjis')) {
-                                                    echo 'selected';
-                                               } ?>>sjis</option>
-                            <option value=hebrew <?php if ((!empty($v_charset)) && ($v_charset == 'hebrew')) {
-                                                        echo 'selected';
-                                                 } ?>>hebrew</option>
-                            <option value=tis620 <?php if ((!empty($v_charset)) && ($v_charset == 'tis620')) {
-                                                        echo 'selected';
-                                                 } ?>>tis620</option>
-                            <option value=euckr <?php if ((!empty($v_charset)) && ($v_charset == 'euckr')) {
-                                                    echo 'selected';
-                                                } ?>>euckr</option>
-                            <option value=koi8u <?php if ((!empty($v_charset)) && ($v_charset == 'koi8u')) {
-                                                    echo 'selected';
-                                                } ?>>koi8u</option>
-                            <option value=gb2312 <?php if ((!empty($v_charset)) && ($v_charset == 'gb2312')) {
-                                                        echo 'selected';
-                                                 } ?>>gb2312</option>
-                            <option value=greek <?php if ((!empty($v_charset)) && ($v_charset == 'greek')) {
-                                                    echo 'selected';
-                                                } ?>>greek</option>
-                            <option value=cp1250 <?php if ((!empty($v_charset)) && ($v_charset == 'cp1250')) {
-                                                        echo 'selected';
-                                                 } ?>>cp1250</option>
-                            <option value=gbk <?php if ((!empty($v_charset)) && ($v_charset == 'gbk')) {
-                                                    echo 'selected';
-                                              } ?>>gbk</option>
-                            <option value=latin5 <?php if ((!empty($v_charset)) && ($v_charset == 'latin5')) {
-                                                        echo 'selected';
-                                                 } ?>>latin5</option>
-                            <option value=armscii8 <?php if ((!empty($v_charset)) && ($v_charset == 'armscii8')) {
-                                                        echo 'selected';
-                                                   } ?>>armscii8</option>
-                            <option value=utf8 <?php if ((!empty($v_charset)) && ($v_charset == 'utf8')) {
-                                                    echo 'selected';
-                                               } ?>>utf8</option>
-                            <option value="utf8mb4"
-                                <?php if (((!empty($v_charset) && $v_charset == 'utf8mb4')) || empty($v_charset)) {
-                                    echo 'selected';
-                                } ?>>utf8mb4</option>
-                            <option value=ucs2 <?php if ((!empty($v_charset)) && ($v_charset == 'ucs2')) {
-                                                    echo 'selected';
-                                               } ?>>ucs2</option>
-                            <option value=cp866 <?php if ((!empty($v_charset)) && ($v_charset == 'cp866')) {
-                                                    echo 'selected';
-                                                } ?>>cp866</option>
-                            <option value=keybcs2 <?php if ((!empty($v_charset)) && ($v_charset == 'keybcs2')) {
-                                                        echo 'selected';
-                                                  } ?>>keybcs2</option>
-                            <option value=macce <?php if ((!empty($v_charset)) && ($v_charset == 'macce')) {
-                                                    echo 'selected';
-                                                } ?>>macce</option>
-                            <option value=macroman <?php if ((!empty($v_charset)) && ($v_charset == 'macroman')) {
-                                                        echo 'selected';
-                                                   } ?>>macroman</option>
-                            <option value=cp852 <?php if ((!empty($v_charset)) && ($v_charset == 'cp852')) {
-                                                    echo 'selected';
-                                                } ?>>cp852</option>
-                            <option value=latin7 <?php if ((!empty($v_charset)) && ($v_charset == 'latin7')) {
-                                                        echo 'selected';
-                                                 } ?>>latin7</option>
-                            <option value=cp1251 <?php if ((!empty($v_charset)) && ($v_charset == 'cp1251')) {
-                                                        echo 'selected';
-                                                 } ?>>cp1251</option>
-                            <option value=cp1256 <?php if ((!empty($v_charset)) && ($v_charset == 'cp1256')) {
-                                                        echo 'selected';
-                                                 } ?>>cp1256</option>
-                            <option value=cp1257 <?php if ((!empty($v_charset)) && ($v_charset == 'cp1257')) {
-                                                        echo 'selected';
-                                                 } ?>>cp1257</option>
-                            <option value=binary <?php if ((!empty($v_charset)) && ($v_charset == 'binary')) {
-                                                        echo 'selected';
-                                                 } ?>>binary</option>
-                            <option value=geostd8 <?php if ((!empty($v_charset)) && ($v_charset == 'geostd8')) {
-                                                        echo 'selected';
-                                                  } ?>>geostd8</option>
-                            <option value=cp932 <?php if ((!empty($v_charset)) && ($v_charset == 'cp932')) {
-                                                    echo 'selected';
-                                                } ?>>cp932</option>
-                            <option value=eucjpms <?php if ((!empty($v_charset)) && ($v_charset == 'eucjpms')) {
-                                                        echo 'selected';
-                                                  } ?>>eucjpms</option>
+                            <?php
+                            $charsets = [
+                                'big5',
+                                'dec8',
+                                'cp850',
+                                'hp8',
+                                'koi8r',
+                                'latin1',
+                                'latin2',
+                                'swe7',
+                                'ascii',
+                                'ujis',
+                                'sjis',
+                                'hebrew',
+                                'tis620',
+                                'euckr',
+                                'koi8u',
+                                'gb2312',
+                                'greek',
+                                'cp1250',
+                                'gbk',
+                                'latin5',
+                                'armscii8',
+                                'utf8',
+                                'utf8mb4',
+                                'ucs2',
+                                'cp866',
+                                'keybcs2',
+                                'macce',
+                                'macroman',
+                                'cp852',
+                                'latin7',
+                                'cp1251',
+                                'cp1256',
+                                'cp1257',
+                                'binary',
+                                'geostd8',
+                                'cp932',
+                                'eucjpms'
+                            ];
+
+                            foreach ($charsets as $ch) {
+                                $selected = '';
+                                $is_selected = (!empty($v_charset) && $v_charset == $ch)
+                                    || ($ch == 'utf8mb4' && empty($v_charset));
+                                if ($is_selected) {
+                                    $selected = ' selected';
+                                }
+                                ?>
+                                <option value="<?= $ch ?>" <?= $selected ?>><?= $ch ?></option>
+                            <?php } ?>
                         </select>
                     </div>
                 </div>
-            <?php } ?>
         </div>
-
-    </form>
-
+            <?php } ?>
+</div>
+</form>
 </div>

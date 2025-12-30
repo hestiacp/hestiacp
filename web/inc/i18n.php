462553524b4620a93faaebdba2a328948baeee44
zapper88144 <zapper8814@hotmail.com>
2025-12-30 22:47:57 +0000

    chore(style): apply PSR-12 fixes to web/inc/i18n.php

--- web/inc/i18n.php
+++ web/inc/i18n.php
@@ -16,14 +16,15 @@ textdomain($domain);
  * @param string Fallback language (default: 'en')
  * @return string Language code (such as 'en' and 'ja')
  */
-function detect_user_language() {
-	if (!empty($_SESSION["language"])) {
-		return $_SESSION["language"];
-	} elseif (!empty($_SESSION["LANGUAGE"])) {
-		return $_SESSION["LANGUAGE"];
-	} else {
-		return "en";
-	}
+function detect_user_language()
+{
+    if (!empty($_SESSION["language"])) {
+        return $_SESSION["language"];
+    } elseif (!empty($_SESSION["LANGUAGE"])) {
+        return $_SESSION["LANGUAGE"];
+    } else {
+        return "en";
+    }
 }
 /**
  * Translate ISO2 to "Language"
@@ -31,50 +32,54 @@ function detect_user_language() {
  * @param string iso2 code
  * @return string Language
  */
-function translate_json($string) {
-	$json = file_get_contents($_SERVER["DOCUMENT_ROOT"] . "/locale/languages.json");
-	$json_a = json_decode($json, true);
-	return $json_a[$string][0] . " (" . $json_a[$string . "_locale"][0] . ")";
+function translate_json($string)
+{
+    $json = file_get_contents($_SERVER["DOCUMENT_ROOT"] . "/locale/languages.json");
+    $json_a = json_decode($json, true);
+    return $json_a[$string][0] . " (" . $json_a[$string . "_locale"][0] . ")";
 }
 /**
  * Support translation strings that contains html
  */
-function htmlify_trans($string, $closingTag) {
-	$arguments = func_get_args();
-	return preg_replace_callback(
-		"/{(.*?)}/", // Ungreedy (*?)
-		function ($matches) use ($arguments, $closingTag) {
-			static $i = 1;
-			$i++;
-			return $arguments[$i] . $matches[1] . $closingTag;
-		},
-		$string,
-	);
+function htmlify_trans($string, $closingTag)
+{
+    $arguments = func_get_args();
+    return preg_replace_callback(
+        "/{(.*?)}/", // Ungreedy (*?)
+        function ($matches) use ($arguments, $closingTag) {
+            static $i = 1;
+            $i++;
+            return $arguments[$i] . $matches[1] . $closingTag;
+        },
+        $string,
+    );
 }
 
-function get_email_template($file, $language) {
-	if (
-		file_exists(
-			$_SERVER["HESTIA"] . "/data/templates/email/" . $language . "/" . $file . ".html",
-		)
-	) {
-		return file_get_contents(
-			$_SERVER["HESTIA"] . "/data/templates/email/" . $language . "/" . $file . ".html",
-		);
-	}
-	if (file_exists($_SERVER["HESTIA"] . "/data/templates/email/" . $file . ".html")) {
-		return file_get_contents($_SERVER["HESTIA"] . "/data/templates/email/" . $file . ".html");
-	}
-	return false;
+function get_email_template($file, $language)
+{
+    if (
+        file_exists(
+            $_SERVER["HESTIA"] . "/data/templates/email/" . $language . "/" . $file . ".html",
+        )
+    ) {
+        return file_get_contents(
+            $_SERVER["HESTIA"] . "/data/templates/email/" . $language . "/" . $file . ".html",
+        );
+    }
+    if (file_exists($_SERVER["HESTIA"] . "/data/templates/email/" . $file . ".html")) {
+        return file_get_contents($_SERVER["HESTIA"] . "/data/templates/email/" . $file . ".html");
+    }
+    return false;
 }
 
-function translate_email($string, $replace) {
-	$array1 = $array2 = [];
-	foreach ($replace as $key => $value) {
-		$array1[] = "{{" . $key . "}}";
-		$array2[] = $value;
-	}
-	return str_replace($array1, $array2, $string);
+function translate_email($string, $replace)
+{
+    $array1 = $array2 = [];
+    foreach ($replace as $key => $value) {
+        $array1[] = "{{" . $key . "}}";
+        $array2[] = $value;
+    }
+    return str_replace($array1, $array2, $string);
 }
 /**
  * Detects user language .
@@ -82,4 +87,6 @@ function translate_email($string, $replace) {
  * @return string Language code (such as 'en' and 'ja')
  */
 
-function detect_login_language() {}
+function detect_login_language()
+{
+}

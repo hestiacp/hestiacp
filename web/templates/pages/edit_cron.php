bcaff3ecb797e23ab7db061fce0d12bf1036e927
zapper88144 <zapper8814@hotmail.com>
2025-12-30 07:42:03 +0000

    chore(style): precompute/split long inputs in edit_cron.php

--- web/templates/pages/edit_cron.php
+++ web/templates/pages/edit_cron.php
@@ -34,29 +34,65 @@
             <?php show_alert_message($_SESSION); ?>
             <div class="u-mb20">
                 <label for="v_cmd" class="form-label"><?= _("Command") ?></label>
-                <input type="text" class="form-control" name="v_cmd" id="v_cmd" value="<?= htmlentities(trim($v_cmd, "'")) ?>">
+                <?php $v_cmd_value = htmlentities(trim($v_cmd, "'")); ?>
+                <input
+                    type="text"
+                    class="form-control"
+                    name="v_cmd"
+                    id="v_cmd"
+                    value="<?= $v_cmd_value ?>">
             </div>
             <div class="sidebar-left-grid">
                 <div class="sidebar-left-grid-sidebar">
                     <div class="u-mb10">
                         <label for="v_min" class="form-label"><?= _("Minute") ?></label>
-                        <input type="text" class="form-control" name="v_min" id="v_min" value="<?= htmlentities(trim($v_min, "'")) ?>">
+                        <?php $v_min_value = htmlentities(trim($v_min, "'")); ?>
+                        <input
+                            type="text"
+                            class="form-control"
+                            name="v_min"
+                            id="v_min"
+                            value="<?= $v_min_value ?>">
                     </div>
                     <div class="u-mb10">
                         <label for="v_hour" class="form-label"><?= _("Hour") ?></label>
-                        <input type="text" class="form-control" name="v_hour" id="v_hour" value="<?= htmlentities(trim($v_hour, "'")) ?>">
+                        <?php $v_hour_value = htmlentities(trim($v_hour, "'")); ?>
+                        <input
+                            type="text"
+                            class="form-control"
+                            name="v_hour"
+                            id="v_hour"
+                            value="<?= $v_hour_value ?>">
                     </div>
                     <div class="u-mb10">
                         <label for="v_day" class="form-label"><?= _("Day") ?></label>
-                        <input type="text" class="form-control" name="v_day" id="v_day" value="<?= htmlentities(trim($v_day, "'")) ?>">
+                        <?php $v_day_value = htmlentities(trim($v_day, "'")); ?>
+                        <input
+                            type="text"
+                            class="form-control"
+                            name="v_day"
+                            id="v_day"
+                            value="<?= $v_day_value ?>">
                     </div>
                     <div class="u-mb10">
                         <label for="v_month" class="form-label"><?= _("Month") ?></label>
-                        <input type="text" class="form-control" name="v_month" id="v_month" value="<?= htmlentities(trim($v_month, "'")) ?>">
+                        <?php $v_month_value = htmlentities(trim($v_month, "'")); ?>
+                        <input
+                            type="text"
+                            class="form-control"
+                            name="v_month"
+                            id="v_month"
+                            value="<?= $v_month_value ?>">
                     </div>
                     <div class="u-mb10">
                         <label for="v_wday" class="form-label"><?= _("Day of Week") ?></label>
-                        <input type="text" class="form-control" name="v_wday" id="v_wday" value="<?= htmlentities(trim($v_wday, "'")) ?>">
+                        <?php $v_wday_value = htmlentities(trim($v_wday, "'")); ?>
+                        <input
+                            type="text"
+                            class="form-control"
+                            name="v_wday"
+                            id="v_wday"
+                            value="<?= $v_wday_value ?>">
                     </div>
                 </div>
                 <div class="sidebar-left-grid-content">

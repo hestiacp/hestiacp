1db706720c8cdf66f2c6e3c1dea5d402d06af253
zapper88144 <zapper8814@hotmail.com>
2025-12-30 01:26:46 +0000

    chore(style): precompute cleaned values and split long attributes in add_dns_rec.php

--- web/templates/pages/add_dns_rec.php
+++ web/templates/pages/add_dns_rec.php
@@ -2,7 +2,8 @@
 <div class="toolbar">
     <div class="toolbar-inner">
         <div class="toolbar-buttons">
-            <a class="button button-secondary button-back js-button-back" href="/list/dns/?domain=<?= htmlentities(trim($v_domain, "'")) ?>&token=<?= $_SESSION["token"] ?>">
+            <?php $back_href = '/list/dns/?domain=' . $v_domain_clean . '&token=' . $token; ?>
+            <a class="button button-secondary button-back js-button-back" href="<?= $back_href ?>">
                 <i class="fas fa-arrow-left icon-blue"></i><?= _("Back") ?>
             </a>
         </div>
@@ -18,7 +19,15 @@
 <div class="container">
 
     <form id="main-form" name="v_add_dns_rec" method="post">
-        <input type="hidden" name="token" value="<?= $_SESSION["token"] ?>">
+        <?php
+        $token = $_SESSION['token'];
+        $v_domain_clean = htmlentities(trim($v_domain, "'"));
+        $v_rec_clean = htmlentities(trim($v_rec, "'"));
+        $v_val_clean = htmlentities(trim($v_val, "'"));
+        $v_priority_clean = htmlentities(trim($v_priority, "'"));
+        $v_ttl_clean = htmlentities(trim($v_ttl, "'"));
+        ?>
+        <input type="hidden" name="token" value="<?= $token ?>">
         <input type="hidden" name="ok_rec" value="add">
 
         <div class="form-container">
@@ -26,90 +35,103 @@
             <?php show_alert_message($_SESSION); ?>
             <div class="u-mb10">
                 <label for="v_domain" class="form-label"><?= _("Domain") ?></label>
-                <input type="text" class="form-control js-dns-record-domain" name="v_domain" id="v_domain" value="<?= htmlentities(trim($v_domain, "'")) ?>" disabled>
-                <input type="hidden" name="v_domain" value="<?= htmlentities(trim($v_domain, "'")) ?>">
+                <input
+                    type="text"
+                    class="form-control js-dns-record-domain"
+                    name="v_domain"
+                    id="v_domain"
+                    value="<?= $v_domain_clean ?>"
+                    disabled>
+                <input type="hidden" name="v_domain" value="<?= $v_domain_clean ?>">
             </div>
             <div class="u-mb10">
                 <label for="v_rec" class="form-label"><?= _("Record") ?></label>
-                <input type="text" class="form-control js-dns-record-input" name="v_rec" id="v_rec" value="<?= htmlentities(trim($v_rec, "'")) ?>">
+                <input
+                    type="text"
+                    class="form-control js-dns-record-input"
+                    name="v_rec"
+                    id="v_rec"
+                    value="<?= $v_rec_clean ?>">
                 <small class="hint"></small>
             </div>
             <div class="u-mb10">
                 <label for="v_type" class="form-label"><?= _("Type") ?></label>
+                <?php
+                $dns_types = [
+                    'A',
+                    'AAAA',
+                    'CAA',
+                    'CNAME',
+                    'DNSKEY',
+                    'DS',
+                    'IPSECKEY',
+                    'KEY',
+                    'MX',
+                    'NS',
+                    'PTR',
+                    'SPF',
+                    'SRV',
+                    'TLSA',
+                    'TXT',
+                ];
+                ?>
                 <select class="form-select" name="v_type" id="v_type">
-                    <option value="A" <?php if ($v_type == 'A') {
-                        echo "selected";
-                                      } ?>>A</option>
-                    <option value="AAAA" <?php if ($v_type == 'AAAA') {
-                        echo "selected";
-                                         } ?>>AAAA</option>
-                    <option value="CAA" <?php if ($v_type == 'CAA') {
-                        echo "selected";
-                                        } ?>>CAA</option>
-                    <option value="CNAME" <?php if ($v_type == 'CNAME') {
-                        echo "selected";
-                                          } ?>>CNAME</option>
-                    <option value="DNSKEY" <?php if ($v_type == 'DNSKEY') {
-                        echo "selected";
-                                           } ?>>DNSKEY</option>
-                    <option value="DS" <?php if ($v_type == 'DS') {
-                        echo "selected";
-                                       } ?>>DS</option>
-                    <option value="IPSECKEY" <?php if ($v_type == 'IPSECKEY') {
-                        echo "selected";
-                                             } ?>>IPSECKEY</option>
-                    <option value="KEY" <?php if ($v_type == 'KEY') {
-                        echo "selected";
-                                        } ?>>KEY</option>
-                    <option value="MX" <?php if ($v_type == 'MX') {
-                        echo "selected";
-                                       } ?>>MX</option>
-                    <option value="NS" <?php if ($v_type == 'NS') {
-                        echo "selected";
-                                       } ?>>NS</option>
-                    <option value="PTR" <?php if ($v_type == 'PTR') {
-                        echo "selected";
-                                        } ?>>PTR</option>
-                    <option value="SPF" <?php if ($v_type == 'SPF') {
-                        echo "selected";
-                                        } ?>>SPF</option>
-                    <option value="SRV" <?php if ($v_type == 'SRV') {
-                        echo "selected";
-                                        } ?>>SRV</option>
-                    <option value="TLSA" <?php if ($v_type == 'TLSA') {
-                        echo "selected";
-                                         } ?>>TLSA</option>
-                    <option value="TXT" <?php if ($v_type == 'TXT') {
-                        echo "selected";
-                                        } ?>>TXT</option>
+                    <?php foreach ($dns_types as $dt) { ?>
+                        <option
+                            value="<?= $dt ?>"
+                            <?= $v_type === $dt ? 'selected' : '' ?>>
+                            <?= $dt ?>
+                        </option>
+                    <?php } ?>
                 </select>
             </div>
             <div class="u-mb10">
                 <label for="v_val" class="form-label"><?= _("IP or Value") ?></label>
                 <div class="u-pos-relative">
-                    <select class="form-select" tabindex="-1" onchange="this.nextElementSibling.value=this.value">
-                        <option value="">&nbsp;</option>
+                    <select
+                        class="form-select"
+                        tabindex="-1"
+                        onchange="this.nextElementSibling.value=this.value">
                         <?php
                         foreach ($v_ips as $ip => $value) {
-                            $display_ip = empty($value['NAT']) ? $ip : "{$value['NAT']}";
-                            echo "<option value='{$display_ip}'>" . htmlentities($display_ip) . "</option>\n";
-                        }
-                        ?>
+                            $display_ip = empty($value['NAT']) ? $ip : $value['NAT'];
+                            ?>
+                            <option
+                                value="<?= $display_ip ?>">
+                                <?= htmlentities($display_ip) ?>
+                            </option>
+                        <?php } ?>
                     </select>
-                    <input type="text" class="form-control list-editor" name="v_val" id="v_val" value="<?= htmlentities(trim($v_val, "'")) ?>" required>
+                    <input
+                        type="text"
+                        class="form-control list-editor"
+                        name="v_val"
+                        id="v_val"
+                        value="<?= $v_val_clean ?>"
+                        required>
                 </div>
             </div>
             <div class="u-mb10">
                 <label for="v_priority" class="form-label">
                     <?= _("Priority") ?> <span class="optional">(<?= _("Optional") ?>)</span>
                 </label>
-                <input type="text" class="form-control" name="v_priority" id="v_priority" value="<?= htmlentities(trim($v_priority, "'")) ?>">
+                <input
+                    type="text"
+                    class="form-control"
+                    name="v_priority"
+                    id="v_priority"
+                    value="<?= $v_priority_clean ?>">
             </div>
             <div class="u-mb10">
                 <label for="v_ttl" class="form-label">
                     <?= _("TTL") ?> <span class="optional">(<?= _("Optional") ?>)</span>
                 </label>
-                <input type="text" class="form-control" name="v_ttl" id="v_ttl" value="<?= htmlentities(trim($v_ttl, "'")) ?>">
+                <input
+                    type="text"
+                    class="form-control"
+                    name="v_ttl"
+                    id="v_ttl"
+                    value="<?= $v_ttl_clean ?>">
             </div>
         </div>
 

f51404acb19e75822f95e0e61826ebd2b70b58f1
zapper88144 <zapper8814@hotmail.com>
2025-12-30 15:58:18 +0000

    chore(style): precompute welcome title, split long image and input attributes in login_a.php

--- web/templates/pages/login/login_a.php
+++ web/templates/pages/login/login_a.php
@@ -1,18 +1,30 @@
 <div class="login">
     <a href="/" class="u-block u-mb40">
-        <img src="/images/logo.svg" alt="<?= htmlentities($_SESSION["APP_NAME"]) ?>" width="100" height="120">
+        <img
+            src="/images/logo.svg"
+            alt="<?= htmlentities($_SESSION["APP_NAME"]) ?>"
+            width="100"
+            height="120">
     </a>
     <form id="login-form" method="post" action="/login/">
         <input type="hidden" name="token" value="<?= $_SESSION["token"] ?>">
+        <?php $welcome_title = sprintf(_("Welcome to %s"), htmlentities($_SESSION["APP_NAME"])); ?>
         <h1 class="login-title">
-            <?= sprintf(_("Welcome to %s"), htmlentities($_SESSION["APP_NAME"])) ?>
+            <?= $welcome_title ?>
         </h1>
         <?php if (!empty($error)) { ?>
             <p class="error"><?= $error ?></p>
         <?php } ?>
         <div class="u-mb10">
             <label for="username" class="form-label"><?= _("Username") ?></label>
-            <input type="text" class="form-control" name="user" id="username" autocomplete="username" required autofocus>
+            <input
+                type="text"
+                class="form-control"
+                name="user"
+                id="username"
+                autocomplete="username"
+                required
+                autofocus>
         </div>
         <div class="u-mb20">
             <label for="password" class="form-label u-side-by-side">
@@ -23,7 +35,13 @@
                     </a>
                 <?php } ?>
             </label>
-            <input type="password" class="form-control" name="password" id="password" autocomplete="current-password" required>
+            <input
+                type="password"
+                class="form-control"
+                name="password"
+                id="password"
+                autocomplete="current-password"
+                required>
         </div>
         <button type="submit" class="button">
             <i class="fas fa-right-to-bracket"></i><?= _("Next") ?>

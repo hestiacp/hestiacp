18ef42ad9e3f3416d90048771129b085e860206e
zapper88144 <zapper8814@hotmail.com>
2025-12-30 22:48:26 +0000

    chore(style): apply PSR-12 fixes to web/inc/policies.php and web/inc/2fa/check.php

--- web/inc/2fa/check.php
+++ web/inc/2fa/check.php
@@ -6,14 +6,14 @@ use RobThree\Auth\Providers\Qr\QRServerProvider;
 require_once __DIR__ . "/../vendor/autoload.php";
 
 if (isset($argv[1]) && isset($argv[2])) {
-	$secret = $argv[1];
-	$token = $argv[2];
+    $secret = $argv[1];
+    $token = $argv[2];
 } elseif (isset($_GET["secret"]) && isset($_GET["token"])) {
-	$secret = htmlspecialchars($_GET["secret"]);
-	$token = htmlspecialchars($_GET["token"]);
+    $secret = htmlspecialchars($_GET["secret"]);
+    $token = htmlspecialchars($_GET["token"]);
 } else {
-	echo "ERROR: Secret or Token is not set as argument!";
-	exit();
+    echo "ERROR: Secret or Token is not set as argument!";
+    exit();
 }
 
 $tfa = new TwoFactorAuth(new QRServerProvider(), "Hestia Control Panel");
@@ -22,5 +22,5 @@ $tfa = new TwoFactorAuth(new QRServerProvider(), "Hestia Control Panel");
 $result = $tfa->verifyCode($secret, $token);
 
 if ($result) {
-	echo "ok";
+    echo "ok";
 }

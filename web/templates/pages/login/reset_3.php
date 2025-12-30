e0ed41aebbfc71a241b1a065a10d84b876657b5c
zapper88144 <zapper8814@hotmail.com>
2025-12-30 15:55:54 +0000

    chore(style): split long image and password fields in reset_3.php

--- web/templates/pages/login/reset_3.php
+++ web/templates/pages/login/reset_3.php
@@ -1,6 +1,10 @@
 <div class="login">
     <a href="/" class="u-block u-mb40">
-        <img src="/images/logo.svg" alt="<?= htmlentities($_SESSION["APP_NAME"]) ?>" width="100" height="120">
+        <img
+            src="/images/logo.svg"
+            alt="<?= htmlentities($_SESSION["APP_NAME"]) ?>"
+            width="100"
+            height="120">
     </a>
     <form method="post">
         <h1 class="login-title">
@@ -15,11 +19,24 @@
             <input type="hidden" name="user" value="<?= htmlentities($_GET["user"]) ?>">
             <input type="hidden" name="code" value="<?= htmlentities($_GET["code"]) ?>">
             <label for="password" class="form-label"><?= _("New Password") ?></label>
-            <input type="password" class="form-control" name="password" id="password" autocomplete="new-password" required autofocus>
+            <input
+                type="password"
+                class="form-control"
+                name="password"
+                id="password"
+                autocomplete="new-password"
+                required
+                autofocus>
         </div>
         <div class="u-mb20">
             <label for="password_confirm" class="form-label"><?= _("Confirm Password") ?></label>
-            <input type="password" class="form-control" name="password_confirm" id="password_confirm" autocomplete="new-password" required>
+            <input
+                type="password"
+                class="form-control"
+                name="password_confirm"
+                id="password_confirm"
+                autocomplete="new-password"
+                required>
         </div>
         <div class="u-side-by-side">
             <button type="submit" class="button">

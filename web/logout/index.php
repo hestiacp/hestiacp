46ae558aa1f8ce7d38b60f8a31811b25fe0784e0
zapper88144 <zapper8814@hotmail.com>
2025-12-30 23:37:01 +0000

    chore(style): apply PSR-12 fixes to web/logout/index.php

--- web/logout/index.php
+++ web/logout/index.php
@@ -6,36 +6,36 @@ include $_SERVER["DOCUMENT_ROOT"] . "/inc/main.php";
 verify_csrf($_GET);
 
 if (!empty($_SESSION["look"])) {
-	$v_user = escapeshellarg($_SESSION["look"]);
-	$v_impersonator = escapeshellarg($_SESSION["user"]);
-	$cmd =
-		HESTIA_CMD .
-		"v-log-action system 'Warning' 'Security' 'User impersonation session ended (User: " .
-		$v_user .
-		", Administrator: " .
-		$v_impersonator .
-		")'";
-	exec($cmd, $output, $return_var);
-	unset($_SESSION["look"]);
-	# Remove current path for filemanager
-	unset($_SESSION["_sf2_attributes"]);
-	unset($_SESSION["_sf2_meta"]);
-	header("Location: /");
+    $v_user = escapeshellarg($_SESSION["look"]);
+    $v_impersonator = escapeshellarg($_SESSION["user"]);
+    $cmd =
+        HESTIA_CMD .
+        "v-log-action system 'Warning' 'Security' 'User impersonation session ended (User: " .
+        $v_user .
+        ", Administrator: " .
+        $v_impersonator .
+        ")'";
+    exec($cmd, $output, $return_var);
+    unset($_SESSION["look"]);
+    # Remove current path for filemanager
+    unset($_SESSION["_sf2_attributes"]);
+    unset($_SESSION["_sf2_meta"]);
+    header("Location: /");
 } else {
-	if ($_SESSION["token"] && $_SESSION["user"]) {
-		unset($_SESSION["userTheme"]);
-		$v_user = escapeshellarg($_SESSION["user"]);
-		$v_session_id = escapeshellarg($_SESSION["token"]);
-		exec(
-			HESTIA_CMD . "v-log-user-logout " . $v_user . " " . $v_session_id,
-			$output,
-			$return_var,
-		);
-	}
+    if ($_SESSION["token"] && $_SESSION["user"]) {
+        unset($_SESSION["userTheme"]);
+        $v_user = escapeshellarg($_SESSION["user"]);
+        $v_session_id = escapeshellarg($_SESSION["token"]);
+        exec(
+            HESTIA_CMD . "v-log-user-logout " . $v_user . " " . $v_session_id,
+            $output,
+            $return_var,
+        );
+    }
 
-	unset($_SESSION);
-	session_unset();
-	session_destroy();
-	header("Location: /login/");
+    unset($_SESSION);
+    session_unset();
+    session_destroy();
+    header("Location: /login/");
 }
 exit();

0fd2eaddbc4111803da11405c5f76c4b6a7aae33
zapper88144 <zapper8814@hotmail.com>
2025-12-30 15:50:58 +0000

    chore(style): replace short PHP tag and split long text and attributes in reset2fa.php

--- web/templates/pages/login/reset2fa.php
+++ web/templates/pages/login/reset2fa.php
@@ -1,6 +1,10 @@
 <div class="login">
     <a href="/" class="u-block u-mb40">
-        <img src="/images/logo.svg" alt="<?= htmlentities($_SESSION["APP_NAME"]) ?>" width="100" height="120">
+        <img
+            src="/images/logo.svg"
+            alt="<?= htmlentities($_SESSION["APP_NAME"]) ?>"
+            width="100"
+            height="120">
     </a>
     <?php if ($success) { ?>
         <div>
@@ -8,7 +12,8 @@
                 <?= _("Account Unlocked") ?>
             </h1>
             <div class="u-mt20">
-                <p><?_("Two-factor authentication is now turned off for your account.<br><br>You may now proceed to log in.");?></p>
+                <p><?= _("Two-factor authentication is now turned off for your account.") ?></p>
+                <p><?= _("You may now proceed to log in.") ?></p>
                 <a href="/login/" class="button button-secondary">
                     <?= _("Log in") ?>
                 </a>
@@ -25,11 +30,24 @@
             <?php } ?>
             <div class="u-mb10">
                 <label for="user" class="form-label"><?= _("Username") ?></label>
-                <input type="text" class="form-control" name="user" id="user" autocomplete="username" required autofocus>
+                <input
+                    type="text"
+                    class="form-control"
+                    name="user"
+                    id="user"
+                    autocomplete="username"
+                    required
+                    autofocus>
             </div>
             <div class="u-mb20">
                 <label for="twofa" class="form-label"><?= _("2FA Reset Code") ?></label>
-                <input type="text" class="form-control" name="twofa" id="twofa" autocomplete="off" required>
+                <input
+                    type="text"
+                    class="form-control"
+                    name="twofa"
+                    id="twofa"
+                    autocomplete="off"
+                    required>
             </div>
             <div class="u-side-by-side">
                 <button type="submit" class="button">

32e17b04790a1f8fa298830cab6bcfa9c7be7b1c
zapper88144 <zapper8814@hotmail.com>
2025-12-31 02:09:09 +0000

    chore(style): apply PSR-12 fixes to web/delete/package/index.php

--- web/delete/package/index.php
+++ web/delete/package/index.php
@@ -8,23 +8,23 @@ verify_csrf($_GET);
 
 // Prevent editing of default package
 if ($_GET["package"] === "default") {
-	header("Location: /list/package/");
-	exit();
+    header("Location: /list/package/");
+    exit();
 }
 
 if ($_SESSION["userContext"] === "admin") {
-	if (!empty($_GET["package"])) {
-		$v_package = escapeshellarg($_GET["package"]);
-		exec(HESTIA_CMD . "v-delete-user-package " . $v_package, $output, $return_var);
-	}
-	check_return_code($return_var, $output);
-	unset($output);
+    if (!empty($_GET["package"])) {
+        $v_package = escapeshellarg($_GET["package"]);
+        exec(HESTIA_CMD . "v-delete-user-package " . $v_package, $output, $return_var);
+    }
+    check_return_code($return_var, $output);
+    unset($output);
 }
 
 $back = $_SESSION["back"];
 if (!empty($back)) {
-	header("Location: " . $back);
-	exit();
+    header("Location: " . $back);
+    exit();
 }
 
 header("Location: /list/package/");

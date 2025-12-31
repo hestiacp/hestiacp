f8beb09f61fc371bbff7fb9491c714995a530f48
zapper88144 <zapper8814@hotmail.com>
2025-12-31 01:40:53 +0000

    chore(style): apply PSR-12 fixes to web/bulk/backup/incremental/index.php

--- web/bulk/backup/incremental/index.php
+++ web/bulk/backup/incremental/index.php
@@ -4,12 +4,12 @@ ob_start();
 include $_SERVER["DOCUMENT_ROOT"] . "/inc/main.php";
 
 if (empty($_POST["backup"])) {
-	header("Location: /list/backup/incremental/");
-	exit();
+    header("Location: /list/backup/incremental/");
+    exit();
 }
 if (empty($_POST["action"])) {
-	header("Location: /list/backup/incremental/");
-	exit();
+    header("Location: /list/backup/incremental/");
+    exit();
 }
 
 $backup = $_POST["backup"];
@@ -19,23 +19,23 @@ $action = $_POST["action"];
 verify_csrf($_POST);
 
 switch ($action) {
-	case "delete":
-		$cmd = "v-delete-user-backup-restic";
-		break;
-	default:
-		header("Location: /list/backup/");
-		exit();
+    case "delete":
+        $cmd = "v-delete-user-backup-restic";
+        break;
+    default:
+        header("Location: /list/backup/");
+        exit();
 }
 
 foreach ($backup as $value) {
-	$value = escapeshellarg($value);
-	exec(HESTIA_CMD . $cmd . " " . $user . " " . $value, $output, $return_var);
-	if ($return_var != 0) {
-		$_SESSION["error_msg"] = implode("<br>", $output);
-		if (empty($_SESSION["error_msg"])) {
-			$_SESSION["error_msg"] = _("Error: Hestia did not return any output.");
-		}
-	}
+    $value = escapeshellarg($value);
+    exec(HESTIA_CMD . $cmd . " " . $user . " " . $value, $output, $return_var);
+    if ($return_var != 0) {
+        $_SESSION["error_msg"] = implode("<br>", $output);
+        if (empty($_SESSION["error_msg"])) {
+            $_SESSION["error_msg"] = _("Error: Hestia did not return any output.");
+        }
+    }
 }
 
 header("Location: /list/backup/incremental/");

83d5ed3a68269c6bb5f4320fb54db44366e3624f
zapper88144 <zapper8814@hotmail.com>
2025-12-31 01:46:49 +0000

    chore(style): apply PSR-12 fixes to web/bulk/firewall/ipset/index.php

--- web/bulk/firewall/ipset/index.php
+++ web/bulk/firewall/ipset/index.php
@@ -10,33 +10,33 @@ verify_csrf($_POST);
 
 // Check user
 if ($_SESSION["userContext"] != "admin") {
-	header("Location: /list/user");
-	exit();
+    header("Location: /list/user");
+    exit();
 }
 
 if (empty($_POST["setname"])) {
-	header("Location: /list/firewall/ipset/");
-	exit();
+    header("Location: /list/firewall/ipset/");
+    exit();
 }
 if (empty($_POST["action"])) {
-	header("Location: /list/firewall/ipset/");
-	exit();
+    header("Location: /list/firewall/ipset/");
+    exit();
 }
 
 $setname = $_POST["setname"];
 $action = $_POST["action"];
 switch ($action) {
-	case "delete":
-		$cmd = "v-delete-firewall-ipset";
-		break;
-	default:
-		header("Location: /list/firewall/ipset/");
-		exit();
+    case "delete":
+        $cmd = "v-delete-firewall-ipset";
+        break;
+    default:
+        header("Location: /list/firewall/ipset/");
+        exit();
 }
 
 foreach ($setname as $value) {
-	$v_name = escapeshellarg($value);
-	exec(HESTIA_CMD . $cmd . " " . $v_name, $output, $return_var);
+    $v_name = escapeshellarg($value);
+    exec(HESTIA_CMD . $cmd . " " . $v_name, $output, $return_var);
 }
 
 header("Location: /list/firewall/ipset/");

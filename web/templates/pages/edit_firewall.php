c7cde120840901a7cc5f8d17a762197e04405497
zapper88144 <zapper8814@hotmail.com>
2025-12-31 05:54:15 +0000

    chore(style): split long label in edit firewall for PSR-12

--- web/templates/pages/edit_firewall.php
+++ web/templates/pages/edit_firewall.php
@@ -71,7 +71,8 @@
             </div>
             <div class="u-mb10">
                 <label for="v_ip" class="form-label">
-                    <?= _("IP Address / IPset IP List") ?> <span class="optional">(<?= _("Support CIDR format") ?>)</span>
+                    <?= _("IP Address / IPset IP List") ?>
+                    <span class="optional">(<?= _("Support CIDR format") ?>)</span>
                 </label>
                 <div class="u-pos-relative">
                     <select

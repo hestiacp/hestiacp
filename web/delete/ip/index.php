50e6c12e85efa6583d8ad232f57af028543ec0dd
zapper88144 <zapper8814@hotmail.com>
2025-12-31 02:06:09 +0000

    chore(style): apply PSR-12 fixes to web/delete/ip/index.php

--- web/delete/ip/index.php
+++ web/delete/ip/index.php
@@ -7,18 +7,18 @@ include $_SERVER["DOCUMENT_ROOT"] . "/inc/main.php";
 verify_csrf($_GET);
 
 if ($_SESSION["userContext"] === "admin") {
-	if (!empty($_GET["ip"])) {
-		$v_ip = escapeshellarg($_GET["ip"]);
-		exec(HESTIA_CMD . "v-delete-sys-ip " . $v_ip, $output, $return_var);
-	}
-	check_return_code($return_var, $output);
-	unset($output);
+    if (!empty($_GET["ip"])) {
+        $v_ip = escapeshellarg($_GET["ip"]);
+        exec(HESTIA_CMD . "v-delete-sys-ip " . $v_ip, $output, $return_var);
+    }
+    check_return_code($return_var, $output);
+    unset($output);
 }
 
 $back = $_SESSION["back"];
 if (!empty($back)) {
-	header("Location: " . $back);
-	exit();
+    header("Location: " . $back);
+    exit();
 }
 
 header("Location: /list/ip/");

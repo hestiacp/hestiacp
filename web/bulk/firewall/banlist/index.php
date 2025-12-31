eee3e867c24b2b99fa77472c2961a3f513e17beb
zapper88144 <zapper8814@hotmail.com>
2025-12-31 01:44:18 +0000

    chore(style): apply PSR-12 fixes to web/bulk/firewall/banlist/index.php

--- web/bulk/firewall/banlist/index.php
+++ web/bulk/firewall/banlist/index.php
@@ -10,36 +10,36 @@ verify_csrf($_POST);
 
 // Check user
 if ($_SESSION["userContext"] != "admin") {
-	header("Location: /list/user");
-	exit();
+    header("Location: /list/user");
+    exit();
 }
 
 if (empty($_POST["ipchain"])) {
-	header("Location: /list/firewall/banlist/");
-	exit();
+    header("Location: /list/firewall/banlist/");
+    exit();
 }
 if (empty($_POST["action"])) {
-	header("Location: /list/firewall/banlist/");
-	exit();
+    header("Location: /list/firewall/banlist/");
+    exit();
 }
 
 $ipchain = $_POST["ipchain"];
 $action = $_POST["action"];
 
 switch ($action) {
-	case "delete":
-		$cmd = "v-delete-firewall-ban";
-		break;
-	default:
-		header("Location: /list/firewall/banlist/");
-		exit();
+    case "delete":
+        $cmd = "v-delete-firewall-ban";
+        break;
+    default:
+        header("Location: /list/firewall/banlist/");
+        exit();
 }
 
 foreach ($ipchain as $value) {
-	[$ip, $chain] = explode(":", $value);
-	$v_ip = escapeshellarg($ip);
-	$v_chain = escapeshellarg($chain);
-	exec(HESTIA_CMD . $cmd . " " . $v_ip . " " . $v_chain, $output, $return_var);
+    [$ip, $chain] = explode(":", $value);
+    $v_ip = escapeshellarg($ip);
+    $v_chain = escapeshellarg($chain);
+    exec(HESTIA_CMD . $cmd . " " . $v_ip . " " . $v_chain, $output, $return_var);
 }
 
 header("Location: /list/firewall/banlist");

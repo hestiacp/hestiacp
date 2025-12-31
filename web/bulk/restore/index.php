5674a99b7491ced09f7e1789d6078645ee0d738d
zapper88144 <zapper8814@hotmail.com>
2025-12-31 00:28:49 +0000

    chore(style): apply PSR-12 fixes to web/bulk/restore/index.php

--- web/bulk/restore/index.php
+++ web/bulk/restore/index.php
@@ -8,12 +8,12 @@ include $_SERVER["DOCUMENT_ROOT"] . "/inc/main.php";
 verify_csrf($_POST);
 
 if (empty($_POST["backup"])) {
-	header("Location: /list/backup/");
-	exit();
+    header("Location: /list/backup/");
+    exit();
 }
 if (empty($_POST["action"])) {
-	header("Location: /list/backup");
-	exit();
+    header("Location: /list/backup");
+    exit();
 }
 
 $action = $_POST["action"];
@@ -27,61 +27,61 @@ $cron = "no";
 $udir = "no";
 
 if (!empty($_POST["web"])) {
-	$web = escapeshellarg(implode(",", $_POST["web"]));
+    $web = escapeshellarg(implode(",", $_POST["web"]));
 }
 if (!empty($_POST["dns"])) {
-	$dns = escapeshellarg(implode(",", $_POST["dns"]));
+    $dns = escapeshellarg(implode(",", $_POST["dns"]));
 }
 if (!empty($_POST["mail"])) {
-	$mail = escapeshellarg(implode(",", $_POST["mail"]));
+    $mail = escapeshellarg(implode(",", $_POST["mail"]));
 }
 if (!empty($_POST["db"])) {
-	$db = escapeshellarg(implode(",", $_POST["db"]));
+    $db = escapeshellarg(implode(",", $_POST["db"]));
 }
 if (!empty($_POST["cron"])) {
-	$cron = "yes";
+    $cron = "yes";
 }
 if (!empty($_POST["udir"])) {
-	$udir = escapeshellarg(implode(",", $_POST["udir"]));
+    $udir = escapeshellarg(implode(",", $_POST["udir"]));
 }
 
 if ($action == "restore") {
-	exec(
-		HESTIA_CMD .
-			"v-schedule-user-restore " .
-			$user .
-			" " .
-			$backup .
-			" " .
-			$web .
-			" " .
-			$dns .
-			" " .
-			$mail .
-			" " .
-			$db .
-			" " .
-			$cron .
-			" " .
-			$udir,
-		$output,
-		$return_var,
-	);
-	if ($return_var == 0) {
-		$_SESSION["error_msg"] = _(
-			"Task has been added to the queue. You will receive an email notification when your restore has been completed.",
-		);
-	} else {
-		$_SESSION["error_msg"] = implode("<br>", $output);
-		if (empty($_SESSION["error_msg"])) {
-			$_SESSION["error_msg"] = _("Error: Hestia did not return any output.");
-		}
-		if ($return_var == 4) {
-			$_SESSION["error_msg"] = _(
-				"An existing restoration task is already running. Please wait for it to finish before launching it again.",
-			);
-		}
-	}
+    exec(
+        HESTIA_CMD .
+            "v-schedule-user-restore " .
+            $user .
+            " " .
+            $backup .
+            " " .
+            $web .
+            " " .
+            $dns .
+            " " .
+            $mail .
+            " " .
+            $db .
+            " " .
+            $cron .
+            " " .
+            $udir,
+        $output,
+        $return_var,
+    );
+    if ($return_var == 0) {
+        $_SESSION["error_msg"] = _(
+            "Task has been added to the queue. You will receive an email notification when your restore has been completed.",
+        );
+    } else {
+        $_SESSION["error_msg"] = implode("<br>", $output);
+        if (empty($_SESSION["error_msg"])) {
+            $_SESSION["error_msg"] = _("Error: Hestia did not return any output.");
+        }
+        if ($return_var == 4) {
+            $_SESSION["error_msg"] = _(
+                "An existing restoration task is already running. Please wait for it to finish before launching it again.",
+            );
+        }
+    }
 }
 
 header("Location: /list/backup/?backup=" . $_POST["backup"]);

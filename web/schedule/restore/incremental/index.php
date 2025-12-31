97b100b6c0ec37faac3befc50c9dbbf20274aafe
zapper88144 <zapper8814@hotmail.com>
2025-12-31 03:06:03 +0000

    chore(style): apply PSR-12 fixes to web/schedule/restore/incremental/index.php

--- web/schedule/restore/incremental/index.php
+++ web/schedule/restore/incremental/index.php
@@ -9,44 +9,44 @@ verify_csrf($_GET);
 $snapshot = escapeshellarg($_GET["snapshot"]);
 
 if (empty($_GET["object"])) {
-	$_GET["object"] = "";
+    $_GET["object"] = "";
 }
 
 if (empty($_GET["type"])) {
-	exec(
-		HESTIA_CMD . "v-schedule-user-restore-restic " . $user . " " . $snapshot,
-		$output,
-		$return_var,
-	);
-	if ($return_var == 0) {
-		$_SESSION["error_msg"] = _(
-			"Task has been added to the queue. You will receive an email notification when your restore has been completed.",
-		);
-	} else {
-		$_SESSION["error_msg"] = implode("<br>", $output);
-		if (empty($_SESSION["error_msg"])) {
-			$_SESSION["error_msg"] = _("Error: Hestia did not return any output.");
-		}
-		if ($return_var == 4) {
-			$_SESSION["error_msg"] = _(
-				"An existing restoration task is already running. Please wait for it to finish before launching it again.",
-			);
-		}
-	}
+    exec(
+        HESTIA_CMD . "v-schedule-user-restore-restic " . $user . " " . $snapshot,
+        $output,
+        $return_var,
+    );
+    if ($return_var == 0) {
+        $_SESSION["error_msg"] = _(
+            "Task has been added to the queue. You will receive an email notification when your restore has been completed.",
+        );
+    } else {
+        $_SESSION["error_msg"] = implode("<br>", $output);
+        if (empty($_SESSION["error_msg"])) {
+            $_SESSION["error_msg"] = _("Error: Hestia did not return any output.");
+        }
+        if ($return_var == 4) {
+            $_SESSION["error_msg"] = _(
+                "An existing restoration task is already running. Please wait for it to finish before launching it again.",
+            );
+        }
+    }
 } else {
-	exec(
-		HESTIA_CMD .
-			"v-schedule-user-restore-restic " .
-			$user .
-			" " .
-			$snapshot .
-			" " .
-			escapeshellarg($_GET["type"]) .
-			" " .
-			escapeshellarg($_GET["object"]),
-		$output,
-		$return_var,
-	);
+    exec(
+        HESTIA_CMD .
+            "v-schedule-user-restore-restic " .
+            $user .
+            " " .
+            $snapshot .
+            " " .
+            escapeshellarg($_GET["type"]) .
+            " " .
+            escapeshellarg($_GET["object"]),
+        $output,
+        $return_var,
+    );
 }
 
 header("Location: /list/backup/incremental/?snapshot=" . $_GET["snapshot"]);

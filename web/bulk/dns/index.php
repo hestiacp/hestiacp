0784f1e44982a0877a1113f169fdab7d00d6fb7f
zapper88144 <zapper8814@hotmail.com>
2025-12-31 00:12:00 +0000

    chore(style): apply PSR-12 fixes to web/bulk/dns/index.php

--- web/bulk/dns/index.php
+++ web/bulk/dns/index.php
@@ -8,113 +8,113 @@ include $_SERVER["DOCUMENT_ROOT"] . "/inc/main.php";
 verify_csrf($_POST);
 
 if (empty($_POST["domain"])) {
-	header("Location: /list/dns/");
-	exit();
+    header("Location: /list/dns/");
+    exit();
 }
 
 if (empty($_POST["action"])) {
-	header("Location: /list/dns/");
-	exit();
+    header("Location: /list/dns/");
+    exit();
 }
 
 $domain = $_POST["domain"];
 if (empty($_POST["record"])) {
-	$record = "";
+    $record = "";
 } else {
-	$record = $_POST["record"];
+    $record = $_POST["record"];
 }
 $action = $_POST["action"];
 
 if ($_SESSION["userContext"] === "admin") {
-	if (empty($_POST["record"])) {
-		switch ($action) {
-			case "rebuild":
-				$cmd = "v-rebuild-dns-domain";
-				break;
-			case "delete":
-				$cmd = "v-delete-dns-domain";
-				break;
-			case "suspend":
-				$cmd = "v-suspend-dns-domain";
-				break;
-			case "unsuspend":
-				$cmd = "v-unsuspend-dns-domain";
-				break;
-			default:
-				header("Location: /list/dns/");
-				exit();
-		}
-	} else {
-		switch ($action) {
-			case "delete":
-				$cmd = "v-delete-dns-record";
-				break;
-			case "suspend":
-				$cmd = "v-suspend-dns-record";
-				break;
-			case "unsuspend":
-				$cmd = "v-unsuspend-dns-record";
-				break;
-			default:
-				header("Location: /list/dns/?domain=" . $domain);
-				exit();
-		}
-	}
+    if (empty($_POST["record"])) {
+        switch ($action) {
+            case "rebuild":
+                $cmd = "v-rebuild-dns-domain";
+                break;
+            case "delete":
+                $cmd = "v-delete-dns-domain";
+                break;
+            case "suspend":
+                $cmd = "v-suspend-dns-domain";
+                break;
+            case "unsuspend":
+                $cmd = "v-unsuspend-dns-domain";
+                break;
+            default:
+                header("Location: /list/dns/");
+                exit();
+        }
+    } else {
+        switch ($action) {
+            case "delete":
+                $cmd = "v-delete-dns-record";
+                break;
+            case "suspend":
+                $cmd = "v-suspend-dns-record";
+                break;
+            case "unsuspend":
+                $cmd = "v-unsuspend-dns-record";
+                break;
+            default:
+                header("Location: /list/dns/?domain=" . $domain);
+                exit();
+        }
+    }
 } else {
-	if (empty($_POST["record"])) {
-		switch ($action) {
-			case "delete":
-				$cmd = "v-delete-dns-domain";
-				break;
-			default:
-				header("Location: /list/dns/");
-				exit();
-		}
-	} else {
-		switch ($action) {
-			case "delete":
-				$cmd = "v-delete-dns-record";
-				break;
-			default:
-				header("Location: /list/dns/?domain=" . $domain);
-				exit();
-		}
-	}
+    if (empty($_POST["record"])) {
+        switch ($action) {
+            case "delete":
+                $cmd = "v-delete-dns-domain";
+                break;
+            default:
+                header("Location: /list/dns/");
+                exit();
+        }
+    } else {
+        switch ($action) {
+            case "delete":
+                $cmd = "v-delete-dns-record";
+                break;
+            default:
+                header("Location: /list/dns/?domain=" . $domain);
+                exit();
+        }
+    }
 }
 
 if (empty($_POST["record"])) {
-	if (is_array($_POST["domain"])) {
-		foreach ($domain as $value) {
-			// DNS
-			$value = escapeshellarg($value);
-			exec(HESTIA_CMD . $cmd . " " . $user . " " . $value . " no", $output, $return_var);
-			$restart = "yes";
-		}
-	} else {
-		header("Location: /list/dns/?domain=" . $domain);
-	}
+    if (is_array($_POST["domain"])) {
+        foreach ($domain as $value) {
+            // DNS
+            $value = escapeshellarg($value);
+            exec(HESTIA_CMD . $cmd . " " . $user . " " . $value . " no", $output, $return_var);
+            $restart = "yes";
+        }
+    } else {
+        header("Location: /list/dns/?domain=" . $domain);
+    }
 } else {
-	foreach ($record as $value) {
-		// DNS Record
-		$value = escapeshellarg($value);
-		$dom = escapeshellarg($domain);
-		exec(
-			HESTIA_CMD . $cmd . " " . $user . " " . $dom . " " . $value . " no",
-			$output,
-			$return_var,
-		);
-		$restart = "yes";
-	}
+    foreach ($record as $value) {
+        // DNS Record
+        $value = escapeshellarg($value);
+        $dom = escapeshellarg($domain);
+        exec(
+            HESTIA_CMD . $cmd . " " . $user . " " . $dom . " " . $value . " no",
+            $output,
+            $return_var,
+        );
+        $restart = "yes";
+    }
 }
 
 if (!empty($restart)) {
-	exec(HESTIA_CMD . "v-restart-dns", $output, $return_var);
+    exec(HESTIA_CMD . "v-restart-dns", $output, $return_var);
 }
 
 if (empty($_POST["record"])) {
-	header("Location: /list/dns/");
-	exit();
+    header("Location: /list/dns/");
+    exit();
 } else {
-	header("Location: /list/dns/?domain=" . $domain);
-	exit();
+    header("Location: /list/dns/?domain=" . $domain);
+    exit();
 }

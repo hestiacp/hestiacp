56925315c75a9132a26d827a1325a10a396d83d7
zapper88144 <zapper8814@hotmail.com>
2025-12-31 01:56:27 +0000

    chore(style): apply PSR-12 fixes to web/delete/cron/index.php

--- web/delete/cron/index.php
+++ web/delete/cron/index.php
@@ -4,24 +4,24 @@ ob_start();
 include $_SERVER["DOCUMENT_ROOT"] . "/inc/main.php";
 
 if ($_SESSION["userContext"] === "admin" && !empty($_GET["user"])) {
-	$user = escapeshellarg($_GET["user"]);
+    $user = escapeshellarg($_GET["user"]);
 }
 
 // Check token
 verify_csrf($_GET);
 
 if (!empty($_GET["job"])) {
-	$v_username = escapeshellarg($user);
-	$v_job = escapeshellarg($_GET["job"]);
-	exec(HESTIA_CMD . "v-delete-cron-job " . $user . " " . $v_job, $output, $return_var);
+    $v_username = escapeshellarg($user);
+    $v_job = escapeshellarg($_GET["job"]);
+    exec(HESTIA_CMD . "v-delete-cron-job " . $user . " " . $v_job, $output, $return_var);
 }
 check_return_code($return_var, $output);
 unset($output);
 
 $back = $_SESSION["back"];
 if (!empty($back)) {
-	header("Location: " . $back);
-	exit();
+    header("Location: " . $back);
+    exit();
 }
 
 header("Location: /list/cron/");

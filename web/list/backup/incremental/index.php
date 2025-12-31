81daef3098ffd82d21a1b10d83bf39d3c24348de
zapper88144 <zapper8814@hotmail.com>
2025-12-31 04:49:53 +0000

    chore(style): apply PSR-12 fixes to web/list/backup/incremental/index.php

--- web/list/backup/incremental/index.php
+++ web/list/backup/incremental/index.php
@@ -7,34 +7,34 @@ include $_SERVER["DOCUMENT_ROOT"] . "/inc/main.php";
 
 // Data & Render page
 if (empty($_GET["snapshot"])) {
-	exec(HESTIA_CMD . "v-list-user-backups-restic $user json", $output, $return_var);
-	$data = json_decode(implode("", $output), true);
-	if (is_array($data)) {
-		$data = array_reverse($data);
-	} else {
-		$data = "";
-	}
+    exec(HESTIA_CMD . "v-list-user-backups-restic $user json", $output, $return_var);
+    $data = json_decode(implode("", $output), true);
+    if (is_array($data)) {
+        $data = array_reverse($data);
+    } else {
+        $data = "";
+    }
 
-	render_page($user, $TAB, "list_backup_incremental");
+    render_page($user, $TAB, "list_backup_incremental");
 } else {
-	if (empty($_GET["browse"])) {
-		$snapshot = escapeshellarg($_GET["snapshot"]);
-		exec(HESTIA_CMD . "v-list-user-backup-restic $user $snapshot json", $output, $return_var);
-		$data = json_decode(implode("", $output), true);
-		render_page($user, $TAB, "list_backup_detail_incremental");
-	} else {
-		if (empty($_GET["folder"])) {
-			$_GET["folder"] = "/home/" . $user_plain;
-		}
-		$folder = escapeshellarg($_GET["folder"]);
-		$snapshot = escapeshellarg($_GET["snapshot"]);
-		exec(HESTIA_CMD . "v-list-user-files-restic $user $snapshot $folder", $output, $return_var);
-		$info = json_decode($output[0], true);
-		unset($output[0]);
-		$files = [];
-		foreach ($output as $value) {
-			$files[] = json_decode($value, true);
-		}
-		render_page($user, $TAB, "list_files_incremental");
-	}
+    if (empty($_GET["browse"])) {
+        $snapshot = escapeshellarg($_GET["snapshot"]);
+        exec(HESTIA_CMD . "v-list-user-backup-restic $user $snapshot json", $output, $return_var);
+        $data = json_decode(implode("", $output), true);
+        render_page($user, $TAB, "list_backup_detail_incremental");
+    } else {
+        if (empty($_GET["folder"])) {
+            $_GET["folder"] = "/home/" . $user_plain;
+        }
+        $folder = escapeshellarg($_GET["folder"]);
+        $snapshot = escapeshellarg($_GET["snapshot"]);
+        exec(HESTIA_CMD . "v-list-user-files-restic $user $snapshot $folder", $output, $return_var);
+        $info = json_decode($output[0], true);
+        unset($output[0]);
+        $files = [];
+        foreach ($output as $value) {
+            $files[] = json_decode($value, true);
+        }
+        render_page($user, $TAB, "list_files_incremental");
+    }
 }

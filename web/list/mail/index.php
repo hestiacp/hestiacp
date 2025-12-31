29d04cb9566068e0bed0f8f319096e567e0b2672
zapper88144 <zapper8814@hotmail.com>
2025-12-31 01:20:30 +0000

    chore(style): apply PSR-12 fixes to web/list/mail/index.php

--- web/list/mail/index.php
+++ web/list/mail/index.php
@@ -7,69 +7,69 @@ include $_SERVER["DOCUMENT_ROOT"] . "/inc/main.php";
 
 // Data & Render page
 if (empty($_GET["domain"])) {
-	exec(HESTIA_CMD . "v-list-mail-domains $user json", $output, $return_var);
-	$data = json_decode(implode("", $output), true);
-	if ($_SESSION["userSortOrder"] == "name") {
-		ksort($data);
-	} else {
-		$data = array_reverse($data, true);
-	}
-	unset($output);
+    exec(HESTIA_CMD . "v-list-mail-domains $user json", $output, $return_var);
+    $data = json_decode(implode("", $output), true);
+    if ($_SESSION["userSortOrder"] == "name") {
+        ksort($data);
+    } else {
+        $data = array_reverse($data, true);
+    }
+    unset($output);
 
-	render_page($user, $TAB, "list_mail");
+    render_page($user, $TAB, "list_mail");
 } elseif (!empty($_GET["dns"])) {
-	exec(
-		HESTIA_CMD .
-			"v-list-mail-domain " .
-			$user .
-			" " .
-			escapeshellarg($_GET["domain"]) .
-			" json",
-		$output,
-		$return_var,
-	);
-	$data = json_decode(implode("", $output), true);
-	$data = array_reverse($data, true);
-	unset($output);
-	exec(HESTIA_CMD . "v-list-user-ips " . $user . " json", $output, $return_var);
-	$ips = json_decode(implode("", $output), true);
-	$ips = array_reverse($ips, true);
-	unset($output);
-	exec(
-		HESTIA_CMD .
-			"v-list-mail-domain-dkim-dns " .
-			$user .
-			" " .
-			escapeshellarg($_GET["domain"]) .
-			" json",
-		$output,
-		$return_var,
-	);
-	$dkim = json_decode(implode("", $output), true);
-	$dkim = array_reverse($dkim, true);
-	unset($output);
+    exec(
+        HESTIA_CMD .
+            "v-list-mail-domain " .
+            $user .
+            " " .
+            escapeshellarg($_GET["domain"]) .
+            " json",
+        $output,
+        $return_var,
+    );
+    $data = json_decode(implode("", $output), true);
+    $data = array_reverse($data, true);
+    unset($output);
+    exec(HESTIA_CMD . "v-list-user-ips " . $user . " json", $output, $return_var);
+    $ips = json_decode(implode("", $output), true);
+    $ips = array_reverse($ips, true);
+    unset($output);
+    exec(
+        HESTIA_CMD .
+            "v-list-mail-domain-dkim-dns " .
+            $user .
+            " " .
+            escapeshellarg($_GET["domain"]) .
+            " json",
+        $output,
+        $return_var,
+    );
+    $dkim = json_decode(implode("", $output), true);
+    $dkim = array_reverse($dkim, true);
+    unset($output);
 
-	render_page($user, $TAB, "list_mail_dns");
+    render_page($user, $TAB, "list_mail_dns");
 } else {
-	exec(
-		HESTIA_CMD .
-			"v-list-mail-accounts " .
-			$user .
-			" " .
-			escapeshellarg($_GET["domain"]) .
-			" json",
-		$output,
-		$return_var,
-	);
-	$data = json_decode(implode("", $output), true);
-	if ($_SESSION["userSortOrder"] == "name") {
-		ksort($data);
-	} else {
-		$data = array_reverse($data, true);
-	}
-	unset($output);
+    exec(
+        HESTIA_CMD .
+            "v-list-mail-accounts " .
+            $user .
+            " " .
+            escapeshellarg($_GET["domain"]) .
+            " json",
+        $output,
+        $return_var,
+    );
+    $data = json_decode(implode("", $output), true);
+    if ($_SESSION["userSortOrder"] == "name") {
+        ksort($data);
+    } else {
+        $data = array_reverse($data, true);
+    }
+    unset($output);
 
-	render_page($user, $TAB, "list_mail_acc");
+    render_page($user, $TAB, "list_mail_acc");
 }
 
 // Back uri

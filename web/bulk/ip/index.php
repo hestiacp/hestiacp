bd7f8040698a6471614044a5c70a88f778e37694
zapper88144 <zapper8814@hotmail.com>
2025-12-31 01:48:27 +0000

    chore(style): apply PSR-12 fixes to web/bulk/ip/index.php

--- web/bulk/ip/index.php
+++ web/bulk/ip/index.php
@@ -8,39 +8,39 @@ include $_SERVER["DOCUMENT_ROOT"] . "/inc/main.php";
 verify_csrf($_POST);
 
 if (empty($_POST["ip"])) {
-	header("Location: /list/ip");
-	exit();
+    header("Location: /list/ip");
+    exit();
 }
 if (empty($_POST["action"])) {
-	header("Location: /list/ip");
-	exit();
+    header("Location: /list/ip");
+    exit();
 }
 
 $ip = $_POST["ip"];
 $action = $_POST["action"];
 
 if ($_SESSION["userContext"] === "admin") {
-	switch ($action) {
-		case "reread IP":
-			exec(HESTIA_CMD . "v-update-sys-ip", $output, $return_var);
-			header("Location: /list/ip/");
-			exit();
-			break;
-		case "delete":
-			$cmd = "v-delete-sys-ip";
-			break;
-		default:
-			header("Location: /list/ip/");
-			exit();
-	}
+    switch ($action) {
+        case "reread IP":
+            exec(HESTIA_CMD . "v-update-sys-ip", $output, $return_var);
+            header("Location: /list/ip/");
+            exit();
+            break;
+        case "delete":
+            $cmd = "v-delete-sys-ip";
+            break;
+        default:
+            header("Location: /list/ip/");
+            exit();
+    }
 } else {
-	header("Location: /list/ip/");
-	exit();
+    header("Location: /list/ip/");
+    exit();
 }
 
 foreach ($ip as $value) {
-	$value = escapeshellarg($value);
-	exec(HESTIA_CMD . $cmd . " " . $value, $output, $return_var);
+    $value = escapeshellarg($value);
+    exec(HESTIA_CMD . $cmd . " " . $value, $output, $return_var);
 }
 
 header("Location: /list/ip/");

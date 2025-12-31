eb1a73a108dd251c056730f1973d0e00b6f2f400
zapper88144 <zapper8814@hotmail.com>
2025-12-31 03:15:50 +0000

    chore(style): apply PSR-12 fixes to web/update/hestia/index.php

--- web/update/hestia/index.php
+++ web/update/hestia/index.php
@@ -8,19 +8,19 @@ include $_SERVER["DOCUMENT_ROOT"] . "/inc/main.php";
 verify_csrf($_GET);
 
 if ($_SESSION["userContext"] === "admin") {
-	if (!empty($_GET["pkg"])) {
-		$v_pkg = escapeshellarg($_GET["pkg"]);
-		exec(HESTIA_CMD . "v-update-sys-hestia " . $v_pkg, $output, $return_var);
-	}
+    if (!empty($_GET["pkg"])) {
+        $v_pkg = escapeshellarg($_GET["pkg"]);
+        exec(HESTIA_CMD . "v-update-sys-hestia " . $v_pkg, $output, $return_var);
+    }
 
-	if ($return_var != 0) {
-		$error = implode("<br>", $output);
-		if (empty($error)) {
-			$error = sprintf(_("Error: %s update failed.", $v_pkg));
-			$_SESSION["error_msg"] = $error;
-		}
-	}
-	unset($output);
+    if ($return_var != 0) {
+        $error = implode("<br>", $output);
+        if (empty($error)) {
+            $error = sprintf(_("Error: %s update failed.", $v_pkg));
+            $_SESSION["error_msg"] = $error;
+        }
+    }
+    unset($output);
 }
 
 header("Location: /list/updates/");

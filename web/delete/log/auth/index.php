de1965d412e513a9633c9844835c37bc88b93d2b
zapper88144 <zapper8814@hotmail.com>
2025-12-31 02:07:52 +0000

    chore(style): apply PSR-12 fixes to web/delete/log/auth/index.php

--- web/delete/log/auth/index.php
+++ web/delete/log/auth/index.php
@@ -7,8 +7,8 @@ verify_csrf($_GET);
 
 // Check if administrator is viewing system log (currently 'admin' user)
 if ($_SESSION["userContext"] === "admin" && isset($_GET["user"])) {
-	$user = escapeshellarg($_GET["user"]);
-	$token = $_SESSION["token"];
+    $user = escapeshellarg($_GET["user"]);
+    $token = $_SESSION["token"];
 }
 
 // Clear log
@@ -18,9 +18,9 @@ unset($output);
 
 $ip = $_SERVER["REMOTE_ADDR"];
 if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
-	if (!empty($_SERVER["HTTP_CF_CONNECTING_IP"])) {
-		$ip = $_SERVER["HTTP_CF_CONNECTING_IP"];
-	}
+    if (!empty($_SERVER["HTTP_CF_CONNECTING_IP"])) {
+        $ip = $_SERVER["HTTP_CF_CONNECTING_IP"];
+    }
 }
 $v_ip = escapeshellarg($ip);
 $user_agent = $_SERVER["HTTP_USER_AGENT"];
@@ -30,19 +30,19 @@ $v_session_id = escapeshellarg($_SESSION["token"]);
 
 // Add current user session back to log unless impersonating another user
 if (!isset($_SESSION["look"])) {
-	exec(
-		HESTIA_CMD .
-			"v-log-user-login " .
-			$user .
-			" " .
-			$v_ip .
-			" success " .
-			$v_session_id .
-			" " .
-			$v_user_agent,
-		$output,
-		$return_var,
-	);
+    exec(
+        HESTIA_CMD .
+            "v-log-user-login " .
+            $user .
+            " " .
+            $v_ip .
+            " success " .
+            $v_session_id .
+            " " .
+            $v_user_agent,
+        $output,
+        $return_var,
+    );
 }
 
 // Flush session messages
@@ -51,9 +51,9 @@ unset($_SESSION["ok_msg"]);
 
 // Set correct page reload target
 if ($_SESSION["userContext"] === "admin" && !empty($_GET["user"])) {
-	header("Location: /list/log/auth/?user=" . $_GET["user"] . "&token=$token");
+    header("Location: /list/log/auth/?user=" . $_GET["user"] . "&token=$token");
 } else {
-	header("Location: /list/log/auth/");
+    header("Location: /list/log/auth/");
 }
 
 exit();

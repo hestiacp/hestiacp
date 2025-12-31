894ba0b13209e8d5e477374fa93127870e97faba
zapper88144 <zapper8814@hotmail.com>
2025-12-31 02:06:49 +0000

    chore(style): apply PSR-12 fixes to web/delete/key/index.php

--- web/delete/key/index.php
+++ web/delete/key/index.php
@@ -7,13 +7,13 @@ include $_SERVER["DOCUMENT_ROOT"] . "/inc/main.php";
 verify_csrf($_GET);
 
 if ($_SESSION["userContext"] === "admin" && !empty($_GET["user"])) {
-	$user = escapeshellarg($_GET["user"]);
+    $user = escapeshellarg($_GET["user"]);
 }
 
 if (!empty($_GET["key"])) {
-	$v_key = escapeshellarg(trim($_GET["key"]));
-	exec(HESTIA_CMD . "v-delete-user-ssh-key " . $user . " " . $v_key, $output, $return_var);
-	check_return_code($return_var, $output);
+    $v_key = escapeshellarg(trim($_GET["key"]));
+    exec(HESTIA_CMD . "v-delete-user-ssh-key " . $user . " " . $v_key, $output, $return_var);
+    check_return_code($return_var, $output);
 }
 
 unset($output);
@@ -21,8 +21,8 @@ unset($output);
 //die();
 $back = $_SESSION["back"];
 if (!empty($back)) {
-	header("Location: " . $back);
-	exit();
+    header("Location: " . $back);
+    exit();
 }
 header("Location: /list/key/");
 exit();

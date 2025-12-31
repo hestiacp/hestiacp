b3e1e762cfd9f3d18a3d9724f5c3b4c30d6d53d7
zapper88144 <zapper8814@hotmail.com>
2025-12-31 00:35:05 +0000

    chore(style): apply PSR-12 fixes to web/bulk/service/index.php

--- web/bulk/service/index.php
+++ web/bulk/service/index.php
@@ -8,45 +8,45 @@ include $_SERVER["DOCUMENT_ROOT"] . "/inc/main.php";
 verify_csrf($_POST);
 
 if (empty($_POST["service"])) {
-	header("Location: /list/server/");
-	exit();
+    header("Location: /list/server/");
+    exit();
 }
 if (empty($_POST["action"])) {
-	header("Location: /list/server/");
-	exit();
+    header("Location: /list/server/");
+    exit();
 }
 
 $service = $_POST["service"];
 $action = $_POST["action"];
 
 if ($_SESSION["userContext"] === "admin") {
-	switch ($action) {
-		case "stop":
-			$cmd = "v-stop-service";
-			break;
-		case "start":
-			$cmd = "v-start-service";
-			break;
-		case "restart":
-			$cmd = "v-restart-service";
-			break;
-		default:
-			header("Location: /list/server/");
-			exit();
-	}
+    switch ($action) {
+        case "stop":
+            $cmd = "v-stop-service";
+            break;
+        case "start":
+            $cmd = "v-start-service";
+            break;
+        case "restart":
+            $cmd = "v-restart-service";
+            break;
+        default:
+            header("Location: /list/server/");
+            exit();
+    }
 
-	if (!empty($_POST["system"]) && $action == "restart") {
-		$_SESSION["error_srv"] = _("The system is going down for reboot NOW!");
-		exec(HESTIA_CMD . "v-restart-system yes", $output, $return_var);
-		unset($output);
-		header("Location: /list/server/");
-		exit();
-	}
+    if (!empty($_POST["system"]) && $action == "restart") {
+        $_SESSION["error_srv"] = _("The system is going down for reboot NOW!");
+        exec(HESTIA_CMD . "v-restart-system yes", $output, $return_var);
+        unset($output);
+        header("Location: /list/server/");
+        exit();
+    }
 
-	foreach ($service as $value) {
-		$value = escapeshellarg($value);
-		exec(HESTIA_CMD . $cmd . " " . $value, $output, $return_var);
-	}
+    foreach ($service as $value) {
+        $value = escapeshellarg($value);
+        exec(HESTIA_CMD . $cmd . " " . $value, $output, $return_var);
+    }
 }
 
 header("Location: /list/server/");

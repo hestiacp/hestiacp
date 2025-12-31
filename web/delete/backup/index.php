d77c4dd3d4b6cd3c496cdc265b16703f533cad94
zapper88144 <zapper8814@hotmail.com>
2025-12-31 01:53:43 +0000

    chore(style): apply PSR-12 fixes to web/delete/backup/index.php

--- web/delete/backup/index.php
+++ web/delete/backup/index.php
@@ -4,23 +4,23 @@ ob_start();
 include $_SERVER["DOCUMENT_ROOT"] . "/inc/main.php";
 
 if ($_SESSION["userContext"] === "admin" && !empty($_GET["user"])) {
-	$user = escapeshellarg($_GET["user"]);
+    $user = escapeshellarg($_GET["user"]);
 }
 
 // Check token
 verify_csrf($_GET);
 
 if (!empty($_GET["backup"])) {
-	$v_backup = escapeshellarg($_GET["backup"]);
-	exec(HESTIA_CMD . "v-delete-user-backup " . $user . " " . $v_backup, $output, $return_var);
+    $v_backup = escapeshellarg($_GET["backup"]);
+    exec(HESTIA_CMD . "v-delete-user-backup " . $user . " " . $v_backup, $output, $return_var);
 }
 check_return_code($return_var, $output);
 unset($output);
 
 $back = $_SESSION["back"];
 if (!empty($back)) {
-	header("Location: " . $back);
-	exit();
+    header("Location: " . $back);
+    exit();
 }
 
 header("Location: /list/backup/");

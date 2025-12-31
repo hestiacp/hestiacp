c70f5552141ee4fddd885f6a6bad4ca62cce0240
zapper88144 <zapper8814@hotmail.com>
2025-12-31 02:03:11 +0000

    chore(style): apply PSR-12 fixes to web/delete/firewall/index.php

--- web/delete/firewall/index.php
+++ web/delete/firewall/index.php
@@ -6,24 +6,24 @@ include $_SERVER["DOCUMENT_ROOT"] . "/inc/main.php";
 
 // Check user
 if ($_SESSION["userContext"] != "admin") {
-	header("Location: /list/user");
-	exit();
+    header("Location: /list/user");
+    exit();
 }
 
 // Check token
 verify_csrf($_GET);
 
 if (!empty($_GET["rule"])) {
-	$v_rule = escapeshellarg($_GET["rule"]);
-	exec(HESTIA_CMD . "v-delete-firewall-rule " . $v_rule, $output, $return_var);
+    $v_rule = escapeshellarg($_GET["rule"]);
+    exec(HESTIA_CMD . "v-delete-firewall-rule " . $v_rule, $output, $return_var);
 }
 check_return_code($return_var, $output);
 unset($output);
 
 $back = $_SESSION["back"];
 if (!empty($back)) {
-	header("Location: " . $back);
-	exit();
+    header("Location: " . $back);
+    exit();
 }
 
 header("Location: /list/firewall/");

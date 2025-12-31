c21cb3a125428a0dc01226e216a7b158490e41dd
zapper88144 <zapper8814@hotmail.com>
2025-12-31 04:42:21 +0000

    chore(style): apply PSR-12 fixes to web/schedule/backup/index.php

--- web/schedule/backup/index.php
+++ web/schedule/backup/index.php
@@ -8,21 +8,21 @@ verify_csrf($_GET);
 
 exec(HESTIA_CMD . "v-schedule-user-backup " . $user, $output, $return_var);
 if ($return_var == 0) {
-	$_SESSION["error_msg"] = _(
-		"Task has been added to the queue. You will receive an email " .
-			"notification when your backup is ready for download.",
-	);
+    $_SESSION["error_msg"] = _(
+        "Task has been added to the queue. You will receive an email " .
+            "notification when your backup is ready for download.",
+    );
 } else {
-	$_SESSION["error_msg"] = implode("<br>", $output);
-	if (empty($_SESSION["error_msg"])) {
-		$_SESSION["error_msg"] = _("Error: Hestia did not return any output.");
-	}
+    $_SESSION["error_msg"] = implode("<br>", $output);
+    if (empty($_SESSION["error_msg"])) {
+        $_SESSION["error_msg"] = _("Error: Hestia did not return any output.");
+    }
 
-	if ($return_var == 4) {
-		$_SESSION["error_msg"] = _(
-			"An existing backup task is already running, please wait for it " . "to complete.",
-		);
-	}
+    if ($return_var == 4) {
+        $_SESSION["error_msg"] = _(
+            "An existing backup task is already running, please wait for it " . "to complete.",
+        );
+    }
 }
 unset($output);
 header("Location: /list/backup/");

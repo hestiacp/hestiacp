502334e7ff8427bb36c2e7065b129492d3ed2102
zapper88144 <zapper8814@hotmail.com>
2025-12-31 01:33:11 +0000

    chore(style): apply PSR-12 fixes to web/bulk/firewall/index.php

--- web/bulk/firewall/index.php
+++ web/bulk/firewall/index.php
@@ -10,41 +10,41 @@ verify_csrf($_POST);
 
 // Check user
 if ($_SESSION["userContext"] != "admin") {
-	header("Location: /list/user");
-	exit();
+    header("Location: /list/user");
+    exit();
 }
 
 if (empty($_POST["rule"])) {
-	header("Location: /list/firewall/");
-	exit();
+    header("Location: /list/firewall/");
+    exit();
 }
 if (empty($_POST["action"])) {
-	header("Location: /list/firewall/");
-	exit();
+    header("Location: /list/firewall/");
+    exit();
 }
 
 $rule = $_POST["rule"];
 $action = $_POST["action"];
 
 switch ($action) {
-	case "delete":
-		$cmd = "v-delete-firewall-rule";
-		break;
-	case "suspend":
-		$cmd = "v-suspend-firewall-rule";
-		break;
-	case "unsuspend":
-		$cmd = "v-unsuspend-firewall-rule";
-		break;
-	default:
-		header("Location: /list/firewall/");
-		exit();
+    case "delete":
+        $cmd = "v-delete-firewall-rule";
+        break;
+    case "suspend":
+        $cmd = "v-suspend-firewall-rule";
+        break;
+    case "unsuspend":
+        $cmd = "v-unsuspend-firewall-rule";
+        break;
+    default:
+        header("Location: /list/firewall/");
+        exit();
 }
 
 foreach ($rule as $value) {
-	$value = escapeshellarg($value);
-	exec(HESTIA_CMD . $cmd . " " . $value, $output, $return_var);
-	$restart = "yes";
+    $value = escapeshellarg($value);
+    exec(HESTIA_CMD . $cmd . " " . $value, $output, $return_var);
+    $restart = "yes";
 }
 
 header("Location: /list/firewall/");

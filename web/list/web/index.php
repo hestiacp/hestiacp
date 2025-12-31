190c5a2d0d3648d9b21684917e0ce896e4c31d4a
zapper88144 <zapper8814@hotmail.com>
2025-12-31 04:18:12 +0000

    chore(style): apply PSR-12 fixes to web/list/web/index.php

--- web/list/web/index.php
+++ web/list/web/index.php
@@ -9,9 +9,9 @@ include $_SERVER["DOCUMENT_ROOT"] . "/inc/main.php";
 exec(HESTIA_CMD . "v-list-web-domains " . $user . " 'json'", $output, $return_var);
 $data = json_decode(implode("", $output), true);
 if ($_SESSION["userSortOrder"] == "name") {
-	ksort($data);
+    ksort($data);
 } else {
-	$data = array_reverse($data, true);
+    $data = array_reverse($data, true);
 }
 $ips = json_decode(shell_exec(HESTIA_CMD . "v-list-sys-ips json"), true);
 

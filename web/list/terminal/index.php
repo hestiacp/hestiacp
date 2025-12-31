bfd61aa0eeaf9911a1680733b9b0b0bc7b70ec28
zapper88144 <zapper8814@hotmail.com>
2025-12-31 04:02:44 +0000

    chore(style): apply PSR-12 fixes to web/list/terminal/index.php

--- web/list/terminal/index.php
+++ web/list/terminal/index.php
@@ -6,8 +6,8 @@ $TAB = "TERMINAL";
 include $_SERVER["DOCUMENT_ROOT"] . "/inc/main.php";
 
 if ($_SESSION["login_shell"] == "nologin") {
-	header("Location: /list/user/");
-	exit();
+    header("Location: /list/user/");
+    exit();
 }
 
 // Render page

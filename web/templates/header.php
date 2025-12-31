16b0eed798fc8001a195473eac3627bd3880a4b2
zapper88144 <zapper8814@hotmail.com>
2025-12-31 05:38:31 +0000

    chore(style): split long CSP header string in templates/header.php

--- web/templates/header.php
+++ web/templates/header.php
@@ -1,13 +1,21 @@
 <!doctype html>
-<?php header("Content-Security-Policy: default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self';"); ?>
+<?php
+$csp =
+    "Content-Security-Policy: default-src 'self'; " .
+    "script-src 'self' 'unsafe-inline'; " .
+    "style-src 'self' 'unsafe-inline'; " .
+    "img-src 'self' data:; " .
+    "font-src 'self';";
+header($csp);
+?>
 <html class="no-js" lang="<?= $_SESSION["LANGUAGE"] ?>">
 
 <head>
-<?php
-require $_SERVER["HESTIA"] . "/web/templates/includes/title.php";
-require $_SERVER["HESTIA"] . "/web/templates/includes/css.php";
-require $_SERVER["HESTIA"] . "/web/templates/includes/js.php";
-?>
+    <?php
+    require $_SERVER["HESTIA"] . "/web/templates/includes/title.php";
+    require $_SERVER["HESTIA"] . "/web/templates/includes/css.php";
+    require $_SERVER["HESTIA"] . "/web/templates/includes/js.php";
+    ?>
 </head>
 
 <body class="page-<?= strtolower($TAB) ?> lang-<?= $_SESSION["language"] ?>">

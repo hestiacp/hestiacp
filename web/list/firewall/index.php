5ce71e5e46f8feb11ac23962953acb5f7ffdf068
zapper88144 <zapper8814@hotmail.com>
2025-12-31 03:43:33 +0000

    chore(style): apply PSR-12 fixes to web/list/firewall/index.php

--- web/list/firewall/index.php
+++ web/list/firewall/index.php
@@ -7,17 +7,17 @@ include $_SERVER["DOCUMENT_ROOT"] . "/inc/main.php";
 
 // Check user
 if ($_SESSION["userContext"] != "admin") {
-	header("Location: /list/user");
-	exit();
+    header("Location: /list/user");
+    exit();
 }
 
 // Data
 exec(HESTIA_CMD . "v-list-firewall json", $output, $return_var);
 $data = json_decode(implode("", $output), true);
 if ($_SESSION["userSortOrder"] == "name") {
-	ksort($data);
+    ksort($data);
 } else {
-	$data = array_reverse($data, true);
+    $data = array_reverse($data, true);
 }
 unset($output);
 

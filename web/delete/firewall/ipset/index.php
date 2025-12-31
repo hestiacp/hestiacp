184100afc46b2371632d7106f97e1135f6bb99a8
zapper88144 <zapper8814@hotmail.com>
2025-12-31 02:05:07 +0000

    chore(style): apply PSR-12 fixes to web/delete/firewall/ipset/index.php

--- web/delete/firewall/ipset/index.php
+++ web/delete/firewall/ipset/index.php
@@ -6,28 +6,28 @@ include $_SERVER["DOCUMENT_ROOT"] . "/inc/main.php";
 
 // Check user
 if ($_SESSION["userContext"] != "admin") {
-	header("Location: /list/user");
-	exit();
+    header("Location: /list/user");
+    exit();
 }
 
 // Check token
 verify_csrf($_GET);
 
 if (!empty($_GET["listname"])) {
-	$v_listname = $_GET["listname"];
-	exec(
-		HESTIA_CMD . "v-delete-firewall-ipset " . escapeshellarg($v_listname),
-		$output,
-		$return_var,
-	);
+    $v_listname = $_GET["listname"];
+    exec(
+        HESTIA_CMD . "v-delete-firewall-ipset " . escapeshellarg($v_listname),
+        $output,
+        $return_var,
+    );
 }
 check_return_code($return_var, $output);
 unset($output);
 
 $back = $_SESSION["back"];
 if (!empty($back)) {
-	header("Location: " . $back);
-	exit();
+    header("Location: " . $back);
+    exit();
 }
 
 header("Location: /list/firewall/ipset/");

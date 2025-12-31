d2c766ade487825ea2aafddebec3a9ba3d7c94ed
zapper88144 <zapper8814@hotmail.com>
2025-12-31 03:14:49 +0000

    chore(style): apply PSR-12 fixes to web/search/index.php

--- web/search/index.php
+++ web/search/index.php
@@ -11,28 +11,28 @@ include $_SERVER["DOCUMENT_ROOT"] . "/inc/main.php";
 verify_csrf($_GET);
 
 if (empty($_GET["u"])) {
-	$_GET["u"] = "";
+    $_GET["u"] = "";
 }
 if (empty($_GET["q"])) {
-	$_GET["q"] = "";
+    $_GET["q"] = "";
 }
 // Data
 $q = escapeshellarg($_GET["q"]);
 $u = escapeshellarg($_GET["u"]);
 
 if ($_SESSION["userContext"] === "admin" && $_SESSION["look"] == "") {
-	if (!empty($_GET["u"])) {
-		$user = $u;
-		exec(
-			HESTIA_CMD . "v-search-user-object " . $user . " " . $q . " json",
-			$output,
-			$return_var,
-		);
-	} else {
-		exec(HESTIA_CMD . "v-search-object " . $q . " json", $output, $return_var);
-	}
+    if (!empty($_GET["u"])) {
+        $user = $u;
+        exec(
+            HESTIA_CMD . "v-search-user-object " . $user . " " . $q . " json",
+            $output,
+            $return_var,
+        );
+    } else {
+        exec(HESTIA_CMD . "v-search-object " . $q . " json", $output, $return_var);
+    }
 } else {
-	exec(HESTIA_CMD . "v-search-user-object " . $user . " " . $q . " json", $output, $return_var);
+    exec(HESTIA_CMD . "v-search-user-object " . $user . " " . $q . " json", $output, $return_var);
 }
 
 $data = json_decode(implode("", $output), true);

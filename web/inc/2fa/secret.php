e5d9adbcd5d3a6af826d97dd3edeae49653f736f
zapper88144 <zapper8814@hotmail.com>
2025-12-31 05:47:55 +0000

    chore(style): split long comment in 2fa secret for PSR-12

--- web/inc/2fa/secret.php
+++ web/inc/2fa/secret.php
@@ -6,7 +6,9 @@ use RobThree\Auth\Providers\Qr\QRServerProvider;
 require_once __DIR__ . "/../vendor/autoload.php";
 $tfa = new TwoFactorAuth(new QRServerProvider(), "Hestia Control Panel");
 
-$secret = $tfa->createSecret(160); // Though the default is an 80 bits secret (for backwards compatibility reasons) we recommend creating 160+ bits secrets (see RFC 4226 - Algorithm Requirements)
+$secret = $tfa->createSecret(160);
+// Though the default is an 80 bits secret (for backwards compatibility reasons)
+// we recommend creating 160+ bits secrets (see RFC 4226 - Algorithm Requirements)
 $qrcode = $tfa->getQRCodeImageAsDataUri(gethostname(), $secret);
 
 echo $secret . "-" . $qrcode;

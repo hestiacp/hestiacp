0895c6ee3c48437e4166e9b2d6d791d273e87ff9
zapper88144 <zapper8814@hotmail.com>
2025-12-31 02:04:38 +0000

    chore(style): apply PSR-12 fixes to web/delete/firewall/banlist/index.php

--- web/delete/firewall/banlist/index.php
+++ web/delete/firewall/banlist/index.php
@@ -6,25 +6,25 @@ include $_SERVER["DOCUMENT_ROOT"] . "/inc/main.php";
 
 // Check user
 if ($_SESSION["userContext"] != "admin") {
-	header("Location: /list/user");
-	exit();
+    header("Location: /list/user");
+    exit();
 }
 
 // Check token
 verify_csrf($_GET);
 
 if (!empty($_GET["ip"]) && !empty($_GET["chain"])) {
-	$v_ip = escapeshellarg($_GET["ip"]);
-	$v_chain = escapeshellarg($_GET["chain"]);
-	exec(HESTIA_CMD . "v-delete-firewall-ban " . $v_ip . " " . $v_chain, $output, $return_var);
+    $v_ip = escapeshellarg($_GET["ip"]);
+    $v_chain = escapeshellarg($_GET["chain"]);
+    exec(HESTIA_CMD . "v-delete-firewall-ban " . $v_ip . " " . $v_chain, $output, $return_var);
 }
 check_return_code($return_var, $output);
 unset($output);
 
 $back = $_SESSION["back"];
 if (!empty($back)) {
-	header("Location: " . $back);
-	exit();
+    header("Location: " . $back);
+    exit();
 }
 
 header("Location: /list/firewall/banlist/");

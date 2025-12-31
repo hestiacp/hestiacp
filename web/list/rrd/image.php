fb986bfd59722390e54612e1d66361d681f8efe8
zapper88144 <zapper8814@hotmail.com>
2025-12-31 02:42:29 +0000

    chore(style): apply PSR-12 fixes to web/list/rrd/image.php

--- web/list/rrd/image.php
+++ web/list/rrd/image.php
@@ -2,16 +2,16 @@
 
 session_start();
 if ($_SESSION["userContext"] != "admin") {
-	exit();
+    exit();
 }
 $real_path = realpath($_SERVER["DOCUMENT_ROOT"] . $_SERVER["QUERY_STRING"]);
 if (empty($real_path)) {
-	exit();
+    exit();
 }
 $dir_name = dirname($real_path);
 $dir_name = dirname($dir_name);
 if ($dir_name != $_SERVER["DOCUMENT_ROOT"] . "/rrd") {
-	exit();
+    exit();
 }
 header("X-Accel-Redirect: " . $_SERVER["QUERY_STRING"]);
 header("Content-Type: image/png");

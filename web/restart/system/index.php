325520cbd1c244a39d6e4e761fc28d79d788af32
zapper88144 <zapper8814@hotmail.com>
2025-12-31 03:08:36 +0000

    chore(style): apply PSR-12 fixes to web/restart/system/index.php

--- web/restart/system/index.php
+++ web/restart/system/index.php
@@ -12,22 +12,22 @@ verify_csrf($_GET);
 // the server turned on. This will prevent duplicate restarts.
 $reset_token_dir = "/var/tmp/";
 if (isset($_GET["system_reset_token"]) && is_numeric($_GET["system_reset_token"])) {
-	clearstatcache();
-	$reset_token_file = $reset_token_dir . "hst_reset_" . $_GET["system_reset_token"];
-	if (file_exists($reset_token_file)) {
-		unlink($reset_token_file);
-		sleep(5);
-		header("location: /list/server/");
-		exit();
-	}
-	if ($_SESSION["userContext"] === "admin") {
-		if (!empty($_GET["hostname"])) {
-			touch($reset_token_file);
-			$_SESSION["error_msg"] = _("The system is going down for reboot NOW!");
-			exec(HESTIA_CMD . "v-restart-system yes", $output, $return_var);
-		}
-		unset($output);
-	}
+    clearstatcache();
+    $reset_token_file = $reset_token_dir . "hst_reset_" . $_GET["system_reset_token"];
+    if (file_exists($reset_token_file)) {
+        unlink($reset_token_file);
+        sleep(5);
+        header("location: /list/server/");
+        exit();
+    }
+    if ($_SESSION["userContext"] === "admin") {
+        if (!empty($_GET["hostname"])) {
+            touch($reset_token_file);
+            $_SESSION["error_msg"] = _("The system is going down for reboot NOW!");
+            exec(HESTIA_CMD . "v-restart-system yes", $output, $return_var);
+        }
+        unset($output);
+    }
 }
 
 header("Location: /list/server/");

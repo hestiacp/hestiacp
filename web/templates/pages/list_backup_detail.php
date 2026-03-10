e1e18ca2f2120d7b3113d952a61e3ec6a3f09952
Hans Henrik <hans@loltek.net>
2026-03-10 08:13:59 +0100

    Add missing HTML/url encoding (5245-chunk8) Was reports that some missing HTML encoding could lead to XSS/javascript injection.
    Reviewable chunk of #5245 , which grew too large.

--- web/templates/pages/list_backup_detail.php
+++ web/templates/pages/list_backup_detail.php
@@ -3,29 +3,29 @@
 	<div class="toolbar-inner">
 		<div class="toolbar-buttons">
 			<a class="button button-secondary button-back js-button-back" href="/list/backup/">
-				<i class="fas fa-arrow-left icon-blue"></i><?= _("Back") ?>
+				<i class="fas fa-arrow-left icon-blue"></i><?= tohtml( _("Back")) ?>
 			</a>
-			<a href="/schedule/restore/?token=<?= $_SESSION["token"] ?>&backup=<?= htmlentities($_GET["backup"]) ?>" class="button button-secondary">
-				<i class="fas fa-arrow-rotate-left icon-green"></i><?= _("Restore All") ?>
+			<a href="/schedule/restore/?<?= tohtml(http_build_query(array("token" => $_SESSION["token"], "backup" => $_GET["backup"]))) ?>" class="button button-secondary">
+				<i class="fas fa-arrow-rotate-left icon-green"></i><?= tohtml( _("Restore All")) ?>
 			</a>
 		</div>
 		<div class="toolbar-right">
 			<form x-data x-bind="BulkEdit" action="/bulk/restore/" method="post">
-				<input type="hidden" name="token" value="<?= $_SESSION["token"] ?>">
-				<input type="hidden" name="backup" value="<?= htmlentities($_GET["backup"]) ?>">
+				<input type="hidden" name="token" value="<?= tohtml($_SESSION["token"]) ?>">
+				<input type="hidden" name="backup" value="<?= tohtml($_GET["backup"]) ?>">
 				<select class="form-select" name="action">
-					<option value=""><?= _("Apply to selected") ?></option>
-					<option value="restore"><?= _("Restore") ?></option>
+					<option value=""><?= tohtml( _("Apply to selected")) ?></option>
+					<option value="restore"><?= tohtml( _("Restore")) ?></option>
 				</select>
-				<button type="submit" class="toolbar-input-submit" title="<?= _("Apply to selected") ?>">
+				<button type="submit" class="toolbar-input-submit" title="<?= tohtml( _("Apply to selected")) ?>">
 					<i class="fas fa-arrow-right"></i>
 				</button>
 			</form>
 			<div class="toolbar-search">
 				<form action="/search/" method="get">
-					<input type="hidden" name="token" value="<?= $_SESSION["token"] ?>">
-					<input type="search" class="form-control js-search-input" name="q" value="<? echo isset($_POST['q']) ? htmlspecialchars($_POST['q']) : '' ?>" title="<?= _("Search") ?>">
-					<button type="submit" class="toolbar-input-submit" title="<?= _("Search") ?>">
+					<input type="hidden" name="token" value="<?= tohtml($_SESSION["token"]) ?>">
+					<input type="search" class="form-control js-search-input" name="q" value="<?= tohtml($_POST['q'] ?? '') ?>" title="<?= tohtml( _("Search")) ?>">
+					<button type="submit" class="toolbar-input-submit" title="<?= tohtml( _("Search")) ?>">
 						<i class="fas fa-magnifying-glass"></i>
 					</button>
 				</form>
@@ -37,21 +37,21 @@
 
 <div class="container">
 
-	<h1 class="u-text-center u-hide-desktop u-mt20 u-pr30 u-mb20 u-pl30"><?= _("Backup Details") ?></h1>
+	<h1 class="u-text-center u-hide-desktop u-mt20 u-pr30 u-mb20 u-pl30"><?= tohtml( _("Backup Details")) ?></h1>
 
 	<div class="units-table js-units-container">
 		<div class="units-table-header">
 			<div class="units-table-cell">
-				<input type="checkbox" class="js-toggle-all-checkbox" title="<?= _("Select all") ?>" <?= $display_mode ?>>
+				<input type="checkbox" class="js-toggle-all-checkbox" title="<?= tohtml( _("Select all")) ?>" <?= tohtml($display_mode) ?>>
 			</div>
-			<div class="units-table-cell"><?= _("Type") ?></div>
-			<div class="units-table-cell"><?= _("Details") ?></div>
-			<div class="units-table-cell"><?= _("Restore") ?></div>
+			<div class="units-table-cell"><?= tohtml( _("Type")) ?></div>
+			<div class="units-table-cell"><?= tohtml( _("Details")) ?></div>
+			<div class="units-table-cell"><?= tohtml( _("Restore")) ?></div>
 		</div>
 
 		<!-- List web domains -->
 		<?php
-			$backup = htmlentities($_GET['backup']);
+			$backup = $_GET['backup'];
 			$web = explode(',',$data[$backup]['WEB']);
 			foreach ($web as $key) {
 				if (!empty($key)) {
@@ -60,30 +60,30 @@
 			<div class="units-table-row js-unit">
 				<div class="units-table-cell">
 					<div>
-						<input id="check<?= $i ?>" class="js-unit-checkbox" type="checkbox" name="web[]" value="<?= $key ?>">
-						<label for="check<?= $i ?>" class="u-hide-desktop"><?= _("Select") ?></label>
+						<input id="check<?= tohtml($i) ?>" class="js-unit-checkbox" type="checkbox" name="web[]" value="<?= tohtml($key) ?>">
+						<label for="check<?= tohtml($i) ?>" class="u-hide-desktop"><?= tohtml( _("Select")) ?></label>
 					</div>
 				</div>
 				<div class="units-table-cell units-table-heading-cell">
-					<span class="u-hide-desktop u-text-bold"><?= _("Type") ?>:</span>
-					<?= _("Web Domain") ?>
+					<span class="u-hide-desktop u-text-bold"><?= tohtml( _("Type")) ?>:</span>
+					<?= tohtml( _("Web Domain")) ?>
 				</div>
 				<div class="units-table-cell u-text-bold">
-					<span class="u-hide-desktop"><?= _("Details") ?>:</span>
-					<?= $key ?>
+					<span class="u-hide-desktop"><?= tohtml( _("Details")) ?>:</span>
+					<?= tohtml($key) ?>
 				</div>
 				<div class="units-table-cell">
 					<ul class="units-table-row-actions">
 						<li class="units-table-row-action shortcut-enter" data-key-action="href">
 							<a
 								class="units-table-row-action-link data-controls js-confirm-action"
-								href="/schedule/restore/?backup=<?= $backup ?>&type=web&object=<?= $key ?>&token=<?= $_SESSION["token"] ?>"
-								title="<?= _("Restore") ?>"
-								data-confirm-title="<?= _("Restore") ?>"
-								data-confirm-message="<?= sprintf(_("Are you sure you want to restore %s?"), $key) ?>"
+								href="/schedule/restore/?<?= tohtml(http_build_query(array("backup" => $backup, "type" => "web", "object" => $key, "token" => $_SESSION["token"]))) ?>"
+								title="<?= tohtml( _("Restore")) ?>"
+								data-confirm-title="<?= tohtml( _("Restore")) ?>"
+								data-confirm-message="<?= tohtml(sprintf(_("Are you sure you want to restore %s?"), $key)) ?>"
 							>
 								<i class="fas fa-arrow-rotate-left icon-green"></i>
-								<span class="u-hide-desktop"><?= _("Restore") ?></span>
+								<span class="u-hide-desktop"><?= tohtml( _("Restore")) ?></span>
 							</a>
 						</li>
 					</ul>
@@ -100,30 +100,30 @@
 			<div class="units-table-row js-unit">
 				<div class="units-table-cell">
 					<div>
-						<input id="check2<?= $i ?>" class="js-unit-checkbox" type="checkbox" name="mail[]" value="<?= $key ?>">
-						<label for="check2<?= $i ?>" class="u-hide-desktop"><?= _("Select") ?></label>
+						<input id="check2<?= tohtml($i) ?>" class="js-unit-checkbox" type="checkbox" name="mail[]" value="<?= tohtml($key) ?>">
+						<label for="check2<?= tohtml($i) ?>" class="u-hide-desktop"><?= tohtml( _("Select")) ?></label>
 					</div>
 				</div>
 				<div class="units-table-cell units-table-heading-cell">
-					<span class="u-hide-desktop u-text-bold"><?= _("Type") ?>:</span>
-					<?= _("Mail Domain") ?>
+					<span class="u-hide-desktop u-text-bold"><?= tohtml( _("Type")) ?>:</span>
+					<?= tohtml( _("Mail Domain")) ?>
 				</div>
 				<div class="units-table-cell u-text-bold">
-					<span class="u-hide-desktop"><?= _("Details") ?>:</span>
-					<?= $key ?>
+					<span class="u-hide-desktop"><?= tohtml( _("Details")) ?>:</span>
+					<?= tohtml($key) ?>
 				</div>
 				<div class="units-table-cell">
 					<ul class="units-table-row-actions">
 						<li class="units-table-row-action shortcut-enter" data-key-action="href">
 							<a
 								class="units-table-row-action-link data-controls js-confirm-action"
-								href="/schedule/restore/?backup=<?= $backup ?>&type=mail&object=<?= $key ?>&token=<?= $_SESSION["token"] ?>"
-								title="<?= _("Restore") ?>"
-								data-confirm-title="<?= _("Restore") ?>"
-								data-confirm-message="<?= sprintf(_("Are you sure you want to restore %s?"), $key) ?>"
+								href="/schedule/restore/?<?= tohtml(http_build_query(array("backup" => $backup, "type" => "mail", "object" => $key, "token" => $_SESSION["token"]))) ?>"
+								title="<?= tohtml( _("Restore")) ?>"
+								data-confirm-title="<?= tohtml( _("Restore")) ?>"
+								data-confirm-message="<?= tohtml(sprintf(_("Are you sure you want to restore %s?"), $key)) ?>"
 							>
 								<i class="fas fa-arrow-rotate-left icon-green"></i>
-								<span class="u-hide-desktop"><?= _("Restore") ?></span>
+								<span class="u-hide-desktop"><?= tohtml( _("Restore")) ?></span>
 							</a>
 						</li>
 					</ul>
@@ -140,30 +140,30 @@
 			<div class="units-table-row js-unit">
 				<div class="units-table-cell">
 					<div>
-						<input id="check3<?= $i ?>" class="js-unit-checkbox" type="checkbox" name="dns[]" value="<?= $key ?>">
-						<label for="check3<?= $i ?>" class="u-hide-desktop"><?= _("Select") ?></label>
+						<input id="check3<?= tohtml($i) ?>" class="js-unit-checkbox" type="checkbox" name="dns[]" value="<?= tohtml($key) ?>">
+						<label for="check3<?= tohtml($i) ?>" class="u-hide-desktop"><?= tohtml( _("Select")) ?></label>
 					</div>
 				</div>
 				<div class="units-table-cell units-table-heading-cell">
-					<span class="u-hide-desktop u-text-bold"><?= _("Type") ?>:</span>
-					<?= _("DNS Zone") ?>
+					<span class="u-hide-desktop u-text-bold"><?= tohtml( _("Type")) ?>:</span>
+					<?= tohtml( _("DNS Zone")) ?>
 				</div>
 				<div class="units-table-cell u-text-bold">
-					<span class="u-hide-desktop"><?= _("Details") ?>:</span>
-					<?= $key ?>
+					<span class="u-hide-desktop"><?= tohtml( _("Details")) ?>:</span>
+					<?= tohtml($key) ?>
 				</div>
 				<div class="units-table-cell">
 					<ul class="units-table-row-actions">
 						<li class="units-table-row-action shortcut-enter" data-key-action="href">
 							<a
 								class="units-table-row-action-link data-controls js-confirm-action"
-								href="/schedule/restore/?backup=<?= $backup ?>&type=dns&object=<?= $key ?>&token=<?= $_SESSION["token"] ?>"
-								title="<?= _("Restore") ?>"
-								data-confirm-title="<?= _("Restore") ?>"
-								data-confirm-message="<?= sprintf(_("Are you sure you want to restore %s?"), $key) ?>"
+								href="/schedule/restore/?<?= tohtml(http_build_query(array("backup" => $backup, "type" => "dns", "object" => $key, "token" => $_SESSION["token"]))) ?>"
+								title="<?= tohtml( _("Restore")) ?>"
+								data-confirm-title="<?= tohtml( _("Restore")) ?>"
+								data-confirm-message="<?= tohtml(sprintf(_("Are you sure you want to restore %s?"), $key)) ?>"
 							>
 								<i class="fas fa-arrow-rotate-left icon-green"></i>
-								<span class="u-hide-desktop"><?= _("Restore") ?></span>
+								<span class="u-hide-desktop"><?= tohtml( _("Restore")) ?></span>
 							</a>
 						</li>
 					</ul>
@@ -180,30 +180,30 @@
 			<div class="units-table-row js-unit">
 				<div class="units-table-cell">
 					<div>
-						<input id="check4<?= $i ?>" class="js-unit-checkbox" type="checkbox" name="db[]" value="<?= $key ?>">
-						<label for="check4<?= $i ?>" class="u-hide-desktop"><?= _("Select") ?></label>
+						<input id="check4<?= tohtml($i) ?>" class="js-unit-checkbox" type="checkbox" name="db[]" value="<?= tohtml($key) ?>">
+						<label for="check4<?= tohtml($i) ?>" class="u-hide-desktop"><?= tohtml( _("Select")) ?></label>
 					</div>
 				</div>
 				<div class="units-table-cell units-table-heading-cell">
-					<span class="u-hide-desktop u-text-bold"><?= _("Type") ?>:</span>
-					<?= _("Database") ?>
+					<span class="u-hide-desktop u-text-bold"><?= tohtml( _("Type")) ?>:</span>
+					<?= tohtml( _("Database")) ?>
 				</div>
 				<div class="units-table-cell u-text-bold">
-					<span class="u-hide-desktop"><?= _("Details") ?>:</span>
-					<?= $key ?>
+					<span class="u-hide-desktop"><?= tohtml( _("Details")) ?>:</span>
+					<?= tohtml($key) ?>
 				</div>
 				<div class="units-table-cell">
 					<ul class="units-table-row-actions">
 						<li class="units-table-row-action shortcut-enter" data-key-action="href">
 							<a
 								class="units-table-row-action-link data-controls js-confirm-action"
-								href="/schedule/restore/?backup=<?= $backup ?>&type=db&object=<?= $key ?>&token=<?= $_SESSION["token"] ?>"
-								title="<?= _("Restore") ?>"
-								data-confirm-title="<?= _("Restore") ?>"
-								data-confirm-message="<?= sprintf(_("Are you sure you want to restore %s?"), $key) ?>"
+								href="/schedule/restore/?<?= tohtml(http_build_query(array("backup" => $backup, "type" => "db", "object" => $key, "token" => $_SESSION["token"]))) ?>"
+								title="<?= tohtml( _("Restore")) ?>"
+								data-confirm-title="<?= tohtml( _("Restore")) ?>"
+								data-confirm-message="<?= tohtml(sprintf(_("Are you sure you want to restore %s?"), $key)) ?>"
 							>
 								<i class="fas fa-arrow-rotate-left icon-green"></i>
-								<span class="u-hide-desktop"><?= _("Restore") ?></span>
+								<span class="u-hide-desktop"><?= tohtml( _("Restore")) ?></span>
 							</a>
 						</li>
 					</ul>
@@ -217,30 +217,30 @@
 			<div class="units-table-row js-unit">
 				<div class="units-table-cell">
 					<div>
-						<input id="check5<?= $i ?>" class="js-unit-checkbox" type="checkbox" name="check" value="<?= $key ?>">
-						<label for="check5<?= $i ?>" class="u-hide-desktop"><?= _("Select") ?></label>
+						<input id="check5<?= tohtml($i) ?>" class="js-unit-checkbox" type="checkbox" name="check" value="<?= tohtml($key) ?>">
+						<label for="check5<?= tohtml($i) ?>" class="u-hide-desktop"><?= tohtml( _("Select")) ?></label>
 					</div>
 				</div>
 				<div class="units-table-cell units-table-heading-cell">
-					<span class="u-hide-desktop u-text-bold"><?= _("Type") ?>:</span>
-					<?= _("Cron Jobs") ?>
+					<span class="u-hide-desktop u-text-bold"><?= tohtml( _("Type")) ?>:</span>
+					<?= tohtml( _("Cron Jobs")) ?>
 				</div>
 				<div class="units-table-cell u-text-bold">
-					<span class="u-hide-desktop"><?= _("Details") ?>:</span>
-					<?= _("Jobs") ?>
+					<span class="u-hide-desktop"><?= tohtml( _("Details")) ?>:</span>
+					<?= tohtml( _("Jobs")) ?>
 				</div>
 				<div class="units-table-cell">
 					<ul class="units-table-row-actions">
 						<li class="units-table-row-action shortcut-enter" data-key-action="href">
 							<a
 								class="units-table-row-action-link data-controls js-confirm-action"
-								href="/schedule/restore/?backup=<?= $backup ?>&type=cron&object=records&token=<?= $_SESSION["token"] ?>"
-								title="<?= _("Restore") ?>"
-								data-confirm-title="<?= _("Restore") ?>"
-								data-confirm-message="<?= sprintf(_("Are you sure you want to restore %s?"), $key) ?>"
+								href="/schedule/restore/?<?= tohtml(http_build_query(array("backup" => $backup, "type" => "cron", "object" => "records", "token" => $_SESSION["token"]))) ?>"
+								title="<?= tohtml( _("Restore")) ?>"
+								data-confirm-title="<?= tohtml( _("Restore")) ?>"
+								data-confirm-message="<?= tohtml(sprintf(_("Are you sure you want to restore %s?"), $key)) ?>"
 							>
 								<i class="fas fa-arrow-rotate-left icon-green"></i>
-								<span class="u-hide-desktop"><?= _("Restore") ?></span>
+								<span class="u-hide-desktop"><?= tohtml( _("Restore")) ?></span>
 							</a>
 						</li>
 					</ul>
@@ -257,30 +257,30 @@
 			<div class="units-table-row js-unit">
 				<div class="units-table-cell">
 					<div>
-						<input id="check6<?= $i ?>" class="js-unit-checkbox" type="checkbox" name="udir[]" value="<?= $key ?>">
-						<label for="check6<?= $i ?>" class="u-hide-desktop"><?= _("Select") ?></label>
+						<input id="check6<?= tohtml($i) ?>" class="js-unit-checkbox" type="checkbox" name="udir[]" value="<?= tohtml($key) ?>">
+						<label for="check6<?= tohtml($i) ?>" class="u-hide-desktop"><?= tohtml( _("Select")) ?></label>
 					</div>
 				</div>
 				<div class="units-table-cell units-table-heading-cell">
-					<span class="u-hide-desktop u-text-bold"><?= _("Type") ?>:</span>
-					<?= _("User Directory") ?>
+					<span class="u-hide-desktop u-text-bold"><?= tohtml( _("Type")) ?>:</span>
+					<?= tohtml( _("User Directory")) ?>
 				</div>
 				<div class="units-table-cell u-text-bold">
-					<span class="u-hide-desktop"><?= _("Details") ?>:</span>
-					<?= $key ?>
+					<span class="u-hide-desktop"><?= tohtml( _("Details")) ?>:</span>
+					<?= tohtml($key) ?>
 				</div>
 				<div class="units-table-cell">
 					<ul class="units-table-row-actions">
 						<li class="units-table-row-action shortcut-enter" data-key-action="href">
 							<a
 								class="units-table-row-action-link data-controls js-confirm-action"
-								href="/schedule/restore/?backup=<?= $backup ?>&type=udir&object=<?= $key ?>&token=<?= $_SESSION["token"] ?>"
-								title="<?= _("Restore") ?>"
-								data-confirm-title="<?= _("Restore") ?>"
-								data-confirm-message="<?= sprintf(_("Are you sure you want to restore %s?"), $key) ?>"
+								href="/schedule/restore/?<?= tohtml(http_build_query(array("backup" => $backup, "type" => "udir", "object" => $key, "token" => $_SESSION["token"]))) ?>"
+								title="<?= tohtml( _("Restore")) ?>"
+								data-confirm-title="<?= tohtml( _("Restore")) ?>"
+								data-confirm-message="<?= tohtml(sprintf(_("Are you sure you want to restore %s?"), $key)) ?>"
 							>
 								<i class="fas fa-arrow-rotate-left icon-green"></i>
-								<span class="u-hide-desktop"><?= _("Restore") ?></span>
+								<span class="u-hide-desktop"><?= tohtml( _("Restore")) ?></span>
 							</a>
 						</li>
 					</ul>

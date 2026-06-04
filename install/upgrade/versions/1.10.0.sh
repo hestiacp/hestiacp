c414a7bc2f6aa877ccc7fdd6810cfc69399a171d
Raphael <rs@scit.ch>
2026-06-04 18:58:00 +0200

    Adjust version number

--- install/upgrade/versions/1.10.0.sh
+++ install/upgrade/versions/1.10.0.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Hestia Control Panel upgrade script for target version 1.9.7
+# Hestia Control Panel upgrade script for target version 1.10.0
 
 #######################################################################################
 #######                      Place additional commands below.                   #######

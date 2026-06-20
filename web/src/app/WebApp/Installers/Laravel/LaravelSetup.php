d8bc6fc015bcb1c29bf33ff15a164b78b8fdf3be
zcraber <shibinjose991@gmail.com>
2026-06-20 17:48:33 +0530

    feat(webapp): update setups to install the latest version of apps
    - Update:
      - Dolibarr to 23.0.3
      - Joomla to 6.1.1 (hopefully, this will fix #5389)
      - MediaWiki to 1.45.3
      - NamelessMC to 2.2.5
      - OpenCart to 4.1.0.3
      - PrestaShop to 9.1.4
      - thirty bees to 1.6.0
    
    - Modify Laravel and Symfony setups to accommodate new changes
    - Update icons

--- web/src/app/WebApp/Installers/Laravel/LaravelSetup.php
+++ web/src/app/WebApp/Installers/Laravel/LaravelSetup.php
@@ -8,36 +8,71 @@ use Hestia\WebApp\BaseSetup;
 use Hestia\WebApp\InstallationTarget\InstallationTarget;
 use function file_get_contents;
 
-class LaravelSetup extends BaseSetup
-{
+class LaravelSetup extends BaseSetup {
     protected array $info = [
-        'name' => 'Laravel',
-        'group' => 'framework',
-        'version' => 'latest',
-        'thumbnail' => 'laravel-thumb.png',
+        "name" => "Laravel",
+        "group" => "framework",
+        "version" => "latest",
+        "thumbnail" => "laravel-logo.svg",
     ];
 
     protected array $config = [
-        'form' => [],
-        'database' => false,
-        'resources' => [
-            'composer' => ['src' => 'laravel/laravel', 'dst' => '/'],
+        "form" => [],
+        "database" => true,
+        "resources" => [
+            "composer" => ["src" => "laravel/laravel", "dst" => "/"],
         ],
-        'server' => [
-            'nginx' => [
-                'template' => 'laravel',
+        "server" => [
+            "nginx" => [
+                "template" => "laravel",
             ],
-            'php' => [
-                'supported' => ['8.1', '8.2', '8.3', '8.4'],
+            "php" => [
+                "supported" => ["8.3", "8.4", "8.5"],
             ],
         ],
     ];
 
-    protected function setupApplication(InstallationTarget $target, array $options): void
-    {
+    protected function setupApplication(InstallationTarget $target, array $options): void {
         $this->appcontext->createFile(
-            $target->getDocRoot('.htaccess'),
-            file_get_contents(__DIR__ . '/.htaccess'),
+            $target->getDocRoot(".htaccess"),
+            file_get_contents(__DIR__ . "/.htaccess"),
         );
+
+        // Update .env to use MySQL
+        $envPath = $target->getDocRoot(".env");
+        $env = $this->appcontext->readFile($envPath);
+
+        $env = preg_replace("/^DB_CONNECTION=.*/m", "DB_CONNECTION=mysql", $env);
+        $env = preg_replace("/^#?\s*DB_HOST=.*/m", "DB_HOST=" . $target->database->host, $env);
+        $env = preg_replace("/^#?\s*DB_PORT=.*/m", "DB_PORT=3306", $env);
+        $env = preg_replace(
+            "/^#?\s*DB_DATABASE=.*/m",
+            "DB_DATABASE=" . $target->database->name,
+            $env,
+        );
+        $env = preg_replace(
+            "/^#?\s*DB_USERNAME=.*/m",
+            "DB_USERNAME=" . $target->database->user,
+            $env,
+        );
+        $env = preg_replace(
+            "/^#?\s*DB_PASSWORD=.*/m",
+            "DB_PASSWORD=" . $target->database->password,
+            $env,
+        );
+
+        $this->appcontext->createFile($envPath, $env);
+
+        // Generate application key
+        $this->appcontext->runPHP($options["php_version"], $target->getDocRoot("artisan"), [
+            "key:generate",
+            "--force",
+        ]);
+
+        // Migrate database
+        $this->appcontext->runPHP($options["php_version"], $target->getDocRoot("artisan"), [
+            "migrate",
+            "--force",
+        ]);
     }
 }

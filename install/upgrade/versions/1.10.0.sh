bd9dbd5fbe417418ee63c306ceb588168db5689a
Jaap Marcus <9754650+jaapmarcus@users.noreply.github.com>
2026-08-14 09:47:34 +0200

    Fix accidently overwrite VERSION
    We source /etc/os-release to lookup the release_id but also contain "VERSION" causing it to be overwritten

--- install/upgrade/versions/1.10.0.sh
+++ install/upgrade/versions/1.10.0.sh
@@ -28,7 +28,12 @@ echo "[ * ] Fix File Manager ignoring user language"
 cp -f "$HESTIA"/install/deb/filemanager/filegator/configuration.php "$HESTIA"/web/fm/configuration.php
 
 if [ -f /etc/os-release ]; then
+	# /etc/os-release defines its own $VERSION, which would otherwise
+	# clobber Hestia's $VERSION since this script is sourced by the caller
+	_HESTIA_VERSION="$VERSION"
 	source /etc/os-release
+	VERSION="$_HESTIA_VERSION"
+	unset _HESTIA_VERSION
 fi
 
 # Set running OS

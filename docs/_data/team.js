786fec47493e974d688d79f4e1801d29aff13132
Raphael <rs@scit.ch>
2026-09-05 14:42:25 +0200

    Small update to the project information and team pages. (#5686)
    * Enhance donation page with infrastructure details
    
    Added project infrastructure information.
    
    * Add styles for project infrastructure layout
    
    * Update README with infrastructure provider details
    
    Added information about project infrastructure provider.
    
    * Add Sahsanum, Enhance team member links
    
    Added globe icon link to project managers and team members.

--- docs/_data/team.js
+++ docs/_data/team.js
@@ -1,10 +1,17 @@
+const globeIcon = {
+	svg: '<svg role="img" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><title>Website</title><path stroke-linecap="round" stroke-linejoin="round" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" /></svg>',
+};
+
 /** @type {import("vitepress").DefaultTheme.TeamMember[]} */
 export const projectManagers = [
 	{
 		avatar: 'https://www.github.com/ScIT-Raphael.png',
 		name: 'Raphael Schneeberger 🇨🇭',
 		title: 'Project Founder',
-		links: [{ icon: 'github', link: 'https://github.com/ScIT-Raphael' }],
+		links: [
+			{ icon: 'github', link: 'https://github.com/ScIT-Raphael' },
+			{ icon: globeIcon, link: 'https://infrahost.ch/en/' },
+		],
 	},
 	{
 		avatar: 'https://www.github.com/jaapmarcus.png',
@@ -18,12 +25,21 @@ export const projectManagers = [
 	},
 ];
 
-const globeIcon = {
-	svg: '<svg role="img" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><title>Website</title><path stroke-linecap="round" stroke-linejoin="round" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" /></svg>',
-};
-
 /** @type {import("vitepress").DefaultTheme.TeamMember[]} */
 export const teamMembers = [
+	{
+		avatar: 'https://www.github.com/sahsanum.png',
+		name: 'sahsanum',
+		links: [{ icon: 'github', link: 'https://github.com/sahsanum' }],
+	},
+	{
+		avatar: 'https://www.github.com/imjuniper.png',
+		name: 'Juniper Bouchard 🇨🇦',
+		links: [
+			{ icon: 'github', link: 'https://github.com/imjuniper' },
+			{ icon: globeIcon, link: 'https://imjuniper.fyi' },
+		],
+	},
 	{
 		avatar: 'https://www.github.com/imjuniper.png',
 		name: 'Juniper Bouchard 🇨🇦',
